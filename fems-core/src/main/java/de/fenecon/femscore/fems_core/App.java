848a72ee80ee4f6ef2944aa199ee593a4d2a6643
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2016-04-24 10:58:00 +0200

    Create Maven project

---
+++ fems-core/src/main/java/de/fenecon/femscore/fems_core/App.java
@@ -0,0 +1,13 @@
+package de.fenecon.femscore.fems_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
