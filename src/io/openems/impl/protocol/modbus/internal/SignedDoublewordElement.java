fe57e5ee34850ccda7b4db8fd1776f2695e8cd7c
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2016-10-19 20:08:54 +0200

    Add copyright notice using Eclipse Releng

--- src/io/openems/impl/protocol/modbus/internal/SignedDoublewordElement.java
+++ src/io/openems/impl/protocol/modbus/internal/SignedDoublewordElement.java
@@ -1,6 +1,6 @@
-/*
+/*******************************************************************************
  * OpenEMS - Open Source Energy Management System
- * Copyright (c) 2016 FENECON GmbH & Co. KG
+ * Copyright (c) 2016 FENECON GmbH and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -11,10 +11,13 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+ *
+ * Contributors:
+ *   FENECON GmbH - initial API and implementation and initial documentation
+ *******************************************************************************/
 package io.openems.impl.protocol.modbus.internal;
 
 import java.nio.ByteBuffer;
