85b0097543edefc0f4c5fab774d9ce0f15053390
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2016-11-06 00:40:59 +0100

    Implement simple Websocket-client for OpenEMS

--- src/app/monitor/current-monitor/current-monitor.component.ts
+++ src/app/monitor/current-monitor/current-monitor.component.ts
@@ -1,30 +1,52 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import {ISubscription} from 'rxjs/Subscription';
-
-import { DataService } from '../../data/data.service';
-import { CurrentData } from '../../data/current-data';
+import { ISubscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-current-monitor',
   templateUrl: './current-monitor.component.html',
   styleUrls: ['./current-monitor.component.css']
 })
-export class CurrentMonitorComponent implements OnInit, OnDestroy {
-  private currentData: CurrentData;
-  private currentDataSubscription: ISubscription;
+export class CurrentMonitorComponent implements OnInit {
+  private data;
 
-  constructor(
-    private dataService: DataService,
-  ) { }
+  constructor() { }
 
   ngOnInit() {
-    this.currentDataSubscription = this.dataService.data.subscribe((data) => {
-      this.currentData = data;
-    });
+    var ws = new WebSocket("ws://localhost:8090");
+    ws.onopen = () => {
+      ws.send(JSON.stringify({
+        "subscription": {
+          "add": {
+            "ess0": [
+              "Soc"
+            ]
+          }
+        }
+      })
+      )
+    }
+
+    ws.onmessage = (message) => {
+      var data = JSON.parse(message.data).data;
+      this.data = data;
+    }
+
+    /*
+    REST
+        return this.http
+          .get("http://localhost:8081/rest/thing/ess0/channel/Soc/current")
+          .toPromise()
+          .then(response => console.log(response))
+          .catch(error => console.log(error));
+    */
+    /*
+          return this.http
+          .get("http://localhost:8090/ws")
+          .toPromise()
+          .then(response => console.log("RESPONSE: " + response))
+          .catch(error => console.log("ERROR: " + error));
+          */
   }
 
-  ngOnDestroy() {
-    this.currentDataSubscription.unsubscribe();
-  }
 }
