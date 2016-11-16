d06b0f7bf3801060cdd94740c10e8254c5a5b93e
matthias.rossmann <matthias.rossmann@FEN021.ad.fenecon.de>
2016-11-16 17:33:13 +0100

    Add AvgFiFoQueue

---
+++ src/io/openems/core/utilities/AvgFiFoQueue.java
@@ -0,0 +1,25 @@
+package io.openems.core.utilities;
+
+import com.google.common.collect.EvictingQueue;
+
+public class AvgFiFoQueue {
+
+	private EvictingQueue<Long> queue;
+
+	public AvgFiFoQueue(int length) {
+		queue = EvictingQueue.create(length);
+	}
+
+	public void add(long number) {
+		queue.add(number);
+	}
+
+	public long avg() {
+		long sum = 0;
+		for (long value : queue) {
+			sum += value;
+		}
+		return sum / queue.size();
+	}
+
+}
