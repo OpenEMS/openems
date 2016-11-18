65b7853ed69a5c4d331ee66f9064541f9485537f
Matthias Roßmann <matthias.rossmann@fenecon.de>
2016-11-18 15:28:19 +0100

    calculation of point by x coordinate of a list of points to Utilities

--- src/io/openems/core/utilities/ControllerUtils.java
+++ src/io/openems/core/utilities/ControllerUtils.java
@@ -1,5 +1,8 @@
 package io.openems.core.utilities;
 
+import java.util.Collections;
+import java.util.List;
+
 public class ControllerUtils {
 
 	public static double calculateCosPhi(long activePower, long reactivePower) {
@@ -32,5 +35,50 @@ public class ControllerUtils {
 		} else {
 			return false;
 		}
+	}
+
+	public static Long getValueOfLine(List<Point> points, long x) {
+		Point smaller = getSmallerPoint(points, x);
+		Point greater = getGreaterPoint(points, x);
+		if (smaller != null && greater != null) {
+			double m = (greater.y - smaller.y) / (greater.x - smaller.x);
+			double t = smaller.y - m * smaller.x;
+			return (long) (m * x + t);
+		} else {
+			throw new ArithmeticException("x is out of Range of the points");
+		}
+	}
+
+	private static Point getGreaterPoint(List<Point> points, long x) {
+		Collections.sort(points, (p1, p2) -> (int) (p1.x - p2.x));
+		for (int i = 0; i < points.size(); i++) {
+			Point p = points.get(i);
+			if (p.x > x) {
+				return p;
+			}
+		}
+		return null;
+	}
+
+	private static Point getSmallerPoint(List<Point> points, long x) {
+		Collections.sort(points, (p1, p2) -> (int) (p2.x - p1.x));
+		for (int i = points.size() - 1; i >= 0; i--) {
+			Point p = points.get(i);
+			if (p.x < x) {
+				return p;
+			}
+		}
+		return null;
+	}
+
+	class Point {
+		final long x;
+		final long y;
+
+		public Point(long x, long y) {
+			this.x = x;
+			this.y = y;
+		}
+
 	}
 }
