7280da22bd0d6d53c6801ff82d4e298050b20e23
Matthias Roßmann <matthias.rossmann@fenecon.de>
2016-11-28 16:09:11 +0100

    Add CosPhiCharacteristicController

---
+++ src/io/openems/impl/controller/symmetric/cosphicharacteristic/CosPhiCharacteristicController.java
@@ -0,0 +1,61 @@
+package io.openems.impl.controller.symmetric.cosphicharacteristic;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import io.openems.api.channel.Channel;
+import io.openems.api.channel.ChannelChangeListener;
+import io.openems.api.channel.ConfigChannel;
+import io.openems.api.controller.Controller;
+import io.openems.api.exception.InvalidValueException;
+import io.openems.core.utilities.ControllerUtils;
+import io.openems.core.utilities.Point;
+
+public class CosPhiCharacteristicController extends Controller {
+
+	public ConfigChannel<Ess> ess = new ConfigChannel<Ess>("ess", this, Ess.class);
+
+	public ConfigChannel<List<Long[]>> cosPhiPoints = new ConfigChannel<List<Long[]>>("cosPhiPoints", this,
+			Long[].class);
+
+	public List<Point> cosPhiCharacteristic;
+
+	public CosPhiCharacteristicController() {
+		cosPhiPoints.changeListener(new ChannelChangeListener() {
+
+			@Override public void channelChanged(Channel channel, Optional<?> newValue, Optional<?> oldValue) {
+				try {
+					List<Point> points = new ArrayList<>();
+					for (Long[] arr : cosPhiPoints.value()) {
+						points.add(new Point(arr[0], arr[1]));
+					}
+					cosPhiCharacteristic = points;
+				} catch (InvalidValueException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	@Override public void run() {
+		try {
+			if (ess.value().setActivePower.peekWrite().isPresent()) {
+				double pRatio = (double) ess.value().setActivePower.peekWrite().get()
+						/ (double) ess.value().nominalPower.value();
+				double cosPhi = ControllerUtils.getValueOfLine(cosPhiCharacteristic, pRatio);
+				ess.value().power.setReactivePower(
+						ControllerUtils.calculateReactivePower(ess.value().setActivePower.peekWrite().get(), cosPhi));
+				ess.value().power.writePower();
+				log.info("Set reactive power [{}] to get cosPhi [{}]",
+						new Object[] { ess.value().power.getReactivePower(), cosPhi });
+			} else {
+				log.error(ess.id() + " no ActivePower is Set.");
+			}
+		} catch (InvalidValueException e) {
+			log.error("No ess found.", e);
+		}
+	}
+
+}
---
+++ src/io/openems/impl/controller/symmetric/cosphicharacteristic/Ess.java
@@ -0,0 +1,33 @@
+package io.openems.impl.controller.symmetric.cosphicharacteristic;
+
+import io.openems.api.channel.ReadChannel;
+import io.openems.api.channel.WriteChannel;
+import io.openems.api.controller.IsThingMap;
+import io.openems.api.controller.ThingMap;
+import io.openems.api.device.nature.ess.SymmetricEssNature;
+import io.openems.core.utilities.Power;
+
+@IsThingMap(type = SymmetricEssNature.class)
+public class Ess extends ThingMap {
+
+	public final WriteChannel<Long> setActivePower;
+	public final WriteChannel<Long> setReactivePower;
+	public final String id;
+	public final ReadChannel<Long> allowedCharge;
+	public final ReadChannel<Long> allowedDischarge;
+	public final Power power;
+	public final ReadChannel<Long> nominalPower;
+
+	public Ess(SymmetricEssNature ess) {
+		super(ess);
+		setActivePower = ess.setActivePower();
+		setReactivePower = ess.setReactivePower();
+		id = ess.id();
+		allowedCharge = ess.allowedCharge();
+		allowedDischarge = ess.allowedDischarge();
+		nominalPower = ess.maxNominalPower();
+		this.power = new Power(ess.allowedDischarge().required(), ess.allowedCharge().required(),
+				ess.allowedApparent().required(), ess.setActivePower().required(), ess.setReactivePower().required());
+	}
+
+}
