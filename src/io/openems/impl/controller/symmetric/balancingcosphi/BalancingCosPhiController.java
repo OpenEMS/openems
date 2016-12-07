8dd5201b6ff3d4696d5ca5da6671cd7760eba104
Matthias Roßmann <matthias.rossmann@fenecon.de>
2016-12-07 11:25:28 +0100

    Add BalancingCosPhiController

---
+++ src/io/openems/impl/controller/symmetric/balancingcosphi/BalancingCosPhiController.java
@@ -0,0 +1,34 @@
+package io.openems.impl.controller.symmetric.balancingcosphi;
+
+import io.openems.api.channel.ConfigChannel;
+import io.openems.api.controller.Controller;
+import io.openems.api.exception.InvalidValueException;
+
+public class BalancingCosPhiController extends Controller {
+
+	public ConfigChannel<Ess> ess = new ConfigChannel<Ess>("ess", this, Ess.class);
+
+	public ConfigChannel<Meter> meter = new ConfigChannel<Meter>("meter", this, Meter.class);
+
+	public ConfigChannel<Double> cosPhi = new ConfigChannel<Double>("cosPhi", this, Double.class);
+
+	public BalancingCosPhiController() {
+		super();
+	}
+
+	public BalancingCosPhiController(String thingId) {
+		super(thingId);
+	}
+
+	@Override public void run() {
+		try {
+			double phi = Math.acos(cosPhi.value());
+			long q = (long) (meter.value().activePower.value() * Math.tan(phi)) - meter.value().reactivePower.value();
+			ess.value().power.setReactivePower(q);
+			ess.value().power.writePower();
+		} catch (InvalidValueException e) {
+			log.error("Failed to read value.", e);
+		}
+	}
+
+}
---
+++ src/io/openems/impl/controller/symmetric/balancingcosphi/Ess.java
@@ -0,0 +1,32 @@
+package io.openems.impl.controller.symmetric.balancingcosphi;
+
+import io.openems.api.channel.ReadChannel;
+import io.openems.api.channel.WriteChannel;
+import io.openems.api.controller.IsThingMap;
+import io.openems.api.controller.ThingMap;
+import io.openems.api.device.nature.ess.SymmetricEssNature;
+import io.openems.core.utilities.Power;
+
+@IsThingMap(type = SymmetricEssNature.class)
+public class Ess extends ThingMap {
+
+	public final WriteChannel<Long> setActivePower;
+	public final WriteChannel<Long> setReactivePower;
+	public final String id;
+	public final ReadChannel<Long> allowedCharge;
+	public final ReadChannel<Long> allowedDischarge;
+	public final Power power;
+
+	public Ess(SymmetricEssNature ess) {
+		super(ess);
+		setActivePower = ess.setActivePower();
+		setReactivePower = ess.setReactivePower();
+		id = ess.id();
+		allowedCharge = ess.allowedCharge();
+		allowedDischarge = ess.allowedDischarge();
+		this.power = new Power(ess.allowedDischarge().required(), ess.allowedCharge().required(),
+				ess.allowedApparent().required(), ess.setActivePower().required(), ess.setReactivePower().required(),
+				1);
+	}
+
+}
