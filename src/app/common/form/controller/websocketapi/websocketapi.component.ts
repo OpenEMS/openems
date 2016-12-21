a52fa505c55ebc8d6be44f2562bcd8d5b6eda5e4
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2016-12-21 13:46:44 +0100

    Adjust Websocket-API view

--- src/app/common/form/controller/websocketapi/websocketapi.component.ts
+++ src/app/common/form/controller/websocketapi/websocketapi.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormThingComponent } from '../../formthing.component';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'form-controller-websocketapi',
@@ -8,8 +8,20 @@ import { FormGroup } from '@angular/forms';
 })
 export class FormControllerWebsocketApiComponent extends FormThingComponent {
 
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    super();
+  }
+
   @Input()
   set form(form: FormGroup) {
+    if(!form.value["priority"]) {
+      form.addControl("priority", this.formBuilder.control(""));
+    }
+    if(!form.value["port"]) {
+      form.addControl("port", this.formBuilder.control(""));
+    }
     super.setForm(form, []);
   }
   
