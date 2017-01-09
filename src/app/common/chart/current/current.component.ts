cc18a188096d40d5788ad7a710b6fc3686830dfe
Lisa Kaindl <lisa.kaindl@AD.FENECON.DE>
2017-01-09 14:03:19 +0100

    Ersten Kreisabschnitt

--- src/app/common/chart/current/current.component.ts
+++ src/app/common/chart/current/current.component.ts
@@ -2,64 +2,84 @@ import { Component, Input, Output, EventEmitter, OnInit, OnChanges, ElementRef, 
 import { BaseChartComponent } from 'ngx-charts';
 import { ColorHelper } from 'ngx-charts';
 import * as d3 from 'd3';
-import { StorageSection, ProductionSection, ConsumptionSection, GridSection } from './section/section';
+//import { StorageSection, ProductionSection, ConsumptionSection, GridSection } from './section/section';
 
+
+export class Section {
+  public outlinePath;
+  public Path;
+
+  constructor(
+    public startAngle: number,
+    public endAngle: number,
+    public percentageAngle: number,
+  ) { }
+}
 @Component({
   selector: 'chart-current',
   templateUrl: './current.component.html'
 })
-export class ChartCurrentComponent extends BaseChartComponent implements OnChanges, OnInit {
+export class ChartCurrentComponent extends BaseChartComponent implements OnInit {
 
-  private translation: string;
-
-  private sections = {
-    grid: new GridSection(),
-    production: new ProductionSection(),
-    consumption: new ConsumptionSection(),
-    storage: new StorageSection()
-  }
-
-  @Input()
-  set grid(value: number) {
-    console.log("grid", value);
-    this.sections.grid.setValue(value);
-  }
-
-  @Input()
-  set production(value: number) {
-    this.sections.production.setValue(value);
-  }
-
-  @Input()
-  set consumption(value: number) {
-    this.sections.consumption.setValue(value);
-  }
-
-  @Input()
-  set storage(value: number) {
-    this.sections.storage.setValue(value);
-  }
+  private sections: Section[];
 
   ngOnInit() {
-    this.update();
+    var width = 800;
+    var height = 800;
+    var radius = Math.min(width, height) / 2;
+    var outerRadius = radius;
+    var innerRadius = radius - 30;
+    var arc = <any>d3.arc()
+      .innerRadius(innerRadius)
+      .outerRadius(outerRadius);
+
+    var grid = new Section(this.deg2rad(226), this.deg2rad(314), this.deg2rad(300));
+    grid.outlinePath = arc
+      .startAngle(grid.startAngle)
+      .endAngle(grid.endAngle)();
+
+    /* var prod = new Section(this.deg2rad(316), this.deg2rad(404), this.deg2rad(390));
+    prod.outlinePath = arc
+      .startAngle(prod.startAngle)
+      .endAngle(prod.endAngle)();
+
+    var cons = new Section(this.deg2rad(46), this.deg2rad(134), this.deg2rad(90));
+    cons.outlinePath = arc
+      .startAngle(cons.startAngle)
+      .endAngle(cons.endAngle)();
+
+    var battery = new Section(this.deg2rad(136), this.deg2rad(224), this.deg2rad(180));
+    battery.outlinePath = arc
+      .startAngle(battery.startAngle)
+      .endAngle(battery.endAngle)(); */
+
+    var grid = new Section(this.deg2rad(226), this.deg2rad(314), this.deg2rad(300));
+    grid.Path = arc
+      .startAngle(grid.startAngle)
+      .endAngle(grid.percentageAngle)();
+
+    /* var prod = new Section(this.deg2rad(316), this.deg2rad(404), this.deg2rad(390));
+    prod.outlinePath = arc
+      .startAngle(prod.startAngle)
+      .endAngle(prod.percentageAngle)();
+
+    var cons = new Section(this.deg2rad(46), this.deg2rad(134), this.deg2rad(90));
+    cons.outlinePath = arc
+      .startAngle(cons.startAngle)
+      .endAngle(cons.percentageAngle)();
+
+    var battery = new Section(this.deg2rad(136), this.deg2rad(224), this.deg2rad(180));
+    battery.outlinePath = arc
+      .startAngle(battery.startAngle)
+      .endAngle(battery.percentageAngle)()*/
+
+
+    this.sections = [
+      grid, // prod, cons, battery
+    ];
   }
 
-  ngOnChanges() {
-    this.update();
-  }
-
-  update() {
-    super.update();
-    let xOffset = this.width / 2;
-    let yOffset = this.height / 2;
-    this.translation = `translate(${xOffset}, ${yOffset})`;
-
-    let radius = Math.min(this.width, this.height) / 2;
-    let outerRadius = radius;
-    let innerRadius = radius - 30;
-
-    for (let section in this.sections) {
-      this.sections[section].update(outerRadius, innerRadius);
-    }
+  private deg2rad(value: number): number {
+    return value * (Math.PI / 180)
   }
 }
