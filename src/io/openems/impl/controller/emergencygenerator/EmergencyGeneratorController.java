0ce97432d4aed4f2de92108f1e6868a894cf7ee6
Matthias Roßmann <matthias.rossmann@fenecon.de>
2017-01-16 17:23:17 +0100

    Add digital output

--- src/io/openems/impl/controller/emergencygenerator/EmergencyGeneratorController.java
+++ src/io/openems/impl/controller/emergencygenerator/EmergencyGeneratorController.java
@@ -2,10 +2,14 @@ package io.openems.impl.controller.emergencygenerator;
 
 import java.util.Optional;
 
+import io.openems.api.channel.Channel;
 import io.openems.api.channel.ConfigChannel;
+import io.openems.api.channel.WriteChannel;
 import io.openems.api.controller.Controller;
 import io.openems.api.device.nature.ess.EssNature;
 import io.openems.api.exception.InvalidValueException;
+import io.openems.api.exception.WriteChannelException;
+import io.openems.core.ThingRepository;
 
 public class EmergencyGeneratorController extends Controller {
 
@@ -15,36 +19,58 @@ public class EmergencyGeneratorController extends Controller {
 
 	public ConfigChannel<Long> minSoc = new ConfigChannel<Long>("minSoc", this, Long.class);
 	public ConfigChannel<Long> maxSoc = new ConfigChannel<Long>("maxSoc", this, Long.class);
+
+	private ThingRepository repo = ThingRepository.getInstance();
+
+	public ConfigChannel<String> outputChannelAddress = new ConfigChannel<String>("outputChannelAddress", this,
+			String.class).changeListener((channel, newValue, oldValue) -> {
+				Optional<String> channelAddress = (Optional<String>) newValue;
+				if (channelAddress.isPresent()) {
+					Optional<Channel> ch = repo.getChannelByAddress(channelAddress.get());
+					if (ch.isPresent()) {
+						outputChannel = (WriteChannel<Boolean>) ch.get();
+					} else {
+						log.error("Channel " + channelAddress.get() + " not found");
+					}
+				} else {
+					log.error("'outputChannelAddress' is not configured!");
+				}
+			});
+
+	private WriteChannel<Boolean> outputChannel;
+
 	private boolean generatorOn = true;
 
 	public EmergencyGeneratorController() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public EmergencyGeneratorController(String thingId) {
 		super(thingId);
-		// TODO Auto-generated constructor stub
 	}
 
-	@Override public void run() {
+	@Override
+	public void run() {
 		try {
 			// Check if grid is available
-			if (meter.value().voltage.valueOptional().isPresent()
-					&& !(meter.value().voltage.value() >= 200 && meter.value().voltage.value() <= 260)) {
+			if (!meter.value().voltage.valueOptional().isPresent()
+					|| !(meter.value().voltage.value() >= 200 && meter.value().voltage.value() <= 260)) {
 				if (ess.value().gridMode.labelOptional().equals(Optional.of(EssNature.OFF_GRID)) && !generatorOn
 						&& ess.value().soc.value() <= minSoc.value()) {
-					// TODO switch generator on
+					// switch generator on
+					outputChannel.pushWrite(true);
 					generatorOn = true;
 				} else if (ess.value().gridMode.labelOptional().equals(Optional.of(EssNature.ON_GRID)) && generatorOn
 						&& ess.value().soc.value() >= maxSoc.value()) {
-					// TODO switch generator off
+					// switch generator off
+					outputChannel.pushWrite(false);
 					generatorOn = false;
 				}
 			}
 		} catch (InvalidValueException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.error("Failed to read value!", e);
+		} catch (WriteChannelException e) {
+			log.error("Error due write to output [" + outputChannelAddress.valueOptional().orElse("<none>") + "]", e);
 		}
 	}
 
--- src/io/openems/impl/controller/emergencygenerator/Ess.java
+++ src/io/openems/impl/controller/emergencygenerator/Ess.java
@@ -3,15 +3,15 @@ package io.openems.impl.controller.emergencygenerator;
 import io.openems.api.channel.ReadChannel;
 import io.openems.api.controller.IsThingMap;
 import io.openems.api.controller.ThingMap;
-import io.openems.impl.device.pro.FeneconProEss;
+import io.openems.api.device.nature.ess.EssNature;
 
-@IsThingMap(type = FeneconProEss.class)
+@IsThingMap(type = EssNature.class)
 public class Ess extends ThingMap {
 
 	public ReadChannel<Long> gridMode;
 	public ReadChannel<Long> soc;
 
-	public Ess(FeneconProEss ess) {
+	public Ess(EssNature ess) {
 		super(ess);
 		gridMode = ess.gridMode().required();
 		soc = ess.soc().required();
