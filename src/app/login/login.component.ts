1908451d1e8785f75c58ba57eca8926ec43b95bf
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2017-01-19 09:32:37 +0100

    Redirect if all websockets are connected

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,28 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subscription } from 'rxjs/Subscription';
 import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { WebsocketService, Websocket } from '../service/websocket.service';
+import { WebappService, Notification } from '../service/webapp.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   private forms: FormGroup[] = [];
+
+  private websocketSubscriptions: Subscription[] = [];
 
   constructor(
     private websocketService: WebsocketService,
     private router: Router,
-    private formBuilder: FormBuilder) {
+    private formBuilder: FormBuilder,
+    private webapp: WebappService) {
   }
 
   ngOnInit() {
     this.websocketService.clearCurrentDevice();
     for (let websocketName in this.websocketService.websockets) {
       let websocket = this.websocketService.websockets[websocketName];
+      this.websocketSubscriptions.push(websocket.event.subscribe((value) => this.websocketEvent(value)));
       let form: FormGroup = this.formBuilder.group({
         "password": this.formBuilder.control('')
       });
@@ -31,13 +37,38 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  loginOrLogout(form: FormGroup) {
+  ngOnDestroy() {
+    for (let subscription of this.websocketSubscriptions) {
+      subscription.unsubscribe();
+    }
+  }
+
+  doLogin(form: FormGroup) {
     let websocket: Websocket = form['_websocket'];
     let password: string = form.value['password']
-    if (websocket.isConnected) {
-      websocket.close();
-    } else {
-      websocket.connectWithPassword(password);
+    websocket.connectWithPassword(password);
+  }
+
+  doLogout(form: FormGroup) {
+    let websocket: Websocket = form['_websocket'];
+    websocket.close();
+  }
+
+  websocketEvent(value: Notification) {
+    let allConnected = true;
+    for (let websocketName in this.websocketService.websockets) {
+      let websocket = this.websocketService.websockets[websocketName];
+      if (!websocket.isConnected) {
+        allConnected = false;
+        break;
+      }
+    }
+    if (allConnected) {
+      this.webapp.notify({
+        type: "success",
+        message: "Alle Verbindungen hergestellt."
+      });
+      this.router.navigate(['/overview']);
     }
   }
 }
