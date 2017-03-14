258aa87e7f8b86172ed7023bac9442bc7a958342
Matthias Roßmann <matthias.rossmann@fenecon.de>
2017-03-14 17:16:07 +0100

    Add BalancingSurplusContoller

---
+++ src/io/openems/impl/controller/symmetric/balancingsurplus/BalancingSurplusController.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * OpenEMS - Open Source Energy Management System
+ * Copyright (c) 2016, 2017 FENECON GmbH and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *   FENECON GmbH - initial API and implementation and initial documentation
+ *******************************************************************************/
+package io.openems.impl.controller.symmetric.balancingsurplus;
+
+import java.util.Set;
+
+import io.openems.api.channel.ConfigChannel;
+import io.openems.api.controller.Controller;
+import io.openems.api.doc.ConfigInfo;
+import io.openems.api.doc.ThingInfo;
+import io.openems.api.exception.InvalidValueException;
+import io.openems.impl.controller.symmetric.balancingoffset.Ess;
+
+@ThingInfo(title = "Self-consumption optimization with surplus feed-in (Symmetric)", description = "Tries to keep the grid meter on zero. For symmetric Ess. If ess is over the surplusMinSoc, the ess discharges with the power of the chargers. ")
+public class BalancingSurplusController extends Controller {
+
+	/*
+	 * Constructors
+	 */
+	public BalancingSurplusController() {
+		super();
+	}
+
+	public BalancingSurplusController(String thingId) {
+		super(thingId);
+	}
+
+	/*
+	 * Config
+	 */
+	@ConfigInfo(title = "Ess", description = "Sets the Ess device.", type = Ess.class)
+	public final ConfigChannel<Ess> ess = new ConfigChannel<Ess>("ess", this);
+
+	@ConfigInfo(title = "Charger", description = "Sets the Chargers connected to the ess.", type = Charger.class, isArray = true)
+	public final ConfigChannel<Set<Charger>> chargers = new ConfigChannel<Set<Charger>>("chargers", this);
+
+	@ConfigInfo(title = "Surplus min soc", description = "The required Soc to start surplus feed-in.", type = Long.class)
+	public final ConfigChannel<Long> surplusMinSoc = new ConfigChannel<Long>("surplusMinSoc", this);
+
+	@ConfigInfo(title = "Grid-Meter", description = "Sets the grid meter.", type = Meter.class)
+	public final ConfigChannel<Meter> meter = new ConfigChannel<Meter>("meter", this);
+
+	/*
+	 * Methods
+	 */
+
+	@Override
+	public void run() {
+		try {
+			Ess ess = this.ess.value();
+			// Calculate required sum values
+			long calculatedPower = meter.value().activePower.value() + ess.activePower.value() - getSurplusPower();
+			ess.power.setActivePower(calculatedPower);
+			ess.power.writePower();
+			// print info message to log
+			String message = ess.id() + " Set ActivePower [" + ess.power.getActivePower() + "]";
+			log.info(message);
+		} catch (InvalidValueException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	private long getSurplusPower() throws InvalidValueException {
+		if (ess.value().soc.value() >= surplusMinSoc.value()) {
+			long power = 0l;
+			for (Charger c : chargers.value()) {
+				power += c.power.value();
+			}
+		}
+		return 0;
+	}
+
+}
---
+++ src/io/openems/impl/controller/symmetric/balancingsurplus/Ess.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * OpenEMS - Open Source Energy Management System
+ * Copyright (c) 2016 FENECON GmbH and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *   FENECON GmbH - initial API and implementation and initial documentation
+ *******************************************************************************/
+package io.openems.impl.controller.symmetric.balancingsurplus;
+
+import io.openems.api.channel.ReadChannel;
+import io.openems.api.channel.WriteChannel;
+import io.openems.api.controller.IsThingMap;
+import io.openems.api.controller.ThingMap;
+import io.openems.api.device.nature.ess.SymmetricEssNature;
+import io.openems.api.exception.InvalidValueException;
+import io.openems.core.utilities.Power;
+
+@IsThingMap(type = SymmetricEssNature.class)
+public class Ess extends ThingMap {
+
+	public final ReadChannel<Integer> minSoc;
+	public final WriteChannel<Long> setActivePower;
+	public final WriteChannel<Long> setReactivePower;
+	public final ReadChannel<Long> soc;
+	public final ReadChannel<Long> activePower;
+	public final ReadChannel<Long> allowedCharge;
+	public final ReadChannel<Long> allowedDischarge;
+	public final ReadChannel<Long> gridMode;
+	public final ReadChannel<Long> systemState;
+	public final Power power;
+
+	public Ess(SymmetricEssNature ess) {
+		super(ess);
+		minSoc = ess.minSoc().required();
+
+		setActivePower = ess.setActivePower().required();
+		setReactivePower = ess.setReactivePower().required();
+
+		soc = ess.soc().required();
+		activePower = ess.activePower().required();
+		allowedCharge = ess.allowedCharge().required();
+		allowedDischarge = ess.allowedDischarge().required();
+		gridMode = ess.gridMode().required();
+		systemState = ess.systemState().required();
+		this.power = new Power(ess.allowedDischarge().required(), ess.allowedCharge().required(),
+				ess.allowedApparent().required(), ess.setActivePower().required(), ess.setReactivePower().required(), 7,
+				7);
+	}
+
+	public long useableSoc() throws InvalidValueException {
+		return soc.value() - minSoc.value();
+	}
+}
