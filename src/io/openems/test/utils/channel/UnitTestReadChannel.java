cf399879c650a41e389a1442874d8f7a5a30802e
Matthias Roßmann <matthias.rossmann@fenecon.de>
2017-05-26 16:22:51 +0200

    add constructor generating thing, add label

--- src/io/openems/test/utils/channel/UnitTestReadChannel.java
+++ src/io/openems/test/utils/channel/UnitTestReadChannel.java
@@ -4,12 +4,28 @@ import io.openems.api.thing.Thing;
 
 public class UnitTestReadChannel<T> extends io.openems.api.channel.ReadChannel<T> {
 
+	public UnitTestReadChannel(String deviceName,String id){
+		super(id,new Thing() {
+			
+			@Override
+			public String id() {
+				return deviceName;
+			}
+		});
+	}
+	
 	public UnitTestReadChannel(String id, Thing parent) {
 		super(id, parent);
 	}
 
 	public void setValue(T value) {
 		this.updateValue(value);
+	}
+
+	@Override
+	public UnitTestReadChannel<T> label(T value, String label) {
+		super.label(value, label);
+		return this;
 	}
 
 }
