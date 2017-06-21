5646476c3691af85fdbbe40456d298b16b23054e
Matthias Roßmann <matthias.rossmann@fenecon.de>
2017-06-21 15:09:41 +0200

    Change loadGenerator Config to generic JsonObject

--- src/io/openems/impl/device/simulator/SimulatorProductionMeter.java
+++ src/io/openems/impl/device/simulator/SimulatorProductionMeter.java
@@ -1,5 +1,13 @@
 package io.openems.impl.device.simulator;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
+
+import com.google.gson.JsonObject;
+
+import io.openems.api.channel.Channel;
+import io.openems.api.channel.ChannelChangeListener;
 import io.openems.api.channel.ConfigChannel;
 import io.openems.api.channel.FunctionalReadChannel;
 import io.openems.api.channel.FunctionalReadChannelFunction;
@@ -7,14 +15,25 @@ import io.openems.api.channel.ReadChannel;
 import io.openems.api.doc.ConfigInfo;
 import io.openems.api.exception.ConfigException;
 import io.openems.core.utilities.ControllerUtils;
+import io.openems.impl.protocol.simulator.SimulatorReadChannel;
 
-public class SimulatorProductionMeter extends SimulatorMeter {
+public class SimulatorProductionMeter extends SimulatorMeter implements ChannelChangeListener {
 
-	@ConfigInfo(title = "ActivePower", type = Long.class)
-	public ConfigChannel<Long> activePower = new ConfigChannel("ActivePower", this);
+	// @ConfigInfo(title = "ActivePower", type = Long.class)
+	// public ConfigChannel<Long> activePower = new ConfigChannel("ActivePower", this);
+	// @ConfigInfo(title = "ReactivePower", type = Long.class)
+	// public ConfigChannel<Long> reactivePower = new ConfigChannel<Long>("ReactivePower", this);
+	@ConfigInfo(title = "ActivePowerGeneratorConfig", type = JsonObject.class)
+	public ConfigChannel<JsonObject> activePowerGeneratorConfig = new ConfigChannel<JsonObject>(
+			"activePowerGeneratorConfig", this).addChangeListener(this);
+	@ConfigInfo(title = "ReactivePowerGeneratorConfig", type = JsonObject.class)
+	public ConfigChannel<JsonObject> reactivePowerGeneratorConfig = new ConfigChannel<JsonObject>(
+			"reactivePowerGeneratorConfig", this).addChangeListener(this);
+	private SimulatorReadChannel<Long> activePower = new SimulatorReadChannel<>("ActivePower", this);
+	private SimulatorReadChannel<Long> reactivePower = new SimulatorReadChannel<>("ReactivePower", this);
 	private FunctionalReadChannel<Long> apparentPower;
-	@ConfigInfo(title = "ReactivePower", type = Long.class)
-	public ConfigChannel<Long> reactivePower = new ConfigChannel<Long>("ReactivePower", this);
+	private LoadGenerator activePowerGenerator;
+	private LoadGenerator reactivePowerGenerator;
 
 	public SimulatorProductionMeter(String thingId) throws ConfigException {
 		super(thingId);
@@ -43,6 +62,71 @@ public class SimulatorProductionMeter extends SimulatorMeter {
 	@Override
 	public ReadChannel<Long> reactivePower() {
 		return reactivePower;
+	}
+
+	@Override
+	protected void update() {
+		super.update();
+		if (activePowerGenerator != null) {
+			activePower.updateValue(activePowerGenerator.getLoad());
+		} else {
+			activePower.updateValue(0L);
+			log.error("activePowerGenerator is null");
+		}
+		if (reactivePowerGenerator != null) {
+			reactivePower.updateValue(reactivePowerGenerator.getLoad());
+		} else {
+			reactivePower.updateValue(0L);
+			log.error("reactivePowerGenerator is null");
+		}
+	}
+
+	@Override
+	public void channelChanged(Channel channel, Optional<?> newValue, Optional<?> oldValue) {
+		if (channel.equals(activePowerGeneratorConfig)) {
+			if (activePowerGeneratorConfig.valueOptional().isPresent()) {
+				activePowerGenerator = getGenerator(activePowerGeneratorConfig.valueOptional().get());
+			}
+		} else if (channel.equals(reactivePowerGeneratorConfig)) {
+			if (reactivePowerGeneratorConfig.valueOptional().isPresent()) {
+				reactivePowerGenerator = getGenerator(reactivePowerGeneratorConfig.valueOptional().get());
+			}
+		}
+	}
+
+	private LoadGenerator getGenerator(JsonObject config) {
+		try {
+			Class<?> clazz = Class.forName(config.get("className").getAsString());
+			if (config.get("config") != null) {
+				try {
+					Constructor<?> constructor = clazz.getConstructor(JsonObject.class);
+					return (LoadGenerator) constructor.newInstance(config.get("config").getAsJsonObject());
+				} catch (NoSuchMethodException e) {
+
+				}
+			}
+			return (LoadGenerator) clazz.newInstance();
+
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
