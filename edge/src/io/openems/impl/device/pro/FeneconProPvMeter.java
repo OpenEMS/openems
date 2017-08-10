OpenEMS/openems

commit 46b0cc081b8dc7d0194607675cdf61c5dcf887f5

replace negative production with zero

edge/src/io/openems/impl/device/pro/FeneconProPvMeter.java
@@ -74,9 +74,9 @@ public class FeneconProPvMeter extends ModbusDeviceNature implements AsymmetricM
 	/*
 	 * Inherited Channels
 	 */
-	private ModbusReadChannel<Long> activePowerL1;
-	private ModbusReadChannel<Long> activePowerL2;
-	private ModbusReadChannel<Long> activePowerL3;
+	private ReadChannel<Long> activePowerL1;
+	private ReadChannel<Long> activePowerL2;
+	private ReadChannel<Long> activePowerL3;
 	private ReadChannel<Long> reactivePowerL1 = new ReadChannel<Long>("ReactivePowerL1", this);
 	private ReadChannel<Long> reactivePowerL2 = new ReadChannel<Long>("ReactivePowerL2", this);
 	private ReadChannel<Long> reactivePowerL3 = new ReadChannel<Long>("ReactivePowerL3", this);
@@ -90,6 +90,9 @@ public class FeneconProPvMeter extends ModbusDeviceNature implements AsymmetricM
 	private ReadChannel<Long> reactivePower;
 	private ReadChannel<Long> apparentPower = new ReadChannel<>("apparentPower", this);
 	private ReadChannel<Long> frequency = new ReadChannel<>("frequency", this);
+	private ModbusReadChannel<Long> orginalActivePowerL1;
+	private ModbusReadChannel<Long> orginalActivePowerL2;
+	private ModbusReadChannel<Long> orginalActivePowerL3;
 
 	@Override
 	public ReadChannel<Long> activePowerL1() {
@@ -186,38 +189,70 @@ public class FeneconProPvMeter extends ModbusDeviceNature implements AsymmetricM
 	@Override
 	protected ModbusProtocol defineModbusProtocol() throws ConfigException {
 		ModbusProtocol protocol = new ModbusProtocol( //
-				new ModbusRegisterRange(121,
-						new UnsignedWordElement(121, //
-								voltageL1 = new ModbusReadLongChannel("VoltageL1", this).unit("mV").multiplier(2)),
+				new ModbusRegisterRange(121, new UnsignedWordElement(121, //
+						voltageL1 = new ModbusReadLongChannel("VoltageL1", this).unit("mV").multiplier(2)),
 						new UnsignedWordElement(122, //
 								voltageL2 = new ModbusReadLongChannel("VoltageL2", this).unit("mV").multiplier(2)),
 						new UnsignedWordElement(123, //
 								voltageL3 = new ModbusReadLongChannel("VoltageL3", this).unit("mV").multiplier(2))),
 				new ModbusRegisterRange(2035, //
 						new UnsignedDoublewordElement(2035, //
-								activeEnergyL1 = new ModbusReadLongChannel("ActiveEnergyL1", this).unit("Wh")
-										.multiplier(2)),
-						new DummyElement(2037, 2065),
-						new UnsignedWordElement(2066, //
-								activePowerL1 = new ModbusReadLongChannel("ActivePowerL1", this).unit("W")
+								activeEnergyL1 = new ModbusReadLongChannel("ActiveEnergyL1", this)
+										.unit("Wh").multiplier(2)),
+						new DummyElement(2037, 2065), new UnsignedWordElement(2066, //
+								orginalActivePowerL1 = new ModbusReadLongChannel("ActivePowerL1", this).unit("W")
 										.delta(10000L))),
 				new ModbusRegisterRange(2135, //
 						new UnsignedDoublewordElement(2135, //
-								activeEnergyL2 = new ModbusReadLongChannel("ActiveEnergyL2", this).unit("Wh")
-										.multiplier(2)),
-						new DummyElement(2137, 2165),
-						new UnsignedWordElement(2166, //
-								activePowerL2 = new ModbusReadLongChannel("ActivePowerL2", this).unit("W")
+								activeEnergyL2 = new ModbusReadLongChannel("ActiveEnergyL2", this).unit(
+										"Wh").multiplier(2)),
+						new DummyElement(2137, 2165), new UnsignedWordElement(2166, //
+								orginalActivePowerL2 = new ModbusReadLongChannel("ActivePowerL2", this).unit("W")
 										.delta(10000L))),
 				new ModbusRegisterRange(2235, //
 						new UnsignedDoublewordElement(2235, //
-								activeEnergyL3 = new ModbusReadLongChannel("ActiveEnergyL3", this).unit("Wh")
-										.multiplier(2)),
-						new DummyElement(2237, 2265),
-						new UnsignedWordElement(2266, //
-								activePowerL3 = new ModbusReadLongChannel("ActivePowerL3", this).unit("W")
+								activeEnergyL3 = new ModbusReadLongChannel("ActiveEnergyL3", this)
+										.unit("Wh").multiplier(2)),
+						new DummyElement(2237, 2265), new UnsignedWordElement(2266, //
+								orginalActivePowerL3 = new ModbusReadLongChannel("ActivePowerL3", this).unit("W")
 										.delta(10000L))));
-		activePower = new FunctionalReadChannel<Long>("reactive", this, (channels) -> {
+		activePowerL1 = new FunctionalReadChannel<Long>("activePowerL1", this, (channels) -> {
+			ReadChannel<Long> power = channels[0];
+			try {
+				if (power.value() >= 0) {
+					return power.value();
+				} else {
+					return 0L;
+				}
+			} catch (InvalidValueException e) {
+				return null;
+			}
+		}, orginalActivePowerL1);
+		activePowerL2 = new FunctionalReadChannel<Long>("activePowerL2", this, (channels) -> {
+			ReadChannel<Long> power = channels[0];
+			try {
+				if (power.value() >= 0) {
+					return power.value();
+				} else {
+					return 0L;
+				}
+			} catch (InvalidValueException e) {
+				return null;
+			}
+		}, orginalActivePowerL2);
+		activePowerL3 = new FunctionalReadChannel<Long>("activePowerL3", this, (channels) -> {
+			ReadChannel<Long> power = channels[0];
+			try {
+				if (power.value() >= 0) {
+					return power.value();
+				} else {
+					return 0L;
+				}
+			} catch (InvalidValueException e) {
+				return null;
+			}
+		}, orginalActivePowerL3);
+		activePower = new FunctionalReadChannel<Long>("activePower", this, (channels) -> {
 			ReadChannel<Long> L1 = channels[0];
 			ReadChannel<Long> L2 = channels[1];
 			ReadChannel<Long> L3 = channels[2];
@@ -227,7 +262,7 @@ public class FeneconProPvMeter extends ModbusDeviceNature implements AsymmetricM
 				return null;
 			}
 		}, activePowerL1, activePowerL2, activePowerL3);
-		reactivePower = new FunctionalReadChannel<Long>("reactive", this, (channels) -> {
+		reactivePower = new FunctionalReadChannel<Long>("reactivePower", this, (channels) -> {
 			ReadChannel<Long> L1 = channels[0];
 			ReadChannel<Long> L2 = channels[1];
 			ReadChannel<Long> L3 = channels[2];

commit 704c4bb0ec559a151eff76e003190a6baeafeb32

Fix ActivePower-bug for ProPvMeter
This bug caused the monitoring to not show anymore data for pv production. The problem is caused by the lower case naming "activePower" introduced in commit 46b0cc081b8dc7d0194607675cdf61c5dcf887f5

edge/src/io/openems/impl/device/pro/FeneconProPvMeter.java
@@ -200,23 +200,23 @@ public class FeneconProPvMeter extends ModbusDeviceNature implements AsymmetricM
 								activeEnergyL1 = new ModbusReadLongChannel("ActiveEnergyL1", this)
 										.unit("Wh").multiplier(2)),
 						new DummyElement(2037, 2065), new UnsignedWordElement(2066, //
-								orginalActivePowerL1 = new ModbusReadLongChannel("ActivePowerL1", this).unit("W")
-										.delta(10000L))),
+								orginalActivePowerL1 = new ModbusReadLongChannel("OriginalActivePowerL1", this)
+										.unit("W").delta(10000L))),
 				new ModbusRegisterRange(2135, //
 						new UnsignedDoublewordElement(2135, //
-								activeEnergyL2 = new ModbusReadLongChannel("ActiveEnergyL2", this).unit(
-										"Wh").multiplier(2)),
+								activeEnergyL2 = new ModbusReadLongChannel("ActiveEnergyL2", this)
+										.unit("Wh").multiplier(2)),
 						new DummyElement(2137, 2165), new UnsignedWordElement(2166, //
-								orginalActivePowerL2 = new ModbusReadLongChannel("ActivePowerL2", this).unit("W")
-										.delta(10000L))),
+								orginalActivePowerL2 = new ModbusReadLongChannel("OriginalActivePowerL2", this)
+										.unit("W").delta(10000L))),
 				new ModbusRegisterRange(2235, //
 						new UnsignedDoublewordElement(2235, //
 								activeEnergyL3 = new ModbusReadLongChannel("ActiveEnergyL3", this)
 										.unit("Wh").multiplier(2)),
 						new DummyElement(2237, 2265), new UnsignedWordElement(2266, //
-								orginalActivePowerL3 = new ModbusReadLongChannel("ActivePowerL3", this).unit("W")
-										.delta(10000L))));
-		activePowerL1 = new FunctionalReadChannel<Long>("activePowerL1", this, (channels) -> {
+								orginalActivePowerL3 = new ModbusReadLongChannel("OriginalActivePowerL3", this)
+										.unit("W").delta(10000L))));
+		activePowerL1 = new FunctionalReadChannel<Long>("ActivePowerL1", this, (channels) -> {
 			ReadChannel<Long> power = channels[0];
 			try {
 				if (power.value() >= 0) {
@@ -228,7 +228,7 @@ public class FeneconProPvMeter extends ModbusDeviceNature implements AsymmetricM
 				return null;
 			}
 		}, orginalActivePowerL1);
-		activePowerL2 = new FunctionalReadChannel<Long>("activePowerL2", this, (channels) -> {
+		activePowerL2 = new FunctionalReadChannel<Long>("ActivePowerL2", this, (channels) -> {
 			ReadChannel<Long> power = channels[0];
 			try {
 				if (power.value() >= 0) {
@@ -240,7 +240,7 @@ public class FeneconProPvMeter extends ModbusDeviceNature implements AsymmetricM
 				return null;
 			}
 		}, orginalActivePowerL2);
-		activePowerL3 = new FunctionalReadChannel<Long>("activePowerL3", this, (channels) -> {
+		activePowerL3 = new FunctionalReadChannel<Long>("ActivePowerL3", this, (channels) -> {
 			ReadChannel<Long> power = channels[0];
 			try {
 				if (power.value() >= 0) {
@@ -252,7 +252,7 @@ public class FeneconProPvMeter extends ModbusDeviceNature implements AsymmetricM
 				return null;
 			}
 		}, orginalActivePowerL3);
-		activePower = new FunctionalReadChannel<Long>("activePower", this, (channels) -> {
+		activePower = new FunctionalReadChannel<Long>("ActivePower", this, (channels) -> {
 			ReadChannel<Long> L1 = channels[0];
 			ReadChannel<Long> L2 = channels[1];
 			ReadChannel<Long> L3 = channels[2];
@@ -262,7 +262,7 @@ public class FeneconProPvMeter extends ModbusDeviceNature implements AsymmetricM
 				return null;
 			}
 		}, activePowerL1, activePowerL2, activePowerL3);
-		reactivePower = new FunctionalReadChannel<Long>("reactivePower", this, (channels) -> {
+		reactivePower = new FunctionalReadChannel<Long>("ReactivePower", this, (channels) -> {
 			ReadChannel<Long> L1 = channels[0];
 			ReadChannel<Long> L2 = channels[1];
 			ReadChannel<Long> L3 = channels[2];