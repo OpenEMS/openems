2c0bd3b5f606ff62f50c8d3e5d5e0f1762f90cde
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2017-08-19 22:42:00 +0200

    Change config to environment variables (2nd try)
    This change makes it easier to use docker

--- backend/src/main/java/io/openems/femsserver/App.java
+++ backend/src/main/java/io/openems/femsserver/App.java
@@ -1,12 +1,5 @@
 package io.openems.femsserver;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Properties;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,53 +15,42 @@ public class App {
 		log.info("FEMS-Server starting...");
 
 		// Configure everything
-		Properties config = getConfig();
-		initOdoo(config);
-		initInfluxdb(config);
-		initFemsWebsocket(config);
-		initBrowserWebsocket(config);
+		initOdoo();
+		initInfluxdb();
+		initFemsWebsocket();
+		initBrowserWebsocket();
 
 		log.info("FEMS-Server started.");
 	}
 
-	private static Properties getConfig() throws IOException {
-		log.info("Read config");
-		Path configLocation = Paths.get("config.properties");
-		try (InputStream stream = Files.newInputStream(configLocation)) {
-			Properties config = new Properties();
-			config.load(stream);
-			return config;
-		}
-	}
-
-	private static void initOdoo(Properties config) throws Exception {
-		int port = Integer.valueOf(config.getProperty("odoo.port"));
+	private static void initOdoo() throws Exception {
+		int port = Integer.valueOf(System.getenv("ODOO_PORT"));
 		log.info("Connect to Odoo on port [" + port + "]");
-		String url = config.getProperty("odoo.url");
-		String database = config.getProperty("odoo.database");
-		String username = config.getProperty("odoo.username");
-		String password = config.getProperty("odoo.password");
+		String url = System.getenv("ODOO_URL");
+		String database = System.getenv("ODOO_DATABASE");
+		String username = System.getenv("ODOO_USERNAME");
+		String password = System.getenv("ODOO_PASSWORD");
 		Odoo.initialize(url, port, database, username, password);
 	}
 
-	private static void initInfluxdb(Properties config) throws Exception {
-		int port = Integer.valueOf(config.getProperty("influx.port"));
+	private static void initInfluxdb() throws Exception {
+		int port = Integer.valueOf(System.getenv("INFLUX_PORT"));
 		log.info("Connect to InfluxDB on port [" + port + "]");
-		String database = config.getProperty("influx.database");
-		String url = config.getProperty("influx.url");
-		String username = config.getProperty("influx.username");
-		String password = config.getProperty("influx.password");
+		String database = System.getenv("INFLUX_DATABASE");
+		String url = System.getenv("INFLUX_URL");
+		String username = System.getenv("INFLUX_USERNAME");
+		String password = System.getenv("INFLUX_PASSWORD");
 		Influxdb.initialize(database, url, port, username, password);
 	}
 
-	private static void initFemsWebsocket(Properties config) throws Exception {
-		int port = Integer.valueOf(config.getProperty("femswebsocket.port"));
+	private static void initFemsWebsocket() throws Exception {
+		int port = Integer.valueOf(System.getenv("OPENEMS_WEBSOCKET_PORT"));
 		log.info("Start FEMS Websocket server on port [" + port + "]");
 		FemsWebsocket.initialize(port);
 	}
 
-	private static void initBrowserWebsocket(Properties config) throws Exception {
-		int port = Integer.valueOf(config.getProperty("browserwebsocket.port"));
+	private static void initBrowserWebsocket() throws Exception {
+		int port = Integer.valueOf(System.getenv("BROWSER_WEBSOCKET_PORT"));
 		log.info("Start Browser Websocket server on port [" + port + "]");
 		BrowserWebsocket.initialize(port);
 	}
