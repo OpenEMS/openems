2079c9d2317ec9fea3e87c51df1c5c94714ade68
Matthias Roßmann <matthias.rossmann@fenecon.de>
2017-08-30 13:46:31 +0200

    add timerecording to provide starttime for the bridges

--- edge/src/io/openems/api/scheduler/Scheduler.java
+++ edge/src/io/openems/api/scheduler/Scheduler.java
@@ -26,19 +26,47 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import io.openems.api.bridge.Bridge;
+import io.openems.api.channel.ConfigChannel;
 import io.openems.api.controller.Controller;
+import io.openems.api.doc.ConfigInfo;
 import io.openems.api.exception.ConfigException;
 import io.openems.api.exception.ReflectionException;
 import io.openems.api.thing.Thing;
+import io.openems.core.ThingRepository;
 import io.openems.core.utilities.AbstractWorker;
 
 public abstract class Scheduler extends AbstractWorker implements Thing {
 	public final static String THINGID_PREFIX = "_scheduler";
 	private static int instanceCounter = 0;
 	protected final Map<String, Controller> controllers = new ConcurrentHashMap<>();
+	private long requiredTime = 0;
+	private long cycleStartTime = 0;
+	protected final ThingRepository thingRepository;
+	private Integer actualCycleTime = null;
+
+	/*
+	 * Config
+	 */
+	@ConfigInfo(title = "Sets the duration of each cycle in milliseconds", type = Integer.class)
+	public ConfigChannel<Integer> cycleTime = new ConfigChannel<Integer>("cycleTime", this);
+
+	// @Override
+	// public ConfigChannel<Integer> cycleTime() {
+	// return cycleTime;
+	// }
+	@Override
+	protected int getCycleTime() {
+		int time = cycleTime.valueOptional().orElse(500);
+		if (actualCycleTime != null) {
+			time = actualCycleTime;
+		}
+		return time;
+	}
 
 	public Scheduler() {
 		super(THINGID_PREFIX + instanceCounter++);
+		thingRepository = ThingRepository.getInstance();
 	}
 
 	public synchronized void addController(Controller controller) throws ReflectionException, ConfigException {
@@ -51,5 +79,38 @@ public abstract class Scheduler extends AbstractWorker implements Thing {
 
 	public synchronized List<Controller> getControllers() {
 		return Collections.unmodifiableList(new ArrayList<>(this.controllers.values()));
+	}
+
+	@Override
+	protected void forever() {
+		cycleStartTime = System.currentTimeMillis();
+		execute();
+		requiredTime = System.currentTimeMillis() - cycleStartTime;
+		long maxTime = 0;
+		for (Bridge bridge : thingRepository.getBridges()) {
+			if (bridge.getRequiredCycleTime() > maxTime) {
+				maxTime = bridge.getRequiredCycleTime();
+			}
+		}
+		maxTime = (maxTime + 100) / 100 * 100;
+		if (maxTime > cycleTime.valueOptional().orElse(500)) {
+			actualCycleTime = (int) maxTime;
+		} else {
+			actualCycleTime = cycleTime.valueOptional().orElse(500);
+		}
+	}
+
+	protected abstract void execute();
+
+	public long getRequiredTime() {
+		return requiredTime;
+	}
+
+	public long getNextCycleStart() {
+		return cycleStartTime + getCycleTime();
+	}
+
+	public long getCycleStartTime() {
+		return cycleStartTime;
 	}
 }
