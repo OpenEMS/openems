682d98a7967562d044a60ebd259b074f4f1ccb3b
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2017-09-11 09:07:26 +0200

    Add "List" to ignored datatypes for FeneconPersistence

--- edge/src/io/openems/impl/persistence/fenecon/FeneconPersistence.java
+++ edge/src/io/openems/impl/persistence/fenecon/FeneconPersistence.java
@@ -24,6 +24,7 @@ import java.net.Inet4Address;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -308,7 +309,7 @@ public class FeneconPersistence extends Persistence implements ChannelChangeList
 			} else if (value instanceof Boolean) {
 				fieldValue = new NumberFieldValue(field, ((Boolean) value) ? 1 : 0);
 			} else if (value instanceof DeviceNature || value instanceof JsonElement || value instanceof Map
-					|| value instanceof Set) {
+					|| value instanceof Set || value instanceof List) {
 				// ignore
 				return;
 			} else {
