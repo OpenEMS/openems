a0ee3c77e59e6d053312dbc2e07cff1f25c6d121
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2017-10-05 12:08:30 +0200

    Improve Energytable

---
+++ ui/src/environments/openems-backend-dev-live.ts
@@ -0,0 +1,11 @@
+import { Environment } from "../app/shared/type/environment";
+import { DefaultTypes } from '../app/shared/service/defaulttypes';
+
+class OpenemsBackendDevEnvironment extends Environment {
+  public readonly production = false;
+  public readonly url = "wss://localhost:443/openems-backend-ui";
+  public readonly backend: DefaultTypes.Backend = "OpenEMS Backend"
+  public debugMode = true;
+}
+
+export const environment = new OpenemsBackendDevEnvironment();
