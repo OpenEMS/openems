7c882395132300d51d1aca37f16945e296ae26dc
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2017-11-21 21:32:27 +0100

    Improve EVCS widget

--- ui/src/app/device/overview/evcs/evcs.component.ts
+++ ui/src/app/device/overview/evcs/evcs.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy, ViewChildren, QueryList } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 
 import { Utils } from '../../../shared/service/utils';
 import { DefaultTypes } from '../../../shared/service/defaulttypes';
 import { CurrentDataAndSummary } from '../../../shared/device/currentdata';
+import { Device } from '../../../shared/device/device';
+import { ChannelComponent } from '../../../shared/config/channel.component';
 
 @Component({
   selector: 'evcs',
@@ -17,5 +19,39 @@ export class EvcsComponent {
   @Input()
   public config: DefaultTypes.Config;
 
+  @Input()
+  public device: Device;
+
   constructor(public utils: Utils) { }
+
+  ngOnDestroy() {
+    this.stopOnDestroy.next();
+    this.stopOnDestroy.complete();
+  }
+
+  /**
+   * Handle config update
+   */
+
+  @ViewChildren(ChannelComponent)
+  private channelComponentChildren: QueryList<ChannelComponent>;
+  private stopOnDestroy: Subject<void> = new Subject<void>();
+  private formInitialized: boolean = false;
+
+  ngAfterViewChecked() {
+    // unfortunately components are not available yet in ngAfterViewInit, so we need to call it again and again, till they are there.
+    if (this.formInitialized || this.channelComponentChildren.length == 0) {
+      return;
+    }
+    this.channelComponentChildren.forEach(channelComponent => {
+      channelComponent.message
+        .takeUntil(this.stopOnDestroy)
+        .subscribe((message) => {
+          if (message != null) {
+            this.device.send(message);
+          }
+        });
+    });
+    this.formInitialized = true;
+  }
 }
