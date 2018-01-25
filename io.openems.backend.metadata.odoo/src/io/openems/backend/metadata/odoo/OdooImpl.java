01683eba53de24a3aa5c1f34da9952b05cf4303f
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-01-25 17:23:43 +0100

    Implement connection to Odoo

--- io.openems.backend.metadata.odoo/src/io/openems/backend/metadata/odoo/OdooImpl.java
+++ io.openems.backend.metadata.odoo/src/io/openems/backend/metadata/odoo/OdooImpl.java
@@ -1,5 +1,14 @@
 package io.openems.backend.metadata.odoo;
 
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+import io.openems.backend.metadata.api.MetadataService;
+import io.openems.common.OpenemsException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,55 +18,44 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
-import org.apache.xmlrpc.XmlRpcException;
-
-import com.google.common.collect.LinkedHashMultimap;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.odoojava.api.OdooApiException;
-import com.odoojava.api.Session;
 
-import io.openems.backend.browserwebsocket.session.BrowserSession;
-import io.openems.backend.browserwebsocket.session.BrowserSessionData;
-import io.openems.backend.metadata.api.MetadataSingleton;
-import io.openems.backend.metadata.api.device.MetadataDeviceModel;
-import io.openems.backend.metadata.odoo.device.OdooDeviceModel;
-import io.openems.common.exceptions.OpenemsException;
-import io.openems.common.session.SessionData;
-import io.openems.common.types.DeviceImpl;
-import io.openems.common.utils.JsonUtils;
-
-public class OdooSingleton implements MetadataSingleton {
-	private Session session;
-	private MetadataDeviceModel deviceModel;
-	private final String url;
-
-	public OdooSingleton(String url, int port, String database, String username, String password)
-			throws OpenemsException {
-		this.session = new Session(url, port, database, username, password);
-		this.connect();
-		try {
-			this.deviceModel = new OdooDeviceModel(this.session);
-		} catch (XmlRpcException | OdooApiException e) {
-			throw new OpenemsException("Initializing OdooDeviceModel failed: " + e.getMessage());
-		}
-		this.url = "http://" + url + ":" + port;
+@Designate(ocd = OdooImpl.Config.class, factory = true)
+@Component(name = "io.openems.backend.metadata.odoo")
+public class OdooImpl implements MetadataService {
+	
+	@ObjectClassDefinition
+	@interface Config {
+		String database();
+		int uid();
+		String password();
+		String url() default "https://www1.fenecon.de";
 	}
 
-	private void connect() throws OpenemsException {
-		try {
-			session.startSession();
-		} catch (Exception e) {
-			throw new OpenemsException("Odoo connection failed: " + e.getMessage());
-		}
+	private String url;
+	private String database;
+	private int uid;
+	private String password;
+
+	@Activate
+	void activate(Config config) {
+		this.url = config.url();
+		this.database = config.database();
+		this.uid = config.uid();
+		this.password = config.password();
 	}
 
-	@Override
-	public MetadataDeviceModel getDeviceModel() {
-		return deviceModel;
+	@Deactivate
+	void deactivate() {
 	}
+	
+	//	private MetadataDeviceModel deviceModel;
+
+	//	@Override
+	//	public MetadataDeviceModel getDeviceModel() {
+	//		return deviceModel;
+	//	}
 
 	/**
 	 * Tries to authenticate at the Odoo server using a sessionId from a cookie. Updates the Session object accordingly.
@@ -67,19 +65,21 @@ public class OdooSingleton implements MetadataSingleton {
 	 * @throws OpenemsException
 	 */
 	@Override
-	public void getInfoWithSession(BrowserSession session) throws OpenemsException {
+	// public void getInfoWithSession(BrowserSession session) throws OpenemsException {
+	public void getInfoWithSession() throws OpenemsException {
 		HttpURLConnection connection = null;
 		try {
 			// get session_id from Session
-			SessionData sessionData = session.getData();
-			if (!(sessionData instanceof BrowserSessionData)) {
-				throw new OpenemsException("Session is of wrong type.");
-			}
-			BrowserSessionData data = (BrowserSessionData) sessionData;
-			if (!(data.getOdooSessionId().isPresent())) {
-				throw new OpenemsException("Session-ID is missing.");
-			}
-			String sessionId = data.getOdooSessionId().get();
+			// SessionData sessionData = session.getData();
+			// if (!(sessionData instanceof BrowserSessionData)) {
+			// 	throw new OpenemsException("Session is of wrong type.");
+			// }
+			// BrowserSessionData data = (BrowserSessionData) sessionData;
+			// if (!(data.getOdooSessionId().isPresent())) {
+			// 	throw new OpenemsException("Session-ID is missing.");
+			// }
+			// String sessionId = data.getOdooSessionId().get();
+			String sessionId = "";
 
 			// send request to Odoo
 			String charset = "US-ASCII";
@@ -101,32 +101,33 @@ public class OdooSingleton implements MetadataSingleton {
 			BufferedReader br = new BufferedReader(new InputStreamReader(is));
 			String line = null;
 			while ((line = br.readLine()) != null) {
+				System.out.println(line);
 				JsonObject j = (new JsonParser()).parse(line).getAsJsonObject();
-				if (j.has("error")) {
-					JsonObject jError = JsonUtils.getAsJsonObject(j, "error");
-					String errorMessage = JsonUtils.getAsString(jError, "message");
-					throw new OpenemsException("Odoo replied with error: " + errorMessage);
-				}
+//				if (j.has("error")) {
+//					JsonObject jError = JsonUtils.getAsJsonObject(j, "error");
+//					String errorMessage = JsonUtils.getAsString(jError, "message");
+//					throw new OpenemsException("Odoo replied with error: " + errorMessage);
+//				}
 
-				if (j.has("result")) {
-					// parse the result
-					JsonObject jResult = JsonUtils.getAsJsonObject(j, "result");
-					JsonObject jUser = JsonUtils.getAsJsonObject(jResult, "user");
-					data.setUserId(JsonUtils.getAsInt(jUser, "id"));
-					data.setUserName(JsonUtils.getAsString(jUser, "name"));
-					JsonArray jDevices = JsonUtils.getAsJsonArray(jResult, "devices");
-					LinkedHashMultimap<String, DeviceImpl> deviceMap = LinkedHashMultimap.create();
-					for (JsonElement jDevice : jDevices) {
-						String name = JsonUtils.getAsString(jDevice, "name");
-						deviceMap.put(name, new DeviceImpl( //
-								name, //
-								JsonUtils.getAsString(jDevice, "comment"), //
-								JsonUtils.getAsString(jDevice, "producttype"), //
-								JsonUtils.getAsString(jDevice, "role")));
-					}
-					data.setDevices(deviceMap);
-					return;
-				}
+//				if (j.has("result")) {
+//					// parse the result
+//					JsonObject jResult = JsonUtils.getAsJsonObject(j, "result");
+//					JsonObject jUser = JsonUtils.getAsJsonObject(jResult, "user");
+//					data.setUserId(JsonUtils.getAsInt(jUser, "id"));
+//					data.setUserName(JsonUtils.getAsString(jUser, "name"));
+//					JsonArray jDevices = JsonUtils.getAsJsonArray(jResult, "devices");
+//					LinkedHashMultimap<String, DeviceImpl> deviceMap = LinkedHashMultimap.create();
+//					for (JsonElement jDevice : jDevices) {
+//						String name = JsonUtils.getAsString(jDevice, "name");
+//						deviceMap.put(name, new DeviceImpl( //
+//								name, //
+//								JsonUtils.getAsString(jDevice, "comment"), //
+//								JsonUtils.getAsString(jDevice, "producttype"), //
+//								JsonUtils.getAsString(jDevice, "role")));
+//					}
+//					data.setDevices(deviceMap);
+//					return;
+//				}
 			}
 		} catch (IOException e) {
 			throw new OpenemsException("IOException while reading from Odoo: " + e.getMessage());
