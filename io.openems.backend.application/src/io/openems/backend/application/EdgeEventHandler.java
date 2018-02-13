ec093dd345f2815dd87e0f266bcc1372730c2d53
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-02-13 09:41:58 +0100

    Handle EdgeOnline-events

---
+++ io.openems.backend.application/src/io/openems/backend/application/EdgeEventHandler.java
@@ -0,0 +1,18 @@
+package io.openems.backend.application;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
+
+import io.openems.backend.common.events.BackendEventConstants;
+
+@Component(property = EventConstants.EVENT_TOPIC + "=" + BackendEventConstants.TOPIC_EDGE_ONLINE)
+public class EdgeEventHandler implements EventHandler {
+
+	@Override
+	public void handleEvent(Event event) {
+		System.out.println(event);
+	}
+	
+}
