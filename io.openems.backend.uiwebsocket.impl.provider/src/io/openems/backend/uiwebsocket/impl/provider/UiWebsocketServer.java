1ff54c7aa3656c6d84d0befa16905310f59e5727
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-02-17 23:25:26 +0100

    Implement config query message

--- io.openems.backend.uiwebsocket.impl.provider/src/io/openems/backend/uiwebsocket/impl/provider/UiWebsocketServer.java
+++ io.openems.backend.uiwebsocket.impl.provider/src/io/openems/backend/uiwebsocket/impl/provider/UiWebsocketServer.java
@@ -1,7 +1,10 @@
 package io.openems.backend.uiwebsocket.impl.provider;
 
 import java.util.Map.Entry;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.java_websocket.WebSocket;
 import org.java_websocket.framing.CloseFrame;
@@ -16,6 +19,8 @@ import io.openems.backend.metadata.api.Edge;
 import io.openems.backend.metadata.api.Role;
 import io.openems.backend.metadata.api.User;
 import io.openems.common.exceptions.OpenemsException;
+import io.openems.common.utils.JsonUtils;
+import io.openems.common.websocket.AbstractWebsocketServer;
 import io.openems.common.websocket.DefaultMessages;
 import io.openems.common.websocket.WebSocketUtils;
 
@@ -23,6 +28,7 @@ public class UiWebsocketServer extends AbstractWebsocketServer {
 
 	private final Logger log = LoggerFactory.getLogger(UiWebsocketServer.class);
 	private final UiWebsocket parent;
+	private final Map<UUID, WebSocket> websocketsMap = new HashMap<>();
 
 	public UiWebsocketServer(UiWebsocket parent, int port) {
 		super(port);
@@ -32,7 +38,7 @@ public class UiWebsocketServer extends AbstractWebsocketServer {
 	@Override
 	protected void _onOpen(WebSocket websocket, ClientHandshake handshake) {
 		String error = "";
-		User user = null;
+		Optional<User> userOpt = Optional.empty();
 
 		// login using session_id from the cookie
 		Optional<String> sessionIdOpt = getSessionIdFromHandshake(handshake);
@@ -40,7 +46,8 @@ public class UiWebsocketServer extends AbstractWebsocketServer {
 			error = "Session-ID is missing in handshake";
 		} else {
 			try {
-				user = this.parent.metadataService.getUserWithSession(sessionIdOpt.get());
+				userOpt = this.parent.metadataService.getUserWithSession(sessionIdOpt.get());
+				// TODO fix bug in Odoo that is not reliably returning all configured devices
 			} catch (OpenemsException e) {
 				error = e.getMessage();
 			}
@@ -51,8 +58,18 @@ public class UiWebsocketServer extends AbstractWebsocketServer {
 			this.send(websocket, DefaultMessages.browserConnectionFailedReply());
 			log.warn("User connection failed. Session [" + sessionIdOpt.orElse("") + "] Error [" + error + "].");
 			websocket.closeConnection(CloseFrame.REFUSE, error);
-			
-		} else if (user != null) {
+
+		} else if (userOpt.isPresent()) {
+			User user = userOpt.get();
+
+			UUID uuid = UUID.randomUUID();
+			synchronized (this.websocketsMap) {
+				// add websocket to local cache
+				this.websocketsMap.put(uuid, websocket);
+			}
+			// store userId together with the websocket
+			websocket.setAttachment(user.getId());
+
 			// send connection successful to browser
 			JsonArray jEdges = new JsonArray();
 			for (Entry<Integer, Role> edgeRole : user.getEdgeRoles().entrySet()) {
@@ -75,12 +92,6 @@ public class UiWebsocketServer extends AbstractWebsocketServer {
 	}
 
 	@Override
-	protected void _onMessage(WebSocket websocket, JsonObject jMessage, Optional<JsonArray> jMessageIdOpt,
-			Optional<String> deviceNameOpt) {
-		log.info("UiWebsocketServer: On Message");
-	}
-
-	@Override
 	protected void _onError(WebSocket websocket, Exception ex) {
 		log.info("UiWebsocketServer: On Error");
 	}
@@ -88,5 +99,101 @@ public class UiWebsocketServer extends AbstractWebsocketServer {
 	@Override
 	protected void _onClose(WebSocket websocket) {
 		log.info("UiWebsocketServer: On Close");
+	}
+
+	@Override
+	protected void _onMessage(WebSocket websocket, JsonObject jMessage) {
+		// get current User
+		int userId = websocket.getAttachment();
+
+		// get MessageId from message
+		Optional<String> messageIdOpt = JsonUtils.getAsOptionalString(jMessage, "messageId");
+
+		// get EdgeId from message
+		Optional<Integer> edgeIdOpt = JsonUtils.getAsOptionalInt(jMessage, "edgeId");
+
+		if (messageIdOpt.isPresent() && edgeIdOpt.isPresent()) {
+			String messageId = messageIdOpt.get();
+			int edgeId = edgeIdOpt.get();
+
+			/*
+			 * verify that User is allowed to access Edge
+			 */
+			Optional<User> userOpt = this.parent.metadataService.getUser(userId);
+			if (!userOpt.isPresent() || !(userOpt.get().getEdgeRole(edgeId).isPresent())) {
+				// TODO Error Access denied
+				return;
+			}
+
+			/*
+			 * TODO Query historic data
+			 */
+			// if (jMessage.has("historicData")) {
+			// // parse deviceId
+			// JsonArray jMessageId = jMessageIdOpt.get();
+			// try {
+			// JsonObject jHistoricData = JsonUtils.getAsJsonObject(jMessage,
+			// "historicData");
+			// JsonObject jReply = WebSocketUtils.historicData(jMessageId, jHistoricData,
+			// deviceIdOpt,
+			// Timedata.instance(), Role.ADMIN);
+			// // TODO read role from device
+			// WebSocketUtils.send(websocket, jReply);
+			// } catch (OpenemsException e) {
+			// log.error(e.getMessage());
+			// }
+			// }
+
+			/*
+			 * TODO Subscribe to currentData
+			 */
+			// if (jMessage.has("currentData")) {
+			// JsonObject jCurrentData;
+			// try {
+			// jCurrentData = JsonUtils.getAsJsonObject(jMessage, "currentData");
+			// log.info("User [" + session.getData().getUserName() + "] subscribed to
+			// current data for device ["
+			// + deviceName + "]: " + StringUtils.toShortString(jCurrentData, 50));
+			// JsonArray jMessageId = jMessageIdOpt.get();
+			// int deviceId = deviceIdOpt.get();
+			// this.currentData(session, websocket, jCurrentData, jMessageId, deviceName,
+			// deviceId);
+			// } catch (OpenemsException e) {
+			// log.error(e.getMessage());
+			// }
+			// }
+
+			/*
+			 * Serve "Config -> Query" from cache
+			 */
+			Optional<JsonObject> jConfigOpt = JsonUtils.getAsOptionalJsonObject(jMessage, "config");
+			if (jConfigOpt.isPresent()) {
+				JsonObject jConfig = jConfigOpt.get();
+				switch (JsonUtils.getAsOptionalString(jConfig, "mode").orElse("")) {
+				case "query":
+					/*
+					 * Query current config
+					 */
+					Edge edge = this.parent.metadataService.getEdge(edgeId).get();
+					JsonObject jReply = DefaultMessages.configQueryReply(messageId, edge.getConfig());
+					WebSocketUtils.send(websocket, jReply);
+					break;
+				}
+
+				/*
+				 * TODO Forward to OpenEMS Edge
+				 */
+				// if ((jMessage.has("config") && !configModeOpt.orElse("").equals("query")) ||
+				// jMessage.has("log")
+				// || jMessage.has("system")) {
+				// try {
+				// forwardMessageToOpenems(session, websocket, jMessage, deviceName);
+				// } catch (OpenemsException e) {
+				// WebSocketUtils.sendNotification(websocket, new JsonArray(),
+				// LogBehaviour.WRITE_TO_LOG,
+				// Notification.EDGE_UNABLE_TO_FORWARD, deviceName, e.getMessage());
+				// }
+			}
+		}
 	}
 }
