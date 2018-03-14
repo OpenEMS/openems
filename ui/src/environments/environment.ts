ed64e0429f44384e5778a7408565f579f4386130
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-03-14 20:26:06 +0100

    Reset UI environment files

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -3,13 +3,13 @@ import { DefaultTypes } from "../app/shared/service/defaulttypes";
 
 class DefaultEnvironment extends Environment {
   public readonly production = false;
-  // For OpenEMS Edge
-  // public readonly url = "ws://" + location.hostname + ":8085";
-  // public readonly backend: DefaultTypes.Backend = "OpenEMS Edge";
-  // For OpenEMS Backend
-  public readonly url = "ws://" + location.hostname + ":8078";
-  public readonly backend: DefaultTypes.Backend = "OpenEMS Backend";
   public debugMode = true;
+  // For OpenEMS Edge
+  public readonly url = "ws://" + location.hostname + ":8085";
+  public readonly backend: DefaultTypes.Backend = "OpenEMS Edge";
+  // For OpenEMS Backend
+  // public readonly url = "ws://" + location.hostname + ":8078";
+  // public readonly backend: DefaultTypes.Backend = "OpenEMS Backend";
 }
 
 export const environment = new DefaultEnvironment();
