319c6b6e79b6d026446e4f2be48347c50b57b472
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-04-15 12:18:35 +0200

    Combine WriteRegisters to a single writeMultipleRegisters

--- io.openems.edge.bridge.modbus.api/src/io/openems/edge/bridge/modbus/api/task/FC16WriteRegistersTask.java
+++ io.openems.edge.bridge.modbus.api/src/io/openems/edge/bridge/modbus/api/task/FC16WriteRegistersTask.java
@@ -1,5 +1,7 @@
 package io.openems.edge.bridge.modbus.api.task;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -28,20 +30,63 @@ public class FC16WriteRegistersTask extends FC3ReadRegistersTask implements Writ
 		super(startAddress, elements);
 	}
 
+	private class CombinedWriteRegisters {
+		public final int startAddress;
+		private final List<Register> registers = new ArrayList<>();
+
+		public CombinedWriteRegisters(int startAddress) {
+			this.startAddress = startAddress;
+		}
+
+		public void add(Register... registers) {
+			for (Register register : registers) {
+				this.registers.add(register);
+			}
+		}
+
+		public Register[] getRegisters() {
+			return this.registers.toArray(new Register[this.registers.size()]);
+		}
+
+		public int getLastAddress() {
+			return this.startAddress + registers.size();
+		}
+	}
+
 	@Override
 	public void executeWrite(ModbusTCPMaster master) throws ModbusException {
-		// TODO find gaps and combine register ranges
+		/*
+		 * Combine WriteRegisters without holes inbetween
+		 */
+		List<CombinedWriteRegisters> writes = new ArrayList<>();
 		ModbusElement<?>[] elements = this.getElements();
 		for (int i = 0; i < elements.length; i++) {
 			ModbusElement<?> element = elements[i];
 			if (element instanceof ModbusRegisterElement) {
 				Optional<Register[]> valueOpt = ((ModbusRegisterElement<?>) element).getNextWriteValueAndReset();
 				if (valueOpt.isPresent()) {
-					master.writeMultipleRegisters(this.getUnitId(), element.getStartAddress(), valueOpt.get());
+					// found value -> add to 'writes'
+					CombinedWriteRegisters write;
+					if (writes.isEmpty() /* no writes created yet */
+							|| writes.get(writes.size() - 1).getLastAddress() + 1 != element
+									.getStartAddress() /* there is a hole between last element and current element */) {
+						write = new CombinedWriteRegisters(element.getStartAddress());
+						writes.add(write);
+					} else {
+						write = writes.get(writes.size() - 1); // no hole -> combine writes
+					}
+					write.add(valueOpt.get());
 				}
 			} else {
 				log.warn("Unable to execute Write for ModbusElement [" + element + "]: No ModbusRegisterElement!");
 			}
+		}
+
+		/*
+		 * Execute combined writes
+		 */
+		for (CombinedWriteRegisters write : writes) {
+			master.writeMultipleRegisters(this.getUnitId(), write.startAddress, write.getRegisters());
 		}
 	}
 }
