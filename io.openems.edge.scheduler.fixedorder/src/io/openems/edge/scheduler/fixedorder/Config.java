f952c91eb30ae1a726f0895eb0abe6ff8b6229d4
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-04-19 22:32:04 +0200

    Improve visualization of FixedOrderController in webconsole

--- io.openems.edge.scheduler.fixedorder/src/io/openems/edge/scheduler/fixedorder/Config.java
+++ io.openems.edge.scheduler.fixedorder/src/io/openems/edge/scheduler/fixedorder/Config.java
@@ -5,7 +5,9 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 import io.openems.edge.scheduler.api.Scheduler;
 
-@ObjectClassDefinition
+@ObjectClassDefinition( //
+		name = "Fixed Order Scheduler", //
+		description = "This Scheduler takes a list of Component IDs and returns the Controllers statically sorted by this order.")
 @interface Config {
 	String service_pid();
 
@@ -17,4 +19,6 @@ import io.openems.edge.scheduler.api.Scheduler;
 
 	@AttributeDefinition(name = "Controller-IDs", description = "IDs of Controllers. Controller execution is going to be sorted in the order of the IDs.")
 	String[] controllers_ids() default {};
+
+	String webconsole_configurationFactory_nameHint() default "Fixed Order Scheduler [{id}]";
 }
