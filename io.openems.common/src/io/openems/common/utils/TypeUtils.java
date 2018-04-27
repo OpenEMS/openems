eba10c89bd9b84f0624daa9e0bbe42c15edc31c6
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-04-27 22:58:36 +0200

    TypeUtils: Throw IllegalArgumentException

--- io.openems.common/src/io/openems/common/utils/TypeUtils.java
+++ io.openems.common/src/io/openems/common/utils/TypeUtils.java
@@ -2,13 +2,12 @@ package io.openems.common.utils;
 
 import java.util.Optional;
 
-import io.openems.common.exceptions.NotImplementedException;
 import io.openems.common.types.OpenemsType;
 
 public class TypeUtils {
 
 	@SuppressWarnings("unchecked")
-	public static <T> T getAsType(OpenemsType type, Object value) throws NotImplementedException {
+	public static <T> T getAsType(OpenemsType type, Object value) {
 		// Extract Optionals
 		if (value instanceof Optional<?>) {
 			value = ((Optional<?>) value).orElse(null);
@@ -21,7 +20,7 @@ public class TypeUtils {
 		case BOOLEAN:
 			return (T) ((Boolean) value);
 		}
-		throw new NotImplementedException(
+		throw new IllegalArgumentException(
 				"Converter for value [" + value + "] to type [" + type + "] is not implemented.");
 	}
 }
