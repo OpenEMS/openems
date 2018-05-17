af5beabbfd2858d022c7ca6277c3fe8335c25d41
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-05-17 10:16:55 +0200

    Improve DetailedLog. Only list changed values + sort alphabetically

--- io.openems.edge.controller.debug.detailedlog/src/io/openems/edge/controller/debug/detailedlog/DebugDetailedLog.java
+++ io.openems.edge.controller.debug.detailedlog/src/io/openems/edge/controller/debug/detailedlog/DebugDetailedLog.java
@@ -1,6 +1,10 @@
 package io.openems.edge.controller.debug.detailedlog;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.osgi.service.cm.ConfigurationAdmin;
@@ -21,6 +25,7 @@ import com.google.common.base.CaseFormat;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 
+import io.openems.common.types.ChannelAddress;
 import io.openems.edge.common.component.AbstractOpenemsComponent;
 import io.openems.edge.common.component.OpenemsComponent;
 import io.openems.edge.controller.api.Controller;
@@ -35,6 +40,9 @@ public class DebugDetailedLog extends AbstractOpenemsComponent implements Contro
 	private final Logger log = LoggerFactory.getLogger(DebugDetailedLog.class);
 
 	private final int WIDTH_FIRST = 30;
+
+	private final Set<String> finishedFirstRun = new HashSet<>();
+	private final Map<ChannelAddress, String> lastPrinted = new HashMap<>();
 
 	private List<OpenemsComponent> _components = new CopyOnWriteArrayList<>();
 
@@ -68,30 +76,62 @@ public class DebugDetailedLog extends AbstractOpenemsComponent implements Contro
 	@Override
 	public void run() {
 		this._components.stream().forEach(component -> {
-			logInfo(this.log, "=======================================");
-			this.log("ID", component.id());
-			this.log("Service-PID", component.servicePid());
-			this.log("Implementation", reducePackageName(component.getClass()));
-			getInheritanceViaReflection(component.getClass(), null).asMap().forEach((inheritance, names) -> {
-				boolean first = true;
-				for (String name : names) {
-					if (first) {
-						log(CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, inheritance.name()), name);
-					} else {
-						log("", name);
+			boolean printedHeader = false;
+
+			if (!this.finishedFirstRun.contains(component.id())) {
+				/*
+				 * Print on first run
+				 */
+				logInfo(this.log, "=======================================");
+				this.log("ID", component.id());
+				this.log("Service-PID", component.servicePid());
+				this.log("Implementation", reducePackageName(component.getClass()));
+				getInheritanceViaReflection(component.getClass(), null).asMap().forEach((inheritance, names) -> {
+					boolean first = true;
+					for (String name : names) {
+						if (first) {
+							log(CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, inheritance.name()), name);
+						} else {
+							log("", name);
+						}
+						first = false;
 					}
-					first = false;
-				}
-			});
-			logInfo(this.log, "---------------------------------------");
+				});
+				this.finishedFirstRun.add(component.id());
+				printedHeader = true;
+			}
+
+			final Map<ChannelAddress, String> shouldPrint = new HashMap<>();
 			component.channels().stream() //
 					.sorted((c1, c2) -> c1.channelId().name().compareTo(c2.channelId().name())) //
 					.forEach(channel -> {
 						String unit = channel.channelDoc().getUnit().getSymbol();
-						this.logInfo(this.log, String.format("%-" + WIDTH_FIRST + "s : %15s %s",
-								channel.channelId().id(), channel.value().asStringWithoutUnit(), unit));
+						String line = String.format("%-" + WIDTH_FIRST + "s : %15s %s", channel.channelId().id(),
+								channel.value().asStringWithoutUnit(), unit);
+						// Print the line only if is not equal to the last printed line
+						if ((!this.lastPrinted.containsKey(channel.address()))
+								|| !(this.lastPrinted.get(channel.address()).equals(line))) {
+							shouldPrint.put(channel.address(), line);
+						}
+						// Add line to last printed lines
+						this.lastPrinted.put(channel.address(), line);
 					});
-			logInfo(this.log, "---------------------------------------");
+
+			if (!shouldPrint.isEmpty()) {
+				if (!printedHeader) {
+					/*
+					 * Print header (this is not the first run)
+					 */
+					logInfo(this.log, "=======================================");
+					this.log("ID", component.id());
+				}
+
+				logInfo(this.log, "---------------------------------------");
+				shouldPrint.values().stream().sorted().forEach(line -> {
+					this.logInfo(this.log, line);
+				});
+				logInfo(this.log, "---------------------------------------");
+			}
 		});
 	}
 
