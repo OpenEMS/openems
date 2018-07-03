2058181be04e36be4f2b3f18f48402cc0a4b33e5
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-07-03 16:34:44 +0200

    Push version to 2018.6.1

--- edge/src/io/openems/App.java
+++ edge/src/io/openems/App.java
@@ -38,8 +38,8 @@ import io.openems.core.Config;
 public class App {
 	private final static Logger log = LoggerFactory.getLogger(App.class);
 
-	// public final static String OPENEMS_VERSION = "2018.6.0";
-	public final static String OPENEMS_VERSION = "2018.7.0-SNAPSHOT";
+	public final static String OPENEMS_VERSION = "2018.6.1";
+	// public final static String OPENEMS_VERSION = "2018.7.0-SNAPSHOT";
 
 	public static void main(String[] args) {
 		log.info("OpenEMS version [" + OPENEMS_VERSION + "] started");
