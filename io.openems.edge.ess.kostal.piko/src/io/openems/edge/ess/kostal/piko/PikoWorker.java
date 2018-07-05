c720c8b6a39079f1059d4ed9eff464938c628a80
huseyinsaht <hueseyin.sahutoglu@fenecon.de>
2018-07-05 15:17:12 +0200

    PikoWorker for Asyncronous

---
+++ io.openems.edge.ess.kostal.piko/src/io/openems/edge/ess/kostal/piko/PikoWorker.java
@@ -0,0 +1,16 @@
+package io.openems.edge.ess.kostal.piko;
+
+import io.openems.edge.common.worker.AbstractWorker;
+
+public class PikoWorker extends AbstractWorker {
+
+	@Override
+	protected void forever() {
+	}
+
+	@Override
+	protected int getCycleTime() {
+		return 1000;
+	}
+
+}
