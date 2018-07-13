c88dd56760e9a73aea4930e08bc42ac8c819f7f7
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-07-13 09:38:21 +0200

    [modbus] Add toBitString(InputRegister[]) helper method

--- io.openems.edge.bridge.modbus/src/io/openems/edge/bridge/modbus/api/task/Utils.java
+++ io.openems.edge.bridge.modbus/src/io/openems/edge/bridge/modbus/api/task/Utils.java
@@ -1,11 +1,14 @@
 package io.openems.edge.bridge.modbus.api.task;
 
+import java.util.Arrays;
 import java.util.BitSet;
+import java.util.stream.Collectors;
 
 import com.ghgande.j2mod.modbus.ModbusException;
 import com.ghgande.j2mod.modbus.io.ModbusTransaction;
 import com.ghgande.j2mod.modbus.msg.ModbusRequest;
 import com.ghgande.j2mod.modbus.msg.ModbusResponse;
+import com.ghgande.j2mod.modbus.procimg.InputRegister;
 
 import io.openems.common.exceptions.OpenemsException;
 import io.openems.edge.bridge.modbus.AbstractModbusBridge;
@@ -21,12 +24,25 @@ public class Utils {
 		return bools;
 	}
 
-	public static ModbusResponse getResponse(ModbusRequest request, int unitId, AbstractModbusBridge bridge) throws OpenemsException, ModbusException {
-		request.setUnitID(unitId);		
+	public static ModbusResponse getResponse(ModbusRequest request, int unitId, AbstractModbusBridge bridge)
+			throws OpenemsException, ModbusException {
+		request.setUnitID(unitId);
 		ModbusTransaction transaction = bridge.getNewModbusTransaction();
 		transaction.setRequest(request);
 		transaction.execute();
-		ModbusResponse response =  transaction.getResponse();
+		ModbusResponse response = transaction.getResponse();
 		return response;
+	}
+
+	public static String toBitString(InputRegister[] registers) {
+		return Arrays.stream(registers).map(register -> {
+			byte[] bs = register.toBytes();
+
+			return String.format("%8s", //
+					Integer.toBinaryString(bs[0] & 0xFF)).replace(' ', '0') //
+					+ " " //
+					+ String.format("%8s", //
+							Integer.toBinaryString(bs[1] & 0xFF)).replace(' ', '0');
+		}).collect(Collectors.joining(" "));
 	}
 }
