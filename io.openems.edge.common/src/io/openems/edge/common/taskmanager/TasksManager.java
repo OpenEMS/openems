ea6d1c1b9bfedfa6c96cfa6bf021bbaab1ddb744
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-07-21 00:25:46 +0200

    Make TaskManager more dynamic. Now tasks can be added and removed at runtime.

--- io.openems.edge.common/src/io/openems/edge/common/taskmanager/TasksManager.java
+++ io.openems.edge.common/src/io/openems/edge/common/taskmanager/TasksManager.java
@@ -1,36 +1,67 @@
 package io.openems.edge.common.taskmanager;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class TasksManager<T extends Task> {
 
-	private final List<T> prioHighTasks = new ArrayList<>();
-	private final List<T> prioLowTasks = new ArrayList<>();
-	private final List<T> prioOnceTasks = new ArrayList<>();
+	private final List<T> allTasks = new CopyOnWriteArrayList<>();
+
+	private final List<T> prioHighTasks = new CopyOnWriteArrayList<>();
+	private final List<T> prioLowTasks = new CopyOnWriteArrayList<>();
+	private final List<T> prioOnceTasks = new CopyOnWriteArrayList<>();
 
 	private final Queue<T> nextLowTasks = new LinkedList<>();
 	private final Queue<T> nextOnceTasks = new LinkedList<>();
 
 	@SafeVarargs
 	public TasksManager(T... tasks) {
+		this.addTasks(tasks);
+	}
+
+	@SafeVarargs
+	public final synchronized void addTasks(T... tasks) {
 		for (T task : tasks) {
-			switch (task.getPriority()) {
-			case HIGH:
-				this.prioHighTasks.add(task);
-				break;
-			case LOW:
-				this.prioLowTasks.add(task);
-				break;
-			case ONCE:
-				this.prioOnceTasks.add(task);
-				break;
-			}
+			this.addTask(task);
 		}
-		// Fill the 'nextOnceTasks'. This happens only once.
-		this.nextOnceTasks.addAll(prioOnceTasks);
+	}
+		
+	public synchronized void addTask(T task) {
+		this.allTasks.add(task);
+		switch (task.getPriority()) {
+		case HIGH:
+			this.prioHighTasks.add(task);
+			break;
+		case LOW:
+			this.prioLowTasks.add(task);
+			break;
+		case ONCE:
+			this.prioOnceTasks.add(task);
+			// Fill the 'nextOnceTasks'. This happens only once.
+			this.nextOnceTasks.add(task);
+			break;
+		}
+	}
+
+	public synchronized void removeTask(T task) {
+		this.allTasks.remove(task);
+		switch (task.getPriority()) {
+		case HIGH:
+			this.prioHighTasks.remove(task);
+			break;
+		case LOW:
+			this.prioLowTasks.remove(task);
+			this.nextLowTasks.remove(task);
+			break;
+		case ONCE:
+			this.prioOnceTasks.remove(task);
+			this.nextOnceTasks.remove(task);
+			break;
+		}
 	}
 
 	public synchronized List<T> getNextReadTasks() {
@@ -59,6 +90,10 @@ public class TasksManager<T extends Task> {
 		if (task != null) {
 			result.add(task);
 		}
-		return result;
+		return Collections.unmodifiableList(result);
+	}
+
+	public synchronized List<T> getAllTasks() {
+		return Collections.unmodifiableList(this.allTasks);
 	}
 }
