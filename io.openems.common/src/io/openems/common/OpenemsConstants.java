ab6f71d9070385f2a215b5975ae12487fa6aabda
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-07-20 17:15:12 +0200

    Set version to 2018.10.0-SNAPSHOT

--- io.openems.common/src/io/openems/common/OpenemsConstants.java
+++ io.openems.common/src/io/openems/common/OpenemsConstants.java
@@ -2,8 +2,8 @@ package io.openems.common;
 
 public class OpenemsConstants {
 
-	public final static String OPENEMS_VERSION = "2018.9.0";
+	// public final static String OPENEMS_VERSION = "2018.9.0";
 
-	// public final static String OPENEMS_VERSION = "2018.9.0-SNAPSHOT";
+	public final static String OPENEMS_VERSION = "2018.10.0-SNAPSHOT";
 
 }
