8519670ec0e36fa73af5508ad5211f6d4b9308f1
huseyinsaht <hueseyin.sahutoglu@fenecon.de>
2018-08-05 15:57:40 +0200

    Beginning of Implementation; Fenecon Pro9-12

---
+++ io.openems.edge.ess.fenecon.pro/src/io/openems/edge/ess/fenecon/pro/Utils.java
@@ -0,0 +1,48 @@
+package io.openems.edge.ess.fenecon.pro;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import io.openems.edge.common.channel.AbstractReadChannel;
+import io.openems.edge.common.channel.IntegerReadChannel;
+import io.openems.edge.common.channel.StateCollectorChannel;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.ess.api.ManagedSymmetricEss;
+import io.openems.edge.ess.api.SymmetricEss;
+
+public class Utils {
+	public static Stream<? extends AbstractReadChannel<?>> initializeChannels(EssFeneconPro c) {
+		// Define the channels. Using streams + switch enables Eclipse IDE to tell us if
+		// we are missing an Enum value.
+		return Stream.of( //
+				Arrays.stream(OpenemsComponent.ChannelId.values()).map(channelId -> {
+					switch (channelId) {
+					case STATE:
+						return new StateCollectorChannel(c, channelId);
+					}
+					return null;
+				}), 
+				Arrays.stream(SymmetricEss.ChannelId.values()).map(channelId -> {
+					switch (channelId) {
+					case SOC:
+					case ACTIVE_POWER:
+					case REACTIVE_POWER:
+					case ACTIVE_CHARGE_ENERGY:
+					case ACTIVE_DISCHARGE_ENERGY:
+						return new IntegerReadChannel(c, channelId);
+					case MAX_ACTIVE_POWER:
+					case GRID_MODE:
+						return new IntegerReadChannel(c, channelId, SymmetricEss.GridMode.UNDEFINED.ordinal());
+					}
+					return null;
+				}), Arrays.stream(ManagedSymmetricEss.ChannelId.values()).map(channelId -> {
+					switch (channelId) {
+					case DEBUG_SET_ACTIVE_POWER:
+					case DEBUG_SET_REACTIVE_POWER:
+						return new IntegerReadChannel(c, channelId);
+					}
+					return null;
+				})
+		).flatMap(channel -> channel);
+	}
+}
