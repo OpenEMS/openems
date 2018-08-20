18f61046df221932cc3daacb82af89127a888f8e
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-08-20 13:42:04 +0200

    Extend AllAlphabetically. It now accepts a sorted list of Controller-IDs.

--- io.openems.edge.scheduler.allalphabetically/src/io/openems/edge/scheduler/allalphabetically/Config.java
+++ io.openems.edge.scheduler.allalphabetically/src/io/openems/edge/scheduler/allalphabetically/Config.java
@@ -1,12 +1,13 @@
 package io.openems.edge.scheduler.allalphabetically;
 
+import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 import io.openems.edge.scheduler.api.Scheduler;
 
 @ObjectClassDefinition( //
 		name = "Scheduler All Alphabetically", //
-		description = "This Scheduler returns all existing Controllers ordered by their ID.")
+		description = "This Scheduler takes an ordered list of Component IDs. All remaining Controllers are afterwards ordered alphabetically by their ID.")
 @interface Config {
 	String service_pid();
 
@@ -15,6 +16,9 @@ import io.openems.edge.scheduler.api.Scheduler;
 	boolean enabled() default true;
 
 	int cycleTime() default Scheduler.DEFAULT_CYCLE_TIME;
+
+	@AttributeDefinition(name = "Controller-IDs", description = "IDs of Controllers. Controller execution is going to be sorted in the order of the IDs.")
+	String[] controllers_ids() default {};
 
 	String webconsole_configurationFactory_nameHint() default "Scheduler All Alphabetically [{id}]";
 }
