c72706e917db1bdac12e98ff386ca8a37883f0db
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-09-12 15:54:17 +0200

    Implement WAGO fieldbus coupler

--- io.openems.edge.WagoTest/readme.md
+++
@@ -1,8 +0,0 @@
-# io.openems.edge.WagoTest Provider
-
-${Bundle-Description}
-
-## Example
-
-## References
-
--- io.openems.edge.io.wago/src/io/openems/edge/wago/Utils.java
+++ io.openems.edge.io.wago/src/io/openems/edge/wago/Utils.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 import io.openems.edge.common.channel.AbstractReadChannel;
-import io.openems.edge.common.channel.BooleanReadChannel;
-import io.openems.edge.common.channel.BooleanWriteChannel;
 import io.openems.edge.common.channel.StateCollectorChannel;
 import io.openems.edge.common.component.OpenemsComponent;
 
@@ -18,16 +16,7 @@ public class Utils {
 						return new StateCollectorChannel(c, channelId);
 					}
 					return null;
-				})
-				// , Arrays.stream(DigitalOutput.ChannelId.values()).map(channelId -> { return
-				// * null; })
-				, Arrays.stream(Wago.ChannelId.values()).map(channelId -> {
-					switch (channelId) {
-						return new BooleanWriteChannel(c, channelId);
-						return new BooleanReadChannel(c, channelId);
-					}
-					return null;
-				}) //
-		).flatMap(channel -> channel);
+				})) //
+				.flatMap(channel -> channel);
 	}
 }
