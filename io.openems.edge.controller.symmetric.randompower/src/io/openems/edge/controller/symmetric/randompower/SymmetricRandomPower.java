2ee47dd9c1cac3d76846a4e4d921edf887725286
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-09-25 13:17:05 +0200

    Migrate SymmetricRandomPower Controller to new power solver

--- io.openems.edge.controller.symmetric.randompower/src/io/openems/edge/controller/symmetric/randompower/SymmetricRandomPower.java
+++ io.openems.edge.controller.symmetric.randompower/src/io/openems/edge/controller/symmetric/randompower/SymmetricRandomPower.java
@@ -13,13 +13,15 @@ import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.component.annotations.ReferencePolicyOption;
 import org.osgi.service.metatype.annotations.Designate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.openems.edge.common.component.AbstractOpenemsComponent;
 import io.openems.edge.common.component.OpenemsComponent;
 import io.openems.edge.controller.api.Controller;
 import io.openems.edge.ess.api.ManagedSymmetricEss;
-import io.openems.edge.ess.power.api.ConstraintType;
 import io.openems.edge.ess.power.api.Phase;
+import io.openems.edge.ess.power.api.PowerException;
 import io.openems.edge.ess.power.api.Pwr;
 import io.openems.edge.ess.power.api.Relationship;
 
@@ -27,7 +29,7 @@ import io.openems.edge.ess.power.api.Relationship;
 @Component(name = "Controller.Symmetric.RandomPower", immediate = true, configurationPolicy = ConfigurationPolicy.REQUIRE)
 public class SymmetricRandomPower extends AbstractOpenemsComponent implements Controller, OpenemsComponent {
 
-//	private final Logger log = LoggerFactory.getLogger(SymmetricLinearPowerBand.class);
+	private final Logger log = LoggerFactory.getLogger(SymmetricRandomPower.class);
 
 	@Reference
 	protected ConfigurationAdmin cm;
@@ -59,7 +61,17 @@ public class SymmetricRandomPower extends AbstractOpenemsComponent implements Co
 	public void run() {
 		int randomPower = ThreadLocalRandom.current().nextInt(this.minPower, this.maxPower + 1);
 
-		// Apply Power
-		this.ess.addPowerConstraint(ConstraintType.CYCLE, Phase.ALL, Pwr.ACTIVE, Relationship.EQUALS, randomPower);
+		// adjust value so that it fits into Min/MaxActivePower
+		randomPower = ess.getPower().fitValueIntoMinMaxActivePower(ess, Phase.ALL, Pwr.ACTIVE, randomPower);
+
+		/*
+		 * set result
+		 */
+		try {
+			this.ess.addPowerConstraintAndValidate("SymmetricRandomPower", Phase.ALL, Pwr.ACTIVE, Relationship.EQUALS,
+					randomPower); //
+		} catch (PowerException e) {
+			this.logError(this.log, e.getMessage());
+		}
 	}
 }
