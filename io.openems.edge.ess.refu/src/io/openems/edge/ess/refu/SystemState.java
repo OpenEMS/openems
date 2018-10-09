c9a27d4a64fa774261634279b218498571b58e2e
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-10-09 13:24:10 +0200

    [refu] implement error handling & prepare release

---
+++ io.openems.edge.ess.refu/src/io/openems/edge/ess/refu/SystemState.java
@@ -0,0 +1,31 @@
+package io.openems.edge.ess.refu;
+
+import io.openems.edge.common.channel.doc.OptionsEnum;
+
+enum SystemState implements OptionsEnum {
+	UNDEFINED(-1, "Undefined"), //
+	OFF(0, "Off"), //
+	INIT(1, "Init"), //
+	PRE_OPERATION(2, "Pre-operation"), //
+	STANDBY(3, "Standby"), //
+	OPERATION(4, "Operation"), //
+	ERROR(5, "Error");
+
+	private int value;
+	private String option;
+
+	private SystemState(int value, String option) {
+		this.value = value;
+		this.option = option;
+	}
+
+	@Override
+	public int getValue() {
+		return value;
+	}
+
+	@Override
+	public String getOption() {
+		return option;
+	}
+}
