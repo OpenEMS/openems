d5c10a03ff10432732be9e524b30fbf68a6ffa75
Andreas Hummelsberger <andreas.hummelsberger@energydepot.de>
2018-11-22 14:34:11 +0100

    Dependencies Version updates

--- io.openems.common/src/io/openems/common/timedata/CommonTimedataService.java
+++ io.openems.common/src/io/openems/common/timedata/CommonTimedataService.java
@@ -26,7 +26,7 @@ public interface CommonTimedataService {
 	public JsonArray queryHistoricData(ZonedDateTime fromDate, ZonedDateTime toDate, JsonObject channels,
 			int resolution, Tag... tags) throws OpenemsException;
 
-	JsonArray queryHistoricData(ZonedDateTime fromDate, ZonedDateTime toDate, JsonObject channels, int resolution,
+	public JsonArray queryHistoricData(ZonedDateTime fromDate, ZonedDateTime toDate, JsonObject channels, int resolution,
 			boolean cumulative, Tag[] tags) throws OpenemsException;
 
 }
