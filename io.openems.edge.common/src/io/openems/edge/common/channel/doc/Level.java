8a003fd2b630112220d42eca1f569daa277550c3
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-12-24 14:17:17 +0100

    Add clarifying Javadoc to Level enum

--- io.openems.edge.common/src/io/openems/edge/common/channel/doc/Level.java
+++ io.openems.edge.common/src/io/openems/edge/common/channel/doc/Level.java
@@ -4,9 +4,22 @@ package io.openems.edge.common.channel.doc;
  * Severity/visibility Level
  */
 public enum Level implements OptionsEnum {
+	/**
+	 * "OK" indicates, that everything is OK and there are no messages.
+	 */
 	OK(0, "Ok"), //
+	/**
+	 * "Info" indicates, that everything is OK, but there is at least one
+	 * informative messages available.
+	 */
 	INFO(1, "Info"), //
+	/**
+	 * "Warning" indicates, that there is at least one warning message available.
+	 */
 	WARNING(2, "Warning"), //
+	/**
+	 * "Fault" indicates, that there is at least one fault message available.
+	 */
 	FAULT(3, "Fault");
 
 	private final int value;
