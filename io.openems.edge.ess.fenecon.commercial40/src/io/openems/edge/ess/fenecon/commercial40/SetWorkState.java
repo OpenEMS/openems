OpenEMS/openems

commit e866cf083cca582be0df8054db5ade82aaec0879

Refactor Channel options to use OptionsEnum (#175)
* Refactor all Channel options to use OptionsEnum.

- Adds an "Undefined" enum value that always needs to exist. It is used when the Channel has no value.
- This simplifies reading and writing option values. It can be used like this:

IntegerWriteChannel channel = this.channel(ChannelId.PCS_MODE);
// Read:
PcsMode pcsMode = this.getPcsModeChannel().value().asEnum();
if (this.getPcsMode() != PcsMode.REMOTE) {
    ...
}
// Write:
channel.setNextWriteValue(PcsMode.REMOTE);
// or:
channel.setNextWriteValueFromObject("Remote");

* Implement startup routine for Pro 9-12 to activate Remote-Mode

io.openems.edge.ess.fenecon.commercial40/src/io/openems/edge/ess/fenecon/commercial40/SetWorkState.java
@@ -1,0 +1,33 @@
+package io.openems.edge.ess.fenecon.commercial40;
+
+import io.openems.edge.common.channel.doc.OptionsEnum;
+
+public enum SetWorkState implements OptionsEnum {
+	UNDEFINED(-1, "Undefined"), //
+	START(4, "Start"), //
+	STANDBY(32, "Standby"), //
+	STOP(64, "Stop");
+
+	private final int value;
+	private final String name;
+
+	private SetWorkState(int value, String name) {
+		this.value = value;
+		this.name = name;
+	}
+
+	@Override
+	public int getValue() {
+		return value;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public OptionsEnum getUndefined() {
+		return UNDEFINED;
+	}
+}

io.openems.edge.fenecon.mini/src/io/openems/edge/fenecon/mini/ess/SetWorkState.java
@@ -1,0 +1,35 @@
+package io.openems.edge.fenecon.mini.ess;
+
+import io.openems.edge.common.channel.doc.OptionsEnum;
+
+public enum SetWorkState implements OptionsEnum {
+	UNDEFINED(-1, "Undefined"), //
+	LOCAL_CONTROL(0, "Local Control"), //
+	START(1, "Start"), //
+	REMOTE_CONTROL_OFF_GRID_STARTING(2, "Remote Control off Grid Starting"), //
+	STOP(3, "Stop"), //
+	EMERGENCY_STOP(4, "Emergency Stop"); //
+
+	private final int value;
+	private final String name;
+
+	private SetWorkState(int value, String name) {
+		this.value = value;
+		this.name = name;
+	}
+
+	@Override
+	public int getValue() {
+		return value;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public OptionsEnum getUndefined() {
+		return UNDEFINED;
+	}
+}

io.openems.edge.fenecon.pro/src/io/openems/edge/fenecon/pro/ess/SetWorkState.java
@@ -1,0 +1,35 @@
+package io.openems.edge.fenecon.pro.ess;
+
+import io.openems.edge.common.channel.doc.OptionsEnum;
+
+public enum SetWorkState implements OptionsEnum {
+	UNDEFINED(-1, "Undefined"), //
+	LOCAL_CONTROL(0, "Local Control"), //
+	START(1, "Start"), //
+	REMOTE_CONTROL_OFF_GRID_STARTING(2, "Remote Control off Grid Starting"), //
+	STOP(3, "Stop"), //
+	EMERGENCY_STOP(4, "Emergency Stop"); //
+	
+	private final int value;
+	private final String name;
+
+	private SetWorkState(int value, String name) {
+		this.value = value;
+		this.name = name;
+	}
+
+	@Override
+	public int getValue() {
+		return value;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public OptionsEnum getUndefined() {
+		return UNDEFINED;
+	}
+}

commit ef693349ec727a3d9b63e252def3d6941546969f

Fix Commercial start/stop bug introduced in e866cf083cca582be0df8054db5ade82aaec0879

io.openems.edge.ess.fenecon.commercial40/src/io/openems/edge/ess/fenecon/commercial40/SetWorkState.java
@@ -4,9 +4,9 @@ import io.openems.edge.common.channel.doc.OptionsEnum;
 
 public enum SetWorkState implements OptionsEnum {
 	UNDEFINED(-1, "Undefined"), //
-	START(4, "Start"), //
+	STOP(4, "Stop"), //
 	STANDBY(32, "Standby"), //
-	STOP(64, "Stop");
+	START(64, "Start"); //
 
 	private final int value;
 	private final String name;