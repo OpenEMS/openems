6fe6ed28c1b8e6570a4e788c48acbb0ad9d8b958
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2019-02-12 21:39:50 +0100

    Move enums to sub-package

---
+++ io.openems.edge.sma/src/io/openems/edge/sma/enums/RepetitionCycleOfTheControlledInverter.java
@@ -0,0 +1,33 @@
+package io.openems.edge.sma.enums;
+
+import io.openems.edge.common.channel.doc.OptionsEnum;
+
+public enum RepetitionCycleOfTheControlledInverter implements OptionsEnum {
+	UNDEFINED(-1, "Undefined"), //
+	DAILY(1189, "Daily"), //
+	ONCE(2622, "Once"), //
+	WEEKLY(2623, "Weekly");
+
+	private final int value;
+	private final String name;
+
+	private RepetitionCycleOfTheControlledInverter(int value, String name) {
+		this.value = value;
+		this.name = name;
+	}
+
+	@Override
+	public int getValue() {
+		return value;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public OptionsEnum getUndefined() {
+		return UNDEFINED;
+	}
+}
