34cf0a43c0e4df691049d7f8a1c8295fc8b3b7c3
DESKTOP-078TSN8\fab1o <fabian.schwarzbeck@fenecon.de>
2019-02-20 22:48:40 +0100

    oio

--- ui/src/app/edge/history/kwh/kwh.component.ts
+++ ui/src/app/edge/history/kwh/kwh.component.ts
@@ -1,13 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { AbstractHistoryChart } from '../abstracthistorychart';
+import { ChannelAddress, Edge, Service, Utils } from '../../../shared/shared';
 
 @Component({
   selector: 'kwh',
   templateUrl: './kwh.component.html'
 })
-export class KwhComponent implements OnInit {
+export class KwhComponent extends AbstractHistoryChart implements OnInit {
 
-  constructor() { }
+  @Input() private fromDate: Date;
+  @Input() private toDate: Date;
+
+
+
+  constructor(protected service: Service, ) {
+    super(service);
+  }
 
   ngOnInit() { }
+
+  ngOnChanges() {
+    this.updateValues();
+  };
+
+  updateValues() {
+    this.querykWh(this.fromDate, this.toDate).then(response => {
+      let result = response.result
+      let values = [];
+
+    });
+  }
+
+  protected getChannelAddresses(edge: Edge): Promise<ChannelAddress[]> {
+    return new Promise((resolve, reject) => {
+      if (edge.isVersionAtLeast('2018.8')) {
+        resolve([new ChannelAddress('_sum', 'EssSoc')]);
+
+      } else {
+        // TODO: remove after full migration
+        this.service.getConfig().then(config => {
+          // get 'Soc'-Channel of all 'EssNatures'
+          let channeladdresses = [];
+          for (let componentId of config.getComponentsImplementingNature("EssNature")) {
+            channeladdresses.push(new ChannelAddress(componentId, 'Soc'));
+          }
+          resolve(channeladdresses);
+        }).catch(reason => reject(reason));
+      }
+    });
+  }
 
 }
