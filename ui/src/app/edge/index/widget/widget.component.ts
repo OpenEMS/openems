6bbacaabf38c224b960cd15e5d1c85e044c99d4d
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2019-03-07 14:53:23 +0100

    Adjustments for EVCS

--- ui/src/app/edge/index/widget/widget.component.ts
+++ ui/src/app/edge/index/widget/widget.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Service, Widget } from '../../../shared/shared';
+import { Service, Widget, Edge } from '../../../shared/shared';
 
 @Component({
   selector: WidgetComponent.SELECTOR,
@@ -11,6 +11,7 @@ export class WidgetComponent {
   private static readonly SELECTOR = "widget";
 
   public widgets: Widget[] = [];
+  public edge: Edge = null;
 
   constructor(
     private service: Service,
@@ -18,7 +19,9 @@ export class WidgetComponent {
   ) { }
 
   ngOnInit() {
-    this.service.setCurrentEdge(this.route);
+    this.service.setCurrentEdge(this.route).then(edge => {
+      this.edge = edge;
+    });
     this.service.getWidgets().then(widgets => this.widgets = widgets);
   }
 
