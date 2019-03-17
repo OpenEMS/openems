db6ac572a95ac7f192200dcf5af24387f34f7edc
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2019-03-17 13:26:03 +0100

    Channel-Initialisation: ComponentManager, Meta, Sum

--- io.openems.edge.core/src/io/openems/edge/core/componentmanager/ComponentManagerImpl.java
+++ io.openems.edge.core/src/io/openems/edge/core/componentmanager/ComponentManagerImpl.java
@@ -65,10 +65,7 @@ import io.openems.common.jsonrpc.response.GetEdgeConfigResponse;
 import io.openems.common.session.Role;
 import io.openems.common.session.User;
 import io.openems.common.types.EdgeConfig;
-import io.openems.common.types.OpenemsType;
 import io.openems.common.utils.JsonUtils;
-import io.openems.edge.common.channel.Doc;
-import io.openems.edge.common.channel.Level;
 import io.openems.edge.common.channel.internal.StateChannel;
 import io.openems.edge.common.component.AbstractOpenemsComponent;
 import io.openems.edge.common.component.ComponentManager;
@@ -97,23 +94,6 @@ public class ComponentManagerImpl extends AbstractOpenemsComponent
 	@Reference
 	protected ConfigurationAdmin cm;
 
-	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
-		CONFIG_NOT_ACTIVATED(new Doc() //
-				.text("A configured OpenEMS Component was not activated") //
-				.type(OpenemsType.BOOLEAN) //
-				.level(Level.WARNING));
-
-		private final Doc doc;
-
-		private ChannelId(Doc doc) {
-			this.doc = doc;
-		}
-
-		public Doc doc() {
-			return this.doc;
-		}
-	}
-
 	@Reference(policy = ReferencePolicy.DYNAMIC, //
 			policyOption = ReferencePolicyOption.GREEDY, //
 			cardinality = ReferenceCardinality.MULTIPLE, //
@@ -121,8 +101,10 @@ public class ComponentManagerImpl extends AbstractOpenemsComponent
 	protected volatile List<OpenemsComponent> components = new CopyOnWriteArrayList<>();
 
 	public ComponentManagerImpl() {
-		Utils.initializeChannels(this).forEach(channel -> this.addChannel(channel));
-
+		super(//
+				OpenemsComponent.ChannelId.values(), //
+				ComponentManager.ChannelId.values() //
+		);
 		this.osgiValidateWorker = new OsgiValidateWorker(this);
 	}
 
@@ -150,7 +132,7 @@ public class ComponentManagerImpl extends AbstractOpenemsComponent
 	}
 
 	protected StateChannel configNotActivatedChannel() {
-		return this.channel(ChannelId.CONFIG_NOT_ACTIVATED);
+		return this.channel(ComponentManager.ChannelId.CONFIG_NOT_ACTIVATED);
 	}
 
 	@Override
