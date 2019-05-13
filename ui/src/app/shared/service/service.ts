6e19eeac0247b1fb2988d13da6f1a795be9da4f9
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2019-05-13 20:59:53 +0200

    Apply FENECON branding

--- ui/src/app/shared/service/service.ts
+++ ui/src/app/shared/service/service.ts
@@ -121,7 +121,7 @@ export class Service implements ErrorHandler {
       // Set the currentPageTitle only once per ActivatedRoute
       if (this.currentActivatedRoute != activatedRoute) {
         if (currentPageTitle == null || currentPageTitle.trim() === '') {
-          this.currentPageTitle = 'OpenEMS UI';
+          this.currentPageTitle = 'FENECON Online-Monitoring';
         } else {
           this.currentPageTitle = currentPageTitle;
         }
