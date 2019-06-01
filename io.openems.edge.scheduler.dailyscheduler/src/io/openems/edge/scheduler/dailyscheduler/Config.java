6b0501d005c6b00543dfa2815092e782fa18aaa9
Sagar Venu <sagar.venu@fenecon.de>
2019-06-01 15:54:00 +0200

    added default json config

--- io.openems.edge.scheduler.dailyscheduler/src/io/openems/edge/scheduler/dailyscheduler/Config.java
+++ io.openems.edge.scheduler.dailyscheduler/src/io/openems/edge/scheduler/dailyscheduler/Config.java
@@ -17,7 +17,19 @@ import io.openems.edge.scheduler.api.Scheduler;
 	int cycleTime() default Scheduler.DEFAULT_CYCLE_TIME;
 
 	@AttributeDefinition(name = "Controller-IDs", description = "IDs of Controllers.  For Ex; [{ \"time\" : \"12:00:00 \" ,\"controller\" : \"ctrlLimitTotalDischarge0\" }, { \"time\": \"13:00:00\",\"controller \" :  \"ctrlLimitTotalDischarge1\" },{\"time\": \"14:00:00\" ,\"controller\": \"ctrlLimitTotalDischarge2 \" },{\"time\": \"15:00:00\" ,\"controller\": \"ctrlLimitTotalDischarge3\" }]")
-	String controllers_ids_json() default "[{ \"time\" : \"12:00:00 \" ,\"controller\" : \"ctrlLimitTotalDischarge0\" }, { \"time\": \"13:00:00\",\"controller \" :  \"ctrlLimitTotalDischarge1\" },{\"time\": \"14:00:00\" ,\"controller\": \"ctrlLimitTotalDischarge2 \" },{\"time\": \"15:00:00\" ,\"controller\": \"ctrlLimitTotalDischarge3\" }]";
+	String controllers_ids_json() default "[{\n" + 
+			"		\"time\": \"14:30:00\",\n" + 
+			"		\"controllers\": []\n" + 
+			"	},\n" + 
+			"	{\n" + 
+			"		\"time\": \"14:30:00\",\n" + 
+			"		\"controllers\": [\"ctrlFixActivePower0\"]\n" + 
+			"	},\n" + 
+			"	{\n" + 
+			"		\"time\": \"15:30:00\",\n" + 
+			"		\"controllers\": [\"ctrlFixActivePower0\", \"ctrlFixActivePower1\"]\n" + 
+			"	}\n" + 
+			"]";
 
 	@AttributeDefinition(name = "Controller-IDs", description = "IDs of Controllers. Controller execution is going to be sorted in the order of the IDs.")
 	String[] controllers_ids() default { "ctrlLimitTotalDischarge0", "ctrlLimitTotalDischarge1",
