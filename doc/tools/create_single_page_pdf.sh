4122125bc35536321fe36c24d897344329027bfb
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2019-06-05 09:39:46 +0200

    First draft of a German translation

--- doc/tools/create_single_page_pdf.sh
+++ doc/tools/create_single_page_pdf.sh
@@ -2,9 +2,12 @@
 #
 # This creates the single_document.pdf.
 #
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd ${DIR}/../modules/ROOT/pages
+LANG="pages" # default: english
+# LANG="de"
 
-echo "Creating ${DIR}/../modules/ROOT/pages/single_document.pdf"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ${DIR}/../modules/ROOT/${LANG}
+
+echo "Creating ${DIR}/../modules/ROOT/${LANG}/single_document.pdf"
 
 asciidoctor-pdf -o single_document.pdf single_document.adoc
