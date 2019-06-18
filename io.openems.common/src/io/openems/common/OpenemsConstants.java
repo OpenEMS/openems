01c1421c77f2c63611bb991e1f727d078e42327d
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2019-06-18 23:06:01 +0200

    Delete or revert non-required files

--- io.openems.common/src/io/openems/common/OpenemsConstants.java
+++ io.openems.common/src/io/openems/common/OpenemsConstants.java
@@ -18,7 +18,7 @@ public class OpenemsConstants {
 	 * 
 	 * This is usually the number of the sprint within the year
 	 */
-	public final static short VERSION_MINOR = 4;
+	public final static short VERSION_MINOR = 5;
 
 	/**
 	 * The patch version of OpenEMS.
@@ -31,7 +31,7 @@ public class OpenemsConstants {
 	 * The additional version string
 	 */
 	// public final static String VERSION_STRING = "";
-	public final static String VERSION_STRING = "dev.0.67+20190613T111520Z";
+	public final static String VERSION_STRING = "SNAPSHOT";
 
 	/**
 	 * The complete version as a SemanticVersion.
