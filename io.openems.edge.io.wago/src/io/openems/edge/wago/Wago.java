b99ce66671b582aa202fc737a3af9e1425f2fcf2
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2019-07-10 13:30:46 +0200

    WAGO: avoid counting problem for modules due to static variables

--- io.openems.edge.io.wago/src/io/openems/edge/wago/Wago.java
+++ io.openems.edge.io.wago/src/io/openems/edge/wago/Wago.java
@@ -103,10 +103,10 @@ public class Wago extends AbstractOpenemsModbusComponent implements DigitalOutpu
 
 	@Activate
 	void activate(ComponentContext context, Config config) {
-		if(super.activate(context, config.id(), config.enabled(), UNIT_ID, this.cm, "Modbus", config.modbus_id())) {
+		if (super.activate(context, config.id(), config.enabled(), UNIT_ID, this.cm, "Modbus", config.modbus_id())) {
 			return;
 		}
-		
+
 		/*
 		 * Async Create Channels dynamically from ea-config.xml file
 		 */
@@ -158,6 +158,7 @@ public class Wago extends AbstractOpenemsModbusComponent implements DigitalOutpu
 	 * @return a list of FieldbusModules
 	 */
 	private List<FieldbusModule> parseXml(Document doc) {
+		FieldbusModuleFactory factory = new FieldbusModuleFactory();
 		List<FieldbusModule> result = new ArrayList<>();
 		Element wagoElement = doc.getDocumentElement();
 		int inputOffset = 0;
@@ -189,8 +190,7 @@ public class Wago extends AbstractOpenemsModbusComponent implements DigitalOutpu
 				kanals[j] = new FieldbusModuleKanal(channelName, channelType);
 			}
 			// Create FieldbusModule instance using factory method
-			FieldbusModule module = FieldbusModule.of(this, moduleArtikelnr, moduleType, kanals, inputOffset,
-					outputOffset);
+			FieldbusModule module = factory.of(this, moduleArtikelnr, moduleType, kanals, inputOffset, outputOffset);
 			inputOffset += module.getInputCoils();
 			outputOffset += module.getOutputCoils();
 			result.add(module);
