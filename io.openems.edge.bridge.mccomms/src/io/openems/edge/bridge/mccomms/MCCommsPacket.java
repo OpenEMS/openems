7807cb651bfefa2ec9af3cf24da478b94abdb1ca
kyle <kyle@microcare.co.za>
2019-08-12 10:58:33 +0200

    Continued re-implementation of MCCommsBridge

--- io.openems.edge.bridge.mccomms/src/io/openems/edge/bridge/mccomms/MCCommsPacket.java
+++ io.openems.edge.bridge.mccomms/src/io/openems/edge/bridge/mccomms/MCCommsPacket.java
@@ -1,10 +1,40 @@
 package io.openems.edge.bridge.mccomms;
 
+import com.google.common.collect.ImmutableRangeMap;
+import com.google.common.collect.Range;
 import com.google.common.collect.RangeMap;
+import io.openems.common.exceptions.OpenemsException;
+import io.openems.edge.bridge.mccomms.Element.MCCommsElement;
 
-/**
- * @author Kyle McLachlan
- */
+import java.util.Map;
+import java.util.Optional;
+
 public class MCCommsPacket {
-	private RangeMap<Integer, MCCommsElement> elements;
+	private final RangeMap<Integer, MCCommsElement> elements;
+	
+	public MCCommsPacket(MCCommsElement ... elements) {
+		ImmutableRangeMap.Builder<Integer, MCCommsElement> builder = ImmutableRangeMap.builder();
+		for (MCCommsElement element : elements) {
+			builder.put(element.getAddressRange(), element);
+		}
+		this.elements = builder.build();
+	}
+	
+	private byte getByte(int whichByte) {
+		return Optional.ofNullable(elements.get(whichByte)).map(e -> e.getBytes()[whichByte - e.getAddressRange().lowerEndpoint()]).orElse(((byte) 170));
+	}
+	
+	public byte[] getBytes() {
+		byte[] returnBytes = new byte[elements.span().upperEndpoint()];
+		for (int i = 0; i < elements.span().upperEndpoint(); i++) {
+			returnBytes[i] = getByte(i);
+		}
+		return returnBytes;
+	}
+	
+	public MCCommsPacket setBytes(byte[] bytes) {
+		for (MCCommsElement value : elements.asMapOfRanges().values()) {
+			
+		}
+	}
 }
