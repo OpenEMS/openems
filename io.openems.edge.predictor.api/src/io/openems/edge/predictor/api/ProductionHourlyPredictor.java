93942ca3703903b61912c40450451d0ebfed4eaa
pooran-c <46567310+pooran-c@users.noreply.github.com>
2019-08-13 13:59:16 +0200

    Predictor API and Persistence-Model Predictor (#686)

---
+++ io.openems.edge.predictor.api/src/io/openems/edge/predictor/api/ProductionHourlyPredictor.java
@@ -0,0 +1,9 @@
+package io.openems.edge.predictor.api;
+
+/**
+ * Provides a production prediction for the next 24 h; e.g. for a photovoltaics
+ * installation.
+ */
+public interface ProductionHourlyPredictor extends HourlyPredictor {
+
+}
