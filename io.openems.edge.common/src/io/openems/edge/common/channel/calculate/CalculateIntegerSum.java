2554f03df9a5c88b4258f2d6b32e41963a230fae
Stefan Feilmeier <mail@stefanfeilmeier.de>
2019-08-16 09:39:01 +0200

    Add Asymmetric Channels to _sum (#716)

--- io.openems.edge.common/src/io/openems/edge/common/channel/calculate/CalculateIntegerSum.java
+++ io.openems.edge.common/src/io/openems/edge/common/channel/calculate/CalculateIntegerSum.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.function.Function;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,19 +16,32 @@ import io.openems.edge.common.channel.Channel;
  */
 public class CalculateIntegerSum {
 
+	public static final Function<Integer, Integer> DIRECT_CONVERTER = (value) -> value;
+	public static final Function<Integer, Integer> DIVIDE_BY_THREE = (value) -> Math.round(value / 3f);
+
 	private final Logger log = LoggerFactory.getLogger(CalculateLongSum.class);
 	private final List<Integer> values = new ArrayList<>();
 
 	/**
 	 * Adds a Channel-Value.
 	 * 
-	 * @param channel
+	 * @param channel the Channel
 	 */
 	public void addValue(Channel<Integer> channel) {
+		this.addValue(channel, DIRECT_CONVERTER);
+	}
+
+	/**
+	 * Adds a Channel-Value.
+	 * 
+	 * @param channel   the Channel
+	 * @param converter is applied to the channel value
+	 */
+	public void addValue(Channel<Integer> channel, Function<Integer, Integer> converter) {
 		Optional<Integer> value = channel.getNextValue().asOptional();
 		if (value.isPresent()) {
 			try {
-				this.values.add(value.get());
+				this.values.add(converter.apply(value.get()));
 			} catch (Exception e) {
 				this.log.error("Adding Channel [" + channel.address() + "] value [" + value + "] failed. "
 						+ e.getClass().getSimpleName() + ": " + e.getMessage());
@@ -40,6 +54,7 @@ public class CalculateIntegerSum {
 	 * Calculates the sum.
 	 * 
 	 * @return the sum or null
+	 * @throws NoSuchElementException on error
 	 */
 	public Integer calculate() throws NoSuchElementException {
 		if (this.values.isEmpty()) {
