b41c2c50f9cbd3438037f748992b79da09fcc575
Felix <felix1.stoecker@st.oth-regensburg.de>
2019-09-24 11:31:48 +0200

    Getting Started : Implement a Device

---
+++ io.openems.edge.meter.simulated/src/io/openems/edge/meter/simulated/MeterSimulated.java
@@ -0,0 +1,93 @@
+package io.openems.edge.meter.simulated;
+
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.metatype.annotations.Designate;
+
+import io.openems.edge.bridge.modbus.api.AbstractOpenemsModbusComponent;
+import io.openems.edge.bridge.modbus.api.BridgeModbus;
+import io.openems.edge.bridge.modbus.api.ModbusProtocol;
+import io.openems.edge.bridge.modbus.api.element.SignedWordElement;
+import io.openems.edge.bridge.modbus.api.task.FC3ReadRegistersTask;
+import io.openems.edge.common.channel.Doc;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.taskmanager.Priority;
+import io.openems.edge.meter.api.MeterType;
+import io.openems.edge.meter.api.SymmetricMeter;
+
+@Designate(ocd = Config.class, factory = true)
+@Component(
+		name = "Meter.Simulated",
+		immediate = true,
+		configurationPolicy = ConfigurationPolicy.REQUIRE)
+public class MeterSimulated extends AbstractOpenemsModbusComponent
+		implements SymmetricMeter, OpenemsComponent {
+
+	private MeterType meterType = MeterType.PRODUCTION;
+
+	@Reference
+	protected ConfigurationAdmin cm;
+
+	public MeterSimulated() {
+		super(
+				OpenemsComponent.ChannelId.values(), //
+				SymmetricMeter.ChannelId.values() //
+		);
+	}
+
+	@Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
+	protected void setModbus(BridgeModbus modbus) {
+		super.setModbus(modbus);
+	}
+
+	@Activate
+	void activate(ComponentContext context, Config config) {
+		this.meterType = config.type();
+
+		super.activate(context, config.id(), config.alias(), config.enabled(), config.modbusUnitId(), this.cm,
+				"Modbus", config.modbus_id());
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		super.deactivate();
+	}
+
+	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+		;
+		private final Doc doc;
+
+		private ChannelId(Doc doc) {
+			this.doc = doc;
+		}
+
+		public Doc doc() {
+			return this.doc;
+		}
+	}
+
+	@Override
+	public MeterType getMeterType() {
+		return this.meterType;
+	}
+
+	@Override
+	protected ModbusProtocol defineModbusProtocol() {
+		return new ModbusProtocol(this,
+				new FC3ReadRegistersTask(1000, Priority.HIGH,
+						m(SymmetricMeter.ChannelId.ACTIVE_POWER, new SignedWordElement(1000))));
+	}
+
+	@Override
+	public String debugLog() {
+		return "L:" + this.getActivePower().value().asString();
+	}
+}
---
+++ io.openems.edge.meter.simulated/test/io/openems/edge/meter/simulated/MeterSimulatedTest.java
@@ -0,0 +1,24 @@
+package io.openems.edge.meter.simulated;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+/*
+ * Example JUNit test case
+ *
+ */
+
+public class MeterSimulatedTest {
+
+	/*
+	 * Example test method
+	 */
+
+	@Test
+	public void simple() {
+		MeterSimulated impl = new MeterSimulated();
+		assertNotNull(impl);
+	}
+
+}
