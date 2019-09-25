9cb484d40d6e9c453a510b40dd877018e45f0f1b
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2019-09-25 17:50:54 +0200

    Review
    - fix typos
    - prefer "this" for class variables
    - remove obsolete log
    - added 'since' javadoc to SymmetricEss; will be undefined for most ESS currently

--- io.openems.edge.controller.ess.limitdischargecellvoltage/src/io/openems/edge/controller/ess/limitdischargecellvoltage/state/Critical.java
+++ io.openems.edge.controller.ess.limitdischargecellvoltage/src/io/openems/edge/controller/ess/limitdischargecellvoltage/state/Critical.java
@@ -1,16 +1,11 @@
 package io.openems.edge.controller.ess.limitdischargecellvoltage.state;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.openems.edge.common.component.ComponentManager;
+import io.openems.edge.controller.ess.limitdischargecellvoltage.Config;
 import io.openems.edge.controller.ess.limitdischargecellvoltage.IState;
 import io.openems.edge.controller.ess.limitdischargecellvoltage.State;
-import io.openems.edge.controller.ess.limitdischargecellvoltage.Config;
 
 public class Critical implements IState {
-
-	private final Logger log = LoggerFactory.getLogger(Critical.class);
 
 	private ComponentManager componentManager;
 	private Config config;
@@ -28,12 +23,11 @@ public class Critical implements IState {
 	@Override
 	public IState getNextStateObject() {
 		// According to the state machine the next state is always charge
-		log.info("Critical.getNextStateObject() --> Charge");
 		return new Charge(this.componentManager, this.config);
 	}
 
 	@Override
 	public void act() {
-		log.info("act() --> nothing to do");
+		// nothing to do
 	}
 }
