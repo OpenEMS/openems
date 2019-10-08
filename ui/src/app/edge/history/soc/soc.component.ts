00155ce4680b8295e46b43ec6f5f04e60e679e77
Fabian Schwarzbeck <fabian.schwarzbeck@fenecon.de>
2019-10-08 13:10:26 +0200

    no message

--- ui/src/app/edge/history/soc/soc.component.ts
+++ ui/src/app/edge/history/soc/soc.component.ts
@@ -186,48 +186,48 @@ export class SocComponent extends AbstractHistoryChart implements OnInit, OnChan
               }
             })
 
-            /*
-            * Autarchy
-            */
-            if (config.hasProducer()) {
-              let autarchy = consumptionData.map((value, index) => {
-                if (value == null) {
-                  return null
-                } else {
-                  return CurrentData.calculateAutarchy(buyFromGridData[index], value);
-                }
-              })
-
-              datasets.push({
-                label: this.translate.instant('General.Autarchy'),
-                data: autarchy,
-                hidden: false
-              })
-
-              /*
-              * Self Consumption
-              */
-              let selfConsumption = productionData.map((value, index) => {
-                if (value == null) {
-                  return null
-                } else {
-                  return CurrentData.calculateSelfConsumption(sellToGridData[index], value, dischargeData[index]);
-                }
-              })
-
-              datasets.push({
-                label: this.translate.instant('General.SelfConsumption'),
-                data: selfConsumption,
-                hidden: false
-              })
-            }
-
             datasets.push({
               label: this.translate.instant('General.Soc'),
               data: data,
               hidden: false
             });
           };
+
+          /*
+          * Autarchy
+          */
+          if (config.hasProducer()) {
+            let autarchy = consumptionData.map((value, index) => {
+              if (value == null) {
+                return null
+              } else {
+                return CurrentData.calculateAutarchy(buyFromGridData[index], value);
+              }
+            })
+
+            datasets.push({
+              label: this.translate.instant('General.Autarchy'),
+              data: autarchy,
+              hidden: false
+            })
+
+            /*
+            * Self Consumption
+            */
+            let selfConsumption = productionData.map((value, index) => {
+              if (value == null) {
+                return null
+              } else {
+                return CurrentData.calculateSelfConsumption(sellToGridData[index], value, dischargeData[index]);
+              }
+            })
+
+            datasets.push({
+              label: this.translate.instant('General.SelfConsumption'),
+              data: selfConsumption,
+              hidden: false
+            })
+          }
 
           this.datasets = datasets;
           this.loading = false;
