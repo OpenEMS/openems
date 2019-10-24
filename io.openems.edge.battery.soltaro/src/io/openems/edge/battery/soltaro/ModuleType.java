ac2e101b9a566c8c2c239ad548b7cd08476789fa
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2019-10-24 17:58:12 +0200

    Improve formatting, remove unnecessary logs

--- io.openems.edge.battery.soltaro/src/io/openems/edge/battery/soltaro/ModuleType.java
+++ io.openems.edge.battery.soltaro/src/io/openems/edge/battery/soltaro/ModuleType.java
@@ -2,19 +2,18 @@ package io.openems.edge.battery.soltaro;
 
 /**
  * 
- * This type defines the module type 
+ * This type defines the module type
  *
  */
 public enum ModuleType {
 
-	MODULE_3_KWH(3000),
-	MODULE_3_5_KWH(3500),
-		;
-		
+	MODULE_3_KWH(3000), //
+	MODULE_3_5_KWH(3500);
+
 	private ModuleType(int capacity) {
 		this.capacity = capacity;
 	}
-	
+
 	private int capacity;
 
 	public int getCapacity_Wh() {
