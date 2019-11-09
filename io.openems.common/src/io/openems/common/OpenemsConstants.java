f6d9d93af6bdd547a959389502ded58a801f027e
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2019-11-09 10:12:36 +0100

    Start development of 2019.16.0-SNAPSHOT

--- io.openems.common/src/io/openems/common/OpenemsConstants.java
+++ io.openems.common/src/io/openems/common/OpenemsConstants.java
@@ -20,7 +20,7 @@ public class OpenemsConstants {
 	 * 
 	 * This is usually the number of the sprint within the year
 	 */
-	public final static short VERSION_MINOR = 15;
+	public final static short VERSION_MINOR = 16;
 
 	/**
 	 * The patch version of OpenEMS.
@@ -32,8 +32,8 @@ public class OpenemsConstants {
 	/**
 	 * The additional version string
 	 */
-	public final static String VERSION_STRING = "";
-	// public final static String VERSION_STRING = "SNAPSHOT";
+	// public final static String VERSION_STRING = "";
+	public final static String VERSION_STRING = "SNAPSHOT";
 
 	/**
 	 * The complete version as a SemanticVersion.
