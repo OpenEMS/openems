b661b48ec07f9db365c0f5faec6cd4c8699ad109
fabianfnc <37406473+fabianfnc@users.noreply.github.com>
2019-12-10 16:56:42 +0100

    UI: add second y axis to energymonitor in history (#1025)
    - add second y axis to energymonitor in history to show soc in %

--- ui/src/app/edge/history/shared.ts
+++ ui/src/app/edge/history/shared.ts
@@ -52,6 +52,7 @@ export type ChartOptions = {
     },
     scales: {
         yAxes: [{
+            position: string,
             scaleLabel: {
                 display: boolean,
                 labelString: string
@@ -61,6 +62,82 @@ export type ChartOptions = {
                 max?: number
             }
         }],
+        xAxes: [{
+            type: "time",
+            time: {
+                minUnit: string,
+                displayFormats: {
+                    millisecond: string,
+                    second: string,
+                    minute: string,
+                    hour: string,
+                    day: string,
+                    week: string,
+                    month: string,
+                    quarter: string,
+                    year: string
+                }
+            }
+        }]
+    },
+    tooltips: {
+        mode: string,
+        intersect: boolean,
+        axis: string,
+        callbacks: {
+            label?(tooltipItem: TooltipItem, data: Data): string,
+            title?(tooltipItems: TooltipItem[], data: Data): string
+        }
+    }
+}
+
+export type ChartOptionsTwoYAxis = {
+    maintainAspectRatio: boolean,
+    legend: {
+        position: "bottom"
+    },
+    elements: {
+        point: {
+            radius: number,
+            hitRadius: number,
+            hoverRadius: number
+        },
+        line: {
+            borderWidth: number,
+            tension: number
+        }
+    },
+    hover: {
+        mode: string,
+        intersect: boolean
+    },
+    scales: {
+        yAxes: [
+            {
+                id: string,
+                position: string,
+                scaleLabel: {
+                    display: boolean,
+                    labelString: string
+                },
+                ticks: {
+                    beginAtZero: boolean,
+                    max?: number
+                }
+            },
+            {
+                id: string,
+                position: string,
+                scaleLabel: {
+                    display: boolean,
+                    labelString: string
+                },
+                ticks: {
+                    beginAtZero: boolean,
+                    max?: number
+                }
+            },
+        ],
         xAxes: [{
             type: "time",
             time: {
@@ -112,6 +189,7 @@ export const DEFAULT_TIME_CHART_OPTIONS: ChartOptions = {
     },
     scales: {
         yAxes: [{
+            position: 'left',
             scaleLabel: {
                 display: true,
                 labelString: ""
@@ -120,6 +198,82 @@ export const DEFAULT_TIME_CHART_OPTIONS: ChartOptions = {
                 beginAtZero: true
             }
         }],
+        xAxes: [{
+            type: 'time',
+            time: {
+                minUnit: 'hour',
+                displayFormats: {
+                    millisecond: 'SSS [ms]',
+                    second: 'HH:mm:ss a', // 17:20:01
+                    minute: 'HH:mm', // 17:20
+                    hour: 'HH:[00]', // 17:20
+                    day: 'll', // Sep 4 2015
+                    week: 'll', // Week 46, or maybe "[W]WW - YYYY" ?
+                    month: 'MMM YYYY', // Sept 2015
+                    quarter: '[Q]Q - YYYY', // Q3
+                    year: 'YYYY' // 2015
+                }
+            }
+        }]
+    },
+    tooltips: {
+        mode: 'index',
+        intersect: false,
+        axis: 'x',
+        callbacks: {
+            title(tooltipItems: TooltipItem[], data: Data): string {
+                let date = new Date(tooltipItems[0].xLabel);
+                return date.toLocaleDateString() + " " + date.toLocaleTimeString();
+            }
+        }
+    }
+};
+
+export const DEFAULT_TIME_CHART_OPTIONS_TWO_Y_AXIS: ChartOptionsTwoYAxis = {
+    maintainAspectRatio: false,
+    legend: {
+        position: 'bottom'
+    },
+    elements: {
+        point: {
+            radius: 0,
+            hitRadius: 0,
+            hoverRadius: 0
+        },
+        line: {
+            borderWidth: 2,
+            tension: 0.1
+        }
+    },
+    hover: {
+        mode: 'point',
+        intersect: true
+    },
+    scales: {
+        yAxes: [
+            {
+                id: 'yAxis1',
+                position: 'left',
+                scaleLabel: {
+                    display: true,
+                    labelString: ""
+                },
+                ticks: {
+                    beginAtZero: true
+                }
+            },
+            {
+                id: 'yAxis2',
+                position: 'right',
+                scaleLabel: {
+                    display: true,
+                    labelString: ""
+                },
+                ticks: {
+                    beginAtZero: true
+                }
+            }
+        ],
         xAxes: [{
             type: 'time',
             time: {
