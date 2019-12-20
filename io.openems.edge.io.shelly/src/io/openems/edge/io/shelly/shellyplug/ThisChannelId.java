176970a5d83b4d2b9b7b673574d09dbdf07f087b
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2019-12-20 18:01:53 +0100

    Start development of Shelly Plug and Shelly 2.5

---
+++ io.openems.edge.io.shelly/src/io/openems/edge/io/shelly/shellyplug/ThisChannelId.java
@@ -0,0 +1,52 @@
+package io.openems.edge.io.shelly.shellyplug;
+
+import io.openems.common.channel.AccessMode;
+import io.openems.common.channel.Level;
+import io.openems.common.channel.Unit;
+import io.openems.common.types.OpenemsType;
+import io.openems.edge.common.channel.BooleanDoc;
+import io.openems.edge.common.channel.BooleanWriteChannel;
+import io.openems.edge.common.channel.Doc;
+
+public enum ThisChannelId implements io.openems.edge.common.channel.ChannelId {
+	/**
+	 * Holds writes to Relay Output for debugging
+	 * 
+	 * <ul>
+	 * <li>Interface: KmtronicRelayOutput
+	 * <li>Type: Boolean
+	 * <li>Range: On/Off
+	 * </ul>
+	 */
+	DEBUG_RELAY(Doc.of(OpenemsType.BOOLEAN)), //
+	/**
+	 * Relay Output 1
+	 * 
+	 * <ul>
+	 * <li>Interface: KmtronicRelayOutput
+	 * <li>Type: Boolean
+	 * <li>Range: On/Off
+	 * </ul>
+	 */
+	RELAY(new BooleanDoc() //
+			.accessMode(AccessMode.READ_WRITE) //
+			.onInit(new BooleanWriteChannel.MirrorToDebugChannel(ThisChannelId.DEBUG_RELAY))), //
+	/**
+	 * Active Power.
+	 */
+	ACTIVE_POWER(Doc.of(OpenemsType.INTEGER) //
+			.unit(Unit.WATT) //
+	),
+
+	SLAVE_COMMUNICATION_FAILED(Doc.of(Level.FAULT)); //
+
+	private final Doc doc;
+
+	private ThisChannelId(Doc doc) {
+		this.doc = doc;
+	}
+
+	public Doc doc() {
+		return this.doc;
+	}
+}
