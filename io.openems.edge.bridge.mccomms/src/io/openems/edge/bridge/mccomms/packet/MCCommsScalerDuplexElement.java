99d682f7c1989486662e205e0d1a96847eaffa40
KyleMicrocare <kyle@microcare.co.za>
2020-01-07 16:41:29 +0200

    added a special element type for duplexed scaler values

---
+++ io.openems.edge.bridge.mccomms/src/io/openems/edge/bridge/mccomms/packet/MCCommsScalerDuplexElement.java
@@ -0,0 +1,82 @@
+package io.openems.edge.bridge.mccomms.packet;
+
+import java.nio.ByteBuffer;
+
+import io.openems.common.exceptions.OpenemsException;
+import io.openems.edge.common.channel.Channel;
+
+/**
+ * A subclass of {@link MCCommsElement} specially adapted to handle scaler duplex values, as illustrated below
+ * <p>
+ * {@code
+ *    a sign  b sign
+ *       |       |
+ *       V       V 
+ *      |_|_|_|_|_|_|_|_|
+ *         ^ ^ ^ | ^ ^ ^
+ *        a value| b value
+ *        bits   | bits
+ * }
+ * </p>
+ */
+public class MCCommsScalerDuplexElement extends MCCommsElement{
+	private final Channel<Double> firstScalerChannel, secondScalerChannel;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param byteAddress the byte address within the packet buffer where the scaler duplex element resides
+	 * @param firstScalerChannel the channel to assign the first scaler value to
+	 * @param secondScalerChannel the channel to assign the second scaler value to
+	 */
+	private MCCommsScalerDuplexElement(int byteAddress, Channel<Double> firstScalerChannel, Channel<Double> secondScalerChannel) {
+		super(byteAddress);
+		this.firstScalerChannel = firstScalerChannel;
+		this.secondScalerChannel = secondScalerChannel;
+	}
+	
+	public static MCCommsScalerDuplexElement newInstanceFromChannels(int byteAddress, Channel<Double> firstScalerChannel, Channel<Double> secondScalerChannel) {
+		return new MCCommsScalerDuplexElement(byteAddress, firstScalerChannel, secondScalerChannel);
+	}
+
+	/**
+	 * @param bytes the bytes to assign to the internal {@link ByteBuffer}; this subclass also immediately assigns the supplied value to the bound channel.
+	 * @return the current instance
+	 * @throws OpenemsException if the internal {@link ByteBuffer} has a capacity
+	 *                          not equal the number of supplied bytes
+	 */
+	@Override
+	MCCommsElement setBytes(byte[] bytes) throws OpenemsException {
+		if (bytes.length != getValueBuffer().capacity()) {
+			throw new OpenemsException("Byte array does not meet required length");
+		}
+		ByteBuffer buffer = ByteBuffer.wrap(bytes).asReadOnlyBuffer();
+		int combined = Byte.toUnsignedInt(buffer.get(0));
+		int a,b;
+		double firstScaler, secondScaler;
+	    a = ((combined >>> 7) << 31) | ((combined << 25) >>> 29);
+	    b = ((combined >>> 3) << 31) | ((combined << 29) >>> 29);
+	    firstScaler = Math.pow(10, a);
+	    secondScaler = Math.pow(10, b);
+	    firstScalerChannel.setNextValue(firstScaler);
+	    secondScalerChannel.setNextValue(secondScaler);
+		return this;
+	}
+
+	/**
+	 * Overridden; does nothing
+	 * @return the current instance
+	 */
+	@Override
+	public MCCommsElement setUnsigned(boolean unsigned) {
+		return this;
+	}
+	
+	/**
+	 * Overridden; does nothing
+	 */
+	@Override
+	public void assignValueToChannel() throws OpenemsException {
+		return;
+	}
+}
