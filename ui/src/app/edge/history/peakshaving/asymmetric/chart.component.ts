54a1d682a6a2a118a6a8ea4f77a5eb6fe500d095
fabianfnc <37406473+fabianfnc@users.noreply.github.com>
2020-02-27 13:20:45 +0100

    UI: Asymmetric Peakshaving Chart
    add asymmetric peakshaving widget & chart to history

---
+++ ui/src/app/edge/history/peakshaving/asymmetric/chart.component.ts
@@ -0,0 +1,199 @@
+import { formatNumber } from '@angular/common';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { DefaultTypes } from 'src/app/shared/service/defaulttypes';
+import { ChannelAddress, Edge, EdgeConfig, Service, Utils } from '../../../../shared/shared';
+import { AbstractHistoryChart } from '../../abstracthistorychart';
+import { ChartOptions, Data, DEFAULT_TIME_CHART_OPTIONS, TooltipItem } from './../../shared';
+
+@Component({
+    selector: 'asymmetricpeakshavingchart',
+    templateUrl: '../../abstracthistorychart.html'
+})
+export class AsymmetricPeakshavingChartComponent extends AbstractHistoryChart implements OnInit, OnChanges {
+
+    @Input() private period: DefaultTypes.HistoryPeriod;
+    @Input() public component: EdgeConfig.Component;
+
+    ngOnChanges() {
+        this.updateChart();
+    };
+
+    constructor(
+        protected service: Service,
+        protected translate: TranslateService,
+        private route: ActivatedRoute,
+    ) {
+        super(service, translate);
+    }
+
+
+    ngOnInit() {
+        this.service.setCurrentComponent('', this.route);
+        this.setLabel();
+    }
+
+    protected updateChart() {
+        this.loading = true;
+        this.queryHistoricTimeseriesData(this.period.from, this.period.to).then(response => {
+            this.service.getCurrentEdge().then(() => {
+                this.service.getConfig().then(() => {
+                    let meterIdActivePowerL1 = this.component.properties['meter.id'] + '/ActivePowerL1';
+                    let meterIdActivePowerL2 = this.component.properties['meter.id'] + '/ActivePowerL1';
+                    let meterIdActivePowerL3 = this.component.properties['meter.id'] + '/ActivePowerL1';
+                    let peakshavingPower = this.component.id + '/_PropertyPeakShavingPower';
+                    let rechargePower = this.component.id + '/_PropertyRechargePower';
+                    let result = response.result;
+                    this.colors = [];
+                    // convert labels
+                    let labels: Date[] = [];
+                    for (let timestamp of result.timestamps) {
+                        labels.push(new Date(timestamp));
+                    }
+                    this.labels = labels;
+
+                    // convert datasets
+                    let datasets = [];
+
+                    if (meterIdActivePowerL1 in result.data) {
+                        let data = result.data[meterIdActivePowerL1].map(value => {
+                            if (value == null) {
+                                return null
+                            } else if (value == 0) {
+                                return 0;
+                            } else {
+                                return value / 1000; // convert to kW
+                            }
+                        });
+                        datasets.push({
+                            label: this.translate.instant('General.Phase') + ' ' + 'L1',
+                            data: data,
+                            hidden: false
+                        });
+                        this.colors.push(this.phase1Color);
+                    }
+                    if (meterIdActivePowerL2 in result.data) {
+                        let data = result.data[meterIdActivePowerL2].map(value => {
+                            if (value == null) {
+                                return null
+                            } else if (value == 0) {
+                                return 0;
+                            } else {
+                                return value / 1000; // convert to kW
+                            }
+                        });
+                        datasets.push({
+                            label: this.translate.instant('General.Phase') + ' ' + 'L2',
+                            data: data,
+                            hidden: false
+                        });
+                        this.colors.push(this.phase2Color);
+                    }
+                    if (meterIdActivePowerL3 in result.data) {
+                        let data = result.data[meterIdActivePowerL3].map(value => {
+                            if (value == null) {
+                                return null
+                            } else if (value == 0) {
+                                return 0;
+                            } else {
+                                return value / 1000; // convert to kW
+                            }
+                        });
+                        datasets.push({
+                            label: this.translate.instant('General.Phase') + ' ' + 'L3',
+                            data: data,
+                            hidden: false
+                        });
+                        this.colors.push(this.phase3Color);
+                    }
+                    if (peakshavingPower in result.data) {
+                        let data = result.data[peakshavingPower].map(value => {
+                            if (value == null) {
+                                return null
+                            } else if (value == 0) {
+                                return 0;
+                            } else {
+                                return value / 1000; // convert to kW
+                            }
+                        });
+                        datasets.push({
+                            label: this.translate.instant('Edge.Index.Widgets.Peakshaving.peakshavingPower'),
+                            data: data,
+                            hidden: false,
+                            borderDash: [3, 3]
+                        });
+                        this.colors.push({
+                            backgroundColor: 'rgba(0,0,0,0)',
+                            borderColor: 'rgba(200,0,0,1)',
+                        })
+                    }
+                    if (rechargePower in result.data) {
+                        let data = result.data[rechargePower].map(value => {
+                            if (value == null) {
+                                return null
+                            } else if (value == 0) {
+                                return 0;
+                            } else {
+                                return value / 1000; // convert to kW
+                            }
+                        });
+                        datasets.push({
+                            label: this.translate.instant('Edge.Index.Widgets.Peakshaving.rechargePower'),
+                            data: data,
+                            hidden: false,
+                            borderDash: [3, 3]
+                        });
+                        this.colors.push({
+                            backgroundColor: 'rgba(0,0,0,0)',
+                            borderColor: 'rgba(0,223,0,1)',
+                        })
+                    }
+                    this.datasets = datasets;
+                    this.loading = false;
+
+                }).catch(reason => {
+                    console.error(reason); // TODO error message
+                    this.initializeChart();
+                    return;
+                });
+            }).catch(reason => {
+                console.error(reason); // TODO error message
+                this.initializeChart();
+                return;
+            });
+        }).catch(reason => {
+            console.error(reason); // TODO error message
+            this.initializeChart();
+            return;
+        });
+    }
+
+    protected getChannelAddresses(edge: Edge, config: EdgeConfig): Promise<ChannelAddress[]> {
+        return new Promise((resolve) => {
+            let result: ChannelAddress[] = [
+                new ChannelAddress(this.component.id, '_PropertyPeakShavingPower'),
+                new ChannelAddress(this.component.id, '_PropertyRechargePower'),
+                new ChannelAddress(this.component.properties['meter.id'], 'ActivePowerL1'),
+                new ChannelAddress(this.component.properties['meter.id'], 'ActivePowerL2'),
+                new ChannelAddress(this.component.properties['meter.id'], 'ActivePowerL3'),
+            ];
+            resolve(result);
+        })
+    }
+
+    protected setLabel() {
+        let options = <ChartOptions>Utils.deepCopy(DEFAULT_TIME_CHART_OPTIONS);
+        options.scales.yAxes[0].scaleLabel.labelString = "kW";
+        options.tooltips.callbacks.label = function (tooltipItem: TooltipItem, data: Data) {
+            let label = data.datasets[tooltipItem.datasetIndex].label;
+            let value = tooltipItem.yLabel;
+            return label + ": " + formatNumber(value, 'de', '1.0-2') + " kW";
+        }
+        this.options = options;
+    }
+
+    public getChartHeight(): number {
+        return window.innerHeight / 1.3;
+    }
+}
--- ui/src/app/edge/live/live.module.ts
+++ ui/src/app/edge/live/live.module.ts
@@ -1,41 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './../../shared/shared.module';
+import { AsymmetricPeakshavingComponent } from './peakshaving/asymmetric/asymmetricpeakshaving.component';
+import { AsymmetricPeakshavingModalComponent } from './peakshaving/asymmetric/modal/modal.component';
 import { AutarchyComponent } from './autarchy/autarchy.component';
 import { AutarchyModalComponent } from './autarchy/modal/modal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { ChannelthresholdComponent } from './channelthreshold/channelthreshold.component';
+import { ChpSocComponent } from './chpsoc/chpsoc.component';
 import { ChpsocModalComponent } from './chpsoc/chpsoc-modal/modal.page';
+import { ConsumptionComponent } from './consumption/consumption.component';
 import { ConsumptionModalComponent } from './consumption/modal/modal.component';
 import { EnergymonitorModule } from './energymonitor/energymonitor.module';
+import { EvcsChart } from './evcsCluster/modal/evcs-chart/evcs.chart';
+import { EvcsClusterComponent } from './evcsCluster/evcsCluster.component';
 import { EvcsComponent } from './evcs/evcs.component';
 import { EvcsModalComponent } from './evcs/modal/modal.page';
 import { EvcsPopoverComponent } from './evcs/modal/popover/popover.page';
-import { ModalComponentEvcsCluster } from './evcsCluster/modal/evcsCluster-modal.page';
 import { FixDigitalOutputComponent } from './fixdigitaloutput/fixdigitaloutput.component';
 import { FixDigitalOutputModalComponent } from './fixdigitaloutput/modal/modal.component';
 import { GridComponent } from './grid/grid.component';
 import { GridModalComponent } from './grid/modal/modal.component';
 import { InfoComponent } from './info/info.component';
 import { LiveComponent } from './live.component';
+import { ModalComponentEvcsCluster } from './evcsCluster/modal/evcsCluster-modal.page';
 import { ModbusApiComponent } from './modbusapi/modbusapi.component';
+import { NgModule } from '@angular/core';
 import { OfflineComponent } from './offline/offline.component';
-import { AsymmetricPeakshavingComponent } from './peakshaving/asymmetric/asymmetricpeakshaving.component';
-import { AsymmetricPeakshavingModalComponent } from './peakshaving/asymmetric/modal/modal.component';
-import { SymmetricPeakshavingModalComponent } from './peakshaving/symmetric/modal/modal.component';
-import { ProductionModalComponent } from './production/modal/modal.component';
 import { ProductionComponent } from './production/production.component';
-import { SelfconsumptionModalComponent } from './selfconsumption/modal/modal.component';
+import { ProductionModalComponent } from './production/modal/modal.component';
 import { SelfConsumptionComponent } from './selfconsumption/selfconsumption.component';
-import { SinglethresholdModalComponent } from './singlethreshold/modal/modal.component';
+import { SelfconsumptionModalComponent } from './selfconsumption/modal/modal.component';
+import { SharedModule } from './../../shared/shared.module';
 import { SinglethresholdComponent } from './singlethreshold/singlethreshold.component';
-import { StorageModalComponent } from './storage/modal/modal.component';
-import { ChpSocComponent } from './chpsoc/chpsoc.component';
-import { ConsumptionComponent } from './consumption/consumption.component';
-import { EvcsChart } from './evcsCluster/modal/evcs-chart/evcs.chart';
-import { EvcsClusterComponent } from './evcsCluster/evcsCluster.component';
+import { SinglethresholdModalComponent } from './singlethreshold/modal/modal.component';
 import { StorageComponent } from './storage/storage.component';
+import { StorageModalComponent } from './storage/modal/modal.component';
 import { SymmetricPeakshavingComponent } from './peakshaving/symmetric/symmetricpeakshaving.component';
+import { SymmetricPeakshavingModalComponent } from './peakshaving/symmetric/modal/modal.component';
 
 @NgModule({
   imports: [
@@ -91,6 +91,7 @@ import { SymmetricPeakshavingComponent } from './peakshaving/symmetric/symmetric
     SelfConsumptionComponent,
     SelfconsumptionModalComponent,
     SinglethresholdComponent,
+    SinglethresholdModalComponent,
     StorageComponent,
     StorageModalComponent,
     SymmetricPeakshavingComponent,
