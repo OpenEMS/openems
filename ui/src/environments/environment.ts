7d35321c8be59683b05aab44df270176f37948b5
fabianfnc <37406473+fabianfnc@users.noreply.github.com>
2020-02-28 09:54:48 +0100

    set edge 8085 as default environment (#1064)

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -5,14 +5,14 @@ export const environment: Environment = {
   debugMode: true,
 
   // For OpenEMS Edge
-  // url: "ws://" + location.hostname + ":8085",
-  // backend: "OpenEMS Edge"
+  url: "ws://" + location.hostname + ":8085",
+  backend: "OpenEMS Edge"
 
   // For OpenEMS Backend
   //url: "ws://" + location.hostname + ":8078",
   //backend: "OpenEMS Backend",
 
-  url: "ws://localhost:8082",
-  backend: "OpenEMS Backend",
+  // url: "ws://localhost:8082",
+  // backend: "OpenEMS Backend",
 
 };
