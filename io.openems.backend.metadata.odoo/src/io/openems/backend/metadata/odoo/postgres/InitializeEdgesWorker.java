8d75206570605a36b04c60bea205e40a62b7ea8d
Stefan Feilmeier <mail@stefanfeilmeier.de>
2020-03-05 18:21:53 +0100

    Backport Backend improvements from fems (#1068)
    * Backend: improve handling of FileTypeConflicts:
    
    - Enable conversion of Float to Integer
    - Try again on error (this is required for _property channels)
    
    * Configure pax logging only if it had not been configured before
    
    * Fix handling of InfluxDB Field Type Conflicts
    
    - properly handle conversion from double to int
    - move to separate handler class
    
    * Reduce logs on startup: introduce "isInitialized" state in Metadata service
    
    * Allow update of EdgeProductType
    
    * Clear debug system out printlns
    
    * Further backports from FEMS

---
+++ io.openems.backend.metadata.odoo/src/io/openems/backend/metadata/odoo/postgres/InitializeEdgesWorker.java
@@ -0,0 +1,127 @@
+package io.openems.backend.metadata.odoo.postgres;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.openems.backend.metadata.odoo.Field;
+import io.openems.backend.metadata.odoo.Field.EdgeDevice;
+import io.openems.backend.metadata.odoo.MyEdge;
+import io.openems.backend.metadata.odoo.postgres.task.UpdateEdgeStatesSum;
+
+public class InitializeEdgesWorker {
+
+	private final Logger log = LoggerFactory.getLogger(InitializeEdgesWorker.class);
+	protected final PostgresHandler parent;
+	private final MyConnection connection;
+	private final Runnable onFinished;
+
+	/**
+	 * Executor for subscriptions task.
+	 */
+	private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+	public InitializeEdgesWorker(PostgresHandler parent, Credentials credentials, Runnable onFinished) {
+		this.parent = parent;
+		this.connection = new MyConnection(credentials);
+		this.onFinished = onFinished;
+	}
+
+	public synchronized void start() {
+		this.executor.execute(() -> task.accept(this));
+	}
+
+	public synchronized void stop() {
+		this.connection.deactivate();
+		// Shutdown executor
+		if (this.executor != null) {
+			try {
+				this.executor.shutdown();
+				this.executor.awaitTermination(5, TimeUnit.SECONDS);
+			} catch (InterruptedException e) {
+				this.parent.logWarn(this.log, "tasks interrupted");
+			} finally {
+				if (!this.executor.isTerminated()) {
+					this.parent.logWarn(this.log, "cancel non-finished tasks");
+				}
+				this.executor.shutdownNow();
+			}
+		}
+	}
+
+	private Consumer<InitializeEdgesWorker> task = (self) -> {
+		// First: mark all Edges as offline
+		try {
+			this.psUpdateAllEdgesOffline(self.connection).execute();
+		} catch (SQLException e) {
+			self.parent.logError(this.log,
+					"Unable to mark Edges offline. " + e.getClass().getSimpleName() + ": " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		/**
+		 * Reads all Edges from Postgres and puts them in a local Cache.
+		 */
+		try {
+			self.parent.logInfo(this.log, "Caching Edges from Postgres");
+			ResultSet rs = this.psQueryAllEdges(self.connection).executeQuery();
+			for (int i = 0; rs.next(); i++) {
+				if (i % 100 == 0) {
+					self.parent.logInfo(this.log, "Caching Edges from Postgres. Finished [" + i + "]");
+				}
+				try {
+					MyEdge edge = self.parent.edgeCache.addOrUpate(rs);
+
+					// Trigger update to Edge States Sum
+					self.parent.getQueueWriteWorker().addTask(new UpdateEdgeStatesSum(edge.getOdooId()));
+				} catch (Exception e) {
+					self.parent.logError(this.log,
+							"Unable to read Edge: " + e.getClass().getSimpleName() + ". " + e.getMessage());
+					e.printStackTrace();
+				}
+			}
+
+		} catch (Exception e) {
+			self.parent.logError(this.log,
+					"Unable to initialize Edges: " + e.getClass().getSimpleName() + ". " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		self.parent.logInfo(this.log, "Caching Edges from Postgres finished");
+		self.onFinished.run();
+	};
+
+	/**
+	 * SELECT {} FROM {edge.device};
+	 * 
+	 * @return the PreparedStatement
+	 * @throws SQLException on error
+	 */
+	private PreparedStatement psQueryAllEdges(MyConnection connection) throws SQLException {
+		return connection.get().prepareStatement(//
+				"SELECT " + Field.getSqlQueryFields(EdgeDevice.values()) //
+						+ " FROM " + EdgeDevice.ODOO_TABLE //
+						+ ";");
+	}
+
+	/**
+	 * UPDATE {} SET openems_is_connected = FALSE;
+	 * 
+	 * @return the PreparedStatement
+	 * @throws SQLException on error
+	 */
+	private PreparedStatement psUpdateAllEdgesOffline(MyConnection connection) throws SQLException {
+		return connection.get().prepareStatement(//
+				"UPDATE " + EdgeDevice.ODOO_TABLE //
+						+ " SET " + Field.EdgeDevice.OPENEMS_IS_CONNECTED.id() + " = FALSE" //
+						+ ";");
+	}
+
+}
---
+++ io.openems.backend.metadata.odoo/src/io/openems/backend/metadata/odoo/postgres/task/InsertOrUpdateDeviceState.java
@@ -0,0 +1,68 @@
+package io.openems.backend.metadata.odoo.postgres.task;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import io.openems.backend.metadata.odoo.Field.EdgeDeviceStatus;
+import io.openems.backend.metadata.odoo.postgres.MyConnection;
+import io.openems.common.channel.Level;
+import io.openems.common.types.ChannelAddress;
+
+public class InsertOrUpdateDeviceState implements DatabaseTask {
+	private final int odooId;
+	private final ChannelAddress channelAddress;
+	private final Level level;
+	private final String stateChannelName;
+	private final Timestamp timestamp;
+
+	public InsertOrUpdateDeviceState(int odooId, ChannelAddress channelAddress, Level level, String stateChannelName,
+			Timestamp timestamp) {
+		this.odooId = odooId;
+		this.channelAddress = channelAddress;
+		this.level = level;
+		this.stateChannelName = stateChannelName;
+		this.timestamp = timestamp;
+	}
+
+	@Override
+	public void execute(MyConnection connection) throws SQLException {
+		PreparedStatement ps = this.psInsertOrUpdateDeviceState(connection);
+		// device_id
+		ps.setInt(1, this.odooId);
+		// channel_address
+		ps.setString(2, this.channelAddress.toString());
+		// level
+		ps.setString(3, this.level.name().toLowerCase());
+		// component_id
+		ps.setString(4, this.channelAddress.getComponentId());
+		// channel_name
+		ps.setString(5, this.stateChannelName);
+		// last_appearance
+		ps.setTimestamp(6, this.timestamp);
+
+		ps.execute();
+	}
+
+	/**
+	 * INSERT INTO {} (...) VALUES (...) ON CONFLICT (..) UPDATE SET
+	 * item=excluded.item;
+	 * 
+	 * <p>
+	 * Be careful to synchronize access to the resulting PreparedStatement.
+	 * 
+	 * @return the PreparedStatement
+	 * @throws SQLException on error
+	 */
+	private PreparedStatement psInsertOrUpdateDeviceState(MyConnection connection) throws SQLException {
+		return connection.get().prepareStatement(//
+				"INSERT INTO " + EdgeDeviceStatus.ODOO_TABLE //
+						+ " (device_id, channel_address, level, component_id, channel_name, last_appearance)" //
+						+ " VALUES(?, ?, ?, ?, ?, ?)" //
+						+ "	ON CONFLICT (device_id, channel_address)" //
+						+ " DO UPDATE SET" //
+						+ " level=EXCLUDED.level,component_id=EXCLUDED.component_id,"//
+						+ "channel_name=EXCLUDED.channel_name,last_appearance=EXCLUDED.last_appearance");
+	}
+
+}
