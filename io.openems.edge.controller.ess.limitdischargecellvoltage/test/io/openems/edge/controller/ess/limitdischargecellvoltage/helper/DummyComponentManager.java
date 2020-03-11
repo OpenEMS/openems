724fd6b6ef9416c5b689b9b633cca91d356b8241
Stefan Feilmeier <mail@stefanfeilmeier.de>
2020-03-11 11:37:54 +0100

    Improvements in EdgeConfig handling (#1074)
    - remove "isEnabled"; it's already covered by the 'enabled' property
    - improve "getAsText" method
    - Websocket.Api: do not collect EdgeConfig if no websocket is connected
    - Create EdgeConfigFactory that takes care of building and caching EdgeConfig
    - Cache EdgeConfig and only incorporate ConfigurationEvents on Change; improves speed a lot

--- io.openems.edge.controller.ess.limitdischargecellvoltage/test/io/openems/edge/controller/ess/limitdischargecellvoltage/helper/DummyComponentManager.java
+++ io.openems.edge.controller.ess.limitdischargecellvoltage/test/io/openems/edge/controller/ess/limitdischargecellvoltage/helper/DummyComponentManager.java
@@ -1,23 +1,13 @@
 package io.openems.edge.controller.ess.limitdischargecellvoltage.helper;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-
-import org.osgi.service.component.ComponentContext;
-
 import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
-import io.openems.common.jsonrpc.base.JsonrpcRequest;
-import io.openems.common.jsonrpc.base.JsonrpcResponseSuccess;
-import io.openems.common.session.User;
-import io.openems.common.types.EdgeConfig;
-import io.openems.edge.common.channel.Channel;
 import io.openems.edge.common.component.ComponentManager;
 import io.openems.edge.common.component.OpenemsComponent;
 import io.openems.edge.ess.api.ManagedSymmetricEss;
 import io.openems.edge.ess.api.SymmetricEss;
 
-public class DummyComponentManager implements ComponentManager {
+public class DummyComponentManager extends io.openems.edge.common.test.DummyComponentManager
+		implements ComponentManager {
 
 	private ManagedSymmetricEss ess = createEss();
 
@@ -36,57 +26,6 @@ public class DummyComponentManager implements ComponentManager {
 				SymmetricEss.ChannelId.values(), //
 				ManagedSymmetricEss.ChannelId.values() // ;
 		);
-	}
-
-	@Override
-	public CompletableFuture<JsonrpcResponseSuccess> handleJsonrpcRequest(User user, JsonrpcRequest request)
-			throws OpenemsNamedException {
-		return null;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return false;
-	}
-
-	@Override
-	public String id() {
-		return null;
-	}
-
-	@Override
-	public ComponentContext getComponentContext() {
-		return null;
-	}
-
-	@Override
-	public Collection<Channel<?>> channels() {
-		return null;
-	}
-
-	@Override
-	public String alias() {
-		return null;
-	}
-
-	@Override
-	public Channel<?> _channel(String channelName) {
-		return null;
-	}
-
-	@Override
-	public List<OpenemsComponent> getEnabledComponents() {
-		return null;
-	}
-
-	@Override
-	public EdgeConfig getEdgeConfig() {
-		return null;
-	}
-
-	@Override
-	public List<OpenemsComponent> getAllComponents() {
-		return null;
 	}
 
 	public void destroyEss() {
