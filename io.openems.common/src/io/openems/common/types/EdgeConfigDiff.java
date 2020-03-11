f1eb8696efce5041aed2113316cb1b3c7e1e508d
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2020-03-11 19:09:44 +0100

    Fix typo

--- io.openems.common/src/io/openems/common/types/EdgeConfigDiff.java
+++ io.openems.common/src/io/openems/common/types/EdgeConfigDiff.java
@@ -100,7 +100,7 @@ public class EdgeConfigDiff {
 		public static enum Change {
 			CREATED("Created"), //
 			DELETED("Deleted"), //
-			UPDATED("Created");
+			UPDATED("Updated");
 
 			private final String name;
 
