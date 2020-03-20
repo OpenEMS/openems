f7176e206ed03d4d7be3121a347ed05569b10946
Fabian Schwarzbeck <fabian.schwarzbeck@fenecon.de>
2020-03-20 10:09:28 +0100

    no message

--- ui/src/app/edge/live/advertisement/advertisement.component.ts
+++ ui/src/app/edge/live/advertisement/advertisement.component.ts
@@ -1,21 +1,26 @@
 import { ActivatedRoute } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Edge, Service } from '../../../shared/shared';
-import { ModalController } from '@ionic/angular';
+import { ModalController, IonSlides } from '@ionic/angular';
 
 @Component({
   selector: AdvertisementComponent.SELECTOR,
   templateUrl: './advertisement.component.html'
 })
+
+
 export class AdvertisementComponent {
+
+  @ViewChild('slider', { static: true }) slides: IonSlides;
 
   private static readonly SELECTOR = "advertisement";
 
   private edge: Edge = null;
 
+
   slideOpts = {
     initialSlide: 1,
-    speed: 400
+    speed: 1600
   };
 
   constructor(
@@ -28,5 +33,19 @@ export class AdvertisementComponent {
     this.service.setCurrentComponent('', this.route).then(edge => {
       this.edge = edge;
     })
+  }
+
+  slidesDidLoad(slides: IonSlides) {
+    slides.startAutoplay();
+  }
+
+  swipeNext() {
+    this.slides.slideNext();
+    this.slides.startAutoplay();
+  }
+
+  swipePrevious() {
+    this.slides.slidePrev()
+    this.slides.startAutoplay();
   }
 }
