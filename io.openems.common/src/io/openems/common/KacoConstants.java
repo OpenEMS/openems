052382dffa203b0ff5add952a66d887d3673321a
energydepot <andreas.hummelsberger@energydepot.de>
2020-03-30 14:41:42 +0200

    added constants for iot2020

--- io.openems.common/src/io/openems/common/KacoConstants.java
+++ io.openems.common/src/io/openems/common/KacoConstants.java
@@ -10,7 +10,10 @@ public class KacoConstants {
 	public final static String UI_FILE = "edge.zip";
 	public final static String UI_FOLDER = "/var/www/html/";
 	public final static String UI_FOLDER_BACKUP = LOCAL_FOLDER + "ui-backup/";
-	public final static String RESTART_CMD = "sudo systemctl restart hy-control.service";
-	public final static String USERNAME = "pi";
-	public final static String PASSWORD = "raspberry";
+	//public final static String RESTART_CMD = "sudo systemctl restart hy-control.service";
+	public final static String RESTART_CMD = "/etc/init.d/hy-control restart";
+	//public final static String USERNAME = "pi";
+	public final static String USERNAME = "root";
+	//public final static String PASSWORD = "raspberry";
+	public final static String PASSWORD = "KNKonstanz2020!";
 }
