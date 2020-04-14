OpenEMS/openems

commit 6b7ac51462bc4a2dadce2230bcd4009c2152a224

Avoid SDNOTIFY warning on operating systems that do not use systemd

io.openems.edge.core/src/io/openems/edge/core/cycle/CycleWorker.java
@@ -36,8 +36,11 @@ public class CycleWorker extends AbstractWorker {
 	@Override
 	protected void forever() {
 		// Kick Operating System Watchdog
-		if (SDNotify.isAvailable()) {
-			SDNotify.sendWatchdog();
+		String socketName = System.getenv().get("NOTIFY_SOCKET");
+		if (socketName != null && socketName.length() == 0) {
+			if (SDNotify.isAvailable()) {
+				SDNotify.sendWatchdog();
+			}
 		}
 
 		try {

io.openems.edge.application/src/io/openems/edge/application/EdgeApp.java
@@ -25,8 +25,11 @@ public class EdgeApp {
 		this.log.info(line);
 
 		// Announce Operating System that OpenEMS Edge started
-		if (SDNotify.isAvailable()) {
-			SDNotify.sendNotify();
+		String socketName = System.getenv().get("NOTIFY_SOCKET");
+		if (socketName != null && socketName.length() == 0) {
+			if (SDNotify.isAvailable()) {
+				SDNotify.sendNotify();
+			}
 		}
 	}
 

commit ceab6ddf8a3696c81d887fa191231e7a8478836e

Fix Systemd Notify Bug
Introduced in 6b7ac51462bc4a2dadce2230bcd4009c2152a224

io.openems.edge.core/src/io/openems/edge/core/cycle/CycleWorker.java
@@ -37,7 +37,7 @@ public class CycleWorker extends AbstractWorker {
 	protected void forever() {
 		// Kick Operating System Watchdog
 		String socketName = System.getenv().get("NOTIFY_SOCKET");
-		if (socketName != null && socketName.length() == 0) {
+		if (socketName != null && socketName.length() != 0) {
 			if (SDNotify.isAvailable()) {
 				SDNotify.sendWatchdog();
 			}