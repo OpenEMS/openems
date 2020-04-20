f101fed808dd88040e0e4c55280b624dcdcae63f
Hüseyin Sahutoglu <hueseyin.sahutoglu@fenecon.de>
2020-04-20 16:09:11 +0200

    no msg

--- io.openems.edge.controller.ess.setpower/src/io/openems/edge/controller/ess/setpower/SetPower.java
+++ io.openems.edge.controller.ess.setpower/src/io/openems/edge/controller/ess/setpower/SetPower.java
@@ -12,13 +12,11 @@ import org.slf4j.LoggerFactory;
 
 import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
 import io.openems.common.types.ChannelAddress;
-import io.openems.common.types.OpenemsType;
-import io.openems.edge.common.channel.Channel;
 import io.openems.edge.common.channel.Doc;
+import io.openems.edge.common.channel.IntegerReadChannel;
 import io.openems.edge.common.component.AbstractOpenemsComponent;
 import io.openems.edge.common.component.ComponentManager;
 import io.openems.edge.common.component.OpenemsComponent;
-import io.openems.edge.common.type.TypeUtils;
 import io.openems.edge.controller.api.Controller;
 import io.openems.edge.ess.api.ManagedSymmetricEss;
 import io.openems.edge.ess.power.api.Phase;
@@ -33,13 +31,12 @@ import io.openems.edge.ess.power.api.Relationship;
 )
 public class SetPower extends AbstractOpenemsComponent implements Controller, OpenemsComponent {
 
-	private Config config = null;
-	private ChannelAddress inputChannelAddress;
-
 	private final Logger log = LoggerFactory.getLogger(SetPower.class);
 
 	@Reference
 	protected ComponentManager componentManager;
+
+	private Config config = null;
 
 	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
 		;
@@ -67,7 +64,6 @@ public class SetPower extends AbstractOpenemsComponent implements Controller, Op
 	@Activate
 	void activate(ComponentContext context, Config config) throws OpenemsNamedException {
 		this.config = config;
-		this.inputChannelAddress = ChannelAddress.fromString(config.inputChannelAddress());
 		super.activate(context, config.id(), config.alias(), config.enabled());
 	}
 
@@ -78,28 +74,22 @@ public class SetPower extends AbstractOpenemsComponent implements Controller, Op
 
 	@Override
 	public void run() throws OpenemsNamedException {
-
-		int requiredPower;
-		try {
-			Channel<?> inputChannel = this.componentManager.getChannel(this.inputChannelAddress);
-			requiredPower = TypeUtils.getAsType(OpenemsType.INTEGER, inputChannel.value().getOrError());
-		} catch (Exception e) {
-			this.logError(this.log, e.getClass().getSimpleName() + ": " + e.getMessage());
-			return;
-		}
+		// get input value
+		ChannelAddress inputChannelAddress = ChannelAddress.fromString(this.config.inputChannelAddress());
+		IntegerReadChannel inputChannel = this.componentManager.getChannel(inputChannelAddress);
+		int requiredPower = inputChannel.value().getOrError();
 
 		ManagedSymmetricEss ess = this.componentManager.getComponent(this.config.ess_id());
 
 		// adjust value so that it fits into Min/MaxActivePower
 		int calculatedPower = ess.getPower().fitValueIntoMinMaxPower(this.id(), ess, Phase.ALL, Pwr.ACTIVE,
-				-requiredPower);
-		if (this.config.invert()) {
-			requiredPower = (-1) * requiredPower;
-		}
-		/*
-		 * set result
-		 */
-		ess.addPowerConstraintAndValidate("SetPower", Phase.ALL, Pwr.ACTIVE, Relationship.EQUALS, calculatedPower);
+				requiredPower);
 
+		if (this.config.invert()) {
+			requiredPower = -1 * requiredPower;
+		}
+
+		// set result
+		ess.addPowerConstraintAndValidate("SetPower", Phase.ALL, Pwr.ACTIVE, Relationship.EQUALS, calculatedPower);
 	}
 }
