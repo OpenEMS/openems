9aeb30ec31fb3c8db291101f0bc2440472509db9
Wolfgang Gerbl <wolfgang.gerbl@fenecon.de>
2020-05-15 12:50:07 +0200

    add battery protection ctrl

---
+++ io.openems.edge.controller.battery.batteryprotection/src/io/openems/edge/controller/battery/batteryprotection/state/Undefined.java
@@ -0,0 +1,38 @@
+package io.openems.edge.controller.battery.batteryprotection.state;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.openems.edge.battery.api.Battery;
+import io.openems.edge.controller.battery.batteryprotection.IState;
+import io.openems.edge.controller.battery.batteryprotection.State;
+import io.openems.edge.ess.api.ManagedSymmetricEss;
+
+public class Undefined extends BaseState implements IState {
+
+	private final Logger log = LoggerFactory.getLogger(Undefined.class);
+
+	public Undefined(ManagedSymmetricEss ess, Battery bms) {
+		super(ess, bms);
+	}
+
+	@Override
+	public State getState() {
+		return State.UNDEFINED;
+	}
+
+	@Override
+	public State getNextState() {
+		// According to the state machine the next state can only be NORMAL
+		if (isNextStateUndefined()) {
+			return State.UNDEFINED;
+		}
+
+		return State.NORMAL;
+	}
+
+	@Override
+	public void act() {
+		log.info("Nothing to do!");
+	}
+}
