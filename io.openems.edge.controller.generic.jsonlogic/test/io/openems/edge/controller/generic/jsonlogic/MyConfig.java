ae073644e7cc79347319505638915687ac1002c2
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2020-05-19 00:04:47 +0200

    WIP JsonLogic-Controller

---
+++ io.openems.edge.controller.generic.jsonlogic/test/io/openems/edge/controller/generic/jsonlogic/MyConfig.java
@@ -0,0 +1,48 @@
+package io.openems.edge.controller.generic.jsonlogic;
+
+import io.openems.edge.common.startstop.StartStopConfig;
+import io.openems.edge.common.test.AbstractComponentConfig;
+
+@SuppressWarnings("all")
+public class MyConfig extends AbstractComponentConfig implements Config {
+
+	protected static class Builder {
+		private String id = "ctrlJsonLogic0";
+		private String rule = null;
+
+		private Builder() {
+
+		}
+
+		public Builder setId(String id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder setRule(String rule) {
+			this.rule = rule;
+			return this;
+		}
+
+		public MyConfig build() {
+			return new MyConfig(this);
+		}
+	}
+
+	public static Builder create() {
+		return new Builder();
+	}
+
+	private final Builder builder;
+
+	private MyConfig(Builder builder) {
+		super(Config.class, builder.id);
+		this.builder = builder;
+	}
+
+	@Override
+	public String rule() {
+		return this.builder.rule;
+	}
+
+}
