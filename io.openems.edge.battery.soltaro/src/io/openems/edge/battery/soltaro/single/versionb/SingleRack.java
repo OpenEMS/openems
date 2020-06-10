714d3d54f309880f33f36e7b8298ac3873ba0500
Wolfgang Gerbl <wolfgang.gerbl@fenecon.de>
2020-06-10 15:28:26 +0200

    write necessary component channels to api channels, fix isUndefined() for state machine

--- io.openems.edge.battery.soltaro/src/io/openems/edge/battery/soltaro/single/versionb/SingleRack.java
+++ io.openems.edge.battery.soltaro/src/io/openems/edge/battery/soltaro/single/versionb/SingleRack.java
@@ -399,6 +399,31 @@ public class SingleRack extends AbstractOpenemsModbusComponent
 			int voltageMillivolt = minCellVoltageOpt.get();
 			this.channel(Battery.ChannelId.MIN_CELL_VOLTAGE).setNextValue(voltageMillivolt);
 		}
+		
+		@SuppressWarnings("unchecked")
+		Optional<Integer> maxCellVoltageOpt = (Optional<Integer>) this
+				.channel(SingleRackChannelId.CLUSTER_1_MAX_CELL_VOLTAGE).value().asOptional();
+		if (minCellVoltageOpt.isPresent()) {
+			int voltageMillivolt = maxCellVoltageOpt.get();
+			this.channel(Battery.ChannelId.MAX_CELL_VOLTAGE).setNextValue(voltageMillivolt);
+		}
+		
+		@SuppressWarnings("unchecked")
+		Optional<Integer> minCelltemperatureOpt = (Optional<Integer>) this
+				.channel(SingleRackChannelId.CLUSTER_1_MIN_CELL_TEMPERATURE).value().asOptional();
+		if (minCelltemperatureOpt.isPresent()) {
+			int temperature = minCelltemperatureOpt.get() / 10;
+			this.channel(Battery.ChannelId.MIN_CELL_TEMPERATURE).setNextValue(temperature);
+		}
+		
+		@SuppressWarnings("unchecked")
+		Optional<Integer> maxCelltemperatureOpt = (Optional<Integer>) this
+				.channel(SingleRackChannelId.CLUSTER_1_MAX_CELL_TEMPERATURE).value().asOptional();
+		if (minCelltemperatureOpt.isPresent()) {
+			int temperature = maxCelltemperatureOpt.get() / 10;
+			this.channel(Battery.ChannelId.MAX_CELL_TEMPERATURE).setNextValue(temperature);
+		}
+		
 
 		// write battery ranges to according channels in battery api
 		// MAX_VOLTAGE x2082
