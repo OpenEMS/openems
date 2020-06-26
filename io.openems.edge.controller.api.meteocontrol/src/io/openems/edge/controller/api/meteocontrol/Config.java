5af713bcf5f88dac68f7d3762a863ad462c9ce8c
energydepot <andreas.hummelsberger@energydepot.de>
2020-06-26 13:16:16 +0200

    Meteo Control: Changed to VCOM Api v2

--- io.openems.edge.controller.api.meteocontrol/src/io/openems/edge/controller/api/meteocontrol/Config.java
+++ io.openems.edge.controller.api.meteocontrol/src/io/openems/edge/controller/api/meteocontrol/Config.java
@@ -6,7 +6,7 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 @ObjectClassDefinition(//
 		name = "Controller Meteo Control", //
-		description = "This Controller sends Data of selected components to Meteo Control Server via their REST Api")
+		description = "This Controller sends Data of selected components to Meteo Control Server via their VCOM Api")
 @interface Config {
 
 	@AttributeDefinition(name = "Component-ID", description = "Unique ID of this Component")
@@ -19,22 +19,19 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 	boolean enabled() default true;
 	
 	@AttributeDefinition(name = "Host", description = "The host url of the meteo control Api (http or https)")
-	String host() default "https://ws.meteocontrol.de";
+	String host() default "https://mii.meteocontrol.de";
+	
+	@AttributeDefinition(name = "API Key", description = "The Apikey for meteo control")
+	String apikey() default "";
 	
 	@AttributeDefinition(name = "Serial", description = "The serial of this Meteo Control client")
 	String serial() default "";
 	
-	@AttributeDefinition(name = "User Name", description = "The user name of this client account")
-	String user() default "";
+	@AttributeDefinition(name = "Measurement Intervall", description = "The interval in seconds for data measurements", type = AttributeType.INTEGER, min = "300")
+	int mInterval() default 300;
 	
-	@AttributeDefinition(name = "Password", description = "The password for this client account",  type = AttributeType.PASSWORD)
-	String password() default "";
-	
-	@AttributeDefinition(name = "Measurement Intervall", description = "The interval in seconds for data measurements")
-	int mInterval() default 900;
-	
-	@AttributeDefinition(name = "Transfer Intervall", description = "The interval in hours for transfering data measurements (minimum 1 hrs)", type = AttributeType.INTEGER, min = "1")
-	int tInterval() default 1;
+	@AttributeDefinition(name = "Transfer Intervall", description = "The interval in minutes for transfering data measurements (minimum 5 minutes)", type = AttributeType.INTEGER, min = "5")
+	int tInterval() default 5;
 	
 	@AttributeDefinition(name = "Ess Id", description = "The Id of the Ess component")
 	String essId() default "ess0";
