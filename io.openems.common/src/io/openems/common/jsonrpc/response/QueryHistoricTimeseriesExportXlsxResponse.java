8046b6cbe54f67408463527e714a346232cb9973
Pooran Chandrashekaraiah <pooran.c@fenecon.de>
2020-07-29 16:54:52 +0200

    Update on the excel sheet in excel export feature

--- io.openems.common/src/io/openems/common/jsonrpc/response/QueryHistoricTimeseriesExportXlsxResponse.java
+++ io.openems.common/src/io/openems/common/jsonrpc/response/QueryHistoricTimeseriesExportXlsxResponse.java
@@ -10,7 +10,6 @@ import java.util.UUID;
 import org.dhatim.fastexcel.Workbook;
 import org.dhatim.fastexcel.Worksheet;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.gson.JsonElement;
 
 import io.openems.common.types.ChannelAddress;
@@ -32,33 +31,71 @@ import io.openems.common.types.ChannelAddress;
  */
 public class QueryHistoricTimeseriesExportXlsxResponse extends Base64PayloadResponse {
 
-	private static class Label {
-		protected final String title;
-		protected final String comment;
+//	private static class Label {
+//		protected final String title;
+//		protected final String comment;
+//
+//		protected Label(String title, String comment) {
+//			this.title = title;
+//			this.comment = comment;
+//		}
+//	}
 
-		protected Label(String title, String comment) {
-			this.title = title;
-			this.comment = comment;
+	public enum channelColNumber {
+		DATETIME(0), //
+		NETZBEZUG(1), //
+		NETZBEZUG_KUMULIERT(2), //
+		NETZEINSPEISUNG(3), //
+		NETZEINSPEISUNG_KUMULIERT(4), //
+		ERZEUGUNG(5), //
+		ERZEUGUNG_KUMULIERT(6), //
+		SPEICHER_BELADUNG(7), //
+		SPEICHER_BELADUNG_KUMULIERT(8), //
+		SPEICHER_ENTLADUNG(9), //
+		SPEICHER_ENTLADUNG_KUMULIERT(10), //
+		VERBRAUCH(11), //
+		VERBRAUCH_KUMULIERT(12), //
+		SPEICHER_LADEZUSTAND(13); //
+
+		private final int colNumber;
+
+		channelColNumber(int colNumber) {
+			this.colNumber = colNumber;
 		}
+
+		public int getColNumber() {
+			return this.colNumber;
+		}
+
 	}
 
-	private final static ImmutableMap<String, Label> LABELS = ImmutableMap.<String, Label>builder() //
-			.put("datetime", //
-					new Label("Date/Time", null)) //
-			.put("_sum/EssSoc", //
-					new Label("Ladezustand [%]", null)) //
-			.put("_sum/EssActivePower", //
-					new Label("Speicher Be-/Entladung [W]",
-							"Positive Werte für Entladung, negative für Beladung. Bei DC-gekoppelten Speichersystemen wird die PV-Leistung hier ebenfalls erfasst.")) //
-			.put("_sum/ConsumptionActivePower", //
-					new Label("Verbrauch [W]",
-							"Dieser Wert wird nicht direkt gemessen, sondern errechnet sich aus Speicher Be-/Entladung, Netzbezug/-einspeisung und Erzeugung.")) //
-			.put("_sum/GridActivePower", //
-					new Label("Netzbezug-/einspeisung [W]",
-							"Positive Werte für Netzbezug, negative für Netzeinspeisung.")) //
-			.put("_sum/ProductionActivePower", //
-					new Label("Erzeugung [W]", null)) //
-			.build();
+	private static int ROW = 0;
+	// Basic info
+	private final static String[] BASICINFO = new String[] { "FEMS-Nr.", "Export erstellt am", "Export Zeitraum" };
+	// This data will come from Edge info
+	private final static String[] TEMPINFO = new String[] { "1234.", "DATE", "TIME" };
+	// Main Header
+	private final static String[] MAINHEADER = new String[] { "Netzbezug [kWh]", "Netzeinspeisung [kWh]",
+			"Erzeugung [kWh]", "Speicher Beladung [kWh]", "Speicher Entladung [kWh]", "Verbrauch [kWh]" };
+	// Comments for main header
+	private final static String[] MOREINFO = new String[] { "(berechnet aus Leistung [W])",
+			"(Letzter kumulierter Wert minus erster kumulierter Wert)" };
+
+	private final static String[] LABELS = new String[] { "Date/Time", //
+			"Netzbezug [W]", // Grid buy
+			"Netzbezug kumuliert [Wh]", //
+			"Netzeinspeisung [W]", // Grid Sell
+			"Netzeinspeisung kumuliert [Wh]", //
+			"Erzeugung [W]", //
+			"Erzeugung kumuliert [Wh]", //
+			"Speicher Beladung [W]", //
+			"Speicher Beladung kumuliert [Wh]", //
+			"Speicher Entladung [W]", //
+			"Speicher Entladung kumuliert [Wh]", //
+			"Verbrauch [W]", //
+			"Verbrauch kumuliert [Wh]", //
+			"Speicher Ladezustand [%]" //
+	};
 
 	public QueryHistoricTimeseriesExportXlsxResponse(UUID id, ZonedDateTime fromDate, ZonedDateTime toDate,
 			SortedMap<ZonedDateTime, SortedMap<ChannelAddress, JsonElement>> historicData,
@@ -79,7 +116,7 @@ public class QueryHistoricTimeseriesExportXlsxResponse extends Base64PayloadResp
 			initializeColWidths(worksheet, COL_WIDTH, historicData.size() + 1);
 
 			// TODO add Energy
-			addData(worksheet, 0, historicData);
+			addData(worksheet, historicData);
 
 			workbook.finish();
 			os.flush();
@@ -109,28 +146,168 @@ public class QueryHistoricTimeseriesExportXlsxResponse extends Base64PayloadResp
 	 * @param row          the start row
 	 * @param historicData the data map
 	 */
-	public static void addData(Worksheet worksheet, int row,
+	public static void addData(Worksheet worksheet,
 			SortedMap<ZonedDateTime, SortedMap<ChannelAddress, JsonElement>> table) {
+		// base info
+		boolean isBaseInfoInitialized = false;
+		// Main header
+		boolean isMainHeader = false;
+		// Header Info
+		boolean isHeaderInfo = false;
+		// Headers
 		boolean isHeaderInitialized = false;
 		for (Entry<ZonedDateTime, SortedMap<ChannelAddress, JsonElement>> tableRow : table.entrySet()) {
+
+			if (!isBaseInfoInitialized) {
+				ROW++;
+				initializeBaseInfo(worksheet);
+				isBaseInfoInitialized = true;
+			}
+
+			if (!isMainHeader) {
+				ROW++;
+				initializeMainHeader(worksheet);
+				isMainHeader = true;
+			}
+
+			if (!isHeaderInfo) {
+				initializeHeaderInfo(worksheet);
+				isHeaderInfo = true;
+			}
+
 			if (!isHeaderInitialized) {
-				initializeDataHeader(worksheet, row++, tableRow.getValue());
+				ROW++;
+				initializeDataHeader(worksheet, tableRow.getValue());
 				isHeaderInitialized = true;
 			}
 
 			int col = 0;
-			worksheet.value(row, col++, tableRow.getKey().toString());
+			ROW++;
+			worksheet.value(ROW, col++, tableRow.getKey().toString());
+			double bufferGridBuy = 0.0;
+			double bufferGridSell = 0.0;
+			double bufferProduction = 0.0;
+			double bufferConsumption = 0.0;
+
 			for (Entry<ChannelAddress, JsonElement> tableColumn : tableRow.getValue().entrySet()) {
 				JsonElement jValue = tableColumn.getValue();
+				String channelName = tableColumn.getKey().toString();
 				if (jValue.isJsonNull()) {
 					continue;
 				}
-				double value = jValue.getAsDouble();
-				worksheet.value(row, col++, Math.round(value));
+				// Grid buy and sell
+				if (channelName.equals("_sum/EssActivePower")) {
+					if (jValue.getAsDouble() > 0) {
+						worksheet.value(ROW, channelColNumber.NETZBEZUG.getColNumber(),
+								"Grid sell " + (jValue.getAsDouble() / 4000));
+					} else {
+						System.out.println("Lesser than zero " + jValue.getAsDouble());
+						worksheet.value(ROW, channelColNumber.NETZEINSPEISUNG.getColNumber(),
+								"Grid buy " + (jValue.getAsDouble() / 4000));
+					}
+				}
+				// Production
+				if (channelName.equals("_sum/ProductionActivePower")) {
+					worksheet.value(ROW, channelColNumber.ERZEUGUNG.getColNumber(),
+							"Production " + (jValue.getAsDouble() / 4000));
+				}
+				// Charging and discharging
+				if (channelName.equals("_sum/EssActivePower")) {
+					if (jValue.getAsDouble() > 0) {
+						worksheet.value(ROW, channelColNumber.SPEICHER_BELADUNG.getColNumber(),
+								"Charging " + (jValue.getAsDouble() / 4000));
+					} else {
+						System.out.println("Lesser than zero " + jValue.getAsDouble());
+						worksheet.value(ROW, channelColNumber.SPEICHER_ENTLADUNG.getColNumber(),
+								"Discharging " + (jValue.getAsDouble() / 4000));
+					}
+				}
+				// State of charge
+				if (channelName.equals("_sum/EssSoc")) {
+					worksheet.value(ROW, channelColNumber.SPEICHER_LADEZUSTAND.getColNumber(),
+							"State of charge " + (jValue.getAsDouble() / 4000));
+				}
+				// Consumption
+				if (channelName.equals("_sum/ConsumptionActivePower")) {
+					worksheet.value(ROW, channelColNumber.VERBRAUCH.getColNumber(),
+							"Consumption " + (jValue.getAsDouble() / 4000));
+				}
+
+				//TODO add ess active energy and 
+				// Testing for energy values
+
+				if (channelName.equals("_sum/GridBuyActiveEnergy")) {
+					bufferGridBuy = bufferGridBuy + jValue.getAsDouble();
+					worksheet.value(ROW, channelColNumber.NETZBEZUG_KUMULIERT.getColNumber(),
+							"Consumption " + bufferGridBuy);
+				}
+
+				if (channelName.equals("_sum/GridSellActiveEnergy")) {
+					bufferGridSell = bufferGridSell + jValue.getAsDouble();
+					worksheet.value(ROW, channelColNumber.NETZEINSPEISUNG_KUMULIERT.getColNumber(),
+							"Consumption " + bufferGridSell);
+				}
+
+				if (channelName.equals("_sum/ProductionActiveEnergy")) {
+					bufferProduction = bufferProduction + jValue.getAsDouble();
+					worksheet.value(ROW, channelColNumber.ERZEUGUNG_KUMULIERT.getColNumber(),
+							"Consumption " + bufferProduction);
+				}
+
+				if (channelName.equals("_sum/ConsumptionActiveEnergy")) {
+					bufferConsumption = bufferConsumption + jValue.getAsDouble();
+					worksheet.value(ROW, channelColNumber.VERBRAUCH_KUMULIERT.getColNumber(),
+							"Consumption " + bufferConsumption);
+				}
 			}
-			row++;
 		}
 //		sheet.style(0, 0).bold().fillColor(Color.GRAY5).borderStyle("thin").wrapText(true);
+	}
+
+	/**
+	 * Methods which populates basic information of the FEMS
+	 * 
+	 * @param worksheet
+	 */
+	private static void initializeBaseInfo(Worksheet worksheet) {
+
+		for (int i = 0; i < BASICINFO.length; i++) {
+			worksheet.value(ROW, 0 /* column 0 */, BASICINFO[i]);
+			worksheet.value(ROW, 1 /* column 1 */, TEMPINFO[i]);
+			ROW++;
+		}
+	}
+
+	/**
+	 * Method which populates the Main header information
+	 * 
+	 * @param worksheet
+	 */
+	private static void initializeMainHeader(Worksheet worksheet) {
+		int col = 1;
+		for (int i = 0; i < MAINHEADER.length; i++) {
+			worksheet.value(ROW, col, MAINHEADER[i]);
+			col = col + 2;
+		}
+		ROW++;
+	}
+
+	/**
+	 * Method to populate the Header information
+	 * 
+	 * @param worksheet
+	 */
+	private static void initializeHeaderInfo(Worksheet worksheet) {
+		int col = 1;
+		for (int i = 0; i < MAINHEADER.length; i++) {
+			if (col % 2 != 0) {
+				worksheet.value(ROW + 1, col, MOREINFO[0]);
+			} else {
+				worksheet.value(ROW + 1, col, MOREINFO[1]);
+			}
+			col++;
+		}
+		ROW++;
 	}
 
 	/**
@@ -140,21 +317,10 @@ public class QueryHistoricTimeseriesExportXlsxResponse extends Base64PayloadResp
 	 * @param row       the row number
 	 * @param sortedMap the channels
 	 */
-	public static void initializeDataHeader(Worksheet worksheet, int row,
-			SortedMap<ChannelAddress, JsonElement> channelsMap) {
+	public static void initializeDataHeader(Worksheet worksheet, SortedMap<ChannelAddress, JsonElement> channelsMap) {
 		int col = 0;
-		worksheet.value(row, col++, LABELS.get("datetime").title);
-		for (Entry<ChannelAddress, JsonElement> entry : channelsMap.entrySet()) {
-			String key = entry.getKey().toString();
-			Label label = LABELS.get(key);
-			if (label == null) {
-				label = new Label(key.toString(), null);
-			}
-			worksheet.value(row, col, label.title);
-			if (label.comment != null) {
-				worksheet.comment(row, col, label.comment);
-			}
-			col++;
+		for (int i = 0; i < LABELS.length; i++) {
+			worksheet.value(ROW, col++, LABELS[i]);
 		}
 //		worksheet.style(0, 0).bold().fillColor(Color.GRAY5).borderStyle("thin").wrapText(true);
 	}
