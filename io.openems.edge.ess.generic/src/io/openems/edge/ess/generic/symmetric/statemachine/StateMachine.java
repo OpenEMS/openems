4a4f10122d3c3feeffdfcc42c6f7c3cb960b2909
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2020-08-13 16:36:56 +0200

    Update StateMachine for Ess.Generic
    (cherry picked from commit bea6fc3043bbdf9df3e7d53ab0842b3970db8788)

---
+++ io.openems.edge.ess.generic/src/io/openems/edge/ess/generic/symmetric/statemachine/StateMachine.java
@@ -0,0 +1,80 @@
+package io.openems.edge.ess.generic.symmetric.statemachine;
+
+import com.google.common.base.CaseFormat;
+
+import io.openems.common.types.OptionsEnum;
+import io.openems.edge.common.statemachine.AbstractStateMachine;
+import io.openems.edge.common.statemachine.StateHandler;
+
+public class StateMachine extends AbstractStateMachine<StateMachine.State, Context> {
+
+	public enum State implements io.openems.edge.common.statemachine.State<State>, OptionsEnum {
+		UNDEFINED(-1, new UndefinedHandler()), //
+
+		START_BATTERY(10, new StartBatteryHandler()), //
+		START_BATTERY_INVERTER(11, new StartBatteryInverterHandler()), //
+		STARTED(12, new StartedHandler()), //
+
+		STOP_BATTERY_INVERTER(20, new StopBatteryHandler()), //
+		STOP_BATTERY(21, new StopBatteryInverterHandler()), //
+		STOPPED(22, new StoppedHandler()), //
+
+		ERROR(30, new ErrorHandler()), //
+		;
+
+		private final int value;
+		protected final StateHandler<State, Context> handler;
+
+		private State(int value, StateHandler<State, Context> handler) {
+			this.value = value;
+			this.handler = handler;
+		}
+
+		@Override
+		public int getValue() {
+			return this.value;
+		}
+
+		@Override
+		public String getName() {
+			return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, this.name());
+		}
+
+		@Override
+		public OptionsEnum getUndefined() {
+			return UNDEFINED;
+		}
+
+		@Override
+		public State[] getStates() {
+			return State.values();
+		}
+	}
+
+	public StateMachine(State initialState) {
+		super(initialState);
+	}
+
+	@Override
+	public StateHandler<State, Context> getStateHandler(State state) {
+		switch (state) {
+		case UNDEFINED:
+			return new UndefinedHandler();
+		case START_BATTERY:
+			return new StartBatteryHandler();
+		case START_BATTERY_INVERTER:
+			return new StartBatteryInverterHandler();
+		case STARTED:
+			return new StartedHandler();
+		case STOP_BATTERY_INVERTER:
+			return new StopBatteryHandler();
+		case STOP_BATTERY:
+			return new StopBatteryInverterHandler();
+		case STOPPED:
+			return new StoppedHandler();
+		case ERROR:
+			return new ErrorHandler();
+		}
+		throw new IllegalArgumentException("Unknown State [" + state + "]");
+	}
+}
