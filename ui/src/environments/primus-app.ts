c5dc96c618aef9ee17fdd2149db78c6d05bacdb3
energydepot <andreas.hummelsberger@energydepot.de>
2020-08-13 11:53:38 +0200

    removed comments

--- ui/src/environments/primus-app.ts
+++ ui/src/environments/primus-app.ts
@@ -4,6 +4,5 @@ export const environment: Environment = {
   production: true,
   debugMode: false,
   url: "wss://www.energydepot.de/primus-ui-dev",
-  //url: "wss://ahu:KNKonstanz2018!@www.energydepot.de/primus-ui-dev",
   backend: "App",
 };
