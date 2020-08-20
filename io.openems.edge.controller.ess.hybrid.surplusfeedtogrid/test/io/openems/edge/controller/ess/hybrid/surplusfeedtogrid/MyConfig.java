7a5c286b5c47ce0dea7190be02e7c243925ffe1c
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2020-08-20 16:27:15 +0200

    Add Hybrid-Ess Surplus-Feed-To-Grid Controller

---
+++ io.openems.edge.controller.ess.hybrid.surplusfeedtogrid/test/io/openems/edge/controller/ess/hybrid/surplusfeedtogrid/MyConfig.java
@@ -0,0 +1,52 @@
+package io.openems.edge.controller.ess.hybrid.surplusfeedtogrid;
+
+import io.openems.edge.common.test.AbstractComponentConfig;
+
+@SuppressWarnings("all")
+public class MyConfig extends AbstractComponentConfig implements Config {
+
+	protected static class Builder {
+		private String id = null;
+		private String essId = null;
+
+		private Builder() {
+
+		}
+
+		public Builder setId(String id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder setEssId(String essId) {
+			this.essId = essId;
+			return this;
+		}
+
+		public MyConfig build() {
+			return new MyConfig(this);
+		}
+	}
+
+	public static Builder create() {
+		return new Builder();
+	}
+
+	private final Builder builder;
+
+	private MyConfig(Builder builder) {
+		super(Config.class, builder.id);
+		this.builder = builder;
+	}
+
+	@Override
+	public String ess_id() {
+		return this.builder.essId;
+	}
+
+	@Override
+	public String ess_target() {
+		return "(&(enabled=true)(!(service.pid=" + this.id() + "))(|(id=" + this.ess_id() + ")))";
+	}
+
+}
---
+++ io.openems.edge.controller.ess.hybrid.surplusfeedtogrid/test/io/openems/edge/controller/ess/hybrid/surplusfeedtogrid/SurplusFeedToGridControllerImplTest.java
@@ -0,0 +1,48 @@
+package io.openems.edge.controller.ess.hybrid.surplusfeedtogrid;
+
+import org.junit.Test;
+
+import io.openems.common.types.ChannelAddress;
+import io.openems.edge.common.test.AbstractComponentTest.TestCase;
+import io.openems.edge.common.test.DummyConfigurationAdmin;
+import io.openems.edge.controller.test.ControllerTest;
+import io.openems.edge.ess.test.DummyHybridEss;
+
+public class SurplusFeedToGridControllerImplTest {
+
+	private final static String CTRL_ID = "ctrl0";
+
+	private final static ChannelAddress CTRL_SURPLUS_FEED_TO_GRID_IS_LIMITED = new ChannelAddress(CTRL_ID,
+			"SurplusFeedToGridIsLimited");
+
+	private final static String ESS_ID = "ess0";
+
+	private final static ChannelAddress ESS_SET_ACTIVE_POWER_GREATER_OR_EQUALS = new ChannelAddress(ESS_ID,
+			"SetActivePowerGreaterOrEquals");
+
+	@Test
+	public void test() throws Exception {
+		final DummyHybridEss ess = new DummyHybridEss(ESS_ID);
+		final ControllerTest test = new ControllerTest(new SurplusFeedToGridControllerImpl()) //
+				.addReference("cm", new DummyConfigurationAdmin()) //
+				.addReference("ess", ess) //
+				.activate(MyConfig.create() //
+						.setId(CTRL_ID) //
+						.setEssId(ESS_ID) //
+						.build());
+
+		ess.setDummySurplusPower(null);
+		test.next(new TestCase() //
+				.output(ESS_SET_ACTIVE_POWER_GREATER_OR_EQUALS, null));
+
+		ess.setDummySurplusPower(5000);
+		test.next(new TestCase() //
+				.output(CTRL_SURPLUS_FEED_TO_GRID_IS_LIMITED, false) //
+				.output(ESS_SET_ACTIVE_POWER_GREATER_OR_EQUALS, 5000));
+
+		ess.setDummyMaxApparentPower(2000);
+		test.next(new TestCase() //
+				.output(CTRL_SURPLUS_FEED_TO_GRID_IS_LIMITED, true) //
+				.output(ESS_SET_ACTIVE_POWER_GREATER_OR_EQUALS, 2000));
+	}
+}
