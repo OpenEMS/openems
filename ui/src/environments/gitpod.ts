d9580bf71c550b2ee7a2ed50b1b2977db9a04446
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2020-09-14 22:44:50 +0200

    Add UI environment for gitpod

---
+++ ui/src/environments/gitpod.ts
@@ -0,0 +1,9 @@
+import { Environment } from "../app/shared/type/environment";
+
+export const environment: Environment = {
+  production: false,
+  debugMode: true,
+  // gitpod puts the port number in front of the hostname
+  url: "ws://8085-" + location.hostname.substring(location.hostname.indexOf("-") + 1),
+  backend: "OpenEMS Edge"
+};
