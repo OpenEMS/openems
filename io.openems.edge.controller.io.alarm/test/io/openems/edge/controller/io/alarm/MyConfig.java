546fb35e347465296c65b2fdf30f96e27bdcb8b6
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2020-09-26 21:12:18 +0200

    Update IO Alarm Controller test

---
+++ io.openems.edge.controller.io.alarm/test/io/openems/edge/controller/io/alarm/MyConfig.java
@@ -0,0 +1,58 @@
+package io.openems.edge.controller.io.alarm;
+
+import io.openems.common.utils.ConfigUtils;
+import io.openems.edge.common.test.AbstractComponentConfig;
+
+@SuppressWarnings("all")
+public class MyConfig extends AbstractComponentConfig implements Config {
+
+	protected static class Builder {
+		private String id;
+		private String[] inputChannelAddresses;
+		private String outputChannelAddress;
+
+		private Builder() {
+		}
+
+		public Builder setId(String id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder setInputChannelAddresses(String... inputChannelAddresses) {
+			this.inputChannelAddresses = inputChannelAddresses;
+			return this;
+		}
+
+		public Builder setOutputChannelAddress(String outputChannelAddress) {
+			this.outputChannelAddress = outputChannelAddress;
+			return this;
+		}
+
+		public MyConfig build() {
+			return new MyConfig(this);
+		}
+	}
+
+	public static Builder create() {
+		return new Builder();
+	}
+
+	private final Builder builder;
+
+	private MyConfig(Builder builder) {
+		super(Config.class, builder.id);
+		this.builder = builder;
+	}
+
+	@Override
+	public String[] inputChannelAddress() {
+		return this.builder.inputChannelAddresses;
+	}
+
+	@Override
+	public String outputChannelAddress() {
+		return this.builder.outputChannelAddress;
+	}
+
+}
