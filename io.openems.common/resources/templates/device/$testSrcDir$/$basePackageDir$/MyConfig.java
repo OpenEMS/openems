74bb42594e3db44891d14d57bb833e81d6fdec44
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2020-09-27 22:41:37 +0200

    Add unit tests to templates

---
+++ io.openems.common/resources/templates/device/$testSrcDir$/$basePackageDir$/MyConfig.java
@@ -0,0 +1,40 @@
+package $basePackageName$;
+
+import io.openems.edge.common.test.AbstractComponentConfig;
+
+@SuppressWarnings("all")
+public class MyConfig extends AbstractComponentConfig implements Config {
+
+	protected static class Builder {
+		private String id;
+
+		private Builder() {
+		}
+
+		public Builder setId(String id) {
+			this.id = id;
+			return this;
+		}
+
+		public MyConfig build() {
+			return new MyConfig(this);
+		}
+	}
+
+	/**
+	 * Create a Config builder.
+	 * 
+	 * @return a {@link Builder}
+	 */
+	public static Builder create() {
+		return new Builder();
+	}
+
+	private final Builder builder;
+
+	private MyConfig(Builder builder) {
+		super(Config.class, builder.id);
+		this.builder = builder;
+	}
+
+}
