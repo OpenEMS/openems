762c85bad1926513e356946c574dde31358e29ee
Fabian Schwarzbeck <fabian.schwarzbeck@fenecon.de>
2020-09-28 19:47:32 +0200

    UI: *restore charge/discharge-energy values for widget + energychart *replace ngOnInit with ionViewWillEneter to set components corretly when switching views

--- ui/src/app/edge/history/storage/widget.component.ts
+++ ui/src/app/edge/history/storage/widget.component.ts
@@ -56,16 +56,9 @@ export class StorageComponent extends AbstractHistoryWidget implements OnInit, O
     protected getChannelAddresses(edge: Edge, config: EdgeConfig): Promise<ChannelAddress[]> {
         return new Promise((resolve) => {
             let channels: ChannelAddress[] = [
-                new ChannelAddress('_sum', 'EssActiveChargeEnergy'),
-                new ChannelAddress('_sum', 'EssActiveDischargeEnergy'),
+                new ChannelAddress('_sum', 'EssDcChargeEnergy'),
+                new ChannelAddress('_sum', 'EssDcDischargeEnergy')
             ];
-            this.essComponents = config.getComponentsImplementingNature("io.openems.edge.ess.api.SymmetricEss").filter(component => !component.factoryId.includes("Ess.Cluster") && component.isEnabled);
-            this.essComponents.forEach(component => {
-                channels.push(
-                    new ChannelAddress(component.id, 'ActiveChargeEnergy'),
-                    new ChannelAddress(component.id, 'ActiveDischargeEnergy'),
-                )
-            })
             resolve(channels);
         });
     }
--- ui/src/app/edge/settings/component/update/index.component.ts
+++ ui/src/app/edge/settings/component/update/index.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Service, Utils, EdgeConfig } from '../../../../shared/shared';
 import { TranslateService } from '@ngx-translate/core';
@@ -13,7 +13,7 @@ interface MyCategorizedComponents extends CategorizedComponents {
   selector: IndexComponent.SELECTOR,
   templateUrl: './index.component.html'
 })
-export class IndexComponent implements OnInit {
+export class IndexComponent {
 
   private static readonly SELECTOR = "indexComponentUpdate";
 
@@ -29,7 +29,7 @@ export class IndexComponent implements OnInit {
   ) {
   }
 
-  ngOnInit() {
+  ionViewWillEnter() {
     this.service.setCurrentComponent(this.translate.instant('Edge.Config.Index.adjustComponents'), this.route);
     this.service.getConfig().then(config => {
       this.config = config;
