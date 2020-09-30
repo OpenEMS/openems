8e1e62173daa15e757b0f2163b281b1138af4d9f
huseyinsaht <34771592+huseyinsaht@users.noreply.github.com>
2020-09-30 13:22:58 +0200

    Implement ESS Delayed Sell-To-Grid Controller (#1239)
    * Inverted Peak Shaving Controller Implementation
    
    * charge condition added
    
    * bundle added in EdgeApp
    
    * More Test cases added
    
    * package name and Junit test updated
    
    * controller bundle name added to EdgeApp
    
    * Review
    
    Co-authored-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>

---
+++ io.openems.edge.controller.ess.delayedselltogrid/src/io/openems/edge/controller/ess/delayedselltogrid/DelayedSellToGrid.java
@@ -0,0 +1,22 @@
+package io.openems.edge.controller.ess.delayedselltogrid;
+
+import io.openems.edge.common.channel.Doc;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.controller.api.Controller;
+
+public interface DelayedSellToGrid extends Controller, OpenemsComponent {
+
+	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+		;
+		private final Doc doc;
+
+		private ChannelId(Doc doc) {
+			this.doc = doc;
+		}
+
+		@Override
+		public Doc doc() {
+			return this.doc;
+		}
+	}
+}
