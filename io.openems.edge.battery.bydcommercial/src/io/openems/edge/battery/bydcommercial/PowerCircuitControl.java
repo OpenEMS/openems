e5b1573e229547f43aabb103c1b9a49e75896310
huseyinsaht <34771592+huseyinsaht@users.noreply.github.com>
2020-09-30 22:37:21 +0200

    BYD Commercial Battery-Box: add Start/Stop (#1222)
    * Byd Commercial C130 Start Stop Implementation
    
    * Pre charge control enum changed to power circuit control
    
    * Missing registers added
    
    * State machine class changed
    
    * Missing context class file added
    
    * Remove unused hasWarnings() method because of unclear definition
    
    Co-authored-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>

--- io.openems.edge.battery.bydcommercial/src/io/openems/edge/battery/bydcommercial/PowerCircuitControl.java
+++ io.openems.edge.battery.bydcommercial/src/io/openems/edge/battery/bydcommercial/PowerCircuitControl.java
@@ -2,17 +2,18 @@ package io.openems.edge.battery.bydcommercial;
 
 import io.openems.common.types.OptionsEnum;
 
-public enum PreChargeControl implements OptionsEnum {
+public enum PowerCircuitControl implements OptionsEnum {
 	UNDEFINED(-1, "Undefined"), //
 	SWITCH_OFF(0x0, "Shut Down Main Power Contactor & Pre-charge"), //
-	SWITCH_ON(0x1, "Switch ON Pre-charge & Main Power Contactor"), //
-	PRE_CHARGING(0x2, "Intermediate State: Pre-charging"), //
-	RUNNING(0x3, "Running");
+	PRE_CHARGING_1(0x1, "Intermediate State: Pre-charging"), //
+	PRE_CHARGING_2(0x2, "Intermediate State: Pre-charging"), //
+	SWITCH_ON(0x3, "Switch On Main Power Contactor, Running Mode"), //
+	PRE_CHARGE_FAIL(0x4, "Switch ON Pre-charge & Main Power Contactor");
 
 	private final int value;
 	private final String name;
 
-	private PreChargeControl(int value, String name) {
+	private PowerCircuitControl(int value, String name) {
 		this.value = value;
 		this.name = name;
 	}
