bb7c7a55b2144047f54ba1452d3ff21d8d022fbc
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2020-11-18 22:30:42 +0100

    Add `invert` to unit test

--- io.openems.edge.meter.schneider.acti9.smartlink/test/io/openems/edge/meter/schneider/acti9/smartlink/MyConfig.java
+++ io.openems.edge.meter.schneider.acti9.smartlink/test/io/openems/edge/meter/schneider/acti9/smartlink/MyConfig.java
@@ -31,6 +31,11 @@ public class MyConfig extends AbstractComponentConfig implements Config {
 			this.type = type;
 			return this;
 		}
+		
+		public Builder setInvert(boolean invert) {
+			this.invert = invert;
+			return this;
+		}
 
 		public MyConfig build() {
 			return new MyConfig(this);
