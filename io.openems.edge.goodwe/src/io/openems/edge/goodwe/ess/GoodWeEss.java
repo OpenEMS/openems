42e20aef82392458d55febe27692fd3ba2b1501f
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2020-11-25 09:18:09 +0100

    GoodWe fixes
    - fix identification of GoodWe battery inverter if there was no modbus connection initially
    - fix nullpointerexception due to late setting of 'this.config'

--- io.openems.edge.goodwe/src/io/openems/edge/goodwe/ess/GoodWeEss.java
+++ io.openems.edge.goodwe/src/io/openems/edge/goodwe/ess/GoodWeEss.java
@@ -45,7 +45,7 @@ public interface GoodWeEss extends SymmetricEss, OpenemsComponent {
 		SERIAL_NUMBER(Doc.of(OpenemsType.STRING) //
 				.unit(Unit.NONE).accessMode(AccessMode.READ_ONLY)),
 		GOODWE_TYPE(Doc.of(GoodweType.values()) //
-				.accessMode(AccessMode.READ_ONLY)),
+				.accessMode(AccessMode.READ_ONLY)), //
 		DSP1_SOFTWARE_VERSION(Doc.of(OpenemsType.INTEGER) //
 				.unit(Unit.NONE).accessMode(AccessMode.READ_ONLY)),
 		DSP2_SOFTWARE_VERSION(Doc.of(OpenemsType.INTEGER) //
