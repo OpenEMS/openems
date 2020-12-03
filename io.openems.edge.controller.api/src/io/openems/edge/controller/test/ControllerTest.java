0dad7fb1626b23e0807951d73b8016f0514d46c0
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2020-12-03 22:27:29 +0100

    OpenEMS Component test framework: validate the implemented Channel-IDs

--- io.openems.edge.controller.api/src/io/openems/edge/controller/test/ControllerTest.java
+++ io.openems.edge.controller.api/src/io/openems/edge/controller/test/ControllerTest.java
@@ -1,6 +1,7 @@
 package io.openems.edge.controller.test;
 
 import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
+import io.openems.common.exceptions.OpenemsException;
 import io.openems.edge.common.component.OpenemsComponent;
 import io.openems.edge.common.test.AbstractComponentTest;
 import io.openems.edge.controller.api.Controller;
@@ -10,7 +11,7 @@ import io.openems.edge.controller.api.Controller;
  */
 public class ControllerTest extends AbstractComponentTest<ControllerTest, Controller> {
 
-	public ControllerTest(Controller controller, OpenemsComponent... components) {
+	public ControllerTest(Controller controller, OpenemsComponent... components) throws OpenemsException {
 		super(controller);
 		for (OpenemsComponent component : components) {
 			this.addComponent(component);
