41c8c64f472fd49734ffd620743900760ac590c2
Stefan Feilmeier <mail@stefanfeilmeier.de>
2020-12-15 11:03:26 +0100

    Fix-Active-Power-Controller: Refactor + add UI Widget (#1325)
    * Refactor Fix-Active-Power-Controller
    
    - Rename to namespace "io.openems.edge.controller.ess"
    - Add 'Mode' to be compatible with UI features
    - Use 'SetActivePowerEquals' Channel at ESS
    - Use @Reference to get the ESS
    - Add JUnit test
    
    * add UI widget
    
    Co-authored-by: Fabian Schwarzbeck <fabian.schwarzbeck@fenecon.de>

--- ui/src/app/edge/live/live.module.ts
+++ ui/src/app/edge/live/live.module.ts
@@ -1,10 +1,9 @@
-import { AdministrationComponent } from './evcs/modal/administration/administration.component';
-import { AsymmetricPeakshavingComponent } from './peakshaving/asymmetric/asymmetricpeakshaving.component';
-import { AsymmetricPeakshavingModalComponent } from './peakshaving/asymmetric/modal/modal.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './../../shared/shared.module';
 import { AutarchyComponent } from './autarchy/autarchy.component';
 import { AutarchyModalComponent } from './autarchy/modal/modal.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
 import { ChannelthresholdComponent } from './channelthreshold/channelthreshold.component';
 import { ChpSocComponent } from './chpsoc/chpsoc.component';
 import { ChpsocModalComponent } from './chpsoc/modal/modal.component';
@@ -13,37 +12,40 @@ import { ConsumptionModalComponent } from './consumption/modal/modal.component';
 import { DigitalInputComponent } from './digitalinput/digitalinput.component';
 import { DigitalInputModalComponent } from './digitalinput/modal/modal.component';
 import { EnergymonitorModule } from './energymonitor/energymonitor.module';
-import { EvcsChart } from './evcsCluster/modal/evcs-chart/evcs.chart';
-import { EvcsClusterComponent } from './evcsCluster/evcsCluster.component';
 import { EvcsComponent } from './evcs/evcs.component';
+import { AdministrationComponent } from './evcs/modal/administration/administration.component';
 import { EvcsModalComponent } from './evcs/modal/modal.page';
 import { EvcsPopoverComponent } from './evcs/modal/popover/popover.page';
+import { EvcsClusterComponent } from './evcsCluster/evcsCluster.component';
+import { EvcsChart } from './evcsCluster/modal/evcs-chart/evcs.chart';
+import { ModalComponentEvcsCluster } from './evcsCluster/modal/evcsCluster-modal.page';
+import { FixActivePowerComponent } from './fixactivepower/fixactivepower.component';
+import { FixActivePowerModalComponent } from './fixactivepower/modal/modal.component';
 import { FixDigitalOutputComponent } from './fixdigitaloutput/fixdigitaloutput.component';
 import { FixDigitalOutputModalComponent } from './fixdigitaloutput/modal/modal.component';
 import { GridComponent } from './grid/grid.component';
 import { GridModalComponent } from './grid/modal/modal.component';
 import { HeatingElementComponent } from './heatingelement/heatingelement.component';
 import { HeatingElementModalComponent } from './heatingelement/modal/modal.component';
-import { InfoComponent } from './info/info.component';
-import { LiveComponent } from './live.component';
-import { ModalComponentEvcsCluster } from './evcsCluster/modal/evcsCluster-modal.page';
-import { NgModule } from '@angular/core';
-import { OfflineComponent } from './offline/offline.component';
-import { ProductionComponent } from './production/production.component';
-import { ProductionModalComponent } from './production/modal/modal.component';
-import { SelfConsumptionComponent } from './selfconsumption/selfconsumption.component';
-import { SelfconsumptionModalComponent } from './selfconsumption/modal/modal.component';
-import { SharedModule } from './../../shared/shared.module';
-import { SinglethresholdComponent } from './singlethreshold/singlethreshold.component';
-import { SinglethresholdModalComponent } from './singlethreshold/modal/modal.component';
-import { StorageComponent } from './storage/storage.component';
-import { StorageModalComponent } from './storage/modal/modal.component';
-import { SymmetricPeakshavingComponent } from './peakshaving/symmetric/symmetricpeakshaving.component';
-import { SymmetricPeakshavingModalComponent } from './peakshaving/symmetric/modal/modal.component';
 import { HeatPumpComponent } from './heatpump/heatpump.component';
 import { HeatPumpModalComponent } from './heatpump/modal/modal.component';
-import { TimeslotPeakshavingComponent } from './peakshaving/timeslot/timeslotpeakshaving.component';
+import { InfoComponent } from './info/info.component';
+import { LiveComponent } from './live.component';
+import { OfflineComponent } from './offline/offline.component';
+import { AsymmetricPeakshavingComponent } from './peakshaving/asymmetric/asymmetricpeakshaving.component';
+import { AsymmetricPeakshavingModalComponent } from './peakshaving/asymmetric/modal/modal.component';
+import { SymmetricPeakshavingModalComponent } from './peakshaving/symmetric/modal/modal.component';
+import { SymmetricPeakshavingComponent } from './peakshaving/symmetric/symmetricpeakshaving.component';
 import { TimeslotPeakshavingModalComponent } from './peakshaving/timeslot/modal/modal.component';
+import { TimeslotPeakshavingComponent } from './peakshaving/timeslot/timeslotpeakshaving.component';
+import { ProductionModalComponent } from './production/modal/modal.component';
+import { ProductionComponent } from './production/production.component';
+import { SelfconsumptionModalComponent } from './selfconsumption/modal/modal.component';
+import { SelfConsumptionComponent } from './selfconsumption/selfconsumption.component';
+import { SinglethresholdModalComponent } from './singlethreshold/modal/modal.component';
+import { SinglethresholdComponent } from './singlethreshold/singlethreshold.component';
+import { StorageModalComponent } from './storage/modal/modal.component';
+import { StorageComponent } from './storage/storage.component';
 
 @NgModule({
   imports: [
@@ -62,6 +64,7 @@ import { TimeslotPeakshavingModalComponent } from './peakshaving/timeslot/modal/
     DigitalInputModalComponent,
     EvcsModalComponent,
     EvcsPopoverComponent,
+    FixActivePowerModalComponent,
     FixDigitalOutputModalComponent,
     GridModalComponent,
     HeatingElementModalComponent,
@@ -92,6 +95,8 @@ import { TimeslotPeakshavingModalComponent } from './peakshaving/timeslot/modal/
     EvcsComponent,
     EvcsModalComponent,
     EvcsPopoverComponent,
+    FixActivePowerComponent,
+    FixActivePowerModalComponent,
     FixDigitalOutputComponent,
     FixDigitalOutputModalComponent,
     FixDigitalOutputModalComponent,
