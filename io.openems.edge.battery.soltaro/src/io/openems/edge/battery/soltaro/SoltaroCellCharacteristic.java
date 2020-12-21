80b81c33c3aa8f79ea3bf4387cf6d715194760ef
Wolfgang Gerbl <wolfgang.gerbl@fenecon.de>
2020-12-21 17:22:49 +0100

    adapt values

--- io.openems.edge.battery.soltaro/src/io/openems/edge/battery/soltaro/SoltaroCellCharacteristic.java
+++ io.openems.edge.battery.soltaro/src/io/openems/edge/battery/soltaro/SoltaroCellCharacteristic.java
@@ -4,22 +4,22 @@ public class SoltaroCellCharacteristic implements CellCharacteristic {
 
 	@Override
 	public int getFinalCellDischargeVoltage_mV() {
-		return 2_800; // 0x2086 Cell under voltage Alarm 
+		return 2_900; //  
 	}
 
 	@Override
 	public int getForceChargeCellVoltage_mV() {
-		return 2_750; // 0x2047 Cell under voltage Protection  recover
+		return 2_800; // 0x2046 Cell under voltage Protection
 	}
 	
 	@Override
 	public int getFinalCellChargeVoltage_mV() {
-		return 3_650; // 0x0080  Cell Over Voltage Alarm
+		return 3_650; // 0x0041  Cell Over Voltage Recover / 0x0080 Cell over Voltage Alarm
 	}
 
 	@Override
 	public int getForceDischargeCellVoltage_mV() {
-		return 3_680; // 0x0041  Cell Over Voltage Recover
+		return 3_680; // 
 	}
 
 }
