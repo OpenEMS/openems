0eab8f5c1b33667ab62cd163b4abece26c41bbe7
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2021-01-28 10:45:27 +0100

    Start development of version 2021.1.9-SNAPSHOT

--- io.openems.common/src/io/openems/common/OpenemsConstants.java
+++ io.openems.common/src/io/openems/common/OpenemsConstants.java
@@ -27,12 +27,12 @@ public class OpenemsConstants {
 	 * 
 	 * This is the number of the bugfix release
 	 */
-	public final static short VERSION_PATCH = 8;
+	public final static short VERSION_PATCH = 9;
 
 	/**
 	 * The additional version string
 	 */
-	public final static String VERSION_STRING = "";
+	public final static String VERSION_STRING = "SNAPSHOT";
 
 	/**
 	 * The complete version as a SemanticVersion.
