05d42346121e2c69d99739fcc7cf0838ab1d1485
dadariga <73471197+dadariga@users.noreply.github.com>
2021-03-19 10:14:48 +0100

    UI: refactoring FixActivePower

--- ui/src/app/edge/live/fixactivepower/fixactivepower.component.ts
+++ ui/src/app/edge/live/fixactivepower/fixactivepower.component.ts
@@ -4,6 +4,8 @@ import { Component, Input } from '@angular/core';
 import { Edge, EdgeConfig, Service } from '../../../shared/shared';
 import { ModalController } from '@ionic/angular';
 import { compileComponentFromMetadata } from '@angular/compiler';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: FixActivePowerComponent.SELECTOR,
@@ -15,11 +17,12 @@ export class FixActivePowerComponent {
 
 
   private static readonly SELECTOR = "fixactivepower";
+  private stopOnDestroy: Subject<void> = new Subject<void>();
 
   private edge: Edge = null;
   public component: EdgeConfig.Component | null = null;
   public chargeState: string;
-  public chargeStateValue: number;
+  public chargeStateValue: number | string;
   state: any;
 
   constructor(
@@ -32,23 +35,31 @@ export class FixActivePowerComponent {
     this.service.setCurrentComponent('', this.route).then(edge => {
       this.edge = edge;
       this.service.getConfig().then(config => {
-        this.component = config.getComponent(this.componentId);
-        if (this.component.properties.power >= 0) {
-          this.chargeState = 'dischargePower';
-          this.chargeStateValue = this.component.properties.power
-        } else if (this.component.properties.power < 0) {
-          this.chargeState = 'chargePower';
-          this.chargeStateValue = this.component.properties.power * -1;
-        }
-        if (this.component.properties.mode == 'MANUAL_ON') {
-          this.state = 'on'
-        } else if (this.component.properties.mode == 'MANUAL_OFF') {
-          this.state = 'off'
-        } else {
-          this.state = ' <ion-icon name="help-outline"></ion-icon>'
-        }
+        edge.currentData.pipe(takeUntil(this.stopOnDestroy)).subscribe(currentData => {
+          this.component = config.getComponent(this.componentId);
+          if (this.component.properties.power >= 0) {
+            this.chargeState = 'dischargePower';
+            this.chargeStateValue = this.component.properties.power
+          } else if (this.component.properties.power < 0) {
+            this.chargeState = 'chargePower';
+            this.chargeStateValue = this.component.properties.power * -1;
+          }
+
+          if (this.component.properties.mode == 'MANUAL_ON') {
+            this.state = 'on'
+          } else if (this.component.properties.mode == 'MANUAL_OFF') {
+            this.state = 'off'
+          } else {
+            this.state = '-'
+          }
+        })
       })
-    })
+    }
+    )
+  }
+  ngOnDestroy() {
+    this.stopOnDestroy.next();
+    this.stopOnDestroy.complete();
   }
 
   async presentModal() {
