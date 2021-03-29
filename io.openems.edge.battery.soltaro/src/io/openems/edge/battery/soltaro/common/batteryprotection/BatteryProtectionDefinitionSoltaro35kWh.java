295c1914d263905ed4ae819bb8989b080ef6c328
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2021-03-29 13:19:57 +0200

    3.5 kWh module: reduce to 1 % of max current

--- io.openems.edge.battery.soltaro/src/io/openems/edge/battery/soltaro/common/batteryprotection/BatteryProtectionDefinitionSoltaro35kWh.java
+++ io.openems.edge.battery.soltaro/src/io/openems/edge/battery/soltaro/common/batteryprotection/BatteryProtectionDefinitionSoltaro35kWh.java
@@ -11,11 +11,8 @@ public class BatteryProtectionDefinitionSoltaro35kWh extends BatteryProtectionDe
 				.addPoint(Math.nextUp(3000), 1) //
 				.addPoint(3350, 1) //
 				.addPoint(3450, 0.9999) //
-
-				// Reduce earlier than in BatteryProtectionDefinitionSoltaro3kWh.
-				.addPoint(3550, 0.02) //
-
-				.addPoint(Math.nextDown(3650), 0.02) //
+				.addPoint(3550, 0.01) //
+				.addPoint(Math.nextDown(3650), 0.01) //
 				.addPoint(3650, 0) //
 				.build();
 	}
