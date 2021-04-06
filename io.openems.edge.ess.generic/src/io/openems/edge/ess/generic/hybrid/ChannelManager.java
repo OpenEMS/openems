f2bfa69f7dd260ddc851f7c4a8f6926aae3bb57e
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2021-04-06 14:25:40 +0200

    Generic HybridEss: add DC power and energy calculation

--- io.openems.edge.ess.generic/src/io/openems/edge/ess/generic/hybrid/ChannelManager.java
+++ io.openems.edge.ess.generic/src/io/openems/edge/ess/generic/hybrid/ChannelManager.java
@@ -2,6 +2,9 @@ package io.openems.edge.ess.generic.hybrid;
 
 import io.openems.edge.battery.api.Battery;
 import io.openems.edge.batteryinverter.api.HybridManagedSymmetricBatteryInverter;
+import io.openems.edge.batteryinverter.api.ManagedSymmetricBatteryInverter;
+import io.openems.edge.common.component.ClockProvider;
+import io.openems.edge.ess.api.HybridEss;
 import io.openems.edge.ess.generic.common.AbstractGenericEssChannelManager;
 import io.openems.edge.ess.generic.common.GenericManagedEss;
 
@@ -9,6 +12,22 @@ public class ChannelManager extends AbstractGenericEssChannelManager<Battery, Hy
 
 	public ChannelManager(GenericManagedEss parent) {
 		super(parent);
+	}
+
+	@Override
+	public void activate(ClockProvider clockProvider, Battery battery,
+			ManagedSymmetricBatteryInverter batteryInverter) {
+		super.activate(clockProvider, battery, batteryInverter);
+
+		this.<Long>addCopyListener(batteryInverter, //
+				HybridManagedSymmetricBatteryInverter.ChannelId.DC_CHARGE_ENERGY, //
+				HybridEss.ChannelId.DC_CHARGE_ENERGY);
+		this.<Long>addCopyListener(batteryInverter, //
+				HybridManagedSymmetricBatteryInverter.ChannelId.DC_DISCHARGE_ENERGY, //
+				HybridEss.ChannelId.DC_DISCHARGE_ENERGY);
+		this.<Long>addCopyListener(batteryInverter, //
+				HybridManagedSymmetricBatteryInverter.ChannelId.DC_DISCHARGE_POWER, //
+				HybridEss.ChannelId.DC_DISCHARGE_POWER);
 	}
 
 }
