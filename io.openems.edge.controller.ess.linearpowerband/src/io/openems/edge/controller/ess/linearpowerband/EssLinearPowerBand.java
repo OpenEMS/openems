8370b63b3e1870286df52fcdf92c88ffbeb9c3c2
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2021-04-08 14:52:21 +0200

    Refactor ESS Linear-Power-Band Controller
    (cherry picked from commit 0eba1908f73871ba5f35e7111bafecd4b7801834)

---
+++ io.openems.edge.controller.ess.linearpowerband/src/io/openems/edge/controller/ess/linearpowerband/EssLinearPowerBand.java
@@ -0,0 +1,26 @@
+package io.openems.edge.controller.ess.linearpowerband;
+
+import io.openems.edge.common.channel.Doc;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.controller.api.Controller;
+
+public interface EssLinearPowerBand extends Controller, OpenemsComponent {
+
+	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+		STATE_MACHINE(Doc.of(State.values()) //
+				.text("Current State of State-Machine")), //
+		;
+
+		private final Doc doc;
+
+		private ChannelId(Doc doc) {
+			this.doc = doc;
+		}
+
+		@Override
+		public Doc doc() {
+			return this.doc;
+		}
+	}
+
+}
---
+++ io.openems.edge.controller.ess.linearpowerband/test/io/openems/edge/controller/ess/linearpowerband/MyConfig.java
@@ -0,0 +1,89 @@
+package io.openems.edge.controller.ess.linearpowerband;
+
+import io.openems.common.utils.ConfigUtils;
+import io.openems.edge.common.test.AbstractComponentConfig;
+
+@SuppressWarnings("all")
+public class MyConfig extends AbstractComponentConfig implements Config {
+
+	protected static class Builder {
+		private String id = null;
+		public String essId;
+		public int minPower;
+		public int maxPower;
+		public int adjustPower;
+
+		private Builder() {
+		}
+
+		public Builder setId(String id) {
+			this.id = id;
+			return this;
+		}
+		
+		public Builder setEssId(String essId) {
+			this.essId = essId;
+			return this;
+		}
+
+		public Builder setMinPower(int minPower) {
+			this.minPower = minPower;
+			return this;
+		}
+
+		public Builder setMaxPower(int maxPower) {
+			this.maxPower = maxPower;
+			return this;
+		}
+
+		public Builder setAdjustPower(int adjustPower) {
+			this.adjustPower = adjustPower;
+			return this;
+		}
+
+		public MyConfig build() {
+			return new MyConfig(this);
+		}
+	}
+
+	/**
+	 * Create a Config builder.
+	 * 
+	 * @return a {@link Builder}
+	 */
+	public static Builder create() {
+		return new Builder();
+	}
+
+	private final Builder builder;
+
+	private MyConfig(Builder builder) {
+		super(Config.class, builder.id);
+		this.builder = builder;
+	}
+
+	@Override
+	public String ess_id() {
+		return this.builder.essId;
+	}
+
+	@Override
+	public int minPower() {
+		return this.builder.minPower;
+	}
+
+	@Override
+	public int maxPower() {
+		return this.builder.maxPower;
+	}
+
+	@Override
+	public int adjustPower() {
+		return this.builder.adjustPower;
+	}
+
+	@Override
+	public String ess_target() {
+		return ConfigUtils.generateReferenceTargetFilter(this.id(), this.ess_id());
+	}
+}
