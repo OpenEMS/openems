a85faa9792ef3bb86b5177db4451ec649e911dde
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2021-04-09 10:26:03 +0200

    WIP flat widget

--- ui/src/app/edge/live/abstractFlatWidget.component.ts
+++ ui/src/app/edge/live/abstractFlatWidget.component.ts
@@ -1,16 +1,41 @@
+import { Inject, Injectable, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { ChannelAddress, Edge, EdgeConfig, Service, Websocket } from "src/app/shared/shared";
 
-export abstract class AbstractFlatWidgetComponent {
+@Injectable()
+export abstract class AbstractFlatWidgetComponent implements OnInit {
 
     public edge: Edge = null;
     public config: EdgeConfig = null;
     public service: Service = null;
     public route: ActivatedRoute = null;
 
-    constructor(
-        public websocket: Websocket
-    ) { }
+    @Inject(Websocket) private websocket: Websocket;
+
+    public ngOnInit() {
+        this.service.setCurrentComponent('', this.route).then(edge => {
+            this.edge = edge;
+            this.service.getConfig().then(config => {
+                this.getChannelAddressess(edge, config);
+                // TODO subscribe
+
+
+            })
+        });
+        this.edge.currentData.pipe(takeUntil(this.stopOnDestroy)).subscribe(currentData => {
+            this.onCurrentData(currentData);
+        }
+        // currentData subscribe
+    }
+
+    // unsubscribe
+
+    ngOnDestroy() {
+        this.stopOnDestroy.next();
+        this.stopOnDestroy.complete();
+    }
+
+    protected abstract getChannelAddressess(edge: Edge, config: EdgeConfig);
 
     protected subscribeOnChannels(selector: string, channelAddress: ChannelAddress[]) {
         this.service.setCurrentComponent('', this.route).then(edge => {
--- ui/src/app/edge/live/fixactivepower/fixactivepower.component.ts
+++ ui/src/app/edge/live/fixactivepower/fixactivepower.component.ts
@@ -4,7 +4,6 @@ import { ModalController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { UUID } from 'angular2-uuid';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 import { ChannelAddress, Edge, EdgeConfig, Service, Websocket } from '../../../shared/shared';
 import { AbstractFlatWidgetComponent } from '../abstractFlatWidget.component';
 import { FixActivePowerModalComponent } from './modal/modal.component';
@@ -15,7 +14,14 @@ import { FixActivePowerModalComponent } from './modal/modal.component';
 })
 export class FixActivePowerComponent extends AbstractFlatWidgetComponent {
 
-  @Input() private componentId: string | null = null;
+  private powerChannel: ChannelAddress;
+  private modeChannel: ChannelAddress;
+
+  @Input()
+  set componentId(componentId: string) {
+    this.powerChannel = new ChannelAddress(this.componentId, "_PropertyPower");
+    this.modeChannel = new ChannelAddress(this.componentId, "_PropertyMode");
+  }
 
   private stopOnDestroy: Subject<void> = new Subject<void>();
   public edge: Edge = null;
@@ -32,48 +38,47 @@ export class FixActivePowerComponent extends AbstractFlatWidgetComponent {
     public modalCtrl: ModalController,
     public service: Service,
     public viewContainerRef: ViewContainerRef,
-    public websocket: Websocket
-
   ) {
-    super(websocket)
+    super();
   }
 
-  ngOnInit() {
-    this.service.setCurrentComponent('', this.route).then(edge => {
-      this.edge = edge;
-      this.service.getConfig().then(config => {
-        this.component = config.components[this.componentId];
-        let power = this.componentId + '/_PropertyPower';
-        let mode = this.componentId + '/_PropertyMode';
-        this.channels.push(ChannelAddress.fromString(power), ChannelAddress.fromString(mode));
-        this.subscribeOnChannels(this.randomselector, this.channels);
-
-        this.edge.currentData.pipe(takeUntil(this.stopOnDestroy)).subscribe(currentData => {
-          let channelPower = currentData.channel[power];
-          let channelMode = currentData.channel[mode]
-
-          if (channelPower >= 0) {
-            this.chargeState = this.translate.instant('General.dischargePower');
-            this.chargeStateValue = this.component.properties.power
-          } else if (channelPower < 0) {
-            this.chargeState = this.translate.instant('General.chargePower');
-            this.chargeStateValue = this.component.properties.power * -1;
-          }
-          if (channelMode == 'MANUAL_ON') {
-            this.state = this.translate.instant('General.on');
-          } else if (channelMode == 'MANUAL_OFF') {
-            this.state = this.translate.instant('General.off');
-          } else {
-            this.state = '-'
-          }
-        });
-      })
-    })
+  protected getChannelAddressess(edge: Edge, config: EdgeConfig) {
+    this.channels.push(this.powerChannel, this.modeChannel);
   }
-  ngOnDestroy() {
-    this.stopOnDestroy.next();
-    this.stopOnDestroy.complete();
-  }
+
+  // ngOnInit() {
+  //   this.service.setCurrentComponent('', this.route).then(edge => {
+  //     this.edge = edge;
+  //     this.service.getConfig().then(config => {
+  //       this.component = config.components[this.componentId];
+  //       let power = this.componentId + '/_PropertyPower';
+  //       let mode = this.componentId + '/_PropertyMode';
+  //       this.channels.push(ChannelAddress.fromString(power), ChannelAddress.fromString(mode));
+  //       this.subscribeOnChannels(this.randomselector, this.channels);
+
+  //       this.edge.currentData.pipe(takeUntil(this.stopOnDestroy)).subscribe(currentData => {
+  //         let channelPower = currentData.channel[power];
+  //         let channelMode = currentData.channel[mode]
+
+  //         if (channelPower >= 0) {
+  //           this.chargeState = this.translate.instant('General.dischargePower');
+  //           this.chargeStateValue = this.component.properties.power
+  //         } else if (channelPower < 0) {
+  //           this.chargeState = this.translate.instant('General.chargePower');
+  //           this.chargeStateValue = this.component.properties.power * -1;
+  //         }
+  //         if (channelMode == 'MANUAL_ON') {
+  //           this.state = this.translate.instant('General.on');
+  //         } else if (channelMode == 'MANUAL_OFF') {
+  //           this.state = this.translate.instant('General.off');
+  //         } else {
+  //           this.state = '-'
+  //         }
+  //       });
+  //     })
+  //   })
+  // }
+
 
   async presentModal() {
     const modal = await this.modalCtrl.create({
