1ae22a1797d347d69f2c50c8fe25252c9359f708
dadariga <73471197+dadariga@users.noreply.github.com>
2021-04-12 14:42:40 +0200

    no message

--- ui/src/app/edge/live/live.module.ts
+++ ui/src/app/edge/live/live.module.ts
@@ -23,7 +23,7 @@ import { EvcsPopoverComponent } from './evcs/modal/popover/popover.page';
 import { EvcsClusterComponent } from './evcsCluster/evcsCluster.component';
 import { EvcsChart } from './evcsCluster/modal/evcs-chart/evcs.chart';
 import { ModalComponentEvcsCluster } from './evcsCluster/modal/evcsCluster-modal.page';
-import { FixDigitalOutputComponent } from './Controller_IO_FixDigitalOutput/Controller_IO_FixDigitalOutput';
+import { FixDigitalOutputComponent } from './Controller_Io_FixDigitalOutput/Controller_Io_FixDigitalOutput';
 import { FixDigitalOutputModalComponent } from './Controller_IO_FixDigitalOutput/modal/modal.component';
 import { FlatWidgetLine } from './flat/flat-widget-line/flat-widget-line';
 import { FlatWidgetComponent } from './flat/flat-widget.component';
