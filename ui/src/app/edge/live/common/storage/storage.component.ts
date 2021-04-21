52c1dc53cb491eae25fdc940c2041eca65799fca
lukasrgr <73471197+lukasrgr@users.noreply.github.com>
2021-04-21 13:32:53 +0200

    > refactored converters > commenting > additional subscribes for modal

--- ui/src/app/edge/live/common/storage/storage.component.ts
+++ ui/src/app/edge/live/common/storage/storage.component.ts
@@ -13,43 +13,44 @@ export class StorageComponent extends AbstractFlatWidget {
     public essComponents: EdgeConfig.Component[] = [];
     public chargerComponents: EdgeConfig.Component[] = [];
     public storageItem: string = null;
+    public isAsymmetric: boolean;
 
     protected getChannelAddresses() {
-        let channelAddresses: ChannelAddress[] = [];
-        for (let component of this.essComponents) {
-            channelAddresses = [new ChannelAddress(component.id, 'Soc'),
-            new ChannelAddress(component.id, 'ActivePower'),
-            new ChannelAddress(component.id, 'Capacity')];
+        let channelAddresses: ChannelAddress[] = [new ChannelAddress('_sum', 'EssSoc')];
+        this.chargerComponents = this.config.getComponentsImplementingNature("io.openems.edge.ess.dccharger.api.EssDcCharger").filter(component => component.isEnabled);
+        for (let component of this.chargerComponents) {
+            channelAddresses.push(
+                new ChannelAddress(component.id, 'ActualPower'),
+            )
         }
-        channelAddresses.push(new ChannelAddress('_sum', 'EssSoc'));
+        this.essComponents = this.config.getComponentsImplementingNature("io.openems.edge.ess.api.SymmetricEss").filter(component => !component.factoryId.includes("Ess.Cluster") && component.isEnabled);
+        for (let component of this.essComponents) {
+            this.isAsymmetric = this.config.factories[component.factoryId].natureIds.includes("io.openems.edge.ess.api.AsymmetricEss")
+            channelAddresses.push(
+                new ChannelAddress(component.id, 'Capacity'),
+            );
+            if (this.isAsymmetric) {
+                channelAddresses.push(
+                    new ChannelAddress(component.id, 'ActivePowerL1'),
+                    new ChannelAddress(component.id, 'ActivePowerL2'),
+                    new ChannelAddress(component.id, 'ActivePowerL3')
+                );
+            }
+        }
+        channelAddresses.push(
+            new ChannelAddress('_sum', 'EssSoc'),
+            new ChannelAddress('_sum', 'EssActivePower'),
+            // channels for modal component, subscribe here for better UX
+            new ChannelAddress('_sum', 'EssActivePowerL1'),
+            new ChannelAddress('_sum', 'EssActivePowerL2'),
+            new ChannelAddress('_sum', 'EssActivePowerL3'),
+            new ChannelAddress('_sum', 'EssCapacity'),
+        )
         return channelAddresses
     }
-    public convertCharge = (value: any): string => {
-        let thisValue = (value / 1000 * -1).toFixed(1);
-        if (value <= 0) {
-            if (thisValue.endsWith('0')) {
-                return parseInt(thisValue).toString() + ' kW';
-            } else {
-                return thisValue + ' kW';
-            }
-        } else {
-            return '-'
-        }
-    }
-    public convertDischarge = (value: any): string => {
-        let thisValue = (value / 1000).toFixed(1);
-        if (value > 0) {
-            if (thisValue.endsWith('0')) {
-                return parseInt(thisValue).toString() + ' kW';
-            } else {
-                return thisValue + ' kW';
-            }
-        } else {
-            return '-'
-        }
-    }
+
     protected onCurrentData(currentData: CurrentData) {
-        this.essComponents = this.config.getComponentsImplementingNature("io.openems.edge.ess.api.SymmetricEss").filter(component => !component.factoryId.includes("Ess.Cluster") && component.isEnabled);
+
         // Check total State_of_Charge for dynamical icon in widget-header
         let soc = currentData.allComponents['_sum' + '/EssSoc'];
         if (soc < 20) {
@@ -66,7 +67,40 @@ export class StorageComponent extends AbstractFlatWidget {
             this.storageItem = 'assets/img/storage_100.png'
         }
     }
-
+    /**
+      * 
+      * @param value takes @Input value or channelAddress for chargePower
+      * @returns only positive value
+      */
+    public convertChargeToOnlyPositive = (value: any): string => {
+        let thisValue = (value / 1000 * -1).toFixed(1);
+        if (value <= 0) {
+            if (thisValue.endsWith('0')) {
+                return parseInt(thisValue).toString() + ' kW';
+            } else {
+                return thisValue + ' kW';
+            }
+        } else {
+            return '-'
+        }
+    }
+    /**
+      * 
+      * @param value takes @Input value or channelAddress for dischargePower
+      * @returns only positive value
+      */
+    public convertDischargeToOnlyPositive = (value: any): string => {
+        let thisValue = (value / 1000).toFixed(1);
+        if (value > 0) {
+            if (thisValue.endsWith('0')) {
+                return parseInt(thisValue).toString() + ' kW';
+            } else {
+                return thisValue + ' kW';
+            }
+        } else {
+            return '-'
+        }
+    }
     async presentModal() {
         const modal = await this.modalController.create({
             component: StorageModalComponent,
