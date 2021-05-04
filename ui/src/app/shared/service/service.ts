431afed37d295fed04765887bc4d05f514f45b7f
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2021-05-04 15:07:15 +0200

    Remove BYD advert widget

--- ui/src/app/shared/service/service.ts
+++ ui/src/app/shared/service/service.ts
@@ -403,17 +403,6 @@ export class Service implements ErrorHandler {
   }
 
   /**
-   * Checks if this Edge is allowed to show BYD Battery Box Firmware Update Widget
-   */
-  public isBatteryBoxAllowed(edge: Edge): boolean {
-    if (['Pro Hybrid GW'].includes(edge.producttype)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  /**
    * checks if fems is allowed to show partner widget
    */
   // TODO: encapsulate data for different partners
