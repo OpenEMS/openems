e15d0d34f1066f1ada13ca7072b441d961427e27
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2021-05-07 17:44:11 +0200

    Add static import for static converter method

--- ui/src/app/edge/live/common/grid/grid.component.ts
+++ ui/src/app/edge/live/common/grid/grid.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { GridModalComponent } from './modal/modal.component';
 import { AbstractFlatWidget } from '../../flat/abstract-flat-widget';
-import { ChannelAddress, CurrentData } from 'src/app/shared/shared';
+import { ChannelAddress, CurrentData, Utils } from 'src/app/shared/shared';
 
 @Component({
   selector: 'grid',
   templateUrl: './grid.component.html'
 })
 export class GridComponent extends AbstractFlatWidget {
+
   private static readonly GRID_ACTIVE_POWER: ChannelAddress = new ChannelAddress('_sum', 'GridActivePower');
   private static readonly GRID_MODE: ChannelAddress = new ChannelAddress('_sum', 'GridMode')
+
+  public readonly CONVERT_WATT_TO_KILOWATT = Utils.CONVERT_WATT_TO_KILOWATT;
+
   public gridBuyAdvancedChannel: number;
   public gridSellAdvancedChannel: number;
   public gridMode: number;
