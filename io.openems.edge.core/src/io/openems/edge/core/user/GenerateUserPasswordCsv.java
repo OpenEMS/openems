4b364aa4bb5ce8332595a8104c60d25b04b77570
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2021-05-07 13:28:43 +0200

    Add Tool to generate password CSV file
    (cherry picked from commit 06c6e20225f085e65377eb50259ae4d17bfa7bc0)

---
+++ io.openems.edge.core/src/io/openems/edge/core/user/GenerateUserPasswordCsv.java
@@ -0,0 +1,33 @@
+package io.openems.edge.core.user;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+import com.google.common.base.Splitter;
+
+import io.openems.common.session.Role;
+import io.openems.edge.common.host.DummyHost;
+
+public class GenerateUserPasswordCsv {
+
+	public static void main(String[] args) throws IOException {
+		final String username = "admin";
+		final Role role = Role.getRole(username);
+		DummyHost host = new DummyHost();
+
+		BufferedWriter writer = new BufferedWriter(new FileWriter("passwords-" + username + ".csv"));
+		writer.write("FEMS-Nummer;Passwort\n");
+		for (int i = 1; i < 4000; i++) {
+			String hostname = "fems" + i;
+			host.withHostname(hostname);
+			String password = UserServiceUtils.generatePassword(host, username, role);
+			writer.write(hostname + ";"
+					+ Splitter.fixedLength(4).splitToStream(password).collect(Collectors.joining(" ")) + "\n");
+		}
+		writer.close();
+
+	}
+
+}
