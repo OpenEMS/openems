68f43222dd805f49b4b8b98db27f3059b8731234
Lukas Rieger <lukas.rieger@fenecon.de>
2021-05-19 11:48:22 +0200

    > deleted methods from component, moved logic in onCurrentData() > refactored modal

--- ui/src/app/edge/live/common/consumption/consumption.component.ts
+++ ui/src/app/edge/live/common/consumption/consumption.component.ts
@@ -11,15 +11,19 @@ export class ConsumptionComponent extends AbstractFlatWidget {
 
   public evcsComponents: EdgeConfig.Component[] | null = null;
   public consumptionMeterComponents: EdgeConfig.Component[] = null;
-  public sumActivePower: number;
-  public evcsChargePower: any[] = [];
+  public sumActivePower: number = 0;
+  public evcsSumOfChargePower: number;
+  public consumptionMetersSumOfActivePower: number;
   public otherPower: number;
   public readonly CONVERT_WATT_TO_KILOWATT = Utils.CONVERT_WATT_TO_KILOWATT;
 
   protected getChannelAddresses() {
 
-    // TODO should be moved to modal
     let channelAddresses: ChannelAddress[] = [
+      new ChannelAddress('_sum', 'ConsumptionActivePower'),
+      new ChannelAddress('_sum', 'ConsumptionChargePower'),
+
+      // TODO should be moved to modal
       new ChannelAddress('_sum', 'ConsumptionActivePowerL1'),
       new ChannelAddress('_sum', 'ConsumptionActivePowerL2'),
       new ChannelAddress('_sum', 'ConsumptionActivePowerL3')
@@ -34,7 +38,10 @@ export class ConsumptionComponent extends AbstractFlatWidget {
     }
 
     // Get EVCSs
-    this.evcsComponents = this.config.getComponentsImplementingNature("io.openems.edge.evcs.api.Evcs").filter(component => !(component.factoryId == 'Evcs.Cluster.SelfConsumption') && !(component.factoryId == 'Evcs.Cluster.PeakShaving') && !component.isEnabled == false);
+    this.evcsComponents = this.config.getComponentsImplementingNature("io.openems.edge.evcs.api.Evcs")
+      .filter(component => !(component.factoryId == 'Evcs.Cluster.SelfConsumption') &&
+        !(component.factoryId == 'Evcs.Cluster.PeakShaving') && !component.isEnabled == false);
+
     for (let component of this.evcsComponents) {
       channelAddresses.push(
         new ChannelAddress(component.id, 'ChargePower'),
@@ -45,39 +52,25 @@ export class ConsumptionComponent extends AbstractFlatWidget {
 
   protected onCurrentData(currentData: CurrentData) {
 
+    this.evcsSumOfChargePower = 0;
+    this.consumptionMetersSumOfActivePower = 0;
+    this.sumActivePower = currentData.allComponents['_sum/ConsumptionActivePower'];
+
     // Iterate over evcsComponents to get ChargePower for every component
     for (let component of this.evcsComponents) {
-      this.evcsChargePower[component.id] = currentData.allComponents[component.id + '/ChargePower'];
+      if (currentData.allComponents[component.id + '/ChargePower']) {
+        this.evcsSumOfChargePower += currentData.allComponents[component.id + '/ChargePower'];
+      }
     }
 
-    // Subscribe on summary
-    this.edge.currentData.subscribe(currentData => {
-      this.sumActivePower = currentData.summary.consumption.activePower;
-    })
-    this.otherPower = this.sumActivePower - this.getTotalOtherPower();
-  }
+    // Iterate over evcsComponents to get ChargePower for every component
+    for (let component of this.consumptionMeterComponents) {
+      if (currentData.allComponents[component.id + '/ActivePower']) {
+        this.consumptionMetersSumOfActivePower += currentData.allComponents[component.id + '/ActivePower'];
+      }
+    }
 
-  public getTotalOtherPower(): number {
-    return this.currentTotalChargingPower() + this.currentTotalConsumptionMeterPower();
-  }
-
-  private currentTotalChargingPower(): number {
-    return this.sumOfChannel(this.evcsComponents, "ChargePower");
-  }
-
-  private currentTotalConsumptionMeterPower(): number {
-    return this.sumOfChannel(this.consumptionMeterComponents, "ActivePower");
-  }
-
-  private sumOfChannel(components: EdgeConfig.Component[], channel: String): number {
-    let sum = 0;
-    components.forEach(component => {
-      let channelValue = this.edge.currentData.value.channel[component.id + "/" + channel];
-      if (channelValue != null) {
-        sum += channelValue;
-      };
-    });
-    return sum;
+    this.otherPower = this.sumActivePower - (this.evcsSumOfChargePower + this.consumptionMetersSumOfActivePower);
   }
 
   async presentModal() {
@@ -87,10 +80,9 @@ export class ConsumptionComponent extends AbstractFlatWidget {
         edge: this.edge,
         evcsComponents: this.evcsComponents,
         consumptionMeterComponents: this.consumptionMeterComponents,
-        currentTotalChargingPower: this.currentTotalChargingPower,
-        currentTotalConsumptionMeterPower: this.currentTotalConsumptionMeterPower,
-        sumOfChannel: this.sumOfChannel,
-        getTotalOtherPower: this.getTotalOtherPower,
+        currentTotalChargingPower: this.evcsSumOfChargePower,
+        currentTotalConsumptionMeterPower: this.consumptionMetersSumOfActivePower,
+        otherPower: this.otherPower,
       }
     });
     return await modal.present();
