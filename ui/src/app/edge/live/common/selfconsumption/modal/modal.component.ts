f57602689f46a31e47e779068a5f742c624ec03a
lukasrgr <73471197+lukasrgr@users.noreply.github.com>
2021-06-22 15:15:07 +0200

    UI: Flatwidget for selfconsumption (#1475)
    Co-authored-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>

--- ui/src/app/edge/live/common/selfconsumption/modal/modal.component.ts
+++ ui/src/app/edge/live/common/selfconsumption/modal/modal.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { Service } from '../../../../shared/shared';
+import { Service } from '../../../../../shared/shared';
 
 @Component({
     selector: 'selfconsumption-modal',
