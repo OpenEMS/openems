27af6c6592de9c4684d984cceebe366f33c3c214
Christian <christian.ott@mailbox.org>
2021-06-25 21:16:27 +0200

    Implement change requests from sebastianasen

--- io.openems.edge.evcs.goe.chargerhome/src/io/openems/edge/evcs/goe/chargerhome/Errors.java
+++ io.openems.edge.evcs.goe.chargerhome/src/io/openems/edge/evcs/goe/chargerhome/Errors.java
@@ -3,32 +3,32 @@ package io.openems.edge.evcs.goe.chargerhome;
 import io.openems.common.types.OptionsEnum;
 
 public enum Errors implements OptionsEnum {
-	UNDEFINED(0, "Undefined"), //
-	RCCB(1, "Residual current operated device Error"), //
-	PHASE(3, "Phase Error"), //
-	NO_GROUND(8, "No Ground"), //
-	INTERNAL(10, "Internal error"); //
-	
-	private final int value;
-	private final String name;
+  UNDEFINED(0, "Undefined"), //
+  RCCB(1, "Residual current operated device Error"), //
+  PHASE(3, "Phase Error"), //
+  NO_GROUND(8, "No Ground"), //
+  INTERNAL(10, "Internal error"); //
+  
+  private final int value;
+  private final String name;
 
-	private Errors(int value, String name) {
-		this.value = value;
-		this.name = name;
-	}
+  private Errors(int value, String name) {
+    this.value = value;
+    this.name = name;
+  }
 
-	@Override
-	public int getValue() {
-		return value;
-	}
+  @Override
+  public int getValue() {
+    return value;
+  }
 
-	@Override
-	public String getName() {
-		return name;
-	}
+  @Override
+  public String getName() {
+    return name;
+  }
 
-	@Override
-	public OptionsEnum getUndefined() {
-		return UNDEFINED;
-	}
+  @Override
+  public OptionsEnum getUndefined() {
+    return UNDEFINED;
+  }
 }
--- io.openems.edge.evcs.goe.chargerhome/src/io/openems/edge/evcs/goe/chargerhome/Report.java
+++ io.openems.edge.evcs.goe.chargerhome/src/io/openems/edge/evcs/goe/chargerhome/Report.java
@@ -1,19 +1,19 @@
 package io.openems.edge.evcs.goe.chargerhome;
 
 public enum Report {
-	REPORT1(200), REPORT2(50), REPORT3(10);
+  REPORT1(200), REPORT2(50), REPORT3(10);
 
-	private int requestSeconds;
+  private int requestSeconds;
 
-	private Report(int requestSeconds) {
-		this.requestSeconds = requestSeconds;
-	}
+  private Report(int requestSeconds) {
+    this.requestSeconds = requestSeconds;
+  }
 
-	public int getRequestSeconds() {
-		return requestSeconds;
-	}
+  public int getRequestSeconds() {
+    return requestSeconds;
+  }
 
-	public void setRequestSeconds(int requestSeconds) {
-		this.requestSeconds = requestSeconds;
-	}
+  public void setRequestSeconds(int requestSeconds) {
+    this.requestSeconds = requestSeconds;
+  }
 }
