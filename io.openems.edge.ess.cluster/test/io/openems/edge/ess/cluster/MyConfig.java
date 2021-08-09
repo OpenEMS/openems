7fa18b744ceaaf8a4450e72414e047ea4a764046
Stefan Feilmeier <mail@stefanfeilmeier.de>
2021-08-09 23:25:07 +0200

    Refactor Ess-Cluster (#1590)
    - Refactor implementation
    - Use ChannelManager for immediate updates to channel values
    - Calculate weighted SoC
    - Add JUnit tests

---
+++ io.openems.edge.ess.cluster/test/io/openems/edge/ess/cluster/MyConfig.java
@@ -0,0 +1,52 @@
+package io.openems.edge.ess.cluster;
+
+import io.openems.edge.common.test.AbstractComponentConfig;
+
+@SuppressWarnings("all")
+public class MyConfig extends AbstractComponentConfig implements Config {
+
+	protected static class Builder {
+		private String id;
+		public boolean isOn;
+		public String outputChannelAddress;
+		public String[] essIds;
+
+		private Builder() {
+		}
+
+		public Builder setId(String id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder setEssIds(String... essIds) {
+			this.essIds = essIds;
+			return this;
+		}
+
+		public MyConfig build() {
+			return new MyConfig(this);
+		}
+	}
+
+	/**
+	 * Create a Config builder.
+	 * 
+	 * @return a {@link Builder}
+	 */
+	public static Builder create() {
+		return new Builder();
+	}
+
+	private final Builder builder;
+
+	private MyConfig(Builder builder) {
+		super(Config.class, builder.id);
+		this.builder = builder;
+	}
+
+	@Override
+	public String[] ess_ids() {
+		return this.builder.essIds;
+	}
+}
