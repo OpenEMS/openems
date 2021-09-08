a0c51b312c9f0e948412cc20c63cc163f6232bda
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2021-09-08 21:00:40 +0200

    Generic-ESS is now a Hybrid-ESS

--- io.openems.edge.ess.generic/src/io/openems/edge/ess/generic/symmetric/GenericManagedSymmetricEssImpl.java
+++ io.openems.edge.ess.generic/src/io/openems/edge/ess/generic/symmetric/GenericManagedSymmetricEssImpl.java
@@ -21,6 +21,7 @@ import org.slf4j.LoggerFactory;
 
 import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
 import io.openems.edge.battery.api.Battery;
+import io.openems.edge.batteryinverter.api.HybridManagedSymmetricBatteryInverter;
 import io.openems.edge.batteryinverter.api.ManagedSymmetricBatteryInverter;
 import io.openems.edge.common.component.ComponentManager;
 import io.openems.edge.common.component.OpenemsComponent;
@@ -187,6 +188,15 @@ public class GenericManagedSymmetricEssImpl
 	@Override
 	protected ManagedSymmetricBatteryInverter getBatteryInverter() {
 		return this.batteryInverter;
+	}
+
+	@Override
+	public Integer getSurplusPower() {
+		if (this.batteryInverter instanceof HybridManagedSymmetricBatteryInverter) {
+			return ((HybridManagedSymmetricBatteryInverter) this.batteryInverter).getSurplusPower();
+		} else {
+			return null;
+		}
 	}
 
 	@Override
