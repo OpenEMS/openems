cc34b7b3344c07f6eb5901adc41163754c080122
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2021-09-28 22:00:29 +0200

    JSON/REST-Api: improve permission handling (#138)
    and add JUnit tests
    
    Reviewed-on: https://git.intranet.fenecon.de/FENECON/fems/pulls/138
    Co-authored-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Co-committed-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>

--- io.openems.edge.common/src/io/openems/edge/common/channel/BooleanReadChannel.java
+++ io.openems.edge.common/src/io/openems/edge/common/channel/BooleanReadChannel.java
@@ -8,12 +8,7 @@ import io.openems.edge.common.component.OpenemsComponent;
 public class BooleanReadChannel extends AbstractReadChannel<OpenemsTypeDoc<Boolean>, Boolean> {
 
 	protected BooleanReadChannel(OpenemsComponent component, ChannelId channelId, BooleanDoc channelDoc) {
-		this(component, channelId, channelDoc, null);
-	}
-
-	protected BooleanReadChannel(OpenemsComponent component, ChannelId channelId, BooleanDoc channelDoc,
-			Boolean initialValue) {
-		super(OpenemsType.BOOLEAN, component, channelId, channelDoc, initialValue);
+		super(OpenemsType.BOOLEAN, component, channelId, channelDoc);
 	}
 
 }
