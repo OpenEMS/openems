1a9c38bb13f0b7155c0a7de7410f4d72303de3e7
Maximilian Lang <maximilian.lang@fenecon.de>
2021-09-28 23:11:18 +0200

    Improve Emergency Capacity Reserve Controller (#137)
    Co-authored-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Reviewed-on: https://git.intranet.fenecon.de/FENECON/fems/pulls/137
    Reviewed-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Co-authored-by: Maximilian Lang <maximilian.lang@fenecon.de>
    Co-committed-by: Maximilian Lang <maximilian.lang@fenecon.de>

--- io.openems.edge.controller.ess.emergencycapacityreserve/src/io/openems/edge/controller/ess/emergencycapacityreserve/EmergencyCapacityReserveImpl.java
+++ io.openems.edge.controller.ess.emergencycapacityreserve/src/io/openems/edge/controller/ess/emergencycapacityreserve/EmergencyCapacityReserveImpl.java
@@ -1,5 +1,7 @@
 package io.openems.edge.controller.ess.emergencycapacityreserve;
 
+import java.util.OptionalInt;
+
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
@@ -12,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
+import io.openems.edge.common.channel.IntegerReadChannel;
 import io.openems.edge.common.channel.value.Value;
 import io.openems.edge.common.component.AbstractOpenemsComponent;
 import io.openems.edge.common.component.ComponentManager;
@@ -92,17 +95,21 @@ public class EmergencyCapacityReserveImpl extends AbstractOpenemsComponent
 	public void run() throws OpenemsNamedException {
 		Context context = this.handleStateMachine();
 
-		if(this.config.isReserveSocEnabled()) {
-			Integer activerPowerConstraint = rampFilter.getFilteredValueAsInteger(context.getTargetPower(),
+		if (this.config.isReserveSocEnabled()) {
+			Integer activePowerConstraint = rampFilter.getFilteredValueAsInteger(context.getTargetPower(),
 					context.getRampPower());
 
-			if (context.maxApparentPower != activerPowerConstraint) {
-				// Set constraint only if we did not reach the max apparent power
-				ess.setActivePowerLessOrEquals(activerPowerConstraint);
+			if (activePowerConstraint != null && context.maxApparentPower > activePowerConstraint) {
+				// Set constraint did not reach max apparent power
+				ess.setActivePowerLessOrEquals(activePowerConstraint);
+				this._setDebugSetActivePowerLessOrEquals(activePowerConstraint);
+			} else {
+				// Set no constraint max apparent power reached
+				ess.setActivePowerLessOrEquals(null);
+				this._setDebugSetActivePowerLessOrEquals(null);
 			}
 
 			// set debug channels
-			this._setDebugSetActivePowerLessOrEquals(activerPowerConstraint);
 			this._setDebugTargetPower(context.getTargetPower());
 			this._setDebugRampPower(context.getRampPower());
 		}
@@ -137,14 +144,26 @@ public class EmergencyCapacityReserveImpl extends AbstractOpenemsComponent
 	private Context handleStateMachine() {
 		this._setStateMachine(this.stateMachine.getCurrentState());
 
-		// check SoC is defined
 		Value<Integer> soc = ess.getSoc();
 		Value<Integer> maxApparentPower = ess.getMaxApparentPower();
-		if (!soc.isDefined() || !maxApparentPower.isDefined()) {
+
+		Integer socToUse = null;
+		if (!soc.isDefined()) {
+			// use last valid soc value
+			OptionalInt lastSocValue = this.getLastValidSoc(ess.getSocChannel());
+			if (lastSocValue.isPresent()) {
+				socToUse = lastSocValue.getAsInt();
+			}
+		} else {
+			// use current soc value
+			socToUse = soc.get();
+		}
+
+		if (socToUse == null || !maxApparentPower.isDefined()) {
 			this.stateMachine.forceNextState(State.NO_LIMIT);
 		}
 
-		Context context = new Context(this, this.sum, maxApparentPower.get(), soc.get(), this.config.reserveSoc());
+		Context context = new Context(this, this.sum, maxApparentPower.get(), socToUse, this.config.reserveSoc());
 		try {
 			this.stateMachine.run(context);
 
@@ -156,6 +175,22 @@ public class EmergencyCapacityReserveImpl extends AbstractOpenemsComponent
 		}
 
 		return context;
+	}
+
+	/**
+	 * Get last defined value of an {@link IntegerReadChannel} as an
+	 * {@link OptionalInt}.
+	 * 
+	 * @param channel {@link IntegerReadChannel} to get values
+	 * @return Last defined value from given {@link IntegerReadChannel}
+	 */
+	private OptionalInt getLastValidSoc(IntegerReadChannel channel) {
+		// get first defined value
+		return channel.getPastValues().values() //
+				.stream() //
+				.filter(value -> value.isDefined()) //
+				.mapToInt(value -> value.get()) //
+				.findFirst();
 	}
 
 }
