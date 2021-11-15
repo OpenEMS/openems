b798b265e16182f883347f6018676d7310d4b50b
Sagar Venu <sagar.venu@fenecon.de>
2021-11-15 23:38:56 +0100

    Add Time-Of-Use Tariff Tibber (#173)
    Implemented Tibber API to retrieve the prices for Time-Of-Use Tariff API service.
    Coorent readme doc for corrently.
    
    Co-authored-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Reviewed-on: https://git.intranet.fenecon.de/FENECON/fems/pulls/173
    Reviewed-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Co-authored-by: Sagar Venu <sagar.venu@fenecon.de>
    Co-committed-by: Sagar Venu <sagar.venu@fenecon.de>

---
+++ io.openems.edge.timeofusetariff.tibber/test/io/openems/edge/timeofusetariff/tibber/MyConfig.java
@@ -0,0 +1,51 @@
+package io.openems.edge.timeofusetariff.tibber;
+
+import io.openems.edge.common.test.AbstractComponentConfig;
+
+@SuppressWarnings("all")
+public class MyConfig extends AbstractComponentConfig implements Config {
+
+	public static class Builder {
+		private String id;
+		public String accessToken;
+
+		private Builder() {
+		}
+
+		public Builder setId(String id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder setAccessToken(String accessToken) {
+			this.accessToken = accessToken;
+			return this;
+		}
+
+		public MyConfig build() {
+			return new MyConfig(this);
+		}
+	}
+
+	/**
+	 * Create a Config builder.
+	 * 
+	 * @return a {@link Builder}
+	 */
+	public static Builder create() {
+		return new Builder();
+	}
+
+	private final Builder builder;
+
+	private MyConfig(Builder builder) {
+		super(Config.class, builder.id);
+		this.builder = builder;
+	}
+
+	@Override
+	public String accessToken() {
+		return this.builder.accessToken;
+	}
+
+}
