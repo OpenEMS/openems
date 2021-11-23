f5765744716bb6c9a13503b2e9d2f6f2e74876f5
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2021-11-23 20:04:26 +0100

    Update Changelog for 2021.20.2

--- ui/src/app/changelog/changelog.component.ts
+++ ui/src/app/changelog/changelog.component.ts
@@ -35,6 +35,12 @@ export class ChangelogComponent {
     changes: Array<string | { roleIsAtLeast: Role, change: string }>
   }[] = [
       {
+        version: '2021.20.2',
+        changes: [
+          Changelog.UI,
+        ]
+      },
+      {
         version: '2021.20.1',
         changes: [
           Changelog.openems('2021.20.0'),
