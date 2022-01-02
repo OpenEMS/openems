88cf83512674d5bcad832da145ceaa49d3abc23f
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2022-01-02 23:05:24 +0100

    Update io.openems.backend.metadata.odoo

--- io.openems.backend.metadata.odoo/src/io/openems/backend/metadata/odoo/postgres/InitializeEdgesWorker.java
+++ io.openems.backend.metadata.odoo/src/io/openems/backend/metadata/odoo/postgres/InitializeEdgesWorker.java
@@ -2,6 +2,7 @@ package io.openems.backend.metadata.odoo.postgres;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
