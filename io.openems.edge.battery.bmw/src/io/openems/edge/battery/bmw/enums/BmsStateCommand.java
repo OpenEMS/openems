841ec51e1e8c9a5fabfe3a55cf7f8b6b3c552a37
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2022-01-08 22:43:30 +0100

    Update io.openems.edge.battery.bmw

--- io.openems.edge.battery.bmw/src/io/openems/edge/battery/bmw/enums/BmsStateCommand.java
+++ io.openems.edge.battery.bmw/src/io/openems/edge/battery/bmw/enums/BmsStateCommand.java
@@ -3,14 +3,13 @@ package io.openems.edge.battery.bmw.enums;
 import io.openems.common.types.OptionsEnum;
 
 public enum BmsStateCommand implements OptionsEnum {
-	UNDEFINED(-1, "Undefined"), //	
-	ENABLE_RESERVED_BATTERY(0, "Enable aux power supply"),
-	WAKE_UP(1, "Wake up from stop"),
-	CLOSE_CONTACTOR(2, "Close Contactor"),
-	CLOSE_PRECHARGE(3, "Close Precharge"),
-	CLEAR_ERROR(14, "Clear BMS Error"),
-	REEST_BMS(15, "Reset BMS")
-	;
+	UNDEFINED(-1, "Undefined"), //
+	ENABLE_RESERVED_BATTERY(0, "Enable aux power supply"), //
+	WAKE_UP(1, "Wake up from stop"), //
+	CLOSE_CONTACTOR(2, "Close Contactor"), //
+	CLOSE_PRECHARGE(3, "Close Precharge"), //
+	CLEAR_ERROR(14, "Clear BMS Error"), //
+	RESET_BMS(15, "Reset BMS");
 
 	private final int value;
 	private final String name;
@@ -22,12 +21,12 @@ public enum BmsStateCommand implements OptionsEnum {
 
 	@Override
 	public int getValue() {
-		return value;
+		return this.value;
 	}
 
 	@Override
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 	@Override
