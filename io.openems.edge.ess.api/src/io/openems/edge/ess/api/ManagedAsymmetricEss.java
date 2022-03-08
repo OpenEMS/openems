0a9046980ff9d752afb284f2ca9992d2c4f20d70
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2022-03-08 06:33:49 +0100

    Update io.openems.edge.ess.api

--- io.openems.edge.ess.api/src/io/openems/edge/ess/api/ManagedAsymmetricEss.java
+++ io.openems.edge.ess.api/src/io/openems/edge/ess/api/ManagedAsymmetricEss.java
@@ -24,7 +24,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
 		/**
 		 * Sets a fixed Active Power on L1.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -38,7 +38,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 				.onInit(new PowerConstraint("SetActivePowerL1Equals", Phase.L1, Pwr.ACTIVE, Relationship.EQUALS))), //
 		/**
 		 * Sets a fixed Active Power on L2.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -52,7 +52,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 				.onInit(new PowerConstraint("SetActivePowerL2Equals", Phase.L2, Pwr.ACTIVE, Relationship.EQUALS))), //
 		/**
 		 * Sets a fixed Active Power on L3.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -66,7 +66,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 				.onInit(new PowerConstraint("SetActivePowerL3Equals", Phase.L3, Pwr.ACTIVE, Relationship.EQUALS))), //
 		/**
 		 * Sets a fixed Reactive Power on L1.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -80,7 +80,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 				.onInit(new PowerConstraint("SetReactivePowerL1Equals", Phase.L1, Pwr.REACTIVE, Relationship.EQUALS))), //
 		/**
 		 * Sets a fixed Reactive Power on L2.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -94,7 +94,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 				.onInit(new PowerConstraint("SetReactivePowerL2Equals", Phase.L2, Pwr.REACTIVE, Relationship.EQUALS))), //
 		/**
 		 * Sets a fixed Reactive Power on L3.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -108,7 +108,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 				.onInit(new PowerConstraint("SetReactivePowerL2Equals", Phase.L3, Pwr.REACTIVE, Relationship.EQUALS))), //
 		/**
 		 * Sets a fixed maximum Active Power on L1.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -123,7 +123,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 						Relationship.LESS_OR_EQUALS))), //
 		/**
 		 * Sets a fixed maximum Active Power on L2.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -138,7 +138,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 						Relationship.LESS_OR_EQUALS))), //
 		/**
 		 * Sets a fixed maximum Active Power on L3.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -153,7 +153,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 						Relationship.LESS_OR_EQUALS))), //
 		/**
 		 * Sets a fixed minimum Active Power on L1.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -168,7 +168,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 						Relationship.GREATER_OR_EQUALS))), //
 		/**
 		 * Sets a fixed minimum Active Power on L2.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -183,7 +183,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 						Relationship.GREATER_OR_EQUALS))), //
 		/**
 		 * Sets a fixed minimum Active Power on L3.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -198,7 +198,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 						Relationship.GREATER_OR_EQUALS))), //
 		/**
 		 * Sets a fixed maximum Reactive Power on L1.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -213,7 +213,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 						Relationship.LESS_OR_EQUALS))), //
 		/**
 		 * Sets a fixed maximum Reactive Power on L2.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -228,7 +228,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 						Relationship.LESS_OR_EQUALS))), //
 		/**
 		 * Sets a fixed maximum Reactive Power on L3.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -243,7 +243,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 						Relationship.LESS_OR_EQUALS))), //
 		/**
 		 * Sets a fixed minimum Reactive Power on L1.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -258,7 +258,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 						Relationship.GREATER_OR_EQUALS))), //
 		/**
 		 * Sets a fixed minimum Reactive Power on L2.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -273,7 +273,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 						Relationship.GREATER_OR_EQUALS))), //
 		/**
 		 * Sets a fixed minimum Reactive Power on L3.
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -288,7 +288,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 						Relationship.GREATER_OR_EQUALS))), //
 		/**
 		 * Holds settings of Active Power L1 for debugging
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -304,7 +304,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 				.persistencePriority(PersistencePriority.HIGH)), //
 		/**
 		 * Holds settings of Reactive Power for debugging
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -320,7 +320,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 				.persistencePriority(PersistencePriority.HIGH)), //
 		/**
 		 * Holds settings of Active Power L2 for debugging
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -336,7 +336,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 				.persistencePriority(PersistencePriority.HIGH)), //
 		/**
 		 * Holds settings of Reactive Power for debugging
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -352,7 +352,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 				.persistencePriority(PersistencePriority.HIGH)), //
 		/**
 		 * Holds settings of Active Power L1 for debugging
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -368,7 +368,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 				.persistencePriority(PersistencePriority.HIGH)), //
 		/**
 		 * Holds settings of Reactive Power for debugging
-		 * 
+		 *
 		 * <ul>
 		 * <li>Interface: Managed Asymmetric Ess
 		 * <li>Type: Integer
@@ -389,6 +389,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 			this.doc = doc;
 		}
 
+		@Override
 		public Doc doc() {
 			return this.doc;
 		}
@@ -419,15 +420,15 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 
 	@Override
 	default void applyPower(int activePower, int reactivePower) throws OpenemsNamedException {
-		int activePowerBy3 = activePower / 3;
-		int reactivePowerBy3 = reactivePower / 3;
+		var activePowerBy3 = activePower / 3;
+		var reactivePowerBy3 = reactivePower / 3;
 		this.applyPower(activePowerBy3, reactivePowerBy3, activePowerBy3, reactivePowerBy3, activePowerBy3,
 				reactivePowerBy3);
 	}
 
 	/**
 	 * Apply the calculated Power
-	 * 
+	 *
 	 * @param activePowerL1
 	 * @param activePowerL2
 	 * @param activePowerL3
@@ -451,7 +452,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	 * Sets an Active Power Equals setpoint on L1 in [W]. Negative values for
 	 * Charge; positive for Discharge. See
 	 * {@link ChannelId#SET_ACTIVE_POWER_L1_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -472,7 +473,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	 * Sets an Active Power Equals setpoint on L2 in [W]. Negative values for
 	 * Charge; positive for Discharge. See
 	 * {@link ChannelId#SET_ACTIVE_POWER_L2_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -493,7 +494,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	 * Sets a Reactive Power Equals setpoint on L3 in [W]. Negative values for
 	 * Charge; positive for Discharge. See
 	 * {@link ChannelId#SET_ACTIVE_POWER_L3_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -513,7 +514,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	/**
 	 * Sets a Reactive Power Equals setpoint on L1 in [var]. See
 	 * {@link ChannelId#SET_REACTIVE_POWER_L1_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -533,7 +534,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	/**
 	 * Sets a Reactive Power Equals setpoint on L2 in [var]. See
 	 * {@link ChannelId#SET_REACTIVE_POWER_L2_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -553,7 +554,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	/**
 	 * Sets a Reactive Power Equals setpoint on L3 in [var]. See
 	 * {@link ChannelId#SET_REACTIVE_POWER_L3_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -574,7 +575,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	 * Sets an Active Power Less Or Equals setpoint on L1 in [W]. Negative values
 	 * for Charge; positive for Discharge. See
 	 * {@link ChannelId#SET_ACTIVE_POWER_L1_LESS_OR_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -595,7 +596,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	 * Sets an Active Power Less Or Equals setpoint on L2 in [W]. Negative values
 	 * for Charge; positive for Discharge. See
 	 * {@link ChannelId#SET_ACTIVE_POWER_L2_LESS_OR_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -616,7 +617,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	 * Sets an Active Power Less Or Equals setpoint on L3 in [W]. Negative values
 	 * for Charge; positive for Discharge. See
 	 * {@link ChannelId#SET_ACTIVE_POWER_L3_LESS_OR_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -637,7 +638,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	 * Sets an Active Power Greater Or Equals setpoint on L1 in [W]. Negative values
 	 * for Charge; positive for Discharge. See
 	 * {@link ChannelId#SET_ACTIVE_POWER_L1_GREATER_OR_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -658,7 +659,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	 * Sets an Active Power Greater Or Equals setpoint on L2 in [W]. Negative values
 	 * for Charge; positive for Discharge. See
 	 * {@link ChannelId#SET_ACTIVE_POWER_L2_GREATER_OR_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -679,7 +680,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	 * Sets an Active Power Greater Or Equals setpoint on L3 in [W]. Negative values
 	 * for Charge; positive for Discharge. See
 	 * {@link ChannelId#SET_ACTIVE_POWER_L3_GREATER_OR_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -699,7 +700,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	/**
 	 * Sets a Reactive Power Less Or Equals setpoint on L1 in [var]. See
 	 * {@link ChannelId#SET_REACTIVE_POWER_L1_LESS_OR_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -719,7 +720,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	/**
 	 * Sets a Reactive Power Less Or Equals setpoint on L2 in [var]. See
 	 * {@link ChannelId#SET_REACTIVE_POWER_L2_LESS_OR_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -739,7 +740,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	/**
 	 * Sets a Reactive Power Less Or Equals setpoint on L3 in [var]. See
 	 * {@link ChannelId#SET_REACTIVE_POWER_L3_LESS_OR_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -760,7 +761,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	/**
 	 * Sets a Reactive Power Greater Or Equals setpoint on L1 in [var]. See
 	 * {@link ChannelId#SET_REACTIVE_POWER_L1_GREATER_OR_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -781,7 +782,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	/**
 	 * Sets a Reactive Power Greater Or Equals setpoint on L2 in [var]. See
 	 * {@link ChannelId#SET_REACTIVE_POWER_L2_GREATER_OR_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
@@ -802,7 +803,7 @@ public interface ManagedAsymmetricEss extends ManagedSymmetricEss, AsymmetricEss
 	/**
 	 * Sets a Reactive Power Greater Or Equals setpoint on L3 in [var]. See
 	 * {@link ChannelId#SET_REACTIVE_POWER_L3_GREATER_OR_EQUALS}.
-	 * 
+	 *
 	 * @param value the next write value
 	 * @throws OpenemsNamedException on error
 	 */
