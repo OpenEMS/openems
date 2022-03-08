16a8158d74fe4fc9fe115b3b2a79c4dbe4b6b1ba
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2022-03-08 16:09:09 +0100

    Update io.openems.edge.predictor.*

--- io.openems.edge.predictor.api/src/io/openems/edge/predictor/api/oneday/Predictor24Hours.java
+++ io.openems.edge.predictor.api/src/io/openems/edge/predictor/api/oneday/Predictor24Hours.java
@@ -14,11 +14,11 @@ public interface Predictor24Hours extends OpenemsComponent {
 
 	/**
 	 * Gets the Channel-Addresses for which this Predictor can provide a prediction.
-	 * 
+	 *
 	 * <p>
 	 * The entries can contain wildcards to match multiple actual
 	 * {@link ChannelAddress}es.
-	 * 
+	 *
 	 * @return an array of {@link ChannelAddress}es
 	 */
 	public ChannelAddress[] getChannelAddresses();
@@ -26,11 +26,11 @@ public interface Predictor24Hours extends OpenemsComponent {
 	/**
 	 * Gives a prediction for the next 24 h for the given {@link ChannelAddress};
 	 * one value per 15 minutes; 96 values in total.
-	 * 
+	 *
 	 * <p>
 	 * E.g. if called at 10:05, the first value stands for 10:00 to 10:15; second
 	 * value for 10:15 to 10:30.
-	 * 
+	 *
 	 * @param channelAddress the {@link ChannelAddress}
 	 * @return the {@link Prediction24Hours}
 	 */
