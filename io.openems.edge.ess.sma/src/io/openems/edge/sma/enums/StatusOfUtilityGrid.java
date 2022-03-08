138c87e2e5cd560eacafe15f823096cf4d4bc292
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2022-03-08 11:26:48 +0100

    Update io.openems.edge.ess.sma

--- io.openems.edge.ess.sma/src/io/openems/edge/sma/enums/StatusOfUtilityGrid.java
+++ io.openems.edge.ess.sma/src/io/openems/edge/sma/enums/StatusOfUtilityGrid.java
@@ -24,12 +24,12 @@ public enum StatusOfUtilityGrid implements OptionsEnum {
 
 	@Override
 	public int getValue() {
-		return value;
+		return this.value;
 	}
 
 	@Override
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 	@Override
