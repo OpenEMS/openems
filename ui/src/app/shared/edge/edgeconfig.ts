OpenEMS/openems

commit 7b78dac5431342598873c4c6b328a8f26276444e

Add Fronius PV Inverter (via Modbus/SunSpec) (#207) (#1749)
- Implement Fronius PV Inverter bundle
- AbstractOpenemsSunSpecComponent readNextBlock() function change to complete when all expected blocks have already been read.
- Add Factory ID in isProducer() method to be displayed in the UI.

Co-authored-by: sebastianasen <47855186+sebastianasen@users.noreply.github.com>

ui/src/app/shared/edge/edgeconfig.ts
@@ -1,3 +1,4 @@
+import { is } from 'date-fns/locale';
 import { ChannelAddress } from '../type/channeladdress';
 import { Widgets } from '../type/widget';
 import { Edge } from './edge';
@@ -262,25 +263,8 @@ export class EdgeConfig {
         // Do we have a Meter with type PRODUCTION?
         for (let component of this.getComponentsImplementingNature("io.openems.edge.meter.api.SymmetricMeter")) {
             if (component.isEnabled) {
-                // TODO make sure 'type' is provided for all Meters
-                if (component.properties['type'] == "PRODUCTION") {
-                    return true;
-                }
-                // TODO remove, once all Edges are at least version 2019.15
-                switch (component.factoryId) {
-                    case 'Fenecon.Mini.PvMeter':
-                    case 'Fenecon.Dess.PvMeter':
-                    case 'Fenecon.Pro.PvMeter':
-                    case 'Kostal.Piko.Charger':
-                    case 'Kaco.BlueplanetHybrid10.PvInverter':
-                    case 'PV-Inverter.Solarlog':
-                    case 'PV-Inverter.KACO.blueplanet':
-                    case 'PV-Inverter.SunSpec':
-                    case 'SolarEdge.PV-Inverter':
-                    case 'Simulator.PvInverter':
-                    case 'Simulator.ProductionMeter.Acting':
-                        return true;
-                }
+
+                return this.isProducer(component);
             }
         }
         return false;
@@ -295,24 +279,25 @@ export class EdgeConfig {
     public isProducer(component: EdgeConfig.Component) {
         if (component.properties['type'] == "PRODUCTION") {
             return true;
-        } else {
-            // TODO properties in OSGi Component annotations are not transmitted correctly with Apache Felix SCR
-            switch (component.factoryId) {
-                case 'Fenecon.Mini.PvMeter':
-                case 'Fenecon.Dess.PvMeter':
-                case 'Fenecon.Pro.PvMeter':
-                case 'Kostal.Piko.Charger':
-                case 'Kaco.BlueplanetHybrid10.PvInverter':
-                case 'PV-Inverter.Solarlog':
-                case 'PV-Inverter.KACO.blueplanet':
-                case 'PV-Inverter.SunSpec':
-                case 'PV-Inverter.SMA.SunnyTripower':
-                case 'SolarEdge.PV-Inverter':
-                case 'Simulator.PvInverter':
-                case 'Simulator.ProductionMeter.Acting':
-                    return true;
-            }
         }
+        // TODO properties in OSGi Component annotations are not transmitted correctly with Apache Felix SCR
+        switch (component.factoryId) {
+            case 'Fenecon.Dess.PvMeter':
+            case 'Fenecon.Mini.PvMeter':
+            case 'Fenecon.Pro.PvMeter':
+            case 'Kaco.BlueplanetHybrid10.PvInverter':
+            case 'Kostal.Piko.Charger':
+            case 'PV-Inverter.Fronius':
+            case 'PV-Inverter.KACO.blueplanet':
+            case 'PV-Inverter.SMA.SunnyTripower':
+            case 'PV-Inverter.Solarlog':
+            case 'PV-Inverter.SunSpec':
+            case 'Simulator.ProductionMeter.Acting':
+            case 'Simulator.PvInverter':
+            case 'SolarEdge.PV-Inverter':
+                return true;
+        }
+
         return false;
     }
 

io.openems.edge.application/EdgeApp.bndrun
@@ -137,6 +137,7 @@
 	bnd.identity;id='io.openems.edge.predictor.persistencemodel',\
 	bnd.identity;id='io.openems.edge.predictor.similardaymodel',\
 	bnd.identity;id='io.openems.edge.pvinverter.cluster',\
+	bnd.identity;id='io.openems.edge.pvinverter.fronius',\
 	bnd.identity;id='io.openems.edge.pvinverter.kaco.blueplanet',\
 	bnd.identity;id='io.openems.edge.pvinverter.sma',\
 	bnd.identity;id='io.openems.edge.pvinverter.solarlog',\
@@ -281,6 +282,7 @@
 	io.openems.edge.predictor.similardaymodel;version=snapshot,\
 	io.openems.edge.pvinverter.api;version=snapshot,\
 	io.openems.edge.pvinverter.cluster;version=snapshot,\
+	io.openems.edge.pvinverter.fronius;version=snapshot,\
 	io.openems.edge.pvinverter.kaco.blueplanet;version=snapshot,\
 	io.openems.edge.pvinverter.sma;version=snapshot,\
 	io.openems.edge.pvinverter.solarlog;version=snapshot,\

io.openems.edge.bridge.modbus/src/io/openems/edge/bridge/modbus/sunspec/AbstractOpenemsSunSpecComponent.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -77,6 +78,10 @@ public abstract class AbstractOpenemsSunSpecComponent extends AbstractOpenemsMod
 			throws OpenemsException {
 		this.readFromCommonBlockNo = readFromCommonBlockNo;
 
+		var expectedBlocks = this.activeModels.keySet().stream() //
+				.map(m -> m.getBlockId()) //
+				.collect(Collectors.toSet());
+
 		// Start the SunSpec read procedure...
 		this.isSunSpec().thenAccept(isSunSpec -> {
 			if (!isSunSpec) {
@@ -84,7 +89,7 @@ public abstract class AbstractOpenemsSunSpecComponent extends AbstractOpenemsMod
 			}
 
 			try {
-				this.readNextBlock(40_002).thenRun(() -> {
+				this.readNextBlock(40_002, expectedBlocks).thenRun(() -> {
 					this.isSunSpecInitializationCompleted = true;
 					this.onSunSpecInitializationCompleted();
 				});
@@ -129,12 +134,28 @@ public abstract class AbstractOpenemsSunSpecComponent extends AbstractOpenemsMod
 	/**
 	 * Reads the next SunSpec block.
 	 * 
-	 * @param startAddress the startAddress
+	 * @param startAddress    the startAddress
+	 * @param remainingBlocks the remaining blocks expected to read
 	 * @return a future that completes once reading the block finished
 	 * @throws OpenemsException on error
 	 */
-	private CompletableFuture<Void> readNextBlock(int startAddress) throws OpenemsException {
+	private CompletableFuture<Void> readNextBlock(int startAddress, Set<Integer> remainingBlocks)
+			throws OpenemsException {
 		final CompletableFuture<Void> finished = new CompletableFuture<Void>();
+
+		// Finish if all expected Blocks have been read
+		if (remainingBlocks.isEmpty()) {
+			finished.complete(null);
+		}
+
+		/*
+		 * Try to read block by block until all required blocks have been read or an
+		 * END_OF_MAP register has been found.
+		 * 
+		 * It may still happen that a device does not have a valid END_OF_MAP register
+		 * and that some blocks are not read - especially when one component is used for
+		 * multiple devices like single and three phase inverter.
+		 */
 		this.readElementsOnceTyped(new UnsignedWordElement(startAddress), new UnsignedWordElement(startAddress + 1))
 				.thenAccept(values -> {
 					int blockId = values.get(0);
@@ -164,6 +185,7 @@ public abstract class AbstractOpenemsSunSpecComponent extends AbstractOpenemsMod
 							Priority priority = activeEntry.getValue();
 							try {
 								this.addBlock(startAddress, sunSpecModel, priority);
+								remainingBlocks.remove(activeEntry.getKey().getBlockId());
 							} catch (OpenemsException e) {
 								this.logWarn(this.log, "Error while adding SunSpec-Model [" + blockId
 										+ "] starting at [" + startAddress + "]: " + e.getMessage());
@@ -180,7 +202,9 @@ public abstract class AbstractOpenemsSunSpecComponent extends AbstractOpenemsMod
 					// Read next block recursively
 					int nextBlockStartAddress = startAddress + 2 + length;
 					try {
-						final CompletableFuture<Void> readNextBlockFuture = this.readNextBlock(nextBlockStartAddress);
+
+						final CompletableFuture<Void> readNextBlockFuture = this.readNextBlock(nextBlockStartAddress,
+								remainingBlocks);
 						// Announce finished when next block (recursively) is finished
 						readNextBlockFuture.thenRun(() -> {
 							finished.complete(null);
@@ -250,7 +274,6 @@ public abstract class AbstractOpenemsSunSpecComponent extends AbstractOpenemsMod
 	 * @param startAddress the address to start reading from
 	 * @param model        the SunSpecModel
 	 * @param priority     the reading priority
-	 * @return future that gets completed when the Block elements are read
 	 * @throws OpenemsException on error
 	 */
 	protected void addBlock(int startAddress, SunSpecModel model, Priority priority) throws OpenemsException {

io.openems.edge.pvinverter.fronius/.classpath
@@ -1,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<classpath>
+	<classpathentry kind="con" path="aQute.bnd.classpath.container"/>
+	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-11"/>
+	<classpathentry kind="src" output="bin" path="src"/>
+	<classpathentry kind="src" output="bin_test" path="test">
+		<attributes>
+			<attribute name="test" value="true"/>
+		</attributes>
+	</classpathentry>
+	<classpathentry kind="output" path="bin"/>
+</classpath>

io.openems.edge.pvinverter.fronius/.gitignore
@@ -1,0 +1,2 @@
+/generated/
+/bin_test/

io.openems.edge.pvinverter.fronius/.project
@@ -1,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<projectDescription>
+	<name>io.openems.edge.pvinverter.fronius</name>
+	<comment></comment>
+	<projects>
+	</projects>
+	<buildSpec>
+		<buildCommand>
+			<name>org.eclipse.jdt.core.javabuilder</name>
+			<arguments>
+			</arguments>
+		</buildCommand>
+		<buildCommand>
+			<name>bndtools.core.bndbuilder</name>
+			<arguments>
+			</arguments>
+		</buildCommand>
+	</buildSpec>
+	<natures>
+		<nature>org.eclipse.jdt.core.javanature</nature>
+		<nature>bndtools.core.bndnature</nature>
+	</natures>
+</projectDescription>

io.openems.edge.pvinverter.fronius/bnd.bnd
@@ -1,0 +1,17 @@
+Bundle-Name: OpenEMS Edge PV Inverter Fronius
+Bundle-Vendor: FENECON GmbH
+Bundle-License: https://opensource.org/licenses/EPL-2.0
+Bundle-Version: 1.0.0.${tstamp}
+
+-buildpath: \
+	${buildpath},\
+	io.openems.common,\
+	io.openems.edge.bridge.modbus,\
+	io.openems.edge.common,\
+	io.openems.edge.meter.api,\
+	io.openems.edge.pvinverter.api,\
+	io.openems.edge.pvinverter.sunspec
+
+-testpath: \
+	${testpath},\
+	com.ghgande.j2mod

commit 5090fa0df04d55cb0f060674750f3f1f945d074c

Bugfix `isProducer()` method (#1770)
Fixes a bug that had been introduced by #1749

Under certain circumstances this bug would cause Production meters to be not displayed.

Thanks danielco for pointing it out in the OpenEMS Community forum: https://community.openems.io/t/trying-to-simulate-pv-production/838

ui/src/app/shared/edge/edgeconfig.ts
@@ -1,4 +1,3 @@
-import { is } from 'date-fns/locale';
 import { ChannelAddress } from '../type/channeladdress';
 import { Widgets } from '../type/widget';
 import { Edge } from './edge';
@@ -262,9 +261,8 @@ export class EdgeConfig {
         }
         // Do we have a Meter with type PRODUCTION?
         for (let component of this.getComponentsImplementingNature("io.openems.edge.meter.api.SymmetricMeter")) {
-            if (component.isEnabled) {
-
-                return this.isProducer(component);
+            if (component.isEnabled && this.isProducer(component)) {
+                return true;
             }
         }
         return false;