9de0cb733a23246ef2fe03e441b39b4ee2c67d9d
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2022-03-28 21:41:53 +0200

    FEMS-Backports for 2022.04
    - Bugfix for InfluxDB v2 implementation:
      - add missing runtime dependencies
      - use ThreadPool for InfluxDB batch writes
      - improve exception handling
    - Add continuous debug output to Ui.Websocket and Edge.Websocket
    - Add fallback for Resolution and Timezone for older UI versions
    - Improvement on Generic.Ess ErrorHandler

--- io.openems.backend.timedata.influx/src/io/openems/backend/timedata/influx/FieldTypeConflictHandler.java
+++ io.openems.backend.timedata.influx/src/io/openems/backend/timedata/influx/FieldTypeConflictHandler.java
@@ -42,35 +42,62 @@ public class FieldTypeConflictHandler {
 		}
 		var field = matcher.group("channel");
 		var thisType = matcher.group("thisType");
-		var requiredType = matcher.group("requiredType");
+		var requiredType = RequiredType.valueOf(matcher.group("requiredType").toUpperCase());
 
 		if (this.specialCaseFieldHandlers.containsKey(field)) {
 			// Special handling had already been added.
 			return;
 		}
 
-		BiConsumer<Point, JsonElement> handler = null;
+		var handler = this.createAndAddHandler(field, requiredType);
+
+		if (handler == null) {
+			this.parent.logWarn(this.log, "Unable to add special field handler for [" + field + "] from [" + thisType
+					+ "] to [" + requiredType + "]");
+		}
+		this.parent.logInfo(this.log,
+				"Add special field handler for [" + field + "] from [" + thisType + "] to [" + requiredType + "]");
+	}
+
+	private static enum RequiredType {
+		STRING, INTEGER, FLOAT;
+	}
+
+	private BiConsumer<Point, JsonElement> createAndAddHandler(String field, RequiredType requiredType) {
+		var handler = this.createHandler(field, requiredType);
+		this.specialCaseFieldHandlers.put(field, handler);
+		return handler;
+	}
+
+	/**
+	 * Creates a Handler for the given field, to convert a Point to a
+	 * 'requiredType'.
+	 * 
+	 * @param field        the field name, i.e. the Channel-Address
+	 * @param requiredType the {@link RequiredType
+	 * @return
+	 */
+	private BiConsumer<Point, JsonElement> createHandler(String field, RequiredType requiredType) {
 		switch (requiredType) {
-		case "string":
-			handler = (builder, jValue) -> {
-				var value = this.getAsFieldTypeString(jValue);
+		case STRING:
+			return (builder, jValue) -> {
+				var value = getAsFieldTypeString(jValue);
 				if (value != null) {
 					builder.addField(field, value);
 				}
 			};
-			break;
 
-		case "integer":
-			handler = (builder, jValue) -> {
+		case INTEGER:
+			return (builder, jValue) -> {
 				try {
-					var value = this.getAsFieldTypeNumber(jValue);
+					var value = getAsFieldTypeNumber(jValue);
 					if (value != null) {
 						builder.addField(field, value);
 					}
 				} catch (NumberFormatException e1) {
 					try {
 						// Failed -> try conversion to float and then to int
-						var value = this.getAsFieldTypeFloat(jValue);
+						var value = getAsFieldTypeFloat(jValue);
 						if (value != null) {
 							builder.addField(field, Math.round(value));
 						}
@@ -80,12 +107,11 @@ public class FieldTypeConflictHandler {
 					}
 				}
 			};
-			break;
 
-		case "float":
-			handler = (builder, jValue) -> {
+		case FLOAT:
+			return (builder, jValue) -> {
 				try {
-					var value = this.getAsFieldTypeFloat(jValue);
+					var value = getAsFieldTypeFloat(jValue);
 					if (value != null) {
 						builder.addField(field, value);
 					}
@@ -94,16 +120,8 @@ public class FieldTypeConflictHandler {
 							+ "] to float: " + e1.getMessage());
 				}
 			};
-			break;
 		}
-
-		if (handler == null) {
-			this.parent.logWarn(this.log, "Unable to add special field handler for [" + field + "] from [" + thisType
-					+ "] to [" + requiredType + "]");
-		}
-		this.parent.logInfo(this.log,
-				"Add special field handler for [" + field + "] from [" + thisType + "] to [" + requiredType + "]");
-		this.specialCaseFieldHandlers.put(field, handler);
+		return null; // can never happen
 	}
 
 	/**
@@ -112,7 +130,7 @@ public class FieldTypeConflictHandler {
 	 * @param jValue the value
 	 * @return the value as String; null if value represents null
 	 */
-	private String getAsFieldTypeString(JsonElement jValue) {
+	private static String getAsFieldTypeString(JsonElement jValue) {
 		if (jValue.isJsonNull()) {
 			return null;
 		}
@@ -126,7 +144,7 @@ public class FieldTypeConflictHandler {
 	 * @return the value as Number; null if value represents null
 	 * @throws NumberFormatException on error
 	 */
-	private Number getAsFieldTypeNumber(JsonElement jValue) throws NumberFormatException {
+	private static Number getAsFieldTypeNumber(JsonElement jValue) throws NumberFormatException {
 		if (jValue.isJsonNull()) {
 			return null;
 		}
@@ -154,7 +172,7 @@ public class FieldTypeConflictHandler {
 	 * @return the value as Float; null if value represents null
 	 * @throws NumberFormatException on error
 	 */
-	private Float getAsFieldTypeFloat(JsonElement jValue) throws NumberFormatException {
+	private static Float getAsFieldTypeFloat(JsonElement jValue) throws NumberFormatException {
 		if (jValue.isJsonNull()) {
 			return null;
 		}
