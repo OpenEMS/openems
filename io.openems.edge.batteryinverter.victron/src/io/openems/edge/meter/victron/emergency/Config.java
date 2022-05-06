0d5034aed3678bbe216921cd6a3bee435865c258
C.Lehne <c.lehne@opernikus.de>
2022-05-06 12:46:41 +0200

    renamed meter to emergency.meter

--- io.openems.edge.batteryinverter.victron/src/io/openems/edge/meter/victron/emergency/Config.java
+++ io.openems.edge.batteryinverter.victron/src/io/openems/edge/meter/victron/emergency/Config.java
@@ -1,4 +1,4 @@
-package io.openems.edge.victron.meter;
+package io.openems.edge.meter.victron;
 
 import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
