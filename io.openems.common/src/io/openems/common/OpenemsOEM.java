b532d2a827e4fba0d203776a53dd1571327ba6d0
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2022-08-03 20:10:38 +0200

    Backend.Api.Controller: reset URI
    Do not risk any firewall issues with customers...

--- io.openems.common/src/io/openems/common/OpenemsOEM.java
+++ io.openems.common/src/io/openems/common/OpenemsOEM.java
@@ -19,7 +19,7 @@ public class OpenemsOEM {
 	/*
 	 * Backend-Api Controller
 	 */
-	public static final String BACKEND_API_URI = "wss://srv0.fenecon.de/openems-backend";
+	public static final String BACKEND_API_URI = "wss://www1.fenecon.de:443/openems-backend2";
 
 	/*
 	 * System-Update.
