b4e49fc90b000042979be249b2437a2f4ca69caa
Pooran Chandrashekaraiah <pooran.c@fenecon.de>
2022-11-02 11:40:47 +0100

    Implementation of Meter Ziehl EFR4001IP (#418)
    Implemented the Meter ziehl.
    - Wordorder has to changed.
    - Scale factor is tested.
    - System tested on the FEMS 30024.
    
    Co-authored-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Reviewed-on: https://git.intranet.fenecon.de/FENECON/fems/pulls/418
    Reviewed-by: Hueseyin Sahutoglu <hueseyin.sahutoglu@fenecon.de>
    Reviewed-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Co-authored-by: Pooran Chandrashekaraiah <pooran.c@fenecon.de>
    Co-committed-by: Pooran Chandrashekaraiah <pooran.c@fenecon.de>

---
+++ io.openems.edge.meter.ziehl/src/io/openems/edge/meter/ziehl/efr4001ip/MeterZiehlEfr4001Ip.java
@@ -0,0 +1,26 @@
+package io.openems.edge.meter.ziehl.efr4001ip;
+
+import io.openems.edge.common.channel.Doc;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.modbusslave.ModbusSlave;
+import io.openems.edge.meter.api.AsymmetricMeter;
+import io.openems.edge.meter.api.SymmetricMeter;
+
+public interface MeterZiehlEfr4001Ip extends SymmetricMeter, AsymmetricMeter, OpenemsComponent, ModbusSlave {
+
+	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+		;
+
+		private final Doc doc;
+
+		private ChannelId(Doc doc) {
+			this.doc = doc;
+		}
+
+		@Override
+		public Doc doc() {
+			return this.doc;
+		}
+	}
+
+}
