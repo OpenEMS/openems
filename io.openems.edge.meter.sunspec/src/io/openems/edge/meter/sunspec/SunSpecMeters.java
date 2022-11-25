5e01bb24966873adf721d5035437015dfaad038c
Thomas Sicking <thomas.sicking@opernikus.de>
2022-11-25 10:16:21 +0100

    Added new Sunspec meter TQ EM 420

--- io.openems.edge.meter.sunspec/src/io/openems/edge/meter/sunspec/SunSpecMeters.java
+++ io.openems.edge.meter.sunspec/src/io/openems/edge/meter/sunspec/SunSpecMeters.java
@@ -1,9 +1,11 @@
 package io.openems.edge.meter.sunspec;
 
 public enum SunSpecMeters {
-	SOLAR_EDGE("SolarEdge", 2), ELGRIS_ZERO_EXPORT("Elgris Zero Export", 1),
-	ELGRIS_SMART_METER("Elgris Smart Meter", 1);
-
+	SOLAR_EDGE("SolarEdge", 2), //
+	ELGRIS_ZERO_EXPORT("Elgris Zero Export", 1), //
+	ELGRIS_SMART_METER("Elgris Smart Meter", 1), //
+	TQ_EM_420("TQ EM420", 1);
+	
 	public String name;
 	public int readFromCommonBlockNo;
 
