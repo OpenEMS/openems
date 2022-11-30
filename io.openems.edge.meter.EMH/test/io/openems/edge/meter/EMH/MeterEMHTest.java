6f7feced858d4ac9385b3c8e6b3f0f0c461bf9d4
DerWahreKlinki <Thomas.Klinkenberg@partec.org>
2022-11-30 07:52:28 +0100

    Added meters EMH and janitza UMG104

---
+++ io.openems.edge.meter.EMH/test/io/openems/edge/meter/EMH/MeterEMHTest.java
@@ -0,0 +1,25 @@
+package io.openems.edge.meter.EMH;
+
+import org.junit.Test;
+
+import io.openems.edge.common.test.AbstractComponentTest.TestCase;
+import io.openems.edge.bridge.modbus.test.DummyModbusBridge;
+import io.openems.edge.common.test.ComponentTest;
+
+public class MeterEMHTest {
+
+	private static final String COMPONENT_ID = "component0";
+	private static final String MODBUS_ID = "modbus0";
+
+	@Test
+	public void test() throws Exception {
+		new ComponentTest(new MeterEMHImpl()) //
+				.addReference("setModbus", new DummyModbusBridge(MODBUS_ID)) //
+				.activate(MyConfig.create() //
+						.setId(COMPONENT_ID) //
+						.setModbusId(MODBUS_ID) //
+						.build())
+				.next(new TestCase());
+	}
+
+}
