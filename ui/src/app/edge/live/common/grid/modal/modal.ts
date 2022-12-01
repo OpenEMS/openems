5f97dfc6b10bc76fdb9441ba0bebfdbb0971ee9d
Lukas Rieger <lukas.rieger@fenecon.de>
2022-12-01 16:05:54 +0100

    UI: Add Current and Voltage for Grid-Meter (#459)
    Co-authored-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Reviewed-on: https://git.intranet.fenecon.de/FENECON/fems/pulls/459
    Reviewed-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Co-authored-by: Lukas Rieger <lukas.rieger@fenecon.de>
    Co-committed-by: Lukas Rieger <lukas.rieger@fenecon.de>

--- ui/src/app/edge/live/common/grid/modal/modal.ts
+++ ui/src/app/edge/live/common/grid/modal/modal.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AbstractModal } from 'src/app/shared/genericComponents/modal/abstractModal';
-import { ChannelAddress, CurrentData, GridMode, Utils } from 'src/app/shared/shared';
+import { ChannelAddress, CurrentData, EdgeConfig, GridMode, Utils } from 'src/app/shared/shared';
 
 @Component({
   selector: 'modal',
@@ -8,37 +8,60 @@ import { ChannelAddress, CurrentData, GridMode, Utils } from 'src/app/shared/sha
 })
 export class ModalComponent extends AbstractModal {
 
-  public readonly GridMode = GridMode;
-  public grid: { mode: GridMode, buyFromGrid: number, sellToGrid: number, phases?: { name: string, value: number }[] } =
+  protected readonly GridMode = GridMode;
+  protected grid: { mode: GridMode, buyFromGrid: number, sellToGrid: number, phases: { name: string, value: number }[] } =
     {
       mode: GridMode.UNDEFINED,
       buyFromGrid: 0,
       sellToGrid: 0,
-      phases: [
-        { name: "", value: 0 },
-        { name: "", value: 0 },
-        { name: "", value: 0 }
-      ]
+      phases: []
     }
-  public readonly CONVERT_TO_WATT = Utils.CONVERT_TO_WATT;
+  protected readonly CONVERT_TO_WATT = Utils.CONVERT_TO_WATT;
+  protected meters: EdgeConfig.Component[] = [];
 
   protected override getChannelAddresses(): ChannelAddress[] {
-    return [
-      new ChannelAddress('_sum', 'GridMode'),
+    let channelAddresses: ChannelAddress[] = [];
+
+    channelAddresses.push(
+      new ChannelAddress("_sum", 'GridMode'),
       new ChannelAddress('_sum', 'GridActivePower'),
       new ChannelAddress('_sum', 'GridActivePowerL1'),
       new ChannelAddress('_sum', 'GridActivePowerL2'),
-      new ChannelAddress('_sum', 'GridActivePowerL3')
-    ]
+      new ChannelAddress('_sum', 'GridActivePowerL3'),
+    )
+
+    this.meters = this.config.getComponentsImplementingNature("io.openems.edge.meter.api.AsymmetricMeter").filter(comp => comp.isEnabled && this.config.isTypeGrid(comp))
+
+    if (this.meters.length == 1) {
+      let componentId = this.meters[0].id;
+
+      channelAddresses.push(
+        new ChannelAddress(componentId, 'ActivePower')
+      )
+
+      for (let phase of [1, 2, 3]) {
+        channelAddresses.push(
+          new ChannelAddress(componentId, 'CurrentL' + phase),
+          new ChannelAddress(componentId, 'VoltageL' + phase),
+          new ChannelAddress(componentId, 'ActivePowerL' + phase),
+        )
+      }
+    }
+
+    //TODO: change due to possiblity of multiple gridmeter
+
+    return channelAddresses;
   }
 
   protected override onCurrentData(currentData: CurrentData): void {
+
     this.grid.mode = currentData.allComponents["_sum/GridMode"]
-    this.grid.buyFromGrid = currentData.allComponents["_sum/GridActivePower"] > 0 ? currentData.allComponents["_sum/GridActivePower"] : 0;
-    this.grid.sellToGrid = currentData.allComponents["_sum/GridActivePower"] < 0 ? (currentData.allComponents["_sum/GridActivePower"] * -1) : 0;
-    this.grid.phases?.forEach((element, index) => {
-      element.name = "Phase L" + (index + 1) + " " + this.translate.instant(currentData.allComponents["_sum/GridActivePowerL" + (index + 1)] > 0 ? "General.gridBuyAdvanced" : "General.gridSellAdvanced");
-      element.value = currentData.allComponents["_sum/GridActivePowerL" + (index + 1)] < 0 ? currentData.allComponents["_sum/GridActivePowerL" + (index + 1)] * -1 : currentData.allComponents["_sum/GridActivePowerL" + (index + 1)] ?? 0;
+
+    this.grid.buyFromGrid = currentData.allComponents[this.meters[0].id + "/ActivePower"] > 0 ? currentData.allComponents[this.meters[0].id + "/ActivePower"] : 0;
+    this.grid.sellToGrid = currentData.allComponents[this.meters[0].id + "/ActivePower"] < 0 ? (currentData.allComponents[this.meters[0].id + "/ActivePower"] * -1) : 0;
+    this.grid.phases.forEach((element, index) => {
+      element.name = "Phase L" + (index + 1) + " " + this.translate.instant(currentData.allComponents[this.meters[0].id + "ActivePowerL" + (index + 1)] > 0 ? "General.gridBuyAdvanced" : "General.gridSellAdvanced");
+      element.value = currentData.allComponents[this.meters[0].id + "ActivePowerL" + (index + 1)] < 0 ? currentData.allComponents[this.meters[0].id + "ActivePowerL" + (index + 1)] * -1 : currentData.allComponents[this.meters[0].id + "ActivePowerL" + (index + 1)] ?? 0;
     })
   }
 }
--- ui/src/app/shared/edge/meter/asymmetric/modal.component.ts
+++ ui/src/app/shared/edge/meter/asymmetric/modal.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { filter } from 'rxjs/operators';
 import { TextIndentation } from 'src/app/shared/genericComponents/modal/modal-line/modal-line';
-import { Utils } from 'src/app/shared/shared';
+import { GridMode, Service, Utils } from 'src/app/shared/shared';
 import { Role } from 'src/app/shared/type/role';
 import { EdgeConfig } from '../../edgeconfig';
 
@@ -8,10 +10,39 @@ import { EdgeConfig } from '../../edgeconfig';
     selector: 'oe-asymmetricMeter',
     templateUrl: './modal.component.html'
 })
-export class AsymmetricMeterComponent {
+export class AsymmetricMeterComponent implements OnInit {
 
-    @Input() public component: EdgeConfig.Component;
+    @Input() protected component: EdgeConfig.Component;
+    @Input() protected type?: 'GRID' | null = null;
     protected readonly Role = Role;
     protected readonly Utils = Utils;
     public readonly TextIndentation = TextIndentation;
+    protected grid: { mode: GridMode, buyFromGrid: number, sellToGrid: number, phases?: { name: string, power: number, current: number, voltage: number }[] } =
+        {
+            mode: GridMode.UNDEFINED,
+            buyFromGrid: 0,
+            sellToGrid: 0,
+            phases: []
+        }
+
+    constructor(public service: Service, public translate: TranslateService) { }
+
+    ngOnInit() {
+        if (this.type == "GRID") {
+            this.service.getCurrentEdge().then((edge) => {
+                edge.currentData.pipe(
+                    filter(currentData => currentData != null))
+                    .subscribe((currentData) => {
+
+                        this.grid.mode = currentData.channel["_sum/GridMode"]
+                        this.grid.phases.forEach((element, index) => {
+                            element.name = "Phase L" + (index + 1) + " " + this.translate.instant(currentData.channel[this.component.id + "/ActivePowerL" + (index + 1)] > 0 ? "General.gridBuyAdvanced" : "General.gridSellAdvanced");
+                            element.power = Math.abs(currentData.channel[this.component.id + "/ActivePowerL" + (index + 1)]) ?? 0;
+                            element.current = currentData.channel[this.component.id + '/CurrentL' + (index + 1)];
+                            element.voltage = currentData.channel[this.component.id + '/VoltageL' + (index + 1)];
+                        });
+                    });
+            });
+        }
+    }
 }
