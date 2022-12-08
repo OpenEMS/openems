2991eeaa2c8c8cbb6c14d1903447c1d27f51c72e
Lukas Rieger <lukas.rieger@fenecon.de>
2022-12-08 15:55:00 +0100

    UI: Injection for Translatemodule + extract oe-modal-meter-phases (#465)
    Reviewed-on: https://git.intranet.fenecon.de/FENECON/fems/pulls/465
    Reviewed-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Co-authored-by: Lukas Rieger <lukas.rieger@fenecon.de>
    Co-committed-by: Lukas Rieger <lukas.rieger@fenecon.de>

--- ui/src/app/edge/live/common/grid/modal/modal.ts
+++ ui/src/app/edge/live/common/grid/modal/modal.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AbstractModal } from 'src/app/shared/genericComponents/modal/abstractModal';
-import { ChannelAddress, CurrentData, EdgeConfig, GridMode, Utils } from 'src/app/shared/shared';
+import { ChannelAddress, CurrentData, EdgeConfig, GridMode } from 'src/app/shared/shared';
 
 @Component({
   selector: 'modal',
@@ -11,9 +11,7 @@ export class ModalComponent extends AbstractModal {
   protected readonly GridMode = GridMode;
   protected grid: { mode: GridMode, buyFromGrid: number, sellToGrid: number } = { mode: GridMode.UNDEFINED, buyFromGrid: null, sellToGrid: null };
 
-  protected readonly CONVERT_TO_WATT = Utils.CONVERT_TO_WATT;
   protected meters: { component: EdgeConfig.Component, isAsymmetric: boolean }[] = []
-  protected phases: Map<EdgeConfig.Component, { key: string, name: string, power: number | null, current: number | null, voltage: number | null }[]> = new Map();
 
   protected override getChannelAddresses(): ChannelAddress[] {
     let channelAddresses: ChannelAddress[] = [];
@@ -35,20 +33,6 @@ export class ModalComponent extends AbstractModal {
       new ChannelAddress('_sum', 'GridActivePowerL2'),
       new ChannelAddress('_sum', 'GridActivePowerL3'),
     )
-    for (let meter of this.meters) {
-      this.phases.set(meter.component, [
-        { key: "L1", name: "", power: null, current: null, voltage: null },
-        { key: "L2", name: "", power: null, current: null, voltage: null },
-        { key: "L3", name: "", power: null, current: null, voltage: null }
-      ]);
-      for (let phase of [1, 2, 3]) {
-        channelAddresses.push(
-          new ChannelAddress(meter.component.id, 'CurrentL' + phase),
-          new ChannelAddress(meter.component.id, 'VoltageL' + phase),
-          new ChannelAddress(meter.component.id, 'ActivePowerL' + phase),
-        )
-      }
-    }
     return channelAddresses;
   }
 
@@ -58,16 +42,6 @@ export class ModalComponent extends AbstractModal {
     let gridActivePower = currentData.allComponents['_sum/GridActivePower']
     this.grid.buyFromGrid = gridActivePower > 0 ? gridActivePower : 0;
     this.grid.sellToGrid = gridActivePower < 0 ? (gridActivePower * -1) : 0;
-
-    for (let meter of this.meters) {
-      this.phases.get(meter.component).forEach((phase) => {
-        var power = currentData.allComponents[meter.component.id + '/ActivePower' + phase.key];
-        phase.name = this.translate.instant("General.phase") + " " + phase.key + this.setTranslatedName(power);
-        phase.power = Utils.absSafely(power);
-        phase.current = currentData.allComponents[meter.component.id + '/Current' + phase.key];
-        phase.voltage = currentData.allComponents[meter.component.id + '/Voltage' + phase.key];
-      });
-    }
   }
 
   protected setTranslatedName = (power: number | null) => {
