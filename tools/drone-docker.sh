99de5c581d6a911ddacda0a23626c558db7a79ea
root <root@gitvm.ad.fenecon.de>
2023-01-17 17:20:11 +0100

    Update drone-docker script

--- tools/drone-docker.sh
+++ tools/drone-docker.sh
@@ -8,7 +8,7 @@ FROM adoptopenjdk/openjdk11
 RUN curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
 
 RUN apt-get update  \
-        && apt-get install -y git nodejs wget curl unzip xvfb libxi6 libgconf-2-4 fonts-liberation \
+        && apt-get install -y apt-utils git nodejs wget curl unzip xvfb libxi6 libgconf-2-4 fonts-liberation \
 
 # Source: https://stackoverflow.com/questions/62588767/running-google-chrome-on-wsl-ubuntu-as-headless-no-sandbox-gives-multiple
 RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
