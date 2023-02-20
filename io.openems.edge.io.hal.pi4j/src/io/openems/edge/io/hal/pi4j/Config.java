54ca94bd795801686d7af8b96459327c5764346a
Lóránt Meszlényi <meszlenyilorant@gmail.com>
2023-02-20 12:26:04 +0100

    Renames component in the config

--- io.openems.edge.io.hal.pi4j/src/io/openems/edge/io/hal/pi4j/Config.java
+++ io.openems.edge.io.hal.pi4j/src/io/openems/edge/io/hal/pi4j/Config.java
@@ -4,12 +4,12 @@ import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 @ObjectClassDefinition(//
-		name = "Controller io.openems.edge.io.hal.pi4j", //
+		name = "IO Rasberry Pi Hardware Abstraction Layer io.openems.edge.io.hal.pi4j", //
 		description = "")
 @interface Config {
 
 	@AttributeDefinition(name = "Component-ID", description = "Unique ID of this Component")
-	String id() default "ctrlio.openems.edge.io.hal.pi4j0";
+	String id() default "io.openems.edge.io.hal.pi4j0";
 
 	@AttributeDefinition(name = "Alias", description = "Human-readable name of this Component; defaults to Component-ID")
 	String alias() default "";
@@ -17,6 +17,6 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 	@AttributeDefinition(name = "Is enabled?", description = "Is this Component enabled?")
 	boolean enabled() default true;
 
-	String webconsole_configurationFactory_nameHint() default "Controller io.openems.edge.io.hal.pi4j [{id}]";
+	String webconsole_configurationFactory_nameHint() default "IO Rasbperry Pi Hardware Abstraction Layer io.openems.edge.io.hal.pi4j [{id}]";
 
 }
--- io.openems.edge.io.hal.pi4j/src/io/openems/edge/io/hal/pi4j/Pi4JComponent.java
+++ io.openems.edge.io.hal.pi4j/src/io/openems/edge/io/hal/pi4j/Pi4JComponent.java
@@ -5,28 +5,29 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.metatype.annotations.Designate;
 
 import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
-import io.openems.edge.common.channel.Doc;
+import io.openems.common.exceptions.OpenemsException;
 import io.openems.edge.common.component.AbstractOpenemsComponent;
+import io.openems.edge.common.component.ComponentManager;
 import io.openems.edge.common.component.OpenemsComponent;
-import io.openems.edge.controller.api.Controller;
-
 import com.pi4j.Pi4J;
 import com.pi4j.context.Context;
 
 
 @Designate(ocd = Config.class, factory = true)
 @Component(//
-		name = "Controller.io.openems.edge.io.hal.pi4j", //
+		name = "IO Rasbperry Pi Hardware Abstraction", //
 		immediate = true, //
 		configurationPolicy = ConfigurationPolicy.REQUIRE //
 )
 public class Pi4JComponent extends AbstractOpenemsComponent implements Pi4JInterface, OpenemsComponent {
 
-	private Config config = null;
+	private Config config;
 	private Context pi4j;
+	private ComponentManager componentManager;
 	
 	public Pi4JComponent() {
 		super(OpenemsComponent.ChannelId.values(),
@@ -40,7 +41,13 @@ public class Pi4JComponent extends AbstractOpenemsComponent implements Pi4JInter
 
 
 	@Activate
-	void activate(ComponentContext context, Config config) {
+	void activate(ComponentContext context, Config config, @Reference ComponentManager componentManager) throws OpenemsException {
+		this.componentManager = componentManager;
+		if(this.componentCount() > 0) {
+			throw new OpenemsException("Component Pi4J can be loaded only once. Illegal attempt loading it twice.");
+		}
+		System.out.println(this.componentCount());
+		System.out.print(config.alias());
 		super.activate(context, config.id(), config.alias(), config.enabled());
 		this.config = config;
 		this.pi4j = Pi4J.newAutoContext();
@@ -50,8 +57,6 @@ public class Pi4JComponent extends AbstractOpenemsComponent implements Pi4JInter
 				.addPlatform(new OpenEmsRaspberryPiPlatform())
 				.autoDetectProviders()
 				.build();
-//		System.out.println(pi4j.platforms().getAll());
-//		context. .platforms().describe().print(System.out);
 	}
 
 	@Deactivate
@@ -61,8 +66,14 @@ public class Pi4JComponent extends AbstractOpenemsComponent implements Pi4JInter
 
 	@Override
 	public void run() throws OpenemsNamedException {
-		// TODO Auto-generated method stub
+		System.out.println(this.componentCount());
 		
+	}
+	
+	private int componentCount() {
+		var pi4JComponents = this.componentManager.getEdgeConfig()
+				.getComponentsByFactory("Pi4JComponent.AllAlphabetically");
+		return pi4JComponents.size();
 	}
 
 }
