063b77b95fe9921e865b744209367147f94d95c3
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2023-02-22 11:29:01 +0100

    Implement the Alpitronic Hypercharger DC charging station (#2071)
    Implementation of the Alpitronic Hypercharger V1-X DC electric vehicle charging station.
    
    The charging station comes with up to four physical connectors. This implementation is designed in a way, that each connector is represented by an individual OpenEMS Component. So if you have a Alpitronic with two connectors, please configure two "EVCS Alpitronic Hypercharger" with the connector configuration property set to SLOT_0 and SLOT_1 respectively.
    
    Supported Firmware versions: 1.7.2 and higher.
    
    Co-authored-by: Lorant Meszlenyi <16509320+hydroid7@users.noreply.github.com>
    Co-authored-by: Sebastian Asen <47855186+sebastianasen@users.noreply.github.com>

---
+++ io.openems.edge.evcs.alpitronic.hypercharger/test/io/openems/edge/evcs/hypercharger/MyConfig.java
@@ -0,0 +1,107 @@
+package io.openems.edge.evcs.hypercharger;
+
+import io.openems.common.test.AbstractComponentConfig;
+import io.openems.common.utils.ConfigUtils;
+import io.openems.edge.evcs.hypercharger.Hypercharger.Connector;
+
+@SuppressWarnings("all")
+public class MyConfig extends AbstractComponentConfig implements Config {
+
+	protected static class Builder {
+
+		private String id;
+		private String modbusId;
+		private int modbusUnitId;
+		private int minHwPower;
+		private int maxHwPower;
+		private Hypercharger.Connector connector;
+
+		private Builder() {
+		}
+
+		public Builder setId(String id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder setModbusId(String modbusId) {
+			this.modbusId = modbusId;
+			return this;
+		}
+
+		public Builder setModbusUnitId(int modbusUnitId) {
+			this.modbusUnitId = modbusUnitId;
+			return this;
+		}
+
+		public Builder setMinHwPower(int minHwPower) {
+			this.minHwPower = minHwPower;
+			return this;
+		}
+
+		public Builder setMaxHwPower(int maxHwPower) {
+			this.maxHwPower = maxHwPower;
+			return this;
+		}
+
+		public Builder setConnector(Hypercharger.Connector connector) {
+			this.connector = connector;
+			return this;
+		}
+
+		public MyConfig build() {
+			return new MyConfig(this);
+		}
+	}
+
+	/**
+	 * Create a Config builder.
+	 *
+	 * @return a {@link Builder}
+	 */
+	public static Builder create() {
+		return new Builder();
+	}
+
+	private final Builder builder;
+
+	private MyConfig(Builder builder) {
+		super(Config.class, builder.id);
+		this.builder = builder;
+	}
+
+	@Override
+	public boolean debugMode() {
+		return false;
+	}
+
+	@Override
+	public String modbus_id() {
+		return this.builder.modbusId;
+	}
+
+	@Override
+	public String Modbus_target() {
+		return ConfigUtils.generateReferenceTargetFilter(this.modbus_id(), this.modbus_id());
+	}
+
+	@Override
+	public int modbusUnitId() {
+		return this.builder.modbusUnitId;
+	}
+
+	@Override
+	public int minHwPower() {
+		return this.builder.minHwPower;
+	}
+
+	@Override
+	public int maxHwPower() {
+		return this.builder.maxHwPower;
+	}
+
+	@Override
+	public Connector connector() {
+		return this.builder.connector;
+	}
+}
