48a955f8400f831422e048b629a433ffe31b7856
Nico Ketzer <60319091+nicoketzer@users.noreply.github.com>
2023-03-12 15:37:34 +0100

    Update Generic HTTP Worker
    Drop of io.openems.edge.io.generic_http_worker
    -->Moved to io.openems.common.worker
    
    Renamed from "generic_http_worker" to GenericHttpWorker
    Deleted unuseful Comments

---
+++ io.openems.common/src/io/openems/common/worker/GenericHttpWorker.java
@@ -0,0 +1,159 @@
+package io.openems.common.worker;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import io.openems.common.exceptions.OpenemsException;
+import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
+
+public class GenericHttpWorker extends AbstractCycleWorker {
+    
+    /*Define default Values*/
+    private String[] urls_to_call = {};
+    private String[] response_to_call = {};
+    private boolean com_error = false;
+    private int com_error_counter = 0;
+    private int timeout = 1000;
+    private Exception last_error;
+
+    public GenericHttpWorker(String[] urls_to_call, int timeout) {
+	int elements = urls_to_call.length;
+	this.timeout = timeout;
+	/*Re-Create Array with right Size and Fill them*/
+	this.urls_to_call = new String[elements]; 
+	this.urls_to_call = urls_to_call;
+	this.response_to_call = new String[elements];
+    }
+
+    // Used Functions
+    private String send_req(String given_url) throws OpenemsNamedException {
+	try {
+	    var url = new URL(given_url);
+	    var con = (HttpURLConnection) url.openConnection();
+	    con.setRequestMethod("GET");
+	    con.setConnectTimeout(this.timeout);
+	    con.setReadTimeout(this.timeout);
+	    var status = con.getResponseCode();
+	    String body;
+	    try (var in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+		// Read HTTP response
+		var content = new StringBuilder();
+		String line;
+		while ((line = in.readLine()) != null) {
+		    content.append(line);
+		    content.append(System.lineSeparator());
+		}
+		body = content.toString();
+	    }
+	    if (status < 300) {
+		return body;
+	    }
+	    throw new OpenemsException("Error while reading from Device. Response code: " + status + ". " + body);
+	} catch (OpenemsNamedException | IOException e) {
+	    this.last_error = e;
+	    throw new OpenemsException("Unable to read from Device. " + e.getClass().getSimpleName() + ": "
+		    + e.getMessage() + " URL:" + given_url);
+	}
+    }
+
+    public boolean send_external_url(String url, String expected_output) {
+	try {
+	    String response = this.send_req(url);
+	    if (expected_output == "") {
+		return true;
+	    } else {
+		if (response.contains(expected_output) && response.length() == expected_output.length()) {
+		    return true;
+		} else {
+		    return false;
+		}
+	    }
+	} catch (OpenemsNamedException e) {
+	    this.last_error = e;
+	    return false;
+	}
+    }
+
+    public Exception get_last_error() {
+	if (this.last_error != null) {
+	    return this.last_error;
+	} else {
+	    return new Exception();
+	}
+    }
+
+    public String get_last_by_id(int element) {
+	if (this.com_error) {
+	    return "_com_error_";
+	}
+	if (element <= this.response_to_call.length && element > -1) {
+	    if (this.response_to_call[element] != null) {
+		return this.response_to_call[element];
+	    } else {
+		return "_no_value_";
+	    }
+	} else {
+	    return "_undefined_";
+	}
+    }
+
+    public String get_last(String element) {
+	if (this.com_error) {
+	    return "_com_error_";
+	}
+	String tmp_element = this.urls_to_call.toString();
+	if (tmp_element.contains(element)) {
+	    int index = -1;
+	    for (int i = 0; i < this.urls_to_call.length; i++) {
+		String tmp_element2 = this.urls_to_call[i];
+		if (tmp_element2 == element) {
+		    index = i;
+		    break;
+		}
+	    }
+	    // Catch
+	    if (index == -1) {
+		return "_undefined_";
+	    } else {
+		return this.get_last_by_id(index);
+	    }
+	} else {
+	    return "_undefined_";
+	}
+    }
+
+    // Worker Part
+
+    @Override
+    protected void forever() throws Throwable {
+	try {
+	    for (int i = 0; i < this.urls_to_call.length; i++) {
+		/*Com-Error Handling to not Spam requests to a Device */
+		if (this.com_error) {
+		    if (this.com_error_counter > 500) {
+			this.com_error = false;
+		    } else {
+			this.com_error_counter++;
+			break;
+		    }
+		}
+		try {
+		    String url = this.urls_to_call[i];
+
+		    String response = this.send_req(url);
+		    this.response_to_call[i] = response;
+		} catch (OpenemsNamedException e) {
+		    this.last_error = e;
+		    throw e;
+		}
+	    }
+	} catch (OpenemsNamedException e) {
+	    this.last_error = e;
+	    this.com_error = true;
+	}
+
+    }
+}
