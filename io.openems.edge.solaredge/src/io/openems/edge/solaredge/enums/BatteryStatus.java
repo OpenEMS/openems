b9363ff2329b79729dfffebbc7a3ec35008dbd4a
Thomas Klinkenberg <tkl@zentrale.partec.org>
2023-03-17 11:30:20 +0100

    Implementation of a solaredge system containing PV hybrid inverter, grid meter, low voltage solaredge home battery

---
+++ io.openems.edge.solaredge/src/io/openems/edge/solaredge/enums/BatteryStatus.java
@@ -0,0 +1,39 @@
+package io.openems.edge.solaredge.enums;
+
+import io.openems.common.types.OptionsEnum;
+
+public  enum BatteryStatus implements OptionsEnum {
+	UNDEFINED(-1, "Undefined"), //
+	SE_BATT_STATUS_OFF(0, "Off"), //
+	SE_BATT_STATUS_STBY(1, "Standby"), //
+	SE_BATT_STATUS_INIT(2, "Init"), //
+	SE_BATT_STATUS_CHARGE(3, "Charge"), //
+	SE_BATT_STATUS_DISCHARGE(4, "Discharge"), //
+	SE_BATT_STATUS_FAULT(5, "Fault"), //
+	// 6 doesn´t exist
+	SE_BATT_STATUS_IDLE(7, "Idle"); //
+			
+
+	private final int value;
+	private final String name;
+
+	private BatteryStatus(int value, String name) {
+		this.value = value;
+		this.name = name;
+	}
+
+	@Override
+	public int getValue() {
+		return this.value;
+	}
+
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public OptionsEnum getUndefined() {
+		return UNDEFINED;
+	}
+}
---
+++ io.openems.edge.solaredge/src/io/openems/edge/solaredge/gridmeter/SolarEdgeGridMeterChannelId.java
@@ -0,0 +1,181 @@
+package io.openems.edge.solaredge.gridmeter;
+import io.openems.common.channel.PersistencePriority;
+import io.openems.common.channel.Unit;
+import io.openems.common.types.OpenemsType;
+import io.openems.edge.common.channel.Doc;
+import io.openems.edge.common.channel.IntegerReadChannel;
+import io.openems.edge.common.channel.value.Value;
+import io.openems.edge.common.component.OpenemsComponent;
+
+
+public interface SolarEdgeGridMeterChannelId extends OpenemsComponent {
+	
+	public static enum ChannelId implements io.openems.edge.common.channel.ChannelId {	
+		/**
+		 * State of Charge.
+		 *
+		 * <ul>
+		 * <li>Interface: SolarEdgeHybridPvinverter
+		 * <li>Type: Integer
+		 * <li>Unit: %
+		 * <li>Range: 0..100
+		 * </ul>
+		 */
+		GRID_POWER(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.WATT) //
+				.persistencePriority(PersistencePriority.HIGH)),
+		
+		/**
+		 * State of Charge.
+		 *
+		 * <ul>
+		 * <li>Interface: SolarEdgeHybridPvinverter
+		 * <li>Type: Integer
+		 * <li>Unit: Exponent for Grid-Power
+		 * <li>Range: 0..100
+		 * </ul>
+		 */
+		GRID_POWER_SCALE(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.NONE) //
+				.persistencePriority(PersistencePriority.HIGH)),		
+		
+		/**
+		 * State of Charge.
+		 *
+		 * <ul>
+		 * <li>Interface: SolarEdgeHybridPvinverter
+		 * <li>Type: Integer
+		 * <li>Unit: %
+		 * <li>Range: 0..100
+		 * </ul>
+		 */
+		CONSUMPTION_POWER(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.WATT) //
+				.persistencePriority(PersistencePriority.HIGH)),
+		
+		/**
+		 * DC Discharge Power.
+		 *
+		 * <ul>
+		 * <li>Interface: SolarEdgeHybridPvinverter
+		 * <li>Type: Integer
+		 * <li>Unit: W
+		 * <li>Range: negative values for Charge; positive for Discharge
+		 * <li>This is the
+		 * {@link io.openems.edge.ess.api.SymmetricEss.ChannelId#ACTIVE_POWER} minus
+		 * {@link io.openems.edge.ess.dccharger.api.EssDcCharger.ChannelId#ACTUAL_POWER},
+		 * i.e. the power that is actually charged to or discharged from the battery.
+		 * </ul>
+		 */
+		DC_DISCHARGE_POWER(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.WATT) //
+				.persistencePriority(PersistencePriority.HIGH) //
+				.text("Actual AC-side battery discharge power of Energy Storage System. " //
+						+ "Negative values for charge; positive for discharge"));
+		private final Doc doc;
+	
+		private ChannelId(Doc doc) {
+			this.doc = doc;
+		}
+	
+		@Override
+		public Doc doc() {
+			return this.doc;
+		}		
+		
+	}
+	
+
+	/**
+	 * Gets the Channel for {@link ChannelId#DC_DISCHARGE_POWER}.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerReadChannel getConsumptionPowerChannel() {
+		return this.channel(ChannelId.CONSUMPTION_POWER);
+	}
+
+	/**
+	 * Gets the DC Discharge Power in [W]. Negative values for Charge; positive for
+	 * Discharge. See {@link ChannelId#DC_DISCHARGE_POWER}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getConsumptionPower() {
+		return this.getConsumptionPowerChannel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#DC_DISCHARGE_POWER} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setConsumptionPower(Integer value) {
+		this.getConsumptionPowerChannel().setNextValue(value);
+	}	
+	
+
+	/**
+	 * Gets the Channel for {@link ChannelId#DC_DISCHARGE_POWER}.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerReadChannel getDcDischargePowerChannel() {
+		return this.channel(ChannelId.DC_DISCHARGE_POWER);
+	}
+
+	/**
+	 * Gets the DC Discharge Power in [W]. Negative values for Charge; positive for
+	 * Discharge. See {@link ChannelId#DC_DISCHARGE_POWER}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getDcDischargePower() {
+		return this.getDcDischargePowerChannel().value();
+	}
+
+	
+	
+	/**
+	 * Gets the Channel for {@link ChannelId#DC_DISCHARGE_POWER}.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerReadChannel getGridPowerScaleChannel() {
+		return this.channel(ChannelId.GRID_POWER_SCALE);
+	}
+
+	/**
+	 * Gets the DC Discharge Power in [W]. Negative values for Charge; positive for
+	 * Discharge. See {@link ChannelId#DC_DISCHARGE_POWER}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getGridPowerScale() {
+		return this.getGridPowerScaleChannel().value();
+	}
+	
+	
+	/**
+	 * Gets the Channel for {@link ChannelId#DC_DISCHARGE_POWER}.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerReadChannel getGridPowerChannel() {
+		return this.channel(ChannelId.GRID_POWER);
+	}
+
+	/**
+	 * Gets the DC Discharge Power in [W]. Negative values for Charge; positive for
+	 * Discharge. See {@link ChannelId#DC_DISCHARGE_POWER}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getGridPower() {
+		return this.getGridPowerChannel().value();
+	}
+	
+	
+
+}
