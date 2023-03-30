05231337076c27f411170ec25b459fa9ecbd2f47
Sn0w3y <hallerjo97@gmail.com>
2023-03-30 18:47:12 +0200

    Update changelog.component.ts
    I optimized the changelog.component.ts so it is configurable without a need to compile it again from the /src/app/assets/changelog.json here.
    
    Example changelog.js:
    
    [ { "version": "x.y.z", "changes": [ { "type": "link", "text": "OpenEMS Releases", "url": "https://github.com/OpenEMS/openems/releases" } ] }, { "version": "1.2.0", "changes": [ "Added support for new battery storage system", "Improved user interface for energy consumption monitoring", { "roleIsAtLeast": "ADMIN", "change": "Bug fixes and security updates" } ] }, { "version": "1.1.1", "changes": [ "Fixed minor bugs in energy production calculation", "Updated translations for several languages", { "roleIsAtLeast": "INSTALLER", "change": "Performance improvements for API calls" } ] }, { "version": "1.1.0", "changes": [ "Implemented customizable dashboard", "Enhanced data visualization with new charts and graphs", { "roleIsAtLeast": "ADMIN", "change": "Updated dependencies to latest versions" } ] }, { "version": "1.0.0", "changes": [ "Initial release of the OpenEMS platform", "Basic functionality for energy management and monitoring" ] } ]

--- ui/src/app/changelog/view/component/changelog.component.ts
+++ ui/src/app/changelog/view/component/changelog.component.ts
@@ -5,42 +5,57 @@ import { environment } from 'src/environments';
 import { Service } from '../../../shared/shared';
 import { Role } from '../../../shared/type/role';
 import { Changelog, Library, OpenemsComponent, Product } from './changelog.constants';
+import { HttpClient } from '@angular/common/http';
 
-@Component({
-  selector: 'changelog',
-  templateUrl: './changelog.component.html'
+@component({
+selector: 'changelog',
+templateUrl: './changelog.component.html'
 })
 export class ChangelogComponent implements OnInit {
 
-  public environment = environment;
+public environment = environment;
 
-  protected slice: number = 10;
-  protected showAll: boolean = false;
-  constructor(
+protected slice: number = 10;
+protected showAll: boolean = false;
+public changelogs: {
+    version: string,
+    changes: Array<string | { roleIsAtLeast: Role, change: string }>
+}[] = [];
+
+constructor(
     public translate: TranslateService,
     public service: Service,
     private route: ActivatedRoute,
-  ) { }
+    private http: HttpClient,
+) { }
 
-  ngOnInit() {
+ngOnInit() {
     this.service.setCurrentComponent({ languageKey: 'Menu.changelog' }, this.route);
-  }
+    this.loadChangelog();
+}
 
-  public readonly roleIsAtLeast = Role.isAtLeast;
-  public numberToRole(role: number): string {
+public readonly roleIsAtLeast = Role.isAtLeast;
+public numberToRole(role: number): string {
     return Role[role].toLowerCase();
-  }
+}
 
-  public readonly changelogs: {
-    version: string,
-    changes: Array<string | { roleIsAtLeast: Role, change: string }>
-  }[] = [
-      {
-        version: 'x.y.z',
-        changes: [
-          Changelog.link("OpenEMS Releases", 'https://github.com/OpenEMS/openems/releases') 
-        ]
-      }
-    ];
-
+private loadChangelog(): void {
+    this.http.get('assets/changelog.json').subscribe(
+        (data: any[]) => {
+            this.changelogs = data.map(item => ({
+                version: item.version,
+                changes: item.changes.map(change => {
+                    if (typeof change === 'string') {
+                        return change;
+                    } else {
+                        return { roleIsAtLeast: Role[change.roleIsAtLeast], change: change.change };
+                    }
+                }),
+            }));
+        },
+        (error) => {
+            console.error('Failed to load changelog.json:', error);
+        }
+    );
+}
 }
