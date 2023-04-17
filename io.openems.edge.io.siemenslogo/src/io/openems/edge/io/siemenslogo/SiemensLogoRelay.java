3ef72c3d8ee75b4a97f603a947e9b8ab3db72944
DerWahreKlinki <102486171+DerWahreKlinki@users.noreply.github.com>
2023-04-17 14:13:40 +0200

    Update SiemensLogoRelay.java

--- io.openems.edge.io.siemenslogo/src/io/openems/edge/io/siemenslogo/SiemensLogoRelay.java
+++ io.openems.edge.io.siemenslogo/src/io/openems/edge/io/siemenslogo/SiemensLogoRelay.java
@@ -14,7 +14,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 
 	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
 		/**
-		 * Holds writes to Relay Output 1 for debugging
+		 * Holds writes to Relay Output 1 for debugging.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -24,7 +24,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 		 */
 		DEBUG_RELAY_1(Doc.of(OpenemsType.BOOLEAN)), //
 		/**
-		 * Relay Output 1
+		 * Relay Output 1.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -37,7 +37,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 				.persistencePriority(PersistencePriority.MEDIUM) //
 				.onInit(new BooleanWriteChannel.MirrorToDebugChannel(ChannelId.DEBUG_RELAY_1))), //
 		/**
-		 * Holds writes to Relay Output 2 for debugging
+		 * Holds writes to Relay Output 2 for debugging.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -48,7 +48,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 		DEBUG_RELAY_2(Doc.of(OpenemsType.BOOLEAN) //
 				.persistencePriority(PersistencePriority.MEDIUM)), //
 		/**
-		 * Relay Output 2
+		 * Relay Output 2.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -61,7 +61,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 				.persistencePriority(PersistencePriority.MEDIUM) //
 				.onInit(new BooleanWriteChannel.MirrorToDebugChannel(ChannelId.DEBUG_RELAY_2))), //
 		/**
-		 * Holds writes to Relay Output 3 for debugging
+		 * Holds writes to Relay Output 3 for debugging.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -72,7 +72,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 		DEBUG_RELAY_3(Doc.of(OpenemsType.BOOLEAN) //
 				.persistencePriority(PersistencePriority.MEDIUM)), //
 		/**
-		 * Relay Output 3
+		 * Relay Output 3.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -85,7 +85,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 				.persistencePriority(PersistencePriority.MEDIUM) //
 				.onInit(new BooleanWriteChannel.MirrorToDebugChannel(ChannelId.DEBUG_RELAY_3))), //
 		/**
-		 * Holds writes to Relay Output 4 for debugging
+		 * Holds writes to Relay Output 4 for debugging.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -96,7 +96,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 		DEBUG_RELAY_4(Doc.of(OpenemsType.BOOLEAN) //
 				.persistencePriority(PersistencePriority.MEDIUM)), //
 		/**
-		 * Relay Output 4
+		 * Relay Output 4.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -109,7 +109,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 				.persistencePriority(PersistencePriority.MEDIUM) //
 				.onInit(new BooleanWriteChannel.MirrorToDebugChannel(ChannelId.DEBUG_RELAY_4))), //
 		/**
-		 * Holds writes to Relay Output 5 for debugging
+		 * Holds writes to Relay Output 5 for debugging.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -120,7 +120,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 		DEBUG_RELAY_5(Doc.of(OpenemsType.BOOLEAN) //
 				.persistencePriority(PersistencePriority.MEDIUM)), //
 		/**
-		 * Relay Output 5
+		 * Relay Output 5.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -133,7 +133,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 				.persistencePriority(PersistencePriority.MEDIUM) //
 				.onInit(new BooleanWriteChannel.MirrorToDebugChannel(ChannelId.DEBUG_RELAY_5))), //
 		/**
-		 * Holds writes to Relay Output 6 for debugging
+		 * Holds writes to Relay Output 6 for debugging.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -144,7 +144,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 		DEBUG_RELAY_6(Doc.of(OpenemsType.BOOLEAN) //
 				.persistencePriority(PersistencePriority.MEDIUM)), //
 		/**
-		 * Relay Output 6
+		 * Relay Output 6.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -157,7 +157,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 				.persistencePriority(PersistencePriority.MEDIUM) //
 				.onInit(new BooleanWriteChannel.MirrorToDebugChannel(ChannelId.DEBUG_RELAY_6))), //
 		/**
-		 * Holds writes to Relay Output 7 for debugging
+		 * Holds writes to Relay Output 7 for debugging.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -168,7 +168,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 		DEBUG_RELAY_7(Doc.of(OpenemsType.BOOLEAN) //
 				.persistencePriority(PersistencePriority.MEDIUM)), //
 		/**
-		 * Relay Output 7
+		 * Relay Output 7.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -181,7 +181,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 				.persistencePriority(PersistencePriority.MEDIUM) //
 				.onInit(new BooleanWriteChannel.MirrorToDebugChannel(ChannelId.DEBUG_RELAY_7))), //
 		/**
-		 * Holds writes to Relay Output 8 for debugging
+		 * Holds writes to Relay Output 8 for debugging.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
@@ -192,7 +192,7 @@ public interface SiemensLogoRelay extends DigitalOutput, OpenemsComponent, Modbu
 		DEBUG_RELAY_8(Doc.of(OpenemsType.BOOLEAN) //
 				.persistencePriority(PersistencePriority.MEDIUM)), //
 		/**
-		 * Relay Output 8
+		 * Relay Output 8.
 		 * 
 		 * <ul>
 		 * <li>Interface: SiemensLogoRelayOutput
