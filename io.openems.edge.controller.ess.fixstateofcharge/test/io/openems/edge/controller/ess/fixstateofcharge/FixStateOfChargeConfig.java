4497a691a7a85bfe572e427db8c4b7bd6f20c1b7
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2023-04-26 23:05:30 +0200

    ESS Fix-SoC/Prepare-Battery-Extension-Controller (#2157)
    Backport from FEMS
    
    Sets active power constraints for charging/discharging of a symmetric energy storage system, to reach a final SoC under the constraints below.
    • State of Charge should be reached at a certain time or as fast as possible
    • State of Charge is held for a configured time or optionally until a specific end condition is fulfilled
    
    The Bundle is divided into two controllers with the same purpose, but different default configurations and use cases.
    
    Controller.Ess.FixStateOfCharge:
    • Useful for service (Target SoC & buffer time are depending on the service case)
    
    Controller.Ess.PrepareBatteryExtension:
    • The only task is to prepare the battery extension (Charge/Discharge to 30%, depending on the user inputs)
    
    Co-authored-by: sebastianasen <47855186+sebastianasen@users.noreply.github.com>
    Co-authored-by: Lukas Rieger <73471197+lukasrgr@users.noreply.github.com>

---
+++ io.openems.edge.controller.ess.fixstateofcharge/test/io/openems/edge/controller/ess/fixstateofcharge/FixStateOfChargeConfig.java
@@ -0,0 +1,156 @@
+package io.openems.edge.controller.ess.fixstateofcharge;
+
+import io.openems.common.test.AbstractComponentConfig;
+import io.openems.common.utils.ConfigUtils;
+import io.openems.edge.controller.ess.fixstateofcharge.api.EndCondition;
+
+@SuppressWarnings("all")
+public class FixStateOfChargeConfig extends AbstractComponentConfig implements ConfigFixStateOfCharge {
+
+	protected static class Builder {
+		private String id;
+		public String essId;
+		public boolean isRunning;
+		public int targetSoc;
+		public boolean targetTimeSpecified;
+		public String targetTime;
+		public int targetTimeBuffer;
+		public boolean selfTermination;
+		public int terminationBuffer;
+		public boolean conditionalTermination;
+		public EndCondition endCondition;
+
+		private Builder() {
+		}
+
+		public Builder setId(String id) {
+			this.id = id;
+			return this;
+		}
+
+		public FixStateOfChargeConfig build() {
+			return new FixStateOfChargeConfig(this);
+		}
+
+		public Builder setEssId(String essId) {
+			this.essId = essId;
+			return this;
+		}
+
+		public Builder setRunning(boolean isRunning) {
+			this.isRunning = isRunning;
+			return this;
+		}
+
+		public Builder setTargetSoc(int targetSoc) {
+			this.targetSoc = targetSoc;
+			return this;
+		}
+
+		public Builder setSpecifyTargetTime(boolean targetTimeSpecified) {
+			this.targetTimeSpecified = targetTimeSpecified;
+			return this;
+		}
+
+		public Builder setTargetTime(String targetTime) {
+			this.targetTime = targetTime;
+			return this;
+		}
+
+		public Builder setTargetTimeBuffer(int targetTimeBuffer) {
+			this.targetTimeBuffer = targetTimeBuffer;
+			return this;
+		}
+
+		public Builder setSelfTermination(boolean selfTermination) {
+			this.selfTermination = selfTermination;
+			return this;
+		}
+
+		public Builder setTerminationBuffer(int terminationBuffer) {
+			this.terminationBuffer = terminationBuffer;
+			return this;
+		}
+
+		public Builder setConditionalTermination(boolean conditionalTermination) {
+			this.conditionalTermination = conditionalTermination;
+			return this;
+		}
+
+		public Builder setEndCondition(EndCondition endCondition) {
+			this.endCondition = endCondition;
+			return this;
+		}
+	}
+
+	/**
+	 * Create a Config builder.
+	 * 
+	 * @return a {@link Builder}
+	 */
+	public static Builder create() {
+		return new Builder();
+	}
+
+	private final Builder builder;
+
+	private FixStateOfChargeConfig(Builder builder) {
+		super(ConfigFixStateOfCharge.class, builder.id);
+		this.builder = builder;
+	}
+
+	@Override
+	public boolean isRunning() {
+		return this.builder.isRunning;
+	}
+
+	@Override
+	public int targetSoc() {
+		return this.builder.targetSoc;
+	}
+
+	@Override
+	public boolean targetTimeSpecified() {
+		return this.builder.targetTimeSpecified;
+	}
+
+	@Override
+	public String targetTime() {
+		return this.builder.targetTime;
+	}
+
+	@Override
+	public int targetTimeBuffer() {
+		return this.builder.targetTimeBuffer;
+	}
+
+	@Override
+	public boolean selfTermination() {
+		return this.builder.selfTermination;
+	}
+
+	@Override
+	public int terminationBuffer() {
+		return this.builder.terminationBuffer;
+	}
+
+	@Override
+	public boolean conditionalTermination() {
+		return this.builder.conditionalTermination;
+	}
+
+	@Override
+	public EndCondition endCondition() {
+		return this.builder.endCondition;
+	}
+
+	@Override
+	public String ess_id() {
+		return this.builder.essId;
+	}
+
+	@Override
+	public String ess_target() {
+		return ConfigUtils.generateReferenceTargetFilter(this.id(), this.ess_id());
+	}
+}
