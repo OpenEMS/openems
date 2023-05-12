7aae67c11d0f5c8098df65604a94cbc98944ae21
Bishal Ghimire <bishal.ghimire@fenecon.de>
2023-05-12 10:00:46 +0200

    Added Interpolarion Manager

--- io.openems.edge.predictor.lstmmodel/src/io/openems/edge/predictor/lstmmodel/interpolation/CubicalInterpolation.java
+++ io.openems.edge.predictor.lstmmodel/src/io/openems/edge/predictor/lstmmodel/interpolation/CubicalInterpolation.java
@@ -1,7 +1,6 @@
 package io.openems.edge.predictor.lstmmodel.interpolation;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,82 +14,90 @@ import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
 
 public class CubicalInterpolation {
-
-	ArrayList<ArrayList<ArrayList<Double>>> interpolatingInterval = new ArrayList<ArrayList<ArrayList<Double>>>();
+	static ArrayList<ArrayList<Double>> interpolationData = new ArrayList<ArrayList<Double>>();
 	ArrayList<Double> data = new ArrayList<Double>();
 	ArrayList<ArrayList<Double>> temp01 = new ArrayList<ArrayList<Double>>();
-	ArrayList<ArrayList<Double>> matrixAInverted = new ArrayList<ArrayList<Double>>();
-	// Do not change this
-	int groupSize = 1;
+	ArrayList<ArrayList<Double>> AInv = new ArrayList<ArrayList<Double>>();
+	ArrayList<Double> result = new ArrayList<Double>();
+	static int groupSize = 1;// Do not change this
+	
 
-	public CubicalInterpolation() {
+
+	static public ArrayList<Double> Interpolate(ArrayList<Double> data) {
+		interpolationData = getInterpolatingData(data);
+
+//		
+
+		
+		ArrayList<ArrayList<Double>> xInterval = groupToInterval(interpolationData.get(0));
+		ArrayList<ArrayList<Double>> yInterval = groupToInterval(interpolationData.get(1));
+		ArrayList<Double> mVector = generateAndSolveTriDiagonal(xInterval, yInterval);
+	
+	
+		for (int i = 0; i < interpolationData.get(2).size(); i++) {
+
+			int tempval = (int) Math.round(interpolationData.get(2).get(i));
+			data.set(tempval, interpolationFunction(xInterval, yInterval, groupToInterval(mVector),
+					interpolationData.get(2).get(i)));
+
+		}
+		return data;
 
 	}
 
-	public void interpolate() {
-
-		ArrayList<Double> x = new ArrayList<Double>(Arrays.asList(1.00, 2.00, 3.00, 4.00));
-		ArrayList<Double> y = new ArrayList<Double>(Arrays.asList(1.0, 0.5, 0.333, 0.25));
-		ArrayList<ArrayList<Double>> xInterval = this.groupToInterval(x);
-		ArrayList<ArrayList<Double>> yInterval = this.groupToInterval(y);
-		ArrayList<Double> mVector = this.generateAndSolveTriDiagonal(xInterval, yInterval);
-		double result = this.interpolationFunction(xInterval, yInterval, this.groupToInterval(mVector), 2.55);
-		System.out.println("result : " + result);
-		this.interpolatingInterval = LinearInterpolation.determineInterpolatingPoints(this.data);
-
-	}
-
-	private ArrayList<Double> generateAndSolveTriDiagonal(//
-			ArrayList<ArrayList<Double>> x, ArrayList<ArrayList<Double>> y) {
+	private static ArrayList<Double> generateAndSolveTriDiagonal(ArrayList<ArrayList<Double>> x,
+			ArrayList<ArrayList<Double>> y) {
 		ArrayList<ArrayList<Double>> cof = new ArrayList<ArrayList<Double>>();
 		ArrayList<ArrayList<Double>> tempMat = new ArrayList<ArrayList<Double>>();
 		List<Double> vector = new ArrayList<Double>();
+		List<Double> mVector = new ArrayList<Double>();
 		int colLen = x.size() - 1;
 		int rowLen = colLen + 2;
 
-		List<List<Double>> array = IntStream.range(0, colLen) //
-				.mapToObj(i -> new ArrayList<Double>(Collections.nCopies(rowLen, 0d))) //
-				.collect(Collectors.toList());
+		List<List<Double>> A = IntStream.range(0, colLen) //
+				.mapToObj(i -> new ArrayList<Double>(Collections.nCopies(rowLen, 0d))).collect(Collectors.toList());
 
 		for (int i = 0; i < x.size(); i++) {
 			if (i < x.size() - 1) {
-				cof = this.generateCof(x.get(i), x.get(i + 1), y.get(i), y.get(i + 1));
+				cof = generateCof(x.get(i), x.get(i + 1), y.get(i), y.get(i + 1));
 				vector.add(cof.get(1).get(0));
 				tempMat.add(cof.get(0));
-			} else {
-				// Do nothing}
 			}
+
+			else {
+				// Do nothing
+			}
+
 		}
 
 		for (int i = 0; i < tempMat.size(); i++) {
+
 			for (int j = 0; j < tempMat.get(0).size(); j++) {
-				array.get(i).set(j + i, tempMat.get(i).get(j));
+
+				A.get(i).set(j + i, tempMat.get(i).get(j));
+
 			}
 
 		}
-		for (int i = 0; i < array.size(); i++) {
-			array.get(i).remove(0);
-			array.get(i).remove(array.get(i).size() - 1);
-		}
-		// solving system of linear equation
-		List<Double> mVector = this.linearEquationSolver(array, vector);
-		// updating the m vector
+		for (int i = 0; i < A.size(); i++) {
+			A.get(i).remove(0);
+			A.get(i).remove(A.get(i).size() - 1);
+			}
+		
+		mVector = linearEquationSolver(A, vector);
 		mVector.add(0, 0.0);
 		mVector.add(0.0);
-		System.out.println("mVector");
-		System.out.println(mVector);
+		
 
-		ArrayList<ArrayList<Double>> mVectorGrouped = this.groupToInterval((ArrayList<Double>) mVector);
-		System.out.println(mVectorGrouped);
+		//ArrayList<ArrayList<Double>> mVectorGrouped = groupToInterval((ArrayList<Double>) mVector);
 		return (ArrayList<Double>) mVector;
 
 	}
 
-	private ArrayList<ArrayList<Double>> generateCof(ArrayList<Double> intervalX1, //
-			ArrayList<Double> intervalX2, //
-			ArrayList<Double> intervalY1, //
-			ArrayList<Double> intervalY2) {
+	private static ArrayList<ArrayList<Double>> generateCof(ArrayList<Double> intervalX1, ArrayList<Double> intervalX2,
+			ArrayList<Double> intervalY1, ArrayList<Double> intervalY2) {
 		ArrayList<Double> cof = new ArrayList<>();
+		ArrayList<Double> Y = new ArrayList<>();
 		double temp1 = intervalX1.get(1) - intervalX1.get(0);
 		double temp2 = intervalX2.get(1) - intervalX1.get(0);
 		double temp3 = intervalX2.get(1) - intervalX2.get(0);
@@ -99,25 +106,19 @@ public class CubicalInterpolation {
 		double cof1 = temp1 / 6.0;
 		double cof2 = temp2 / 3.0;
 		double cof3 = temp3 / 6.0;
-
+		double y = (temp4 / temp3) - (temp5 / temp1);
 		cof.add(cof1);
 		cof.add(cof2);
 		cof.add(cof3);
-		double y = (temp4 / temp3) - (temp5 / temp1);
-		ArrayList<Double> intermediatelist = new ArrayList<>();
-		intermediatelist.add(y);
+		Y.add(y);
 		ArrayList<ArrayList<Double>> result = new ArrayList<ArrayList<Double>>();
 		result.add(cof);
-		result.add(intermediatelist);
+		result.add(Y);
 		return result;
 	}
 
-	// Attempting to solve for the
-	// linear equation in the
-	// form A*X=B
-	ArrayList<Double> linearEquationSolver(List<List<Double>> matA, List<Double> vectB) {
+	static ArrayList<Double> linearEquationSolver(List<List<Double>> matA, List<Double> vectB) {		
 		ArrayList<Double> x = new ArrayList<Double>();
-		// using library Apache math
 		double[][] tempMat = new double[matA.size()][matA.get(0).size()];
 		double[] tempVect = new double[vectB.size()];
 		for (int i = 0; i < matA.size(); i++) {
@@ -128,8 +129,8 @@ public class CubicalInterpolation {
 			}
 		}
 
-		RealMatrix matrixA = new Array2DRowRealMatrix(tempMat, false);
-		DecompositionSolver solver = new LUDecomposition(matrixA).getSolver();
+		RealMatrix MatA = new Array2DRowRealMatrix(tempMat, false);
+		DecompositionSolver solver = new LUDecomposition(MatA).getSolver();
 		RealVector vect = new ArrayRealVector(tempVect, false);
 		RealVector solution = solver.solve(vect);
 
@@ -137,18 +138,17 @@ public class CubicalInterpolation {
 			x.add(solution.getEntry(i));
 		}
 
-		System.out.print(x);
 		return x;
 
 	}
 
-	public ArrayList<ArrayList<Double>> groupToInterval(ArrayList<Double> arr) {
+	public static ArrayList<ArrayList<Double>> groupToInterval(ArrayList<Double> arr) {
 		ArrayList<ArrayList<Double>> result = new ArrayList<ArrayList<Double>>();
 		for (int i = 0; i < arr.size(); i++) {
 			ArrayList<Double> temp = new ArrayList<Double>();
-			if (i + this.groupSize < arr.size()) {
+			if (i + groupSize < arr.size()) {
 				temp.add(arr.get(i));
-				temp.add(arr.get(i + this.groupSize));
+				temp.add(arr.get(i + groupSize));
 				result.add(temp);
 			}
 
@@ -156,32 +156,75 @@ public class CubicalInterpolation {
 		return result;
 	}
 
-	public int identifyInterval(ArrayList<ArrayList<Double>> x, double interpolationValue) {
+	public static int identifyInterval(ArrayList<ArrayList<Double>> x, double interpolationValue) {
 		for (int i = 0; i < x.size(); i++) {
 			if (x.get(i).get(0) <= interpolationValue && x.get(i).get(x.get(i).size() - 1) >= interpolationValue) {
 				return i;
+
 			}
 		}
 		return -1;
+
 	}
 
-	public double interpolationFunction(ArrayList<ArrayList<Double>> xInter, ArrayList<ArrayList<Double>> yInt,
+	public static double interpolationFunction(ArrayList<ArrayList<Double>> xInter, ArrayList<ArrayList<Double>> yInt,
 			ArrayList<ArrayList<Double>> mInter, double interpolationValue) {
-
-		int index = this.identifyInterval(xInter, interpolationValue);
+		int index = identifyInterval(xInter, interpolationValue);
+		double result = 0.0;
 		double temp1 = 0.0;
 		double temp2 = 0.0;
 		double temp3 = 0.0;
 		temp1 = (double) xInter.get(index).get(1) - interpolationValue;
 		temp2 = (double) interpolationValue - xInter.get(index).get(0);
 		temp3 = xInter.get(index).get(1) - xInter.get(index).get(0);
-		double res0 = (//
-		(Math.pow(temp1, 3) * mInter.get(index).get(0) //
-				+ Math.pow(temp2, 3) * mInter.get(index).get(1))//
+		double res0 = ((Math.pow(temp1, 3) * mInter.get(index).get(0) + Math.pow(temp2, 3) * mInter.get(index).get(1))
 				/ (6 * temp3));
 		double res1 = ((temp1) * yInt.get(index).get(0) + temp2 * yInt.get(index).get(1)) / (temp3);
 		double res2 = (temp3 * (temp1 * mInter.get(index).get(0) + temp2 * mInter.get(index).get(1))) / 6;
-		double result = res0 + res1 - res2;
+		result = res0 + res1 - res2;
+
 		return result;
+
+	}
+
+	public static ArrayList<ArrayList<Double>> getInterpolatingData(ArrayList<Double> allData) {
+		// int numberOfDataPointsToConsider=10;
+		ArrayList<ArrayList<Double>> result = new ArrayList<ArrayList<Double>>();
+		ArrayList<Double> xVal = new ArrayList<Double>();
+		ArrayList<Double> yVal = new ArrayList<Double>();
+		ArrayList<Double> interpolatingPoints = new ArrayList<Double>();
+		for (int i = 0; i < allData.size(); i++) {
+			if (Double.isNaN(allData.get(i)) || allData.get(i) == null) {
+				interpolatingPoints.add((double) i);
+
+			} else {
+				xVal.add((double) i);
+				yVal.add((double) allData.get(i));
+			}
+			result.add(xVal);
+			result.add(yVal);
+			result.add(interpolatingPoints);
+
+		}
+
+		return result;
+
+	}
+
+	static boolean canInterpolate(ArrayList<Double> data) {
+		ArrayList<Double> check = new ArrayList<Double>();
+		for (int i = 0; i < data.size(); i++) {
+			if (Double.isNaN(data.get(i)) || data.get(i) == null) {
+				// Do nothing
+			} else {
+				check.add(data.get(i));
+			}
+			if (check.size() >= 4) {
+				return true;
+			} else {
+				// Do nothing
+			}
+		}
+		return false;
 	}
 }
--- io.openems.edge.predictor.lstmmodel/src/io/openems/edge/predictor/lstmmodel/interpolation/LinearInterpolation.java
+++ io.openems.edge.predictor.lstmmodel/src/io/openems/edge/predictor/lstmmodel/interpolation/LinearInterpolation.java
@@ -1,98 +1,79 @@
 package io.openems.edge.predictor.lstmmodel.interpolation;
 
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 public class LinearInterpolation {
+	ArrayList<Double> Data = new ArrayList<Double>();
 
-	ArrayList<Double> data = new ArrayList<Double>();
+	public LinearInterpolation(ArrayList<Double> data) {
 
-	public LinearInterpolation(ArrayList<Float> data) {
+		this.Data = data;
 
-		this.data = new ArrayList<Double>(//
-				data.stream() //
-						.mapToDouble(Float::doubleValue)//
-						.boxed()//
-						.collect(Collectors.toCollection(ArrayList::new))//
-		);
-
-		this.data = replaceNullWitNan(this.data);
-		System.out.println("Data Before Interpolation");
-		System.out.print(this.data);
-
-		if (Double.isNaN(this.data.get(0))) {
-			this.data.set(0, calculateMean(this.data));
-		}
-		if (Double.isNaN(this.data.get(this.data.size() - 1))) {
-			this.data.set(this.data.size() - 1, calculateMean(this.data));
-		} else {
-			//
-		}
-		ArrayList<ArrayList<ArrayList<Double>>> coordinate = determineInterpolatingPoints(this.data);
+		ArrayList<ArrayList<ArrayList<Double>>> coordinate = determineInterpolatingPoints(Data);
 		for (int i = 0; i < coordinate.size(); i++) {
-			ArrayList<Double> val = this.computeInterpolation(coordinate.get(i).get(0).get(0),
+			ArrayList<Double> val = computeInterpolation(coordinate.get(i).get(0).get(0),
 					coordinate.get(i).get(0).get(1), coordinate.get(i).get(1).get(0), coordinate.get(i).get(1).get(1));
-			this.data = this.conCat(this.data, val, coordinate.get(i).get(0).get(0), coordinate.get(i).get(0).get(1));
+			Data = conCat(Data, val, coordinate.get(i).get(0).get(0), coordinate.get(i).get(0).get(1));
 
 		}
-		System.out.println("Data after Interpolation");
 
-		System.out.println(this.data);
 	}
 
 	static ArrayList<ArrayList<ArrayList<Double>>> determineInterpolatingPoints(ArrayList<Double> data) {
+		
 
-		double x1 = -1;
-		double x2 = -1;
-		double y1 = -1.0000;
-		double y2 = -1.000000;
-		int flag = 0;
-		boolean flag1 = false;
+			double x1 = -1;
+			double x2 = -1;
+			double y1 = -1.0000;
+			double y2 = -1.000000;
+			int flag = 0;
+			boolean flag1 = false;
 
-		ArrayList<ArrayList<ArrayList<Double>>> coOrdinates = new ArrayList<ArrayList<ArrayList<Double>>>();
+			ArrayList<ArrayList<ArrayList<Double>>> Coordinate = new ArrayList<ArrayList<ArrayList<Double>>>();
 
-		for (int i = 0; i < data.size(); i++) {
-			ArrayList<Double> x = new ArrayList<Double>();
-			ArrayList<Double> y = new ArrayList<Double>();
-			ArrayList<ArrayList<Double>> temp = new ArrayList<ArrayList<Double>>();
+			for (int i = 0; i < data.size(); i++) {
+				ArrayList<Double> x = new ArrayList<Double>();
+				ArrayList<Double> y = new ArrayList<Double>();
+				ArrayList<ArrayList<Double>> temp = new ArrayList<ArrayList<Double>>();
 
-			if (Double.isNaN(data.get(i))) {
-				flag1 = true;
-			} else {
-				flag1 = false;
+				if (Double.isNaN(data.get(i))) {
+					flag1 = true;
+				} else {
+					flag1 = false;
+				}
+
+				if (flag1 == true && flag == 0) {
+					x1 = i - 1;
+					y1 = data.get(i - 1);
+					flag = 1;
+
+				} else if (flag1 == true && flag == 1) {
+
+				} else if (flag1 == false && flag == 1) {
+					x2 = i;
+					y2 = data.get(i);
+					flag = 0;
+					x.add(x1);
+					x.add(x2);
+					y.add(y1);
+					y.add(y2);
+
+					temp.add(x);
+					temp.add(y);
+					Coordinate.add(temp);
+					Double.isNaN(i);
+
+				} else {
+				}
 			}
-
-			if (flag1 == true && flag == 0) {
-				x1 = i - 1;
-				y1 = data.get(i - 1);
-				flag = 1;
-
-			} else if (flag1 == false && flag == 1) {
-				x2 = i;
-				y2 = data.get(i);
-				flag = 0;
-				x.add(x1);
-				x.add(x2);
-				y.add(y1);
-				y.add(y2);
-
-				temp.add(x);
-				temp.add(y);
-				coOrdinates.add(temp);
-				Double.isNaN(i);
-
-			} else {
-				// nothing
-			}
-		}
-		return coOrdinates;
-
+			return Coordinate;
+		
 	}
 
 	private ArrayList<Double> computeInterpolation(double x1, double x2, double y1, double y2) {
 		ArrayList<Double> val = new ArrayList<Double>();
 		for (int i = 0; i < (x2 - x1); i++) {
-			val.add(y1 * ((x2 - (i + x1)) / (x2 - x1)) + y2 * ((i) / (x2 - x1)));
+			val.add((y1 * ((x2 - (i + x1)) / (x2 - x1)) + y2 * ((i) / (x2 - x1))));
 		}
 		return val;
 	}
@@ -102,28 +83,6 @@ public class LinearInterpolation {
 		int tempX2 = (int) x2;
 		for (int i = 1; i < (tempX2 - tempX1); i++) {
 			data.set((i + tempX1), val.get(i));
-		}
-		return data;
-	}
-
-	static double calculateMean(ArrayList<Double> data) {
-		double sum = 0;
-		for (int i = 0; i < data.size(); i++) {
-			if (Double.isNaN(data.get(i))) {
-					//
-			} else {
-				sum = sum + data.get(i);
-			}
-		}
-		return sum / data.size();
-	}
-
-	static ArrayList<Double> replaceNullWitNan(ArrayList<Double> data) {
-
-		for (int i = 0; i < data.size(); i++) {
-			if (data.get(i) == null) {
-				data.set(i, Double.NaN);
-			}
 		}
 		return data;
 	}
