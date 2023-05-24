828c4157f289f418dc7622348c6a6dc40f2b5ae9
Lukas Rieger <lukas.rieger@fenecon.de>
2023-05-24 11:08:17 +0200

    update import

--- ui/src/app/edge/history/common/production/overview/overview.ts
+++ ui/src/app/edge/history/common/production/overview/overview.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractHistoryChartOverview } from '../../../../../shared/genericComponents/chart/abstractHistoryChartOverView';
+import { AbstractHistoryChartOverview } from '../../../../../shared/genericComponents/chart/abstractHistoryChartOverview';
 import { ChannelAddress, EdgeConfig } from '../../../../../shared/shared';
 
 @Component({
