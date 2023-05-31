7ae61fe169cba3c74410ab446191e18a14deed21
Huseyin Sahutoglu <hueseyin.sahutoglu@fenecon.de>
2023-05-31 09:29:32 +0200

    checkstyle

--- io.openems.edge.bridge.modbus/src/io/openems/edge/bridge/modbus/api/ElementToChannelConverter.java
+++ io.openems.edge.bridge.modbus/src/io/openems/edge/bridge/modbus/api/ElementToChannelConverter.java
@@ -80,6 +80,148 @@ public class ElementToChannelConverter {
 			StaticConverters.INVERT);
 
 	/**
+	 * Converts only negative values from Element to Channel and inverts them (makes
+	 * the value positive).
+	 */
+	public static final ElementToChannelConverter KEEP_NEGATIVE_AND_INVERT = chain(INVERT, KEEP_POSITIVE);
+
+	/**
+	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_1} and
+	 * CONVERT_POSITIVE.
+	 */
+	public static final ElementToChannelConverter SCALE_FACTOR_1_AND_KEEP_POSITIVE = chain(SCALE_FACTOR_1,
+			KEEP_POSITIVE);
+
+	/**
+	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_2} and INVERT.
+	 */
+	public static final ElementToChannelConverter SCALE_FACTOR_2_AND_INVERT = chain(SCALE_FACTOR_2, INVERT);
+
+	/**
+	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_1} and
+	 * CONVERT_NEGATIVE_AND_INVERT.
+	 */
+	public static final ElementToChannelConverter SCALE_FACTOR_1_AND_KEEP_NEGATIVE_AND_INVERT = chain(SCALE_FACTOR_1, KEEP_NEGATIVE_AND_INVERT);
+
+	/**
+	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_2} and
+	 * CONVERT_POSITIVE.
+	 */
+	public static final ElementToChannelConverter SCALE_FACTOR_2_AND_KEEP_POSITIVE = chain(SCALE_FACTOR_2, KEEP_POSITIVE);
+
+	/**
+	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_2} and @see
+	 * {@link ElementToChannelConverter#KEEP_NEGATIVE_AND_INVERT}.
+	 */
+	public static final ElementToChannelConverter SCALE_FACTOR_2_AND_KEEP_NEGATIVE_AND_INVERT = chain(SCALE_FACTOR_2, KEEP_NEGATIVE_AND_INVERT);
+
+	/**
+	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_2_AND_KEEP_NEGATIVE}
+	 * and @see {@link ElementToChannelConverter#INVERT}.
+	 */
+	public static ElementToChannelConverter SCALE_FACTOR_2_AND_KEEP_NEGATIVE = chain(SCALE_FACTOR_2_AND_KEEP_NEGATIVE_AND_INVERT, INVERT);
+
+	/**
+	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_1} and INVERT_IF_TRUE.
+	 * 
+	 * @param invert input value for {@link #INVERT_IF_TRUE(boolean)}
+	 * @return the {@link ElementToChannelConverter}
+	 */
+	// CHECKSTYLE:OFF
+	public static final ElementToChannelConverter SCALE_FACTOR_1_AND_INVERT_IF_TRUE(boolean invert) {
+		// CHECKSTYLE:ON
+		return chain(SCALE_FACTOR_1, INVERT_IF_TRUE(invert));
+	}
+
+	/**
+	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_2} and INVERT_IF_TRUE.
+	 * 
+	 * @param invert input value for {@link #INVERT_IF_TRUE(boolean)}
+	 * @return the {@link ElementToChannelConverter}
+	 */
+	// CHECKSTYLE:OFF
+	public static final ElementToChannelConverter SCALE_FACTOR_2_AND_INVERT_IF_TRUE(boolean invert) {
+		// CHECKSTYLE:ON
+		return chain(SCALE_FACTOR_2, INVERT_IF_TRUE(invert));
+	}
+
+	/**
+	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_3} and INVERT_IF_TRUE.
+	 * 
+	 * @param invert input value for {@link #INVERT_IF_TRUE(boolean)}
+	 * @return the {@link ElementToChannelConverter}
+	 */
+	// CHECKSTYLE:OFF
+	public static final ElementToChannelConverter SCALE_FACTOR_3_AND_INVERT_IF_TRUE(boolean invert) {
+		// CHECKSTYLE:ON
+		return chain(SCALE_FACTOR_3, INVERT_IF_TRUE(invert));
+	}
+
+	/**
+	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_MINUS_1} and
+	 * INVERT_IF_TRUE.
+	 * 
+	 * @param invert input value for {@link #INVERT_IF_TRUE(boolean)}
+	 * @return the {@link ElementToChannelConverter}
+	 */
+	// CHECKSTYLE:OFF
+	public static final ElementToChannelConverter SCALE_FACTOR_MINUS_1_AND_INVERT_IF_TRUE(boolean invert) {
+		// CHECKSTYLE:ON
+		return chain(SCALE_FACTOR_MINUS_1, INVERT_IF_TRUE(invert));
+	}
+
+	private final Function<Object, Object> elementToChannel;
+	private final Function<Object, Object> channelToElement;
+
+	/**
+	 * This constructs and back-and-forth converter from Element to Channel and
+	 * back.
+	 *
+	 * @param elementToChannel from Element to Channel
+	 * @param channelToElement from Channel to Element
+	 */
+	public ElementToChannelConverter(Function<Object, Object> elementToChannel,
+			Function<Object, Object> channelToElement) {
+		this.elementToChannel = elementToChannel;
+		this.channelToElement = channelToElement;
+	}
+
+	/**
+	 * This constructs a forward-only converter from Element to Channel.
+	 * Back-conversion throws an Exception.
+	 *
+	 * @param elementToChannel Element to Channel
+	 */
+	public ElementToChannelConverter(Function<Object, Object> elementToChannel) {
+		this.elementToChannel = elementToChannel;
+		this.channelToElement = value -> {
+			throw new IllegalArgumentException("Backwards-Conversion for [" + value + "] is not implemented.");
+		};
+	}
+
+	/**
+	 * Convert an Element value to a Channel value. If the value can or should not
+	 * be converted, this method returns null.
+	 *
+	 * @param value the Element value
+	 * @return the converted value or null
+	 */
+	public Object elementToChannel(Object value) {
+		return this.elementToChannel.apply(value);
+	}
+
+	/**
+	 * Convert a Channel value to an Element value. If the value can or should not
+	 * be converted, this method returns null.
+	 *
+	 * @param value the Channel value
+	 * @return the converted value or null
+	 */
+	public Object channelToElement(Object value) {
+		return this.channelToElement.apply(value);
+	}
+
+	/**
 	 * Sets the value to 'zero' if parameter is true; otherwise
 	 * {@link #DIRECT_1_TO_1}.
 	 *
@@ -147,154 +289,6 @@ public class ElementToChannelConverter {
 					}
 					return value;
 				});
-	}
-
-	/**
-	 * Converts only negative values from Element to Channel and inverts them (makes
-	 * the value positive).
-	 */
-	public static final ElementToChannelConverter KEEP_NEGATIVE_AND_INVERT = ElementToChannelConverter.chain(INVERT,
-			KEEP_POSITIVE);
-
-	/**
-	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_1} and
-	 * CONVERT_POSITIVE.
-	 */
-	public static final ElementToChannelConverter SCALE_FACTOR_1_AND_KEEP_POSITIVE = ElementToChannelConverter
-			.chain(SCALE_FACTOR_1, KEEP_POSITIVE);
-
-	/**
-	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_2} and INVERT.
-	 */
-	public static final ElementToChannelConverter SCALE_FACTOR_2_AND_INVERT = ElementToChannelConverter
-			.chain(SCALE_FACTOR_2, INVERT);
-
-	/**
-	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_1} and
-	 * CONVERT_NEGATIVE_AND_INVERT.
-	 */
-	public static final ElementToChannelConverter SCALE_FACTOR_1_AND_KEEP_NEGATIVE_AND_INVERT = ElementToChannelConverter
-			.chain(SCALE_FACTOR_1, KEEP_NEGATIVE_AND_INVERT);
-
-	/**
-	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_2} and
-	 * CONVERT_POSITIVE.
-	 */
-	public static final ElementToChannelConverter SCALE_FACTOR_2_AND_KEEP_POSITIVE = ElementToChannelConverter
-			.chain(SCALE_FACTOR_2, KEEP_POSITIVE);
-
-	/**
-	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_2} and @see
-	 * {@link ElementToChannelConverter#KEEP_NEGATIVE_AND_INVERT}.
-	 */
-	public static final ElementToChannelConverter SCALE_FACTOR_2_AND_KEEP_NEGATIVE_AND_INVERT = ElementToChannelConverter
-			.chain(SCALE_FACTOR_2, KEEP_NEGATIVE_AND_INVERT);
-
-	/**
-	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_2_AND_KEEP_NEGATIVE}
-	 * and @see {@link ElementToChannelConverter#INVERT}.
-	 */
-	public static ElementToChannelConverter SCALE_FACTOR_2_AND_KEEP_NEGATIVE = ElementToChannelConverter
-			.chain(SCALE_FACTOR_2_AND_KEEP_NEGATIVE_AND_INVERT, INVERT);
-
-	/**
-	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_1} and INVERT_IF_TRUE.
-	 * 
-	 * @param invert input value for {@link #INVERT_IF_TRUE(boolean)}
-	 * @return the {@link ElementToChannelConverter}
-	 */
-	// CHECKSTYLE:OFF
-	public static final ElementToChannelConverter SCALE_FACTOR_1_AND_INVERT_IF_TRUE(boolean invert) {
-		// CHECKSTYLE:ON
-		return ElementToChannelConverter.chain(SCALE_FACTOR_1, INVERT_IF_TRUE(invert));
-	}
-
-	/**
-	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_2} and INVERT_IF_TRUE.
-	 * 
-	 * @param invert input value for {@link #INVERT_IF_TRUE(boolean)}
-	 * @return the {@link ElementToChannelConverter}
-	 */
-	// CHECKSTYLE:OFF
-	public static final ElementToChannelConverter SCALE_FACTOR_2_AND_INVERT_IF_TRUE(boolean invert) {
-		// CHECKSTYLE:ON
-		return ElementToChannelConverter.chain(SCALE_FACTOR_2, INVERT_IF_TRUE(invert));
-	}
-
-	/**
-	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_3} and INVERT_IF_TRUE.
-	 * 
-	 * @param invert input value for {@link #INVERT_IF_TRUE(boolean)}
-	 * @return the {@link ElementToChannelConverter}
-	 */
-	// CHECKSTYLE:OFF
-	public static final ElementToChannelConverter SCALE_FACTOR_3_AND_INVERT_IF_TRUE(boolean invert) {
-		// CHECKSTYLE:ON
-		return ElementToChannelConverter.chain(SCALE_FACTOR_3, INVERT_IF_TRUE(invert));
-	}
-
-	/**
-	 * Applies {@link ElementToChannelConverter#SCALE_FACTOR_MINUS_1} and
-	 * INVERT_IF_TRUE.
-	 * 
-	 * @param invert input value for {@link #INVERT_IF_TRUE(boolean)}
-	 * @return the {@link ElementToChannelConverter}
-	 */
-	// CHECKSTYLE:OFF
-	public static final ElementToChannelConverter SCALE_FACTOR_MINUS_1_AND_INVERT_IF_TRUE(boolean invert) {
-		// CHECKSTYLE:ON
-		return ElementToChannelConverter.chain(SCALE_FACTOR_MINUS_1, INVERT_IF_TRUE(invert));
-	}
-
-	private final Function<Object, Object> elementToChannel;
-	private final Function<Object, Object> channelToElement;
-
-	/**
-	 * This constructs and back-and-forth converter from Element to Channel and
-	 * back.
-	 *
-	 * @param elementToChannel from Element to Channel
-	 * @param channelToElement from Channel to Element
-	 */
-	public ElementToChannelConverter(Function<Object, Object> elementToChannel,
-			Function<Object, Object> channelToElement) {
-		this.elementToChannel = elementToChannel;
-		this.channelToElement = channelToElement;
-	}
-
-	/**
-	 * This constructs a forward-only converter from Element to Channel.
-	 * Back-conversion throws an Exception.
-	 *
-	 * @param elementToChannel Element to Channel
-	 */
-	public ElementToChannelConverter(Function<Object, Object> elementToChannel) {
-		this.elementToChannel = elementToChannel;
-		this.channelToElement = value -> {
-			throw new IllegalArgumentException("Backwards-Conversion for [" + value + "] is not implemented.");
-		};
-	}
-
-	/**
-	 * Convert an Element value to a Channel value. If the value can or should not
-	 * be converted, this method returns null.
-	 *
-	 * @param value the Element value
-	 * @return the converted value or null
-	 */
-	public Object elementToChannel(Object value) {
-		return this.elementToChannel.apply(value);
-	}
-
-	/**
-	 * Convert a Channel value to an Element value. If the value can or should not
-	 * be converted, this method returns null.
-	 *
-	 * @param value the Channel value
-	 * @return the converted value or null
-	 */
-	public Object channelToElement(Object value) {
-		return this.channelToElement.apply(value);
 	}
 
 	/**
