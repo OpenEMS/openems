0a099799db20d88e6c19710107a87a6c96aa4f5e
Lukas Rieger <lukas.rieger@fenecon.de>
2023-06-12 15:36:04 +0200

    UI: Add AdvertWidget for 'Extended Warranty' (#681)
    Reviewed-on: https://git.intranet.fenecon.de/FENECON/fems/pulls/681
    Reviewed-by: Jan Seidemann <jan.seidemann@fenecon.de>
    Reviewed-by: Fabian Thral <fabian.thral@fenecon.de>
    Reviewed-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Co-authored-by: Lukas Rieger <lukas.rieger@fenecon.de>
    Co-committed-by: Lukas Rieger <lukas.rieger@fenecon.de>

--- ui/src/app/edge/live/advertisement/feneconHomeExtension/feneconHomeExtension.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'feneconHomeExtension',
-    templateUrl: './feneconHomeExtension.html'
-})
-export class FeneconHomeExtensionComponent { }
