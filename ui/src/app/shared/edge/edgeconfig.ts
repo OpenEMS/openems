OpenEMS/openems

commit 5e7d18c4be3b462b5a543d1613a9de1964a022ec

Introduce ElectricityMeter as replacement for SymmetricMeter & AsymmetricMeter (#2144)
Distinguishing between `SymmetricMeter` and `AsymmetricMeter` and having `AsymmetricMeter extends SymmetricMeter` did never really make sense physically. In fact the _asymmetric_ meter (i.e. the three-phase meter) is the _most normal_ case. Special cases are:
- a _symmetric_ meter, i.e. one that has the same values on all phases; this can be handled entirely by deriving the individual phase values from the sum values, via dividing the power by three.
- a _single-phase_ meter, i.e. one that only has values on one phase. This requires active configuration and then allows setting the other phases to zero. The `SinglePhaseMeter` Nature is kept, because it allows a use-case when UI should really only show one value for these devices.

The special cases can be represented properly by this newly introduced `ElectricityMeter`, which combines most of the Channels and replaces SymmetricMeter and AsymmetricMeter entirely. It also clearly distinguishes _electricity meters_ from _heat meters_ that are slowly coming to OpenEMS.

This PR includes:
- Replace SymmetricMeter and AsymmetricMeter
- Breaking:
  - Renamed Meter.Virtual.Asymmetric.Add -> Meter.Virtual.Add
  - Dropped Meter.Virtual.Symmetric.Add
  - Renamed Meter.Virtual.Symmetric.Subtract -> Meter.Virtual.Subtract
- Simulator Grid Meter Reacting: add Voltage and Current sum and per phase for better testing
- UI fixes
  - show negative current per phase
  - fix translation issues

It also tries to clarify the sometimes confusing representation of positive and negative values for Power and Current. See the Javadoc in ElectricityMeter and the discussion at https://community.openems.io/t/change-energy-channel-assignment-based-on-meter-type/1603/6.

As this PR touches a lot of files, it is kept as small, simple and non-breaking as possible. Follow-up PRs will add more features,

ui/src/app/shared/edge/edgeconfig.ts
@@ -212,6 +212,15 @@ export class EdgeConfig {
                 result.push.apply(result, this.getComponentsByFactory(factoryId));
             }
         }
+
+        // Backwards compatibilty
+        // TODO drop after full migration to ElectricityMeter
+        switch (natureId) {
+            // ElectricityMeter replaces SymmetricMeter (and AsymmetricMeter implicitely)
+            case "io.openems.edge.meter.api.ElectricityMeter":
+                result.concat(this.getComponentsImplementingNature("io.openems.edge.meter.api.SymmetricMeter"));
+        }
+
         return result;
     }
 
@@ -244,7 +253,7 @@ export class EdgeConfig {
      * Determines if Edge has a Meter device
      */
     public hasMeter(): boolean {
-        if (this.getComponentIdsImplementingNature('io.openems.edge.meter.api.SymmetricMeter').length > 0) {
+        if (this.getComponentIdsImplementingNature('io.openems.edge.meter.api.ElectricityMeter').length > 0) {
             return true;
         } else {
             return false;
@@ -260,7 +269,7 @@ export class EdgeConfig {
             return true;
         }
         // Do we have a Meter with type PRODUCTION?
-        for (let component of this.getComponentsImplementingNature("io.openems.edge.meter.api.SymmetricMeter")) {
+        for (let component of this.getComponentsImplementingNature("io.openems.edge.meter.api.ElectricityMeter")) {
             if (component.isEnabled && this.isProducer(component)) {
                 return true;
             }
@@ -336,6 +345,8 @@ export class EdgeConfig {
             case 'Fenecon.Mini.GridMeter':
             case 'Kostal.Piko.GridMeter':
             case 'SolarEdge.Grid-Meter':
+            case 'Simulator.GridMeter.Acting':
+            case 'Simulator.GridMeter.Reacting':
                 return true;
         }
         return false;
@@ -353,7 +364,8 @@ export class EdgeConfig {
             {
                 category: { title: 'Zähler', icon: 'speedometer-outline' },
                 factories: [
-                    this.getFactoriesByNature("io.openems.edge.meter.api.SymmetricMeter"),
+                    this.getFactoriesByNature("io.openems.edge.meter.api.SymmetricMeter"), // TODO replaced by ElectricityMeter
+                    this.getFactoriesByNature("io.openems.edge.meter.api.ElectricityMeter"),
                     this.getFactoriesByNature("io.openems.edge.ess.dccharger.api.EssDcCharger"),
                 ]
             },

commit c8b6186342ce02ebedeb9b23a3f0a8f9f5f7ac41

UI: fix fallback for SymmetricMeter (#2228)
This bug had been introduced in #2144

ui/src/app/shared/edge/edgeconfig.ts
@@ -196,6 +196,17 @@ export class EdgeConfig {
                 result.push.apply(result, this.getComponentIdsByFactory(factoryId));
             }
         }
+
+
+        // Backwards compatibilty
+        // TODO drop after full migration to ElectricityMeter
+        switch (natureId) {
+            // ElectricityMeter replaces SymmetricMeter (and AsymmetricMeter implicitely)
+            case "io.openems.edge.meter.api.ElectricityMeter":
+                result.push(...this.getComponentIdsImplementingNature("io.openems.edge.meter.api.SymmetricMeter"));
+        }
+
+
         return result;
     }
 
@@ -218,7 +229,7 @@ export class EdgeConfig {
         switch (natureId) {
             // ElectricityMeter replaces SymmetricMeter (and AsymmetricMeter implicitely)
             case "io.openems.edge.meter.api.ElectricityMeter":
-                result.concat(this.getComponentsImplementingNature("io.openems.edge.meter.api.SymmetricMeter"));
+                result.push(...this.getComponentsImplementingNature("io.openems.edge.meter.api.SymmetricMeter"));
         }
 
         return result;