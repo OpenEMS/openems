13d98bb8bae263d2f860d6460f35d3028c7095e0
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2023-06-17 22:41:03 +0200

    Add 'CumulatedActiveTime' Channel for Controllers with Relay (#677) (#2230)
    Co-authored-by: Sagar Venu <32655208+venu-sagar@users.noreply.github.com>

--- io.openems.edge.controller.channelthreshold/src/io/openems/edge/controller/channelthreshold/ControllerChannelThreshold.java
+++ io.openems.edge.controller.channelthreshold/src/io/openems/edge/controller/channelthreshold/ControllerChannelThreshold.java
@@ -1,5 +1,9 @@
 package io.openems.edge.controller.channelthreshold;
 
+import static io.openems.common.channel.PersistencePriority.HIGH;
+import static io.openems.common.channel.Unit.CUMULATED_SECONDS;
+import static io.openems.common.types.OpenemsType.LONG;
+
 import io.openems.edge.common.channel.Doc;
 import io.openems.edge.common.component.OpenemsComponent;
 import io.openems.edge.controller.api.Controller;
@@ -8,7 +12,11 @@ public interface ControllerChannelThreshold extends Controller, OpenemsComponent
 
 	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
 		STATE_MACHINE(Doc.of(State.values()) //
-				.text("Current State of State-Machine"));
+				.text("Current State of State-Machine")),
+
+		CUMULATED_ACTIVE_TIME(Doc.of(LONG)//
+				.unit(CUMULATED_SECONDS) //
+				.persistencePriority(HIGH));
 
 		private final Doc doc;
 
