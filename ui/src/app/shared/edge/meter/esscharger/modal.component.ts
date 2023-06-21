12388271daf66be12b963eaf4c84e56d21edc3df
Lukas Rieger <lukas.rieger@fenecon.de>
2023-06-21 17:17:04 +0200

    refactor renamed converters; change electricityMeter and essCharger to use new Converter
    (cherry picked from commit 7c38b0ac29905a514c03f2e5a95ce58082c4159a)

--- ui/src/app/shared/edge/meter/esscharger/modal.component.ts
+++ ui/src/app/shared/edge/meter/esscharger/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Converter } from 'src/app/shared/genericComponents/shared/converter';
 import { Utils } from 'src/app/shared/shared';
 import { Role } from 'src/app/shared/type/role';
 import { EdgeConfig } from '../../edgeconfig';
@@ -11,4 +12,5 @@ export class EssChargerComponent {
     @Input() public component: EdgeConfig.Component;
     protected readonly Role = Role;
     protected readonly Utils = Utils;
+    protected readonly Converter = Converter;
 }
