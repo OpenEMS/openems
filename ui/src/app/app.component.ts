7051fa1cde28f955edaa6696a31989d5381dce0c
Lukas Rieger <lukas.rieger@fenecon.de>
2023-06-28 10:18:12 +0200

    add missing injection of globalRouteChangeHandler

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Subject } from 'rxjs';
 import { filter, takeUntil } from 'rxjs/operators';
 
 import { environment } from '../environments';
+import { GlobalRouteChangeHandler } from './shared/service/globalRouteChangeHandler';
 import { Service, UserPermission, Websocket } from './shared/shared';
 import { Language } from './shared/type/language';
 
@@ -31,7 +32,8 @@ export class AppComponent implements OnInit, OnDestroy {
     public service: Service,
     public toastController: ToastController,
     public websocket: Websocket,
-    private titleService: Title
+    private titleService: Title,
+    private globalRouteChangeHandler: GlobalRouteChangeHandler
   ) {
     service.setLang(Language.getByKey(localStorage.LANGUAGE) ?? Language.getByBrowserLang(navigator.language));
 
