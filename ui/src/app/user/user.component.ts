ef9320c5cd87d04d2a2ad2a51053dc4828599ef2
Lukas Rieger <lukas.rieger@fenecon.de>
2023-07-13 17:40:33 +0200

    UI: Backport OpenEMS to FEMS (#712)
    Co-authored-by: Lukas Rieger <73471197+lukasrgr@users.noreply.github.com>
    Co-authored-by: Sagar <sagar.venu@fenecon.de>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Co-authored-by: Kai J <99220919+da-Kai@users.noreply.github.com>
    Co-authored-by: Sebastian Asen <47855186+sebastianasen@users.noreply.github.com>
    Reviewed-on: https://git.intranet.fenecon.de/FENECON/fems/pulls/712
    Co-authored-by: Lukas Rieger <lukas.rieger@fenecon.de>
    Co-committed-by: Lukas Rieger <lukas.rieger@fenecon.de>

--- ui/src/app/user/user.component.ts
+++ ui/src/app/user/user.component.ts
@@ -238,8 +238,7 @@ export class UserComponent implements OnInit {
   }
 
   public toggleDebugMode(event: CustomEvent) {
-
-    sessionStorage.setItem("DEBUGMODE", event.detail['checked']);
+    localStorage.setItem("DEBUGMODE", event.detail['checked']);
     this.environment.debugMode = event.detail['checked'];
   }
 
