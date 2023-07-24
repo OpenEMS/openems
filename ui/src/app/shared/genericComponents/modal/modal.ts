8d5cb0e97480a1558831124b545eb872d1ac019b
Mohammadmahdi Ataei <mohammadmahdi.ataei@fenecon.de>
2023-07-24 08:59:57 +0200

    UI: EVCS add save-button (#674)
    Co-authored-by: Mohammadmahdi Ataei <Mohammadmahdi.Ataei@fenecon.de>
    Reviewed-on: https://git.intranet.fenecon.de/FENECON/fems/pulls/674
    Reviewed-by: Jan Seidemann <jan.seidemann@fenecon.de>
    Reviewed-by: Lukas Rieger <lukas.rieger@fenecon.de>
    Reviewed-by: Sebastian Asen <sebastian.asen@fenecon.de>
    Reviewed-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Co-authored-by: Mohammadmahdi Ataei <mohammadmahdi.ataei@fenecon.de>
    Co-committed-by: Mohammadmahdi Ataei <mohammadmahdi.ataei@fenecon.de>

--- ui/src/app/shared/genericComponents/modal/modal.ts
+++ ui/src/app/shared/genericComponents/modal/modal.ts
@@ -6,6 +6,12 @@ import { Edge, EdgeConfig, Service, Websocket } from "../../shared";
 import { Role } from "../../type/role";
 import { Icon } from "../../type/widget";
 
+export enum Status {
+    SUCCESS,
+    ERROR,
+    PENDING
+}
+
 @Component({
     selector: 'oe-modal',
     templateUrl: './modal.html',
@@ -24,10 +30,12 @@ export class ModalComponent {
     /** Title in Header */
     @Input() public title: string;
 
-    @Input() protected toolbarButtons: { url: string, icon: Icon }[] | { url: string, icon: Icon } | null = null;
+    @Input() protected toolbarButtons: { url: string, icon: Icon }[] | { url: string, icon: Icon } | {
+        callback: () =>
+            {}, icon: Icon
+    } | null = null;
 
     @Input() protected helpKey: string | null = null;
-
     public readonly Role = Role;
 
     private edge: Edge = null;
@@ -44,8 +52,10 @@ export class ModalComponent {
     // Changes applied together
     public applyChanges() {
         let updateComponentArray: { name: string, value: any }[] = [];
+        this.service.startSpinner('spinner');
         for (let key in this.formGroup.controls) {
             let control = this.formGroup.controls[key];
+            this.formGroup.controls[key];
 
             // Check if formControl-value didn't change
             if (control.pristine) {
@@ -64,7 +74,7 @@ export class ModalComponent {
                     this.service.toast(this.translate.instant('General.changeAccepted'), 'success');
                 }).catch(reason => {
                     this.service.toast(this.translate.instant('General.changeFailed') + '\n' + reason.error.message, 'danger');
-                });
+                }).finally(() => this.service.stopSpinner('spinner'));
         }
         this.formGroup.markAsPristine();
     }
