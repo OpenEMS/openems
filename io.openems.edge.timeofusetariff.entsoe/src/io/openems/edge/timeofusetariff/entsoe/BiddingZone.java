c05195ed0f3e171b67e165a6d9725a394931797e
Sagar Bandi Venu <sagar.venu@fenecon.de>
2023-08-02 10:11:03 +0200

    Refactored Bidding zone

--- io.openems.edge.timeofusetariff.entsoe/src/io/openems/edge/timeofusetariff/entsoe/BiddingZone.java
+++ io.openems.edge.timeofusetariff.entsoe/src/io/openems/edge/timeofusetariff/entsoe/BiddingZone.java
@@ -1,28 +1,42 @@
 package io.openems.edge.timeofusetariff.entsoe;
 
+/**
+ * https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html#_areas.
+ */
 public enum BiddingZone {
-	GERMANY("10Y1001A1001A82H", "BZN|DE-LU"), //
-	AUSTRIA("10YAT-APG------L", "BZN|AT"), //
-	SWEDEN_ZONE_1("10Y1001A1001A44P", "BZN|SE1"), //
-	SWEDEN_ZONE_2("10Y1001A1001A45N", "BZN|SE2"), //
-	SWEDEN_ZONE_3("10Y1001A1001A46L", "BZN|SE3"), //
-	SWEDEN_ZONE_4("10Y1001A1001A47J", "BZN|SE4"), //
+	/**
+	 * BZN|DE-LU.
+	 */
+	GERMANY("10Y1001A1001A82H"), //
+	/**
+	 * BZN|AT.
+	 */
+	AUSTRIA("10YAT-APG------L"), //
+	/**
+	 * BZN|SE1.
+	 */
+	SWEDEN_ZONE_1("10Y1001A1001A44P"), //
+	/**
+	 * BZN|SE2.
+	 */
+	SWEDEN_ZONE_2("10Y1001A1001A45N"), //
+	/**
+	 * BZN|SE3.
+	 */
+	SWEDEN_ZONE_3("10Y1001A1001A46L"), //
+	/**
+	 * BZN|SE4.
+	 */
+	SWEDEN_ZONE_4("10Y1001A1001A47J"), //
 	;
 
 	private final String code;
-	private final String zone;
 
-	private BiddingZone(String code, String zone) {
+	private BiddingZone(String code) {
 		this.code = code;
-		this.zone = zone;
 	}
 
 	public String getCode() {
 		return this.code;
 	}
-
-	public String getZone() {
-		return this.zone;
-	}
-
 }
