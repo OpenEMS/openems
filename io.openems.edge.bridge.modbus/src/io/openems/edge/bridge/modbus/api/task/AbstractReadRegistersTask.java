4c8f3a67cab91088be90dd761b2fb6db5061afb3
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2023-08-24 01:41:28 +0200

    ModbusBridge: Fix ClassCastException to Register[] (#2320)
    This fixes the issue reported in https://community.openems.io/t/modbus-tcp-gerat-mit-eclipse-debuggen/1839/2
    
    * Add JUnit test that shows previous implementation error
    * Parse FC4 response to Register[]
    
    Tested on private setup

--- io.openems.edge.bridge.modbus/src/io/openems/edge/bridge/modbus/api/task/AbstractReadRegistersTask.java
+++ io.openems.edge.bridge.modbus/src/io/openems/edge/bridge/modbus/api/task/AbstractReadRegistersTask.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 import com.ghgande.j2mod.modbus.msg.ModbusRequest;
 import com.ghgande.j2mod.modbus.msg.ModbusResponse;
-import com.ghgande.j2mod.modbus.procimg.InputRegister;
+import com.ghgande.j2mod.modbus.procimg.Register;
 
 import io.openems.common.exceptions.OpenemsException;
 import io.openems.edge.bridge.modbus.api.element.ModbusElement;
@@ -12,23 +12,25 @@ import io.openems.edge.bridge.modbus.api.element.ModbusRegisterElement;
 import io.openems.edge.common.taskmanager.Priority;
 
 @SuppressWarnings("rawtypes")
-public abstract class AbstractReadInputRegistersTask<REQUEST extends ModbusRequest, RESPONSE extends ModbusResponse>
-		extends AbstractReadTask<REQUEST, RESPONSE, ModbusRegisterElement, InputRegister> {
+public abstract class AbstractReadRegistersTask<//
+		REQUEST extends ModbusRequest, //
+		RESPONSE extends ModbusResponse> //
+		extends AbstractReadTask<REQUEST, RESPONSE, ModbusRegisterElement, Register> {
 
-	public AbstractReadInputRegistersTask(String name, Class<RESPONSE> responseClazz, int startAddress,
-			Priority priority, ModbusElement... elements) {
+	public AbstractReadRegistersTask(String name, Class<RESPONSE> responseClazz, int startAddress, Priority priority,
+			ModbusElement... elements) {
 		super(name, responseClazz, ModbusRegisterElement.class, startAddress, priority, elements);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	protected void handleResponse(ModbusRegisterElement element, int position, InputRegister[] response)
+	protected final void handleResponse(ModbusRegisterElement element, int position, Register[] response)
 			throws OpenemsException {
 		element.setInputValue(Arrays.copyOfRange(response, position, position + element.length));
 	}
 
 	@Override
-	protected int calculateNextPosition(ModbusElement modbusElement, int position) {
+	protected final int calculateNextPosition(ModbusElement modbusElement, int position) {
 		return position + modbusElement.length;
 	}
 }
--- io.openems.edge.bridge.modbus/test/io/openems/edge/bridge/modbus/api/task/FC3ReadRegistersTaskTest.java
+++ io.openems.edge.bridge.modbus/test/io/openems/edge/bridge/modbus/api/task/FC3ReadRegistersTaskTest.java
@@ -1,6 +1,9 @@
 package io.openems.edge.bridge.modbus.api.task;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.Test;
 
@@ -17,15 +20,31 @@ import io.openems.edge.common.taskmanager.Priority;
 public class FC3ReadRegistersTaskTest {
 
 	@Test
-	public void testToLogMessage() throws OpenemsException {
+	public void test() throws OpenemsException {
 		var component = new DummyModbusComponent();
-		var task = new FC3ReadRegistersTask(20, Priority.LOW, new UnsignedDoublewordElement(20));
+		var value = new AtomicReference<Long>();
+		var element20 = new UnsignedDoublewordElement(20);
+		element20.onUpdateCallback(v -> value.set(v));
+		var element22 = new UnsignedDoublewordElement(22);
+		element22.onUpdateCallback(v -> value.set(v));
+
+		var task = new FC3ReadRegistersTask(20, Priority.LOW, element20, element22);
 		task.setParent(component);
 		var request = task.createModbusRequest();
 		var response = (ReadMultipleRegistersResponse) request.getResponse();
-		response.setRegisters(new Register[] { new SimpleRegister(100), new SimpleRegister(200) });
+		response.setRegisters(new Register[] { //
+				new SimpleRegister(100), new SimpleRegister(200), //
+				new SimpleRegister(300), new SimpleRegister(400) });
 
-		assertEquals("FC3ReadHoldingRegisters [device0;unitid=1;priority=LOW;ref=20/0x14;length=2;response=0064 00c8]",
+		assertEquals(
+				"FC3ReadHoldingRegisters [device0;unitid=1;priority=LOW;ref=20/0x14;length=4;response=0064 00c8 012c 0190]",
 				task.toLogMessage(LogVerbosity.READS_AND_WRITES_VERBOSE, request, response));
+
+		var registers = task.parseResponse(response);
+		assertNull(value.get());
+		task.handleResponse(element20, 0, registers);
+		assertEquals(Long.valueOf(6553800), value.get());
+		task.handleResponse(element22, 2, registers);
+		assertEquals(Long.valueOf(19661200), value.get());
 	}
 }
