9a97ab6f9b6ef2081e23203967ba1371f04f88c6
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2023-09-01 12:47:59 +0200

    Update release scripts

--- tools/build-debian-package.sh
+++ tools/build-debian-package.sh
@@ -8,10 +8,8 @@ main() {
     initialize_environment
     print_header
     check_dependencies
-    update_version_in_code
-    build_edge & # run in parallel
-    build_ui & # run in parallel
-    wait # wait for edge+ui
+    common_update_version_in_code
+    common_build_edge_and_ui_in_parallel
     prepare_deb_template
     build_deb
     clean_deb_template
@@ -23,17 +21,11 @@ initialize_environment() {
     SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
     cd ${SCRIPT_DIR}/..
 
-    # Code files relevant for version
-    SRC_OPENEMS_CONSTANTS="io.openems.common/src/io/openems/common/OpenemsConstants.java"
-    SRC_PACKAGE_JSON="ui/package.json"
-    SRC_CHANGELOG_COMPONENT="ui/src/app/changelog/view/component/changelog.constants.ts"
+    # Include commons
+    source $SCRIPT_DIR/common.sh
+    common_initialize_environment
 
-    # Set environment variables
-    THEME="openems"
-    PACKAGE_NAME="openems-edge"
-    VERSION_STRING=""
-    VERSION="$(cd ui && node -p "require('./package.json').version" && cd ..)"
-
+    # Build detailed SNAPSHOT name
     if [[ "$VERSION" == *"-SNAPSHOT" ]]; then
         GIT_BRANCH="$(git branch --show-current)"
         GIT_BRANCH="${GIT_BRANCH/\//"."}"
@@ -64,52 +56,6 @@ print_header() {
 # Check for required dependencies
 check_dependencies() {
     hash dpkg 2>/dev/null || { echo >&2 "I require 'dpkg' but it's not installed. Aborting."; exit 1; }
-}
-
-# Inserts the version number into the Code
-update_version_in_code() {
-    echo "# Update version in Code"
-    echo "## Update $SRC_OPENEMS_CONSTANTS"
-    sed --in-place "s/\(VERSION_STRING = \)\"\(.*\)\";$/\1\"$VERSION_STRING\";/" $SRC_OPENEMS_CONSTANTS
-
-    echo "## Update $SRC_PACKAGE_JSON"
-    sed --in-place "s/\(\"version\": \"\).*\(\".*$\)/\1$VERSION\2/" $SRC_PACKAGE_JSON
-
-    echo "## Update $SRC_CHANGELOG_COMPONENT"
-    sed --in-place "s/\(long: \"\).*\(\"\)$/\1$VERSION\2/" $SRC_CHANGELOG_COMPONENT
-}
-
-# Build OpenEMS Edge
-build_edge() {
-    echo "# Build OpenEMS Edge"
-    ./gradlew --build-cache build buildEdge resolve.EdgeApp resolve.BackendApp
-    git diff --exit-code io.openems.edge.application/EdgeApp.bndrun io.openems.backend.application/BackendApp.bndrun
-}
-
-# Build OpenEMS UI
-build_ui() {
-    echo "# Build OpenEMS UI"
-    if [ "${NODE_MODULES_CACHE}" != "" -a -d "$NODE_MODULES_CACHE" ]; then
-        echo "## Use cached node_modules"
-        mv -f "${NODE_MODULES_CACHE}" "ui/node_modules"
-    fi
-    cd ui
-
-    # Install dependencies from package.json
-    npm ci
-    if [ "${NG_CLI_CACHE_PATH}" != "" ]; then 
-        echo "## Angular Cache: $NG_CLI_CACHE_PATH"
-        node_modules/.bin/ng config cli.cache.path "$NG_CLI_CACHE_PATH"
-    fi
-    # Lint and Build
-    node_modules/.bin/ng lint
-    node_modules/.bin/ng build -c "${THEME},${THEME}-edge-prod,prod"
-    cd ..
-
-    if [ "${NODE_MODULES_CACHE}" != "" ]; then
-        echo "## Refresh node_modules cache"
-        mv -f "ui/node_modules" "${NODE_MODULES_CACHE}"
-    fi
 }
 
 # Prepare Debian template
