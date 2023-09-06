9f5540ed8c39255a0e90ed02f790afb1e3187d19
DerWahreKlinki <thomas@partec.org>
2023-09-06 13:24:32 +0200

    Added ModbusSlave-funtionality to controller
    The status of the controller (regulation active or not) is readable via modbus

--- io.openems.edge.controller.io.channelsinglethreshold/src/io/openems/edge/controller/io/channelsinglethreshold/ControllerIoChannelSingleThreshold.java
+++ io.openems.edge.controller.io.channelsinglethreshold/src/io/openems/edge/controller/io/channelsinglethreshold/ControllerIoChannelSingleThreshold.java
@@ -3,8 +3,9 @@ package io.openems.edge.controller.io.channelsinglethreshold;
 import static io.openems.common.channel.PersistencePriority.HIGH;
 import static io.openems.common.channel.Unit.CUMULATED_SECONDS;
 import static io.openems.common.types.OpenemsType.LONG;
-
+import static io.openems.common.types.OpenemsType.BOOLEAN;
 import io.openems.common.channel.Level;
+import io.openems.edge.common.channel.BooleanReadChannel;
 import io.openems.edge.common.channel.Doc;
 import io.openems.edge.common.channel.StateChannel;
 import io.openems.edge.common.channel.value.Value;
@@ -13,13 +14,17 @@ import io.openems.edge.controller.api.Controller;
 
 public interface ControllerIoChannelSingleThreshold extends Controller, OpenemsComponent {
 
+
+
 	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
 		AWAITING_HYSTERESIS(Doc.of(Level.INFO) //
 				.text("Would change State, but hystesis is active")),
 
 		CUMULATED_ACTIVE_TIME(Doc.of(LONG)//
 				.unit(CUMULATED_SECONDS) //
-				.persistencePriority(HIGH)) //
+				.persistencePriority(HIGH)), //
+		
+		REGULATION_ACTIVE(Doc.of(BOOLEAN))
 		; //
 
 		private final Doc doc;
@@ -34,6 +39,34 @@ public interface ControllerIoChannelSingleThreshold extends Controller, OpenemsC
 		}
 	}
 
+	/**
+	 * Internal method to set the 'nextValue' on {@link ChannelId#REGULATION_ACTIVE}
+	 * Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setRegulationActive(boolean value) {
+		this.getRegulationActiveChannel().setNextValue(value);
+	}
+	
+	/**
+	 * Gets the actual regulation state. See {@link ChannelId#REGULATION_ACTIVE}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Boolean> getRegulationActive() {
+		return this.getRegulationActiveChannel().value();
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#REGULATION_ACTIVE}.
+	 *
+	 * @return the Channel
+	 */
+	public default BooleanReadChannel getRegulationActiveChannel() {
+		return this.channel(ChannelId.REGULATION_ACTIVE);
+	}	
+	
 	/**
 	 * Gets the Channel for {@link ChannelId#AWAITING_HYSTERESIS}.
 	 *
