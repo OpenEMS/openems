ff4f13427b9eceaeda287998ab2052b8fbd8aa0c
iammahdi96 <54065538+mahdiataie@users.noreply.github.com>
2023-09-06 11:31:11 +0200

    UI: add skeleton for user contact details. (#2317)

--- ui/src/app/user/user.component.ts
+++ ui/src/app/user/user.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { FormlyFieldConfig } from '@ngx-formly/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Changelog } from 'src/app/changelog/view/component/changelog.constants';
 import { environment } from '../../environments';
@@ -12,6 +11,7 @@ import { GetUserInformationResponse } from '../shared/jsonrpc/response/getUserIn
 import { Service, Websocket } from '../shared/shared';
 import { COUNTRY_OPTIONS } from '../shared/type/country';
 import { Language } from '../shared/type/language';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 type UserInformation = {
   firstname: string,
@@ -34,7 +34,7 @@ export class UserComponent implements OnInit {
   public readonly languages: Language[] = Language.ALL;
   public currentLanguage: Language;
   public isEditModeDisabled: boolean = true;
-  public form: { formGroup: FormGroup, fields: FormlyFieldConfig[], model: UserInformation };
+  public form: { formGroup: FormGroup, model: UserInformation };
   public showInformation: boolean = false;
 
   constructor(
@@ -48,11 +48,9 @@ export class UserComponent implements OnInit {
     // Set currentLanguage to 
     this.currentLanguage = Language.getByKey(localStorage.LANGUAGE) ?? Language.DEFAULT;
     this.service.setCurrentComponent({ languageKey: 'Menu.user' }, this.route);
-
     this.getUserInformation().then((userInformation) => {
       this.form = {
         formGroup: new FormGroup({}),
-        fields: this.getFields(),
         model: userInformation
       };
       this.showInformation = true;
@@ -88,7 +86,6 @@ export class UserComponent implements OnInit {
       this.getUserInformation().then((userInformation) => {
         this.form = {
           formGroup: new FormGroup({}),
-          fields: this.getFields(),
           model: userInformation
         };
       });
@@ -99,88 +96,81 @@ export class UserComponent implements OnInit {
 
   public enableAndDisableFormFields(): boolean {
     // Update Fields
-    this.form?.fields[0].fieldGroup.forEach(element => {
-      element.templateOptions.disabled = !element.templateOptions.disabled;
+    this.userInformationFields.forEach(element => {
+      element.props.disabled = !element.props.disabled;
     });
     return this.isEditModeDisabled = !this.isEditModeDisabled;
   }
 
-
-  public getFields(): FormlyFieldConfig[] {
-
-    return [{
-      fieldGroup: [
-        {
-          key: "firstname",
-          type: "input",
-          templateOptions: {
-            label: this.translate.instant("Register.Form.firstname"),
-            disabled: true
-          }
-        },
-        {
-          key: "lastname",
-          type: "input",
-          templateOptions: {
-            label: this.translate.instant("Register.Form.lastname"),
-            disabled: true
-          }
-        },
-        {
-          key: "street",
-          type: "input",
-          templateOptions: {
-            label: this.translate.instant("Register.Form.street"),
-            disabled: true
-          }
-        },
-        {
-          key: "zip",
-          type: "input",
-          templateOptions: {
-            label: this.translate.instant("Register.Form.zip"),
-            disabled: true
-          }
-        },
-        {
-          key: "city",
-          type: "input",
-          templateOptions: {
-            label: this.translate.instant("Register.Form.city"),
-            disabled: true
-          }
-        },
-        {
-          key: "country",
-          type: "select",
-          templateOptions: {
-            label: this.translate.instant("Register.Form.country"),
-            options: COUNTRY_OPTIONS(this.translate),
-            disabled: true
-          }
-        },
-        {
-          key: "email",
-          type: "input",
-          templateOptions: {
-            label: this.translate.instant("Register.Form.email"),
-            disabled: true
-          },
-          validators: {
-            validation: [Validators.email]
-          }
-        },
-        {
-          key: "phone",
-          type: "input",
-          templateOptions: {
-            label: this.translate.instant("Register.Form.phone"),
-            disabled: true
-          }
-        }
-      ]
-    }];
-  }
+  /** Needs to be predefined to make wrapper work with ion-skeleton */
+  protected userInformationFields: FormlyFieldConfig[] = [{
+    key: "firstname",
+    type: "input",
+    templateOptions: {
+      label: this.translate.instant("Register.Form.firstname"),
+      disabled: true
+    }
+  },
+  {
+    key: "lastname",
+    type: "input",
+    templateOptions: {
+      label: this.translate.instant("Register.Form.lastname"),
+      disabled: true
+    }
+  },
+  {
+    key: "street",
+    type: "input",
+    templateOptions: {
+      label: this.translate.instant("Register.Form.street"),
+      disabled: true
+    }
+  },
+  {
+    key: "zip",
+    type: "input",
+    templateOptions: {
+      label: this.translate.instant("Register.Form.zip"),
+      disabled: true
+    }
+  },
+  {
+    key: "city",
+    type: "input",
+    templateOptions: {
+      label: this.translate.instant("Register.Form.city"),
+      disabled: true
+    }
+  },
+  {
+    key: "country",
+    type: "select",
+    templateOptions: {
+      label: this.translate.instant("Register.Form.country"),
+      options: COUNTRY_OPTIONS(this.translate),
+      disabled: true
+    }
+  },
+  {
+    key: "email",
+    type: "input",
+    templateOptions: {
+      label: this.translate.instant("Register.Form.email"),
+      disabled: true
+    },
+    validators: {
+      validation: [Validators.email]
+    }
+  },
+  {
+    key: "phone",
+    type: "input",
+    templateOptions: {
+      label: this.translate.instant("Register.Form.phone"),
+      disabled: true
+    }
+  }];
 
   public getUserInformation(): Promise<UserInformation> {
 
