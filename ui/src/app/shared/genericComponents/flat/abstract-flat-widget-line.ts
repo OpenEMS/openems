b0c1538082ed06571c797c569917b399f4ebe633
Lukas Rieger <lukas.rieger@fenecon.de>
2023-09-07 12:01:01 +0200

    fix linting

--- ui/src/app/shared/genericComponents/flat/abstract-flat-widget-line.ts
+++ ui/src/app/shared/genericComponents/flat/abstract-flat-widget-line.ts
@@ -75,7 +75,7 @@ export abstract class AbstractFlatWidgetLine implements OnChanges, OnDestroy {
   public ngOnDestroy() {
     // Unsubscribe from OpenEMS
     if (this.edge != null && this._channelAddress) {
-      this.edge.unsubscribeFromChannels(this.websocket, [this._channelAddress])
+      this.edge.unsubscribeFromChannels(this.websocket, [this._channelAddress]);
     }
 
     // Unsubscribe from CurrentData subject
