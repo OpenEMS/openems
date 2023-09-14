5fbc60ee411b4dae6e7a8b32d186f88aa5b57064
Sascha Marcel Hacker <smhrambo@googlemail.com>
2023-09-14 16:28:01 +0200

    init

---
+++ tools/docker/openems-ui/src/ui/src/themes/openems/environments/edge-dev.ts
@@ -0,0 +1,13 @@
+import { Environment } from "src/environments";
+import { theme } from "./theme";
+
+export const environment: Environment = {
+    ...theme, ...{
+
+        backend: 'OpenEMS Edge',
+        url: "ws://" + location.hostname + (location.port ? ":" + location.port : "") + "/websocket",
+
+        production: false,
+        debugMode: true
+    }
+};
