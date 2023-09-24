a286584d2bd1d14d7fcd64aace9842b488c9136b
Lukas Rieger <73471197+lukasrgr@users.noreply.github.com>
2023-09-24 22:51:11 +0200

    UI: Unit tests for autarchy, selfconsumption and consumption (#2241)
    - Refactor modals of autarchy, selfConsumption and consumption-modal in live
    - add unittests

--- ui/src/app/edge/live/common/selfconsumption/modal/modal.ts
+++ ui/src/app/edge/live/common/selfconsumption/modal/modal.ts
@@ -1,6 +1,24 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { AbstractFormlyComponent, OeFormlyView } from 'src/app/shared/genericComponents/shared/oe-formly-component';
+import { EdgeConfig } from 'src/app/shared/shared';
+import { Role } from 'src/app/shared/type/role';
 
 @Component({
-    templateUrl: './modal.html'
+    templateUrl: '../../../../../shared/formly/formly-field-modal/template.html'
 })
-export class ModalComponent { }
+export class ModalComponent extends AbstractFormlyComponent {
+    protected override generateView(config: EdgeConfig, role: Role): OeFormlyView {
+        return ModalComponent.generateView(this.translate);
+    }
+
+    public static generateView(translate: TranslateService): OeFormlyView {
+        return {
+            title: translate.instant('General.selfConsumption'),
+            lines: [{
+                type: 'info-line',
+                name: translate.instant("Edge.Index.Widgets.selfconsumptionInfo")
+            }]
+        };
+    }
+}
