414a3990fb38e1b7f1132ddbdd3837e214897e43
Lukas Rieger <lukas.rieger@fenecon.de>
2023-09-26 12:02:48 +0200

    Merge remote-tracking branch 'intern/feature/ui-dev/nativeApp' into develop
    # Conflicts:
    #	cnf/pom.xml
    #	io.openems.edge.core/test/io/openems/edge/app/integratedsystem/TestFeneconHome30.java
    #	ui/package-lock.json

---
+++ ui/capacitor.config.ts
@@ -0,0 +1,30 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fenecon',
+  webDir: 'target',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 1000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  }
+};
+
+export default config;
