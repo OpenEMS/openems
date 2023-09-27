6a02a22acba4774b536a93011aa6810057bad604
Chris Blättermann <chris.blaettermann@voltstorage.com>
2023-09-27 14:29:28 +0200

    added capacitor dependencies

--- ui/capacitor.config.ts
+++ ui/capacitor.config.ts
@@ -1,29 +1,11 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'fenecon',
+  appId: 'io.openems.app',
+  appName: 'OpenEMS',
   webDir: 'target',
   server: {
     androidScheme: 'https'
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 1000,
-      launchAutoHide: true,
-      launchFadeOutDuration: 1000,
-      backgroundColor: "#ffffffff",
-      androidSplashResourceName: "splash",
-      androidScaleType: "CENTER_CROP",
-      showSpinner: true,
-      androidSpinnerStyle: "large",
-      iosSpinnerStyle: "small",
-      spinnerColor: "#999999",
-      splashFullScreen: true,
-      splashImmersive: true,
-      layoutName: "launch_screen",
-      useDialog: true,
-    },
   }
 };
 
