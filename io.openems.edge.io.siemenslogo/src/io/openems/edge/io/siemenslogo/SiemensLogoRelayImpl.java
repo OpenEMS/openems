f5a3b8822c771e7d3635c7dee01cba8b66a370e5
DerWahreKlinki <102486171+DerWahreKlinki@users.noreply.github.com>
2023-09-28 18:32:28 +0200

    Update SiemensLogoRelayImpl.java
    Module does not use different addresses for reading and writing any more

--- io.openems.edge.io.siemenslogo/src/io/openems/edge/io/siemenslogo/SiemensLogoRelayImpl.java
+++ io.openems.edge.io.siemenslogo/src/io/openems/edge/io/siemenslogo/SiemensLogoRelayImpl.java
@@ -38,7 +38,7 @@ public class SiemensLogoRelayImpl extends AbstractSiemensLogoRelay
 		implements SiemensLogoRelay, DigitalOutput, ModbusComponent, OpenemsComponent, ModbusSlave {
 
 	private int readOffset = 0;
-	private int writeOffset = 0;
+	
 
 	@Reference
 	protected ConfigurationAdmin cm;
@@ -71,7 +71,7 @@ public class SiemensLogoRelayImpl extends AbstractSiemensLogoRelay
 	protected ModbusProtocol defineModbusProtocol() throws OpenemsException {
 
 		this.readOffset = this.config.modbusOffsetReadAddress();
-		this.writeOffset = this.config.modbusOffsetWriteAddress();
+		
 		return new ModbusProtocol(this, //
 				/*
 				 * For Read: Read Coils
@@ -89,22 +89,22 @@ public class SiemensLogoRelayImpl extends AbstractSiemensLogoRelay
 				/*
 				 * For Write: Write Single Coil
 				 */
-				new FC5WriteCoilTask(0 + this.writeOffset,
-						m(SiemensLogoRelay.ChannelId.RELAY_1, new CoilElement(0 + this.writeOffset))), //
-				new FC5WriteCoilTask(1 + this.writeOffset,
-						m(SiemensLogoRelay.ChannelId.RELAY_2, new CoilElement(1 + this.writeOffset))), //
-				new FC5WriteCoilTask(2 + this.writeOffset,
-						m(SiemensLogoRelay.ChannelId.RELAY_3, new CoilElement(2 + this.writeOffset))), //
-				new FC5WriteCoilTask(3 + this.writeOffset,
-						m(SiemensLogoRelay.ChannelId.RELAY_4, new CoilElement(3 + this.writeOffset))), //
-				new FC5WriteCoilTask(4 + this.writeOffset,
-						m(SiemensLogoRelay.ChannelId.RELAY_5, new CoilElement(4 + this.writeOffset))), //
-				new FC5WriteCoilTask(5 + this.writeOffset,
-						m(SiemensLogoRelay.ChannelId.RELAY_6, new CoilElement(5 + this.writeOffset))), //
-				new FC5WriteCoilTask(6 + this.writeOffset,
-						m(SiemensLogoRelay.ChannelId.RELAY_7, new CoilElement(6 + this.writeOffset))), //
-				new FC5WriteCoilTask(7 + this.writeOffset,
-						m(SiemensLogoRelay.ChannelId.RELAY_8, new CoilElement(7 + this.writeOffset))) //
+				new FC5WriteCoilTask(0 + this.readOffset,
+						m(SiemensLogoRelay.ChannelId.RELAY_1, new CoilElement(0 + this.readOffset))), //
+				new FC5WriteCoilTask(1 + this.readOffset,
+						m(SiemensLogoRelay.ChannelId.RELAY_2, new CoilElement(1 + this.readOffset))), //
+				new FC5WriteCoilTask(2 + this.readOffset,
+						m(SiemensLogoRelay.ChannelId.RELAY_3, new CoilElement(2 + this.readOffset))), //
+				new FC5WriteCoilTask(3 + this.readOffset,
+						m(SiemensLogoRelay.ChannelId.RELAY_4, new CoilElement(3 + this.readOffset))), //
+				new FC5WriteCoilTask(4 + this.readOffset,
+						m(SiemensLogoRelay.ChannelId.RELAY_5, new CoilElement(4 + this.readOffset))), //
+				new FC5WriteCoilTask(5 + this.readOffset,
+						m(SiemensLogoRelay.ChannelId.RELAY_6, new CoilElement(5 + this.readOffset))), //
+				new FC5WriteCoilTask(6 + this.readOffset,
+						m(SiemensLogoRelay.ChannelId.RELAY_7, new CoilElement(6 + this.readOffset))), //
+				new FC5WriteCoilTask(7 + this.readOffset,
+						m(SiemensLogoRelay.ChannelId.RELAY_8, new CoilElement(7 + this.readOffset))) //
 		);
 	}
 
