1d79928b5825849be85df436b1109f1f4590eab6
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2023-10-19 15:59:18 +0200

    Bugfix for ENTSO-E

---
+++ io.openems.edge.timeofusetariff.entsoe/src/io/openems/edge/timeofusetariff/entsoe/Token.java
@@ -0,0 +1,19 @@
+package io.openems.edge.timeofusetariff.entsoe;
+
+public final class Token {
+
+	// DO NOT PUBLISH THIS TOKEN
+	public static final String TOKEN = "";
+	// DO NOT PUBLISH THIS TOKEN
+
+	private Token() {
+	}
+
+	protected static final String parseOrNull(String token) {
+		if (token != null && !token.isBlank()) {
+			return token;
+		}
+		return TOKEN;
+	}
+
+}
