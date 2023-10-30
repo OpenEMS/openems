51dd0b020c2210acc8ee80bbeca7c24dc5296578
Lóránt Meszlényi <meszlenyilorant@gmail.com>
2023-10-30 13:55:34 +0100

    Makes configuration safer with using enums.

--- io.openems.edge.bridge.can.linux/src/io/openems/edge/bridge/can/linux/Config.java
+++ io.openems.edge.bridge.can.linux/src/io/openems/edge/bridge/can/linux/Config.java
@@ -7,8 +7,8 @@ import io.openems.edge.bridge.can.CanHardwareType;
 import io.openems.edge.bridge.can.LogVerbosity;
 
 @ObjectClassDefinition(//
-		name = "Bridge CAN Linux V5", //
-		description = "Provides a service for reading and writing single CAN frames to/from a CAN device (Linux V5, Kunbus Connect S).")
+		name = "Bridge CAN Linux SocketCAN", //
+		description = "Provides a service for reading and writing single CAN frames to/from a CAN device.")
 @interface Config {
 
 	@AttributeDefinition(name = "Component-ID", description = "Unique ID of this Component")
@@ -21,18 +21,21 @@ import io.openems.edge.bridge.can.LogVerbosity;
 	boolean enabled() default true;
 
 	@AttributeDefinition(name = "CAN Baudrate", description = "CAN Baudrate in kBaud (not implemented yet).")
-	String can_baudrate() default "250";
+	BaudRate can_baudrate() default BaudRate.BAUD_500_KBPS;
 
-	@AttributeDefinition(name = "CAN Hardware", description = "Uses the integrated Simulator or the CAN Hardware (Kunbus RevPi CAN Connector).")
-	CanHardwareType selected_hardware() default CanHardwareType.KUNBUS_REV_PI_CAN_EXTENSION;
+	@AttributeDefinition(name = "CAN Driver", description = "Uses the integrated Simulator or the CAN Hardware (Kunbus RevPi CAN Connector).")
+	CanHardwareType selected_hardware() default CanHardwareType.SOCKETCAN;
 
 	@AttributeDefinition(name = "Invalidate elements after how many CAN read errors", description = "Typically each CAN Identfier has a defined cycle time. If a CAN frame is not received "
 			+ "after (X * cycle time) ms, the appropriate channel elements are invalidated ")
 	int invalidateElementsAfterReadErrors() default 10;
 
-	@AttributeDefinition(name = "Log-Verbosity", description = "The log verbosity.")
+	@AttributeDefinition(name = "Log-Verbosity", description = "The log verbosity. Useful for developing new devices that use this module.")
 	LogVerbosity logVerbosity() default LogVerbosity.NONE;
+	
+	@AttributeDefinition(name = "Interface Name", description = "Name of the interface to bind on, e. g. \"vcan0\" or \"can0\"")
+	String interface_name() default "can0";
 
-	String webconsole_configurationFactory_nameHint() default "Bridge CAN Linux V5[{id}]";
+	String webconsole_configurationFactory_nameHint() default "Bridge CAN Linux SocketCAN[{id}]";
 
 }
