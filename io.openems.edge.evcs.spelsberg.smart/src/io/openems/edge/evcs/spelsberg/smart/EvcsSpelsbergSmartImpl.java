569145b89935c3d8b5657d84bc7eb2f2954bfe16
Roland Edelhoff <red@spelsberg.de>
2023-11-09 13:42:49 +0100

    combine Modbus registers in a single Task by using dummyRegisters + apply checkstyle format

--- io.openems.edge.evcs.spelsberg.smart/src/io/openems/edge/evcs/spelsberg/smart/EvcsSpelsbergSmartImpl.java
+++ io.openems.edge.evcs.spelsberg.smart/src/io/openems/edge/evcs/spelsberg/smart/EvcsSpelsbergSmartImpl.java
@@ -71,8 +71,7 @@ public class EvcsSpelsbergSmartImpl extends AbstractOpenemsModbusComponent
 	private final WriteHandler writeHandler = new WriteHandler(this);
 
 	public EvcsSpelsbergSmartImpl() {
-		super(//
-				OpenemsComponent.ChannelId.values(), ModbusComponent.ChannelId.values(), Evcs.ChannelId.values(),
+		super(OpenemsComponent.ChannelId.values(), ModbusComponent.ChannelId.values(), Evcs.ChannelId.values(), //
 				ManagedEvcs.ChannelId.values(), EvcsSpelsbergSmart.ChannelId.values());
 	}
 
@@ -88,7 +87,7 @@ public class EvcsSpelsbergSmartImpl extends AbstractOpenemsModbusComponent
 		 * the fixed hardware limits used for charging
 		 */
 		Evcs.addCalculatePowerLimitListeners(this);
-		applyConfig(config);
+		this.applyConfig(config);
 	}
 
 	@Modified
@@ -97,7 +96,7 @@ public class EvcsSpelsbergSmartImpl extends AbstractOpenemsModbusComponent
 				"Modbus", config.modbus_id())) {
 			return;
 		}
-		applyConfig(config);
+		this.applyConfig(config);
 	}
 
 	@Override
@@ -119,75 +118,64 @@ public class EvcsSpelsbergSmartImpl extends AbstractOpenemsModbusComponent
 						m(EvcsSpelsbergSmart.ChannelId.CABLE_STATE, new UnsignedWordElement(1004))),
 
 				new FC3ReadRegistersTask(1008, Priority.LOW,
-						m(EvcsSpelsbergSmart.ChannelId.CURRENT_L1, new UnsignedWordElement(1008))),
-
-				new FC3ReadRegistersTask(1010, Priority.LOW,
-						m(EvcsSpelsbergSmart.ChannelId.CURRENT_L2, new UnsignedWordElement(1010))),
-
-				new FC3ReadRegistersTask(1012, Priority.LOW,
+						m(EvcsSpelsbergSmart.ChannelId.CURRENT_L1, new UnsignedWordElement(1008)),
+						new DummyRegisterElement(1009),
+						m(EvcsSpelsbergSmart.ChannelId.CURRENT_L2, new UnsignedWordElement(1010)),
+						new DummyRegisterElement(1011),
 						m(EvcsSpelsbergSmart.ChannelId.CURRENT_L3, new UnsignedWordElement(1012))),
 
 				new FC3ReadRegistersTask(1020, Priority.HIGH,
-						m(Evcs.ChannelId.CHARGE_POWER, new UnsignedDoublewordElement(1020))),
-
-				new FC3ReadRegistersTask(1020, Priority.HIGH,
-						m(EvcsSpelsbergSmart.ChannelId.POWER_TOTAL, new UnsignedDoublewordElement(1020))),
-
-				new FC3ReadRegistersTask(1024, Priority.HIGH,
-						m(EvcsSpelsbergSmart.ChannelId.POWER_L1, new UnsignedDoublewordElement(1024))),
-
-				new FC3ReadRegistersTask(1028, Priority.HIGH,
-						m(EvcsSpelsbergSmart.ChannelId.POWER_L2, new UnsignedDoublewordElement(1028))),
-
-				new FC3ReadRegistersTask(1032, Priority.HIGH,
+						m(Evcs.ChannelId.CHARGE_POWER, new UnsignedDoublewordElement(1020)),
+						m(EvcsSpelsbergSmart.ChannelId.POWER_TOTAL, new UnsignedDoublewordElement(1020)),
+						new DummyRegisterElement(1022), new DummyRegisterElement(1023),
+						m(EvcsSpelsbergSmart.ChannelId.POWER_L1, new UnsignedDoublewordElement(1024)),
+						new DummyRegisterElement(1026), new DummyRegisterElement(1027),
+						m(EvcsSpelsbergSmart.ChannelId.POWER_L2, new UnsignedDoublewordElement(1028)),
+						new DummyRegisterElement(1030), new DummyRegisterElement(1031),
 						m(EvcsSpelsbergSmart.ChannelId.POWER_L3, new UnsignedDoublewordElement(1032))),
 
 				new FC3ReadRegistersTask(1036, Priority.LOW,
 						m(Evcs.ChannelId.ACTIVE_CONSUMPTION_ENERGY, new UnsignedDoublewordElement(1036))),
 
 				new FC3ReadRegistersTask(1100, Priority.LOW,
-						m(EvcsSpelsbergSmart.ChannelId.MAX_HARDWARE_CURRENT, new UnsignedWordElement(1100))),
-
-				new FC3ReadRegistersTask(1102, Priority.LOW,
+						m(EvcsSpelsbergSmart.ChannelId.MAX_HARDWARE_CURRENT, new UnsignedWordElement(1100)),
+						new DummyRegisterElement(1101),
 						m(EvcsSpelsbergSmart.ChannelId.MIN_HARDWARE_CURRENT, new UnsignedWordElement(1102))),
 
 				new FC3ReadRegistersTask(1502, Priority.HIGH,
 						m(EvcsSpelsbergSmart.ChannelId.CHARGE_ENERGY_SESSION, new UnsignedWordElement(1502))),
 
 				new FC3ReadRegistersTask(1504, Priority.LOW,
-						m(EvcsSpelsbergSmart.ChannelId.CHARGE_START_TIME, new UnsignedDoublewordElement(1504))),
-
-				new FC3ReadRegistersTask(1508, Priority.LOW,
-						m(EvcsSpelsbergSmart.ChannelId.CHARGE_DURATION_SESSION, new UnsignedDoublewordElement(1508))),
-
-				new FC3ReadRegistersTask(1512, Priority.LOW,
+						m(EvcsSpelsbergSmart.ChannelId.CHARGE_START_TIME, new UnsignedDoublewordElement(1504)),
+						new DummyRegisterElement(1506), new DummyRegisterElement(1507),
+						m(EvcsSpelsbergSmart.ChannelId.CHARGE_DURATION_SESSION, new UnsignedDoublewordElement(1508)),
+						new DummyRegisterElement(1510), new DummyRegisterElement(1511),
 						m(EvcsSpelsbergSmart.ChannelId.CHARGE_STOP_TIME, new UnsignedDoublewordElement(1512))),
 
 				new FC3ReadRegistersTask(2000, Priority.LOW,
 						m(EvcsSpelsbergSmart.ChannelId.CHARGE_SAVE_CURRENT_LIMIT, new UnsignedWordElement(2000))),
 
-				new FC3ReadRegistersTask(6000, Priority.HIGH,
-						m(EvcsSpelsbergSmart.ChannelId.LIFE_BIT, new UnsignedWordElement(6000))),
-
-				new FC6WriteRegisterTask(6000, m(EvcsSpelsbergSmart.ChannelId.LIFE_BIT, new UnsignedWordElement(6000))),
-
 				new FC16WriteRegistersTask(5000,
 						m(EvcsSpelsbergSmart.ChannelId.APPLY_CHARGE_POWER_LIMIT, new UnsignedDoublewordElement(5000))),
 
 				new FC6WriteRegisterTask(5004,
-						m(EvcsSpelsbergSmart.ChannelId.APPLY_CHARGE_CURRENT_LIMIT, new UnsignedWordElement(5004))));
+						m(EvcsSpelsbergSmart.ChannelId.APPLY_CHARGE_CURRENT_LIMIT, new UnsignedWordElement(5004))),
 
-		addStatusCallback();
-		addPhaseDetectionCallback();
-		addPowerConsumptionCallback();
+				new FC3ReadRegistersTask(6000, Priority.LOW,
+						m(EvcsSpelsbergSmart.ChannelId.LIFE_BIT, new UnsignedWordElement(6000))),
+
+				new FC6WriteRegisterTask(6000,
+						m(EvcsSpelsbergSmart.ChannelId.LIFE_BIT, new UnsignedWordElement(6000))));
+
+		this.addStatusCallback();
+		this.addPhaseDetectionCallback();
+		this.addPowerConsumptionCallback();
 		return modbusProtocol;
 	}
 
 	@Override
 	public String debugLog() {
-		return "Status: " + getStatus().getName() + " | " + "Charge Power Limit: "
-				+ getSetChargePowerLimit().orElse(null) + " | " + "Actual Charge Power: " + getChargePowerTotal()
-				+ " | " + "Charged Energy: " + getEnergySessionChannel().value();
+		return "Status: " + getStatus().getName() + " | " + "Charging Power: " + getChargePowerTotal();
 	}
 
 	@Override
@@ -204,12 +192,12 @@ public class EvcsSpelsbergSmartImpl extends AbstractOpenemsModbusComponent
 
 	@Override
 	public int getConfiguredMinimumHardwarePower() {
-		return currentToPower(this.config.minHwCurrent());
+		return this.currentToPower(this.config.minHwCurrent());
 	}
 
 	@Override
 	public int getConfiguredMaximumHardwarePower() {
-		return currentToPower(this.config.maxHwCurrent());
+		return this.currentToPower(this.config.maxHwCurrent());
 	}
 
 	@Override
@@ -251,7 +239,7 @@ public class EvcsSpelsbergSmartImpl extends AbstractOpenemsModbusComponent
 		}
 		switch (event.getTopic()) {
 		case EdgeEventConstants.TOPIC_CYCLE_EXECUTE_WRITE:
-			writeHandler.run();
+			this.writeHandler.run();
 			break;
 		}
 	}
@@ -260,8 +248,8 @@ public class EvcsSpelsbergSmartImpl extends AbstractOpenemsModbusComponent
 		this.config = config;
 		this._setChargingType(ChargingType.AC);
 		this._setPhases(Phases.THREE_PHASE.getValue());
-		this._setFixedMinimumHardwarePower(currentToPower(config.minHwCurrent()));
-		this._setFixedMaximumHardwarePower(currentToPower(config.maxHwCurrent()));
+		this._setFixedMinimumHardwarePower(this.currentToPower(config.minHwCurrent()));
+		this._setFixedMaximumHardwarePower(this.currentToPower(config.maxHwCurrent()));
 		this._setPowerPrecision(DEFAULT_POWER_RECISION); // 1A steps
 	}
 
@@ -277,26 +265,15 @@ public class EvcsSpelsbergSmartImpl extends AbstractOpenemsModbusComponent
 			 * Maps the EVCS state to a {@link Status}.
 			 */
 			switch (state) {
-			case CHARGING:
-				this._setStatus(Status.CHARGING);
-				break;
-			case NO_PERMISSION:
-			case CHARGING_STATION_RESERVED:
-				this._setStatus(Status.CHARGING_REJECTED);
-				break;
-			case ERROR:
-				this._setStatus(Status.ERROR);
-				break;
-			case NO_VEHICLE_ATTACHED:
-				this._setStatus(Status.NOT_READY_FOR_CHARGING);
-				break;
-			case CHARGING_PAUSED:
-				this._setStatus(Status.READY_FOR_CHARGING);
-				break;
-			case UNDEFINED:
-			default:
-				this._setStatus(Status.UNDEFINED);
+			case CHARGING -> this._setStatus(Status.CHARGING);
+			case NO_PERMISSION, CHARGING_STATION_RESERVED -> this._setStatus(Status.CHARGING_REJECTED);
+			case ERROR -> this._setStatus(Status.ERROR);
+			case NO_VEHICLE_ATTACHED -> this._setStatus(Status.NOT_READY_FOR_CHARGING);
+			case CHARGING_PAUSED -> this._setStatus(Status.CHARGING_REJECTED);
+			case UNDEFINED -> this._setStatus(Status.UNDEFINED);
+			default -> this._setStatus(Status.UNDEFINED);
 			}
+			;
 
 			if (getLifeBit() == -1) {
 				this._setModbusCommunicationFailed(true);
@@ -313,12 +290,15 @@ public class EvcsSpelsbergSmartImpl extends AbstractOpenemsModbusComponent
 		final Consumer<Value<Integer>> setPhasesCallback = ignore -> {
 
 			var phases = 0;
-			if (getChargePowerL1() > 0)
+			if (getChargePowerL1() > 0) {
 				phases++;
-			if (getChargePowerL2() > 0)
+			}
+			if (getChargePowerL2() > 0) {
 				phases++;
-			if (getChargePowerL3() > 0)
+			}
+			if (getChargePowerL3() > 0) {
 				phases++;
+			}
 
 			this._setPhases(phases);
 
