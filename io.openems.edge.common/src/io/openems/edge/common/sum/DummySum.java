3731969499ada5706fce1cb642c6f3d0f3647d88
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2023-11-14 15:58:51 +0100

    Emergency Capacity Reserve Controller: fix handling of negative AC production (#878)
    Before, this could lead to discharge in FORCE_CHARGE mode, when AC production was negative.
    
    Reviewed-by: Sebastian Asen <47855186+sebastianasen@users.noreply.github.com>

--- io.openems.edge.common/src/io/openems/edge/common/sum/DummySum.java
+++ io.openems.edge.common/src/io/openems/edge/common/sum/DummySum.java
@@ -2,6 +2,7 @@ package io.openems.edge.common.sum;
 
 import io.openems.edge.common.component.OpenemsComponent;
 import io.openems.edge.common.test.AbstractDummyOpenemsComponent;
+import io.openems.edge.common.test.TestUtils;
 
 /**
  * Provides a simple, simulated Sum component that can be used together with the
@@ -23,6 +24,17 @@ public class DummySum extends AbstractDummyOpenemsComponent<DummySum> implements
 	@Override
 	public void updateChannelsBeforeProcessImage() {
 		// nothing here
+	}
+
+	/**
+	 * Set {@link Sum.ChannelId#PRODUCTION_AC_ACTIVE_POWER}.
+	 *
+	 * @param value the value
+	 * @return myself
+	 */
+	public DummySum withProductionAcActivePower(int value) {
+		TestUtils.withValue(this, Sum.ChannelId.PRODUCTION_AC_ACTIVE_POWER, value);
+		return this.self();
 	}
 
 }
