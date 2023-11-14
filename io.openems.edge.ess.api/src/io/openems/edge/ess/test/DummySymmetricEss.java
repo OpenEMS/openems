13ac3e00bb96546703bf803d50bd591f382c2a04
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2023-11-14 14:10:24 +0100

    Sum ESS State-of-Charge: unify calculation
    Unify the calculation of a average State-of-Charge of multiple Energy Storage Systems. Before, the calculation was different for EssCluster and Sum. Adding fully tested `CalculateSoC` helper class.
    
    Reviewed-by: Sebastian Asen <47855186+sebastianasen@users.noreply.github.com>

--- io.openems.edge.ess.api/src/io/openems/edge/ess/test/DummySymmetricEss.java
+++ io.openems.edge.ess.api/src/io/openems/edge/ess/test/DummySymmetricEss.java
@@ -1,19 +1,48 @@
 package io.openems.edge.ess.test;
 
+import io.openems.edge.common.channel.Channel;
+import io.openems.edge.common.component.AbstractOpenemsComponent;
 import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.test.TestUtils;
 import io.openems.edge.ess.api.SymmetricEss;
 
-public class DummySymmetricEss extends AbstractDummySymmetricEss<DummySymmetricEss> implements SymmetricEss {
+public class DummySymmetricEss extends AbstractOpenemsComponent implements SymmetricEss {
 
 	public DummySymmetricEss(String id) {
-		super(id, //
+		this(id, //
 				OpenemsComponent.ChannelId.values(), //
 				SymmetricEss.ChannelId.values() //
 		);
 	}
 
-	@Override
-	protected DummySymmetricEss self() {
+	protected DummySymmetricEss(String id, io.openems.edge.common.channel.ChannelId[] firstInitialChannelIds,
+			io.openems.edge.common.channel.ChannelId[]... furtherInitialChannelIds) {
+		super(firstInitialChannelIds, furtherInitialChannelIds);
+		for (Channel<?> channel : this.channels()) {
+			channel.nextProcessImage();
+		}
+		super.activate(null, id, "", true);
+	}
+
+	/**
+	 * Set {@link SymmetricEss.ChannelId#SOC} of this {@link SymmetricEss}.
+	 *
+	 * @param value the value
+	 * @return myself
+	 */
+	public DummySymmetricEss withSoc(Integer value) {
+		TestUtils.withValue(this, SymmetricEss.ChannelId.SOC, value);
+		return this;
+	}
+
+	/**
+	 * Set {@link SymmetricEss.ChannelId#CAPACITY} of this {@link SymmetricEss}.
+	 *
+	 * @param value the value
+	 * @return myself
+	 */
+	public DummySymmetricEss withCapacity(Integer value) {
+		TestUtils.withValue(this, SymmetricEss.ChannelId.CAPACITY, value);
 		return this;
 	}
 
