40aa08949699cacb82d156ebcaf104d9f81e81e6
Sagar Bandi Venu <sagar.venu@fenecon.de>
2023-11-21 11:35:32 +0100

    UI Time-Of-Use: refactor chart in Live modal (#939)
    Reviewed-on: https://git.intranet.fenecon.de/FENECON/fems/pulls/939
    Reviewed-by: Lukas Rieger <lukas.rieger@fenecon.de>
    Reviewed-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>
    Co-authored-by: Sagar Bandi Venu <sagar.venu@fenecon.de>
    Co-committed-by: Sagar Bandi Venu <sagar.venu@fenecon.de>

--- ui/src/app/edge/live/Controller/Ess/TimeOfUseTariffDischarge/modal/scheduleChart.ts
+++ ui/src/app/edge/live/Controller/Ess/TimeOfUseTariffDischarge/modal/scheduleChart.ts
@@ -5,7 +5,8 @@ import { TranslateService } from '@ngx-translate/core';
 import { AbstractHistoryChart } from 'src/app/edge/history/abstracthistorychart';
 import { TooltipItem } from 'src/app/edge/history/shared';
 import { ComponentJsonApiRequest } from 'src/app/shared/jsonrpc/request/componentJsonApiRequest';
-import { ChannelAddress, Currency, Edge, EdgeConfig, Service, Utils, Websocket } from 'src/app/shared/shared';
+import { TimeOfUseTariffUtils } from 'src/app/shared/service/utils';
+import { ChannelAddress, Currency, Edge, EdgeConfig, Service, Websocket } from 'src/app/shared/shared';
 import { GetScheduleRequest } from '../../../../../../shared/jsonrpc/request/getScheduleRequest';
 import { GetScheduleResponse } from '../../../../../../shared/jsonrpc/response/getScheduleResponse';
 
@@ -49,93 +50,29 @@ export class ScheduleChartComponent extends AbstractHistoryChart implements OnIn
         this.autoSubscribeChartRefresh();
         this.service.startSpinner(this.spinnerId);
         this.loading = true;
-        this.colors = [];
 
         this.edge.sendRequest(
             this.websocket,
             new ComponentJsonApiRequest({ componentId: this.component.id, payload: new GetScheduleRequest() }),
         ).then(response => {
-            var result = (response as GetScheduleResponse).result;
-            var length = result.schedule.length;
+            const result = (response as GetScheduleResponse).result;
+            const length = result.schedule.length;
 
-            // Initializing States.
-            var barCharge = Array(length).fill(null);
-            var barBalancing = Array(length).fill(null);
-            var barDelayDischarge = Array(length).fill(null);
+            // Extracting prices and states from the schedule array
+            const { priceArray, stateArray, timestampArray } = {
+                priceArray: result.schedule.map(entry => entry.price),
+                stateArray: result.schedule.map(entry => entry.state),
+                timestampArray: result.schedule.map(entry => entry.timestamp),
+            };
 
-            // convert labels
-            let labels: Date[] = [];
+            const scheduleChartData = TimeOfUseTariffUtils.getScheduleChartData(length, priceArray, stateArray, timestampArray, this.translate, this.component.factoryId);
 
-            for (let index = 0; index < length; index++) {
-                let price = Utils.formatPrice(result.schedule[index].price);
-                var state = result.schedule[index].state;
-                var timestamp = result.schedule[index].timestamp;
+            this.datasets = scheduleChartData.datasets;
+            this.colors = scheduleChartData.colors;
+            this.labels = scheduleChartData.labels;
 
-                //Only use full hours as a timestamp
-                labels.push(new Date(timestamp));
-
-                switch (state) {
-                    case 0:
-                        // Delay Discharge
-                        barDelayDischarge[index] = price;
-                        break;
-                    case 1:
-                        // Allows Discharge
-                        barBalancing[index] = price;
-                        break;
-                    case 3:
-                        // Charge
-                        barCharge[index] = price;
-                        break;
-                }
-            }
-
-            let datasets = [];
-
-            // Set datasets
-            datasets.push({
-                type: 'bar',
-                label: this.translate.instant('Edge.Index.Widgets.TIME_OF_USE_TARIFF.STATE.BALANCING'),
-                data: barBalancing,
-                order: 3,
-            });
-            this.colors.push({
-                // Dark Green
-                backgroundColor: 'rgba(51,102,0,0.8)',
-                borderColor: 'rgba(51,102,0,1)',
-            });
-
-            // Set dataset for Quarterly Prices being charged.
-            datasets.push({
-                type: 'bar',
-                label: this.translate.instant('Edge.Index.Widgets.TIME_OF_USE_TARIFF.STATE.CHARGE'),
-                data: barCharge,
-                order: 3,
-            });
-            this.colors.push({
-                // Sky blue
-                backgroundColor: 'rgba(0, 204, 204,0.5)',
-                borderColor: 'rgba(0, 204, 204,0.7)',
-            });
-
-            // Set dataset for buy from grid
-            datasets.push({
-                type: 'bar',
-                label: this.translate.instant('Edge.Index.Widgets.TIME_OF_USE_TARIFF.STATE.DELAY_DISCHARGE'),
-                data: barDelayDischarge,
-                order: 3,
-            });
-            this.colors.push({
-                // Black
-                backgroundColor: 'rgba(0,0,0,0.8)',
-                borderColor: 'rgba(0,0,0,0.9)',
-            });
-
-            this.datasets = datasets;
-            this.labels = labels;
             this.loading = false;
             this.setLabel();
-            // this.service.stopSpinner(this.spinnerId);
             this.stopSpinner();
         }).catch((reason) => {
             console.error(reason);
@@ -179,6 +116,9 @@ export class ScheduleChartComponent extends AbstractHistoryChart implements OnIn
     }
 
     public getChartHeight(): number {
-        return window.innerHeight / 4;
+
+        return this.service.isSmartphoneResolution
+            ? window.innerHeight / 3
+            : window.innerHeight / 4;
     }
 }
