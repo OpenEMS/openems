8876a0878c89d4503c18d0819ae9a39b22be13e3
Michael Grill <michael.grill.10@gmail.com>
2023-11-29 12:37:00 +0100

    Update BridgeHttpImplTest.java
    use dummy request to wait

--- io.openems.edge.bridge.http/test/io/openems/edge/bridge/http/BridgeHttpImplTest.java
+++ io.openems.edge.bridge.http/test/io/openems/edge/bridge/http/BridgeHttpImplTest.java
@@ -102,7 +102,8 @@ public class BridgeHttpImplTest {
 			lock.notify();
 		}
 
-		Thread.sleep(100);
+		// wait until last request is finished
+		this.bridgeHttp.request("dummy").get();
 		synchronized (lock) {
 			this.nextCycle();
 			lock.wait();
