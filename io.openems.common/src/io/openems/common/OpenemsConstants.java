a6c6a3bfae93e605297faa2f265f0e1877b4d6b5
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2023-12-04 18:28:29 +0100

    Start development of version 2024.1.0-SNAPSHOT

--- io.openems.common/src/io/openems/common/OpenemsConstants.java
+++ io.openems.common/src/io/openems/common/OpenemsConstants.java
@@ -21,7 +21,7 @@ public class OpenemsConstants {
 	 * <p>
 	 * This is the year of the release.
 	 */
-	public static final short VERSION_MAJOR = 2023;
+	public static final short VERSION_MAJOR = 2024;
 
 	/**
 	 * The minor version of OpenEMS.
@@ -29,7 +29,7 @@ public class OpenemsConstants {
 	 * <p>
 	 * This is the month of the release.
 	 */
-	public static final short VERSION_MINOR = 12;
+	public static final short VERSION_MINOR = 1;
 
 	/**
 	 * The patch version of OpenEMS.
@@ -43,7 +43,7 @@ public class OpenemsConstants {
 	/**
 	 * The additional version string.
 	 */
-	public static final String VERSION_STRING = "";
+	public static final String VERSION_STRING = "SNAPSHOT";
 
 	/**
 	 * The complete version as a SemanticVersion.
