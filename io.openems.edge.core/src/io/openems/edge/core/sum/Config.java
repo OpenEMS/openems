eac21535f7a1b08f5843b13a582f489b293cfa56
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2023-12-30 23:40:25 +0100

    Core.Sum: persist maximum ever values (#2476)
    Before this change calculation of maximum channels was always reset after restart. Configuration is a proper place to store this data, as there is no history required (i.e. Timedata does not fit) and it's easy to adjust values by customer service.
    
    - drop respective channels from Electricity Meter
    - mark Channels RESERVED in modbus registers

--- io.openems.edge.core/src/io/openems/edge/core/sum/Config.java
+++ io.openems.edge.core/src/io/openems/edge/core/sum/Config.java
@@ -8,6 +8,18 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 		description = "The global OpenEMS Summary data.")
 @interface Config {
 
+	@AttributeDefinition(name = "Maximum ever Sell-to-Grid power [W]", description = "Range: negative or zero")
+	int gridMinActivePower() default 0;
+
+	@AttributeDefinition(name = "Maximum ever Buy-from-Grid power [W]", description = "Range: positive or zero")
+	int gridMaxActivePower() default 0;
+
+	@AttributeDefinition(name = "Maximum ever Production power [W]", description = "Includes AC- and DC-side production. Range: positive or zero")
+	int productionMaxActivePower() default 0;
+
+	@AttributeDefinition(name = "Maximum ever Consumption power [W]", description = "Range: positive or zero")
+	int consumptionMaxActivePower() default 0;
+
 	@AttributeDefinition(name = "Ignore Component Warnings/Faults", description = "Component-IDs for which Fault or Warning Channels should be ignored.")
 	String[] ignoreStateComponents() default {};
 
