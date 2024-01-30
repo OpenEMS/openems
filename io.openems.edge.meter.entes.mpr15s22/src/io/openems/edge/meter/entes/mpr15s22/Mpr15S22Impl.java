7623ccf1ad5bc61e658dd260e9bfb579a238c849
Samuel Bloch <samuel.bloch@inomotech.com>
2024-01-30 15:00:52 +0100

    creating tasks to read modbus registers

--- io.openems.edge.meter.entes.mpr15s22/src/io/openems/edge/meter/entes/mpr15s22/Mpr15S22Impl.java
+++ io.openems.edge.meter.entes.mpr15s22/src/io/openems/edge/meter/entes/mpr15s22/Mpr15S22Impl.java
@@ -79,31 +79,32 @@ public class Mpr15S22Impl extends AbstractOpenemsModbusComponent
 	@Override
 	protected ModbusProtocol defineModbusProtocol() throws OpenemsException {
 		return new ModbusProtocol(this, //
+				// Measurement registers
 				new FC3ReadRegistersTask(0, Priority.HIGH,
 						m(Mpr15S22.ChannelId.VOLTAGE_L1_N, new UnsignedDoublewordElement(0)), //
 						m(Mpr15S22.ChannelId.VOLTAGE_L2_N, new UnsignedDoublewordElement(2)), //
-						m(Mpr15S22.ChannelId.VOLTAGE_L3_N, new UnsignedDoublewordElement(4)), //
-						m(Mpr15S22.ChannelId.VOLTAGE_L4_N, new UnsignedDoublewordElement(6)), //
+						m(Mpr15S22.ChannelId.VOLTAGE_L3_N, new UnsignedDoublewordElement(4))), //
+				new FC3ReadRegistersTask(8, Priority.HIGH,
 						m(Mpr15S22.ChannelId.VOLTAGE_L1_L2, new UnsignedDoublewordElement(8)), //
 						m(Mpr15S22.ChannelId.VOLTAGE_L2_L3, new UnsignedDoublewordElement(10)), //
 						m(Mpr15S22.ChannelId.VOLTAGE_L3_L1, new UnsignedDoublewordElement(12)), //
 						m(ElectricityMeter.ChannelId.CURRENT_L1, new UnsignedDoublewordElement(14)), //
 						m(ElectricityMeter.ChannelId.CURRENT_L2, new UnsignedDoublewordElement(16)), //
-						m(ElectricityMeter.ChannelId.CURRENT_L3, new UnsignedDoublewordElement(18)), //
-						m(Mpr15S22.ChannelId.CURRENT_L4, new UnsignedDoublewordElement(20)), //
+						m(ElectricityMeter.ChannelId.CURRENT_L3, new UnsignedDoublewordElement(18))), //
+				new FC3ReadRegistersTask(22, Priority.HIGH,
 						m(Mpr15S22.ChannelId.NEUTRAL_CURRENT, new UnsignedDoublewordElement(22)), //
 						m(Mpr15S22.ChannelId.MEASURED_FREQUENCY, new UnsignedDoublewordElement(24)), //
 						m(Mpr15S22.ChannelId.ACTIVE_POWER_L1_N, new FloatDoublewordElement(26)), //
 						m(Mpr15S22.ChannelId.ACTIVE_POWER_L2_N, new FloatDoublewordElement(28)), //
-						m(Mpr15S22.ChannelId.ACTIVE_POWER_L3_N, new FloatDoublewordElement(30)), //
-						m(Mpr15S22.ChannelId.ACTIVE_POWER_L4_N, new FloatDoublewordElement(32)), //
+						m(Mpr15S22.ChannelId.ACTIVE_POWER_L3_N, new FloatDoublewordElement(30))), //
+				new FC3ReadRegistersTask(34, Priority.HIGH,
 						m(Mpr15S22.ChannelId.TOTAL_IMPORT_ACTIVE_POWER, new FloatDoublewordElement(34)), //
 						m(Mpr15S22.ChannelId.TOTAL_EXPORT_ACTIVE_POWER, new FloatDoublewordElement(36)), //
 						m(Mpr15S22.ChannelId.SUM_ACTIVE_POWER, new FloatDoublewordElement(38)), //
 						m(ElectricityMeter.ChannelId.REACTIVE_POWER_L1, new FloatDoublewordElement(40)), //
 						m(ElectricityMeter.ChannelId.REACTIVE_POWER_L2, new FloatDoublewordElement(42)), //
-						m(ElectricityMeter.ChannelId.REACTIVE_POWER_L3, new FloatDoublewordElement(44)), //
-						m(Mpr15S22.ChannelId.REACTIVE_POWER_L4, new FloatDoublewordElement(46)), //
+						m(ElectricityMeter.ChannelId.REACTIVE_POWER_L3, new FloatDoublewordElement(44))), //
+				new FC3ReadRegistersTask(48, Priority.HIGH,
 						m(Mpr15S22.ChannelId.QUADRANT_1_TOTAL_REACTIVE_POWER, new FloatDoublewordElement(48)), //
 						m(Mpr15S22.ChannelId.QUADRANT_2_TOTAL_REACTIVE_POWER, new FloatDoublewordElement(50)), //
 						m(Mpr15S22.ChannelId.QUADRANT_3_TOTAL_REACTIVE_POWER, new FloatDoublewordElement(52)), //
@@ -111,108 +112,84 @@ public class Mpr15S22Impl extends AbstractOpenemsModbusComponent
 						m(Mpr15S22.ChannelId.SUM_REACTIVE_POWER, new FloatDoublewordElement(56)), //
 						m(Mpr15S22.ChannelId.APPARENT_POWER_L1_N, new FloatDoublewordElement(58)), //
 						m(Mpr15S22.ChannelId.APPARENT_POWER_L2_N, new FloatDoublewordElement(60)), //
-						m(Mpr15S22.ChannelId.APPARENT_POWER_L3_N, new FloatDoublewordElement(62)), //
-						m(Mpr15S22.ChannelId.APPARENT_POWER_L4_N, new FloatDoublewordElement(64)), //
+						m(Mpr15S22.ChannelId.APPARENT_POWER_L3_N, new FloatDoublewordElement(62))), //
+				new FC3ReadRegistersTask(66, Priority.HIGH,
 						m(Mpr15S22.ChannelId.TOTAL_IMPORT_APPARENT_POWER, new FloatDoublewordElement(66)), //
 						m(Mpr15S22.ChannelId.TOTAL_EXPORT_APPARENT_POWER, new FloatDoublewordElement(68)), //
 						m(Mpr15S22.ChannelId.SUM_APPARENT_POWER, new FloatDoublewordElement(70)), //
 						m(Mpr15S22.ChannelId.POWER_FACTOR_L1, new SignedDoublewordElement(72)), //
 						m(Mpr15S22.ChannelId.POWER_FACTOR_L2, new SignedDoublewordElement(74)), //
-						m(Mpr15S22.ChannelId.POWER_FACTOR_L3, new SignedDoublewordElement(76)), //
-						m(Mpr15S22.ChannelId.POWER_FACTOR_L4, new SignedDoublewordElement(78)), //
+						m(Mpr15S22.ChannelId.POWER_FACTOR_L3, new SignedDoublewordElement(76))), //
+				new FC3ReadRegistersTask(80, Priority.HIGH,
 						m(Mpr15S22.ChannelId.SUM_POWER_FACTOR, new SignedDoublewordElement(80)), //
 						m(Mpr15S22.ChannelId.COSPHI_L1, new SignedDoublewordElement(82)), //
 						m(Mpr15S22.ChannelId.COSPHI_L2, new SignedDoublewordElement(84)), //
-						m(Mpr15S22.ChannelId.COSPHI_L3, new SignedDoublewordElement(86)), //
-						m(Mpr15S22.ChannelId.COSPHI_L4, new SignedDoublewordElement(88)), //
+						m(Mpr15S22.ChannelId.COSPHI_L3, new SignedDoublewordElement(86))), //
+				new FC3ReadRegistersTask(90, Priority.HIGH,
 						m(Mpr15S22.ChannelId.SUM_COS_PHI, new SignedDoublewordElement(90)), //
-						m(Mpr15S22.ChannelId.ROTATION_FIELD, new SignedDoublewordElement(92)), //
-						m(Mpr15S22.ChannelId.VOLTAGE_UNBALANCE, new UnsignedDoublewordElement(94)), //
-						m(Mpr15S22.ChannelId.CURRENT_UNBALANCE, new UnsignedDoublewordElement(96)), //
+						m(Mpr15S22.ChannelId.ROTATION_FIELD, new SignedDoublewordElement(92))), //
+				new FC3ReadRegistersTask(98, Priority.HIGH,
 						m(Mpr15S22.ChannelId.L1_PHASE_VOLTAGE_ANGLE, new UnsignedDoublewordElement(98)), //
 						m(Mpr15S22.ChannelId.L2_PHASE_VOLTAGE_ANGLE, new UnsignedDoublewordElement(100)), //
 						m(Mpr15S22.ChannelId.L3_PHASE_VOLTAGE_ANGLE, new UnsignedDoublewordElement(102)), //
 						m(Mpr15S22.ChannelId.L4_PHASE_VOLTAGE_ANGLE, new UnsignedDoublewordElement(104)), //
 						m(Mpr15S22.ChannelId.L1_PHASE_CURRENT_ANGLE, new UnsignedDoublewordElement(106)), //
 						m(Mpr15S22.ChannelId.L2_PHASE_CURRENT_ANGLE, new UnsignedDoublewordElement(108)), //
-						m(Mpr15S22.ChannelId.L3_PHASE_CURRENT_ANGLE, new UnsignedDoublewordElement(110)), //
-						m(Mpr15S22.ChannelId.L4_PHASE_CURRENT_ANGLE, new UnsignedDoublewordElement(112)), //
-						m(Mpr15S22.ChannelId.ANALOG_INPUT_1, new FloatDoublewordElement(114)), //
-						m(Mpr15S22.ChannelId.ANALOG_INPUT_2, new FloatDoublewordElement(116)), //
-						m(Mpr15S22.ChannelId.ANALOG_INPUT_3, new FloatDoublewordElement(118)), //
-						m(Mpr15S22.ChannelId.ANALOG_INPUT_4, new FloatDoublewordElement(120)), //
-						m(Mpr15S22.ChannelId.ANALOG_INPUT_5, new FloatDoublewordElement(122)), //
-						m(Mpr15S22.ChannelId.ANALOG_INPUT_6, new FloatDoublewordElement(124)), //
-						m(Mpr15S22.ChannelId.ANALOG_INPUT_7, new FloatDoublewordElement(126)), //
-						m(Mpr15S22.ChannelId.ANALOG_INPUT_8, new FloatDoublewordElement(128)), //
-						m(Mpr15S22.ChannelId.ANALOG_OUTPUT_1, new FloatDoublewordElement(130)), //
-						m(Mpr15S22.ChannelId.ANALOG_OUTPUT_2, new FloatDoublewordElement(132)), //
-						m(Mpr15S22.ChannelId.ANALOG_OUTPUT_3, new FloatDoublewordElement(134)), //
-						m(Mpr15S22.ChannelId.ANALOG_OUTPUT_4, new FloatDoublewordElement(136)), //
-						m(Mpr15S22.ChannelId.TEMPERATURE_INPUT_1, new FloatDoublewordElement(138)), //
-						m(Mpr15S22.ChannelId.TEMPERATURE_INPUT_2, new FloatDoublewordElement(140)), //
-						m(Mpr15S22.ChannelId.TEMPERATURE_INPUT_3, new FloatDoublewordElement(142)), //
-						m(Mpr15S22.ChannelId.TEMPERATURE_INPUT_4, new FloatDoublewordElement(144)), //
-						m(Mpr15S22.ChannelId.TEMPERATURE_INPUT_5, new FloatDoublewordElement(146)), //
-						m(Mpr15S22.ChannelId.TEMPERATURE_INPUT_6, new FloatDoublewordElement(148)), //
-						m(Mpr15S22.ChannelId.TEMPERATURE_INPUT_7, new FloatDoublewordElement(150)), //
-						m(Mpr15S22.ChannelId.TEMPERATURE_INPUT_8, new FloatDoublewordElement(152)), //
+						m(Mpr15S22.ChannelId.L3_PHASE_CURRENT_ANGLE, new UnsignedDoublewordElement(110))), //
+				new FC3ReadRegistersTask(154, Priority.HIGH,
 						m(Mpr15S22.ChannelId.HOUR_METER_NON_RESETABLE, new UnsignedDoublewordElement(154)), //
 						m(Mpr15S22.ChannelId.WORKING_HOUR_COUNTER, new UnsignedDoublewordElement(156)), //
 						m(Mpr15S22.ChannelId.INPUT_STATUS, new UnsignedDoublewordElement(158)), //
-						m(Mpr15S22.ChannelId.OUTPUT_STATUS, new UnsignedDoublewordElement(160)),
-								new DummyRegisterElement(162, 199), //
+						m(Mpr15S22.ChannelId.OUTPUT_STATUS, new UnsignedDoublewordElement(160))),
+
+				// Energy registers
+				new FC3ReadRegistersTask(200, Priority.HIGH,
 						m(Mpr15S22.ChannelId.CONSUMED_ACTIVE_ENERGY_L1, new UnsignedQuadruplewordElement(200)), //
 						m(Mpr15S22.ChannelId.CONSUMED_ACTIVE_ENERGY_L2, new UnsignedQuadruplewordElement(204)), //
-						m(Mpr15S22.ChannelId.CONSUMED_ACTIVE_ENERGY_L3, new UnsignedQuadruplewordElement(208)), //
-						m(Mpr15S22.ChannelId.CONSUMED_ACTIVE_ENERGY_L4, new UnsignedQuadruplewordElement(212)), //
+						m(Mpr15S22.ChannelId.CONSUMED_ACTIVE_ENERGY_L3, new UnsignedQuadruplewordElement(208))), //
+				new FC3ReadRegistersTask(216, Priority.HIGH,
 						m(Mpr15S22.ChannelId.TOTAL_CONSUMED_ENERGY_L1_L3, new UnsignedQuadruplewordElement(216)), //
 						m(Mpr15S22.ChannelId.DELIVERED_ACTIVE_ENERGY_L1, new UnsignedQuadruplewordElement(220)), //
 						m(Mpr15S22.ChannelId.DELIVERED_ACTIVE_ENERGY_L2, new UnsignedQuadruplewordElement(224)), //
-						m(Mpr15S22.ChannelId.DELIVERED_ACTIVE_ENERGY_L3, new UnsignedQuadruplewordElement(228)), //
-						m(Mpr15S22.ChannelId.DELIVERED_ACTIVE_ENERGY_L4, new UnsignedQuadruplewordElement(232)), //
+						m(Mpr15S22.ChannelId.DELIVERED_ACTIVE_ENERGY_L3, new UnsignedQuadruplewordElement(228))), //
+				new FC3ReadRegistersTask(236, Priority.HIGH,
 						m(Mpr15S22.ChannelId.TOTAL_DELIVERED_ENERGY_L1_L3, new UnsignedQuadruplewordElement(236)), //
 						m(Mpr15S22.ChannelId.CONSUMED_APPARENT_ENERGY_L1, new UnsignedQuadruplewordElement(240)), //
 						m(Mpr15S22.ChannelId.CONSUMED_APPARENT_ENERGY_L2, new UnsignedQuadruplewordElement(244)), //
-						m(Mpr15S22.ChannelId.CONSUMED_APPARENT_ENERGY_L3, new UnsignedQuadruplewordElement(248)), //
-						m(Mpr15S22.ChannelId.CONSUMED_APPARENT_ENERGY_L4, new UnsignedQuadruplewordElement(252)), //
+						m(Mpr15S22.ChannelId.CONSUMED_APPARENT_ENERGY_L3, new UnsignedQuadruplewordElement(248))), //
+				new FC3ReadRegistersTask(256, Priority.HIGH,
 						m(Mpr15S22.ChannelId.TOTAL_CONSUMED_APPARENT_ENERGY_L1_L3,
 								new UnsignedQuadruplewordElement(256)), //
 						m(Mpr15S22.ChannelId.DELIVERED_APPARENT_ENERGY_L1, new UnsignedQuadruplewordElement(260)), //
 						m(Mpr15S22.ChannelId.DELIVERED_APPARENT_ENERGY_L2, new UnsignedQuadruplewordElement(264)), //
-						m(Mpr15S22.ChannelId.DELIVERED_APPARENT_ENERGY_L3, new UnsignedQuadruplewordElement(268)), //
-						m(Mpr15S22.ChannelId.DELIVERED_APPARENT_ENERGY_L4, new UnsignedQuadruplewordElement(272)), //
+						m(Mpr15S22.ChannelId.DELIVERED_APPARENT_ENERGY_L3, new UnsignedQuadruplewordElement(268))), //
+				new FC3ReadRegistersTask(276, Priority.HIGH,
 						m(Mpr15S22.ChannelId.TOTAL_DELIVERED_APPARENT_ENERGY_L1_L3,
 								new UnsignedQuadruplewordElement(276)), //
 						m(Mpr15S22.ChannelId.QUADRANT_1_REACTIVE_ENERGY_L1, new UnsignedQuadruplewordElement(280)), //
 						m(Mpr15S22.ChannelId.QUADRANT_1_REACTIVE_ENERGY_L2, new UnsignedQuadruplewordElement(284)), //
-						m(Mpr15S22.ChannelId.QUADRANT_1_REACTIVE_ENERGY_L3, new UnsignedQuadruplewordElement(288)), //
-						m(Mpr15S22.ChannelId.QUADRANT_1_REACTIVE_ENERGY_L4, new UnsignedQuadruplewordElement(292)), //
+						m(Mpr15S22.ChannelId.QUADRANT_1_REACTIVE_ENERGY_L3, new UnsignedQuadruplewordElement(288))), //
+				new FC3ReadRegistersTask(296, Priority.HIGH,
 						m(Mpr15S22.ChannelId.QUADRANT_1_TOTAL_REACTIVE_ENERGY, new UnsignedQuadruplewordElement(296)), //
 						m(Mpr15S22.ChannelId.QUADRANT_2_REACTIVE_ENERGY_L1, new UnsignedQuadruplewordElement(300)), //
 						m(Mpr15S22.ChannelId.QUADRANT_2_REACTIVE_ENERGY_L2, new UnsignedQuadruplewordElement(304)), //
-						m(Mpr15S22.ChannelId.QUADRANT_2_REACTIVE_ENERGY_L3, new UnsignedQuadruplewordElement(308)), //
-						m(Mpr15S22.ChannelId.QUADRANT_2_REACTIVE_ENERGY_L4, new UnsignedQuadruplewordElement(312)), //
+						m(Mpr15S22.ChannelId.QUADRANT_2_REACTIVE_ENERGY_L3, new UnsignedQuadruplewordElement(308))), //
+				new FC3ReadRegistersTask(316, Priority.HIGH,
 						m(Mpr15S22.ChannelId.QUADRANT_2_TOTAL_REACTIVE_ENERGY, new UnsignedQuadruplewordElement(316)), //
 						m(Mpr15S22.ChannelId.QUADRANT_3_REACTIVE_ENERGY_L1, new UnsignedQuadruplewordElement(320)), //
 						m(Mpr15S22.ChannelId.QUADRANT_3_REACTIVE_ENERGY_L2, new UnsignedQuadruplewordElement(324)), //
-						m(Mpr15S22.ChannelId.QUADRANT_3_REACTIVE_ENERGY_L3, new UnsignedQuadruplewordElement(328)), //
-						m(Mpr15S22.ChannelId.QUADRANT_3_REACTIVE_ENERGY_L4, new UnsignedQuadruplewordElement(332)), //
+						m(Mpr15S22.ChannelId.QUADRANT_3_REACTIVE_ENERGY_L3, new UnsignedQuadruplewordElement(328))), //
+				new FC3ReadRegistersTask(336, Priority.HIGH,
 						m(Mpr15S22.ChannelId.QUADRANT_3_TOTAL_REACTIVE_ENERGY, new UnsignedQuadruplewordElement(336)), //
 						m(Mpr15S22.ChannelId.QUADRANT_4_REACTIVE_ENERGY_L1, new UnsignedQuadruplewordElement(340)), //
 						m(Mpr15S22.ChannelId.QUADRANT_4_REACTIVE_ENERGY_L2, new UnsignedQuadruplewordElement(344)), //
-						m(Mpr15S22.ChannelId.QUADRANT_4_REACTIVE_ENERGY_L3, new UnsignedQuadruplewordElement(348)), //
-						m(Mpr15S22.ChannelId.QUADRANT_4_REACTIVE_ENERGY_L4, new UnsignedQuadruplewordElement(352)), //
+						m(Mpr15S22.ChannelId.QUADRANT_4_REACTIVE_ENERGY_L3, new UnsignedQuadruplewordElement(348))), //
+				new FC3ReadRegistersTask(356, Priority.HIGH,
 						m(Mpr15S22.ChannelId.QUADRANT_4_TOTAL_REACTIVE_ENERGY, new UnsignedQuadruplewordElement(356)), //
 						m(Mpr15S22.ChannelId.NUMBER_OF_PULSE_METER, new UnsignedDoublewordElement(360)), //
 						m(Mpr15S22.ChannelId.TOTAL_PULSE_METER_INPUT_1, new UnsignedDoublewordElement(362)), //
-						m(Mpr15S22.ChannelId.TOTAL_PULSE_METER_INPUT_2, new UnsignedDoublewordElement(364)), //
-						m(Mpr15S22.ChannelId.TOTAL_PULSE_METER_INPUT_3, new UnsignedDoublewordElement(366)), //
-						m(Mpr15S22.ChannelId.TOTAL_PULSE_METER_INPUT_4, new UnsignedDoublewordElement(368)), //
-						m(Mpr15S22.ChannelId.TOTAL_PULSE_METER_INPUT_5, new UnsignedDoublewordElement(370)), //
-						m(Mpr15S22.ChannelId.TOTAL_PULSE_METER_INPUT_6, new UnsignedDoublewordElement(372)), //
-						m(Mpr15S22.ChannelId.TOTAL_PULSE_METER_INPUT_7, new UnsignedDoublewordElement(374)), //
-						m(Mpr15S22.ChannelId.TOTAL_PULSE_METER_INPUT_8, new UnsignedDoublewordElement(376))));
+						m(Mpr15S22.ChannelId.TOTAL_PULSE_METER_INPUT_2, new UnsignedDoublewordElement(364)) //
+				));
 	}
 
 	@Override
