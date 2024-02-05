37c95ed4f5ee6dad9926849ab60aa315148054c7
Sn0w3y <hallerjo97@gmail.com>
2024-02-05 20:03:07 +0100

    Implement Shelly 3EM
    https://www.shelly.com/de/products/shop/shelly-3em-1

---
+++ io.openems.edge.io.shelly/src/io/openems/edge/io/shelly/shellyplus3em/IoShellyPlus3EM.java
@@ -0,0 +1,134 @@
+package io.openems.edge.io.shelly.shellyplus3em;
+
+import org.osgi.service.event.EventHandler;
+
+import io.openems.common.channel.AccessMode;
+import io.openems.common.channel.Level;
+import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
+import io.openems.common.types.OpenemsType;
+import io.openems.edge.common.channel.BooleanDoc;
+import io.openems.edge.common.channel.BooleanWriteChannel;
+import io.openems.edge.common.channel.Doc;
+import io.openems.edge.common.channel.StateChannel;
+import io.openems.edge.common.channel.value.Value;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.io.api.DigitalOutput;
+import io.openems.edge.meter.api.ElectricityMeter;
+import io.openems.edge.meter.api.SinglePhaseMeter;
+
+public interface IoShellyPlus3EM
+		extends DigitalOutput, SinglePhaseMeter, ElectricityMeter, OpenemsComponent, EventHandler {
+
+	public static enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+		/**
+		 * Holds writes to Relay Output for debugging.
+		 *
+		 * <ul>
+		 * <li>Interface: ShellyPlug
+		 * <li>Type: Boolean
+		 * <li>Range: On/Off
+		 * </ul>
+		 */
+		DEBUG_RELAY(Doc.of(OpenemsType.BOOLEAN)), //
+		/**
+		 * Relay Output.
+		 *
+		 * <ul>
+		 * <li>Interface: ShellyPlug
+		 * <li>Type: Boolean
+		 * <li>Range: On/Off
+		 * </ul>
+		 */
+		RELAY(new BooleanDoc() //
+				.accessMode(AccessMode.READ_WRITE) //
+				.onChannelSetNextWriteMirrorToDebugChannel(ChannelId.DEBUG_RELAY)),
+		/**
+		 * Slave Communication Failed Fault.
+		 *
+		 * <ul>
+		 * <li>Interface: ShellyPlug
+		 * <li>Type: State
+		 * </ul>
+		 */
+		SLAVE_COMMUNICATION_FAILED(Doc.of(Level.FAULT)); //
+	
+
+		private final Doc doc;
+
+		private ChannelId(Doc doc) {
+			this.doc = doc;
+		}
+
+		@Override
+		public Doc doc() {
+			return this.doc;
+		}
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#RELAY}.
+	 *
+	 * @return the Channel
+	 */
+	public default BooleanWriteChannel getRelayChannel() {
+		return this.channel(ChannelId.RELAY);
+	}
+
+	/**
+	 * Gets the Relay Output 1. See {@link ChannelId#RELAY}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Boolean> getRelay() {
+		return this.getRelayChannel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on {@link ChannelId#RELAY} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setRelay(Boolean value) {
+		this.getRelayChannel().setNextValue(value);
+	}
+
+	/**
+	 * Sets the Relay Output. See {@link ChannelId#RELAY}.
+	 *
+	 * @param value the next write value
+	 * @throws OpenemsNamedException on error
+	 */
+	public default void setRelay(boolean value) throws OpenemsNamedException {
+		this.getRelayChannel().setNextWriteValue(value);
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#SLAVE_COMMUNICATION_FAILED}.
+	 *
+	 * @return the Channel
+	 */
+	public default StateChannel getSlaveCommunicationFailedChannel() {
+		return this.channel(ChannelId.SLAVE_COMMUNICATION_FAILED);
+	}
+
+	/**
+	 * Gets the Slave Communication Failed State. See
+	 * {@link ChannelId#SLAVE_COMMUNICATION_FAILED}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Boolean> getSlaveCommunicationFailed() {
+		return this.getSlaveCommunicationFailedChannel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#SLAVE_COMMUNICATION_FAILED} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setSlaveCommunicationFailed(boolean value) {
+		this.getSlaveCommunicationFailedChannel().setNextValue(value);
+	}
+
+}
---
+++ io.openems.edge.io.shelly/src/io/openems/edge/io/shelly/shellyplus3em/IoShellyPlus3EMImpl.java
@@ -0,0 +1,229 @@
+package io.openems.edge.io.shelly.shellyplus3em;
+
+import java.util.Objects;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceScope;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+import org.osgi.service.event.propertytypes.EventTopics;
+import org.osgi.service.metatype.annotations.Designate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import io.openems.edge.bridge.http.api.BridgeHttp;
+import io.openems.edge.common.channel.BooleanWriteChannel;
+import io.openems.edge.common.component.AbstractOpenemsComponent;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.event.EdgeEventConstants;
+import io.openems.edge.io.api.DigitalOutput;
+import io.openems.edge.meter.api.ElectricityMeter;
+import io.openems.edge.meter.api.MeterType;
+import io.openems.edge.meter.api.SinglePhase;
+import io.openems.edge.meter.api.SinglePhaseMeter;
+
+@Designate(ocd = Config.class, factory = true)
+@Component(//
+		name = "IO.Shelly.Plus3EM", //
+		immediate = true, //
+		configurationPolicy = ConfigurationPolicy.REQUIRE//
+)
+@EventTopics({ //
+		EdgeEventConstants.TOPIC_CYCLE_EXECUTE_WRITE //
+})
+public class IoShellyPlus3EMImpl extends AbstractOpenemsComponent
+		implements IoShellyPlus3EM, DigitalOutput, SinglePhaseMeter, ElectricityMeter, OpenemsComponent, EventHandler {
+
+	private final Logger log = LoggerFactory.getLogger(IoShellyPlus3EMImpl.class);
+	private final BooleanWriteChannel[] digitalOutputChannels;
+
+	private MeterType meterType = null;
+	private SinglePhase phase = null;
+	private String baseUrl;
+
+	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
+	private BridgeHttp httpBridge;
+
+	public IoShellyPlus3EMImpl() {
+		super(//
+				OpenemsComponent.ChannelId.values(), //
+				ElectricityMeter.ChannelId.values(), //
+				DigitalOutput.ChannelId.values(), //
+				IoShellyPlus3EM.ChannelId.values() //
+		);
+		this.digitalOutputChannels = new BooleanWriteChannel[] { //
+				this.channel(IoShellyPlus3EM.ChannelId.RELAY) //
+		};
+
+		ElectricityMeter.calculateSumActivePowerFromPhases(this);
+	}
+
+	@Activate
+	private void activate(ComponentContext context, Config config) {
+		super.activate(context, config.id(), config.alias(), config.enabled());
+		this.meterType = config.type();
+		this.baseUrl = "http://" + config.ip();
+
+		if (!this.isEnabled()) {
+			return;
+		}
+
+		this.httpBridge.subscribeJsonEveryCycle(this.baseUrl + "/status", this::processHttpResult);
+	}
+
+	@Override
+	@Deactivate
+	protected void deactivate() {
+		super.deactivate();
+	}
+
+	@Override
+	public BooleanWriteChannel[] digitalOutputChannels() {
+		return this.digitalOutputChannels;
+	}
+
+	@Override
+	public String debugLog() {
+	    var b = new StringBuilder();
+	    var valueOpt = this.getRelayChannel().value().asOptional();
+	    if (valueOpt.isPresent()) {
+	        b.append(valueOpt.get() ? "ON" : "OFF");
+	    } else {
+	        b.append("Unknown");
+	    }
+	    b.append("|");
+	    b.append(this.getActivePowerChannel().value().asString());
+
+	    return b.toString();
+	}
+
+
+	@Override
+	public void handleEvent(Event event) {
+		if (!this.isEnabled()) {
+			return;
+		}
+
+		switch (event.getTopic()) {
+		case EdgeEventConstants.TOPIC_CYCLE_EXECUTE_WRITE -> {
+			this.executeWrite(this.getRelayChannel(), 0);
+		}
+		}
+	}
+	
+	
+	private void processHttpResult(JsonElement result, Throwable error) {
+	    
+	    this._setSlaveCommunicationFailed(result == null);
+	    if (error != null) {
+	        this._setRelay(null);
+	        this._setActivePower(null);
+	        this._setActiveProductionEnergy(null);
+	        this.logDebug(this.log, error.getMessage());
+	        return;
+	    }
+
+	    try {
+	        JsonObject jsonResponse = result.getAsJsonObject();
+	        
+
+	        JsonArray relays = jsonResponse.getAsJsonArray("relays");
+	        if (relays != null && relays.size() > 0) {
+	            JsonObject relay = relays.get(0).getAsJsonObject();
+	            boolean isOn = relay.get("ison").getAsBoolean();
+	            this.getRelayChannel().setNextWriteValue(isOn);
+	            this._setRelay(isOn);
+
+	        }
+
+
+	        JsonArray emeters = jsonResponse.getAsJsonArray("emeters");
+	        for (int i = 0; i < emeters.size(); i++) {
+	            JsonObject emeter = emeters.get(i).getAsJsonObject();
+	            Float power = emeter.get("power").getAsFloat();
+	            Float voltage = emeter.get("voltage").getAsFloat();
+	            Float current = emeter.get("current").getAsFloat();
+	            
+	            setValuesForPhase(i + 1, voltage, current, power);
+	        }
+
+	    } catch (Exception e) {
+	        this._setRelay(null);
+	        this._setActivePower(null);
+	        this._setActiveProductionEnergy(null);
+	        this.logDebug(this.log, e.getMessage());
+	    }
+	}
+
+
+
+	private void setValuesForPhase(int phase, Float voltage, Float current, Float power) {
+	    Integer scaledVoltage = Math.round(voltage * 1000);
+	    Integer scaledCurrent = Math.round(current * 1000);
+	    Integer scaledPower = Math.round(power);
+	    
+	    switch (phase) {
+	        case 1:
+	            this._setActivePowerL1(scaledPower);
+	            this._setVoltageL1(scaledVoltage);
+	            this._setCurrentL1(scaledCurrent);
+	            break;
+	        case 2:
+	            this._setActivePowerL2(scaledPower);
+	            this._setVoltageL2(scaledVoltage);
+	            this._setCurrentL2(scaledCurrent);
+	            break;
+	        case 3:
+	            this._setActivePowerL3(scaledPower);
+	            this._setVoltageL3(scaledVoltage);
+	            this._setCurrentL3(scaledCurrent);
+	            break;
+	    }
+
+	}
+
+
+	/**
+	 * Execute on Cycle Event "Execute Write".
+	 * 
+	 * @param channel write channel
+	 * @param index   index
+	 */
+	private void executeWrite(BooleanWriteChannel channel, int index) {
+		var readValue = channel.value().get();
+		var writeValue = channel.getNextWriteValueAndReset();
+		if (writeValue.isEmpty()) {
+			// no write value
+			return;
+		}
+		if (Objects.equals(readValue, writeValue.get())) {
+			// read value = write value
+			return;
+		}
+		final var url = this.baseUrl + "/relay/" + index + "?turn=" + (writeValue.get() ? "on" : "off");
+		this.httpBridge.request(url).whenComplete((t, e) -> {
+			this._setSlaveCommunicationFailed(e != null);
+		});
+	}
+
+
+	@Override
+	public MeterType getMeterType() {
+		return this.meterType;
+	}
+
+	@Override
+	public SinglePhase getPhase() {
+		return this.phase;
+	}
+
+}
