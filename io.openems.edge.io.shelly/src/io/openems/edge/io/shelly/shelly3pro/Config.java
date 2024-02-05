96d54c7f6431f0244806fc553a029ee1942e72da
Sn0w3y <hallerjo97@gmail.com>
2024-02-05 01:35:50 +0100

    Implement Shelly3Pro Relay

---
+++ io.openems.edge.io.shelly/src/io/openems/edge/io/shelly/shelly3pro/Config.java
@@ -0,0 +1,22 @@
+package io.openems.edge.io.shelly.shelly3pro;
+
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+@ObjectClassDefinition(//
+		name = "IO Shelly Pro 3", //
+		description = "Implements the Shelly Pro 3 Switch.")
+@interface Config {
+
+	@AttributeDefinition(name = "Component-ID", description = "Unique ID of this Component")
+	String id() default "io0";
+
+	@AttributeDefinition(name = "Alias", description = "Human-readable name of this Component; defaults to Component-ID")
+	String alias() default "";
+
+	@AttributeDefinition(name = "Is enabled?", description = "Is this Component enabled?")
+	boolean enabled() default true;
+
+	@AttributeDefinition(name = "IP-Address", description = "The IP address of the Shelly device.")
+	String ip();
+}
