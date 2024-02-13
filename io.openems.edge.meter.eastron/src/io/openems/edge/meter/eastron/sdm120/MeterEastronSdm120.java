94ee91eece249738615df1bf398b9025afda92e1
Sn0w3y <hallerjo97@gmail.com>
2024-02-13 19:14:28 +0100

    Add files via upload

---
+++ io.openems.edge.meter.eastron/src/io/openems/edge/meter/eastron/sdm120/MeterEastronSdm120.java
@@ -0,0 +1,31 @@
+package io.openems.edge.meter.eastron.sdm120;
+
+import io.openems.common.channel.Unit;
+import io.openems.common.types.OpenemsType;
+import io.openems.edge.common.channel.Doc;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.modbusslave.ModbusSlave;
+import io.openems.edge.meter.api.ElectricityMeter;
+
+public interface MeterEastronSdm120 extends ElectricityMeter, OpenemsComponent, ModbusSlave {
+
+	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+		REACTIVE_PRODUCTION_ENERGY(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.VOLT_AMPERE_REACTIVE_HOURS)), //
+		REACTIVE_CONSUMPTION_ENERGY(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.VOLT_AMPERE_REACTIVE_HOURS)), //
+		;
+
+		private final Doc doc;
+
+		private ChannelId(Doc doc) {
+			this.doc = doc;
+		}
+
+		@Override
+		public Doc doc() {
+			return this.doc;
+		}
+	}
+
+}
---
+++ io.openems.edge.meter.eastron/src/io/openems/edge/meter/eastron/sdm120/MeterEastronSdm120Impl.java
@@ -0,0 +1,198 @@
+package io.openems.edge.meter.eastron.sdm120;
+
+import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.DIRECT_1_TO_1;
+import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.SCALE_FACTOR_3;
+
+import java.nio.ByteOrder;
+
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+import org.osgi.service.event.propertytypes.EventTopics;
+import org.osgi.service.metatype.annotations.Designate;
+
+import io.openems.common.channel.AccessMode;
+import io.openems.common.exceptions.OpenemsException;
+import io.openems.edge.bridge.modbus.api.AbstractOpenemsModbusComponent;
+import io.openems.edge.bridge.modbus.api.BridgeModbus;
+import io.openems.edge.bridge.modbus.api.ModbusComponent;
+import io.openems.edge.bridge.modbus.api.ModbusProtocol;
+import io.openems.edge.bridge.modbus.api.element.DummyRegisterElement;
+import io.openems.edge.bridge.modbus.api.element.FloatDoublewordElement;
+import io.openems.edge.bridge.modbus.api.element.WordOrder;
+import io.openems.edge.bridge.modbus.api.task.FC4ReadInputRegistersTask;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.event.EdgeEventConstants;
+import io.openems.edge.common.modbusslave.ModbusSlave;
+import io.openems.edge.common.modbusslave.ModbusSlaveTable;
+import io.openems.edge.common.taskmanager.Priority;
+import io.openems.edge.meter.api.ElectricityMeter;
+import io.openems.edge.meter.api.MeterType;
+import io.openems.edge.meter.api.SinglePhase;
+import io.openems.edge.meter.api.SinglePhaseMeter;
+import io.openems.edge.timedata.api.Timedata;
+import io.openems.edge.timedata.api.TimedataProvider;
+import io.openems.edge.timedata.api.utils.CalculateEnergyFromPower;
+
+@Designate(ocd = Config.class, factory = true)
+@Component(//
+		name = "Meter.Microcare.SDM120", //
+		immediate = true, //
+		configurationPolicy = ConfigurationPolicy.REQUIRE //
+)
+@EventTopics({ //
+		EdgeEventConstants.TOPIC_CYCLE_AFTER_PROCESS_IMAGE //
+})
+public class MeterEastronSdm120Impl extends AbstractOpenemsModbusComponent
+		implements MeterEastronSdm120, SinglePhaseMeter, ElectricityMeter, ModbusComponent, OpenemsComponent,
+		ModbusSlave, TimedataProvider, EventHandler {
+
+	private final CalculateEnergyFromPower calculateProductionEnergy = new CalculateEnergyFromPower(this,
+			ElectricityMeter.ChannelId.ACTIVE_PRODUCTION_ENERGY);
+	private final CalculateEnergyFromPower calculateConsumptionEnergy = new CalculateEnergyFromPower(this,
+			ElectricityMeter.ChannelId.ACTIVE_CONSUMPTION_ENERGY);
+
+	private MeterType meterType = null;
+	private SinglePhase phase = null;
+
+	@Reference
+	private ConfigurationAdmin cm;
+
+	@Reference(policy = ReferencePolicy.DYNAMIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.OPTIONAL)
+	private volatile Timedata timedata;
+
+	@Override
+	@Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
+	protected void setModbus(BridgeModbus modbus) {
+		super.setModbus(modbus);
+	}
+
+	public MeterEastronSdm120Impl() {
+		super(//
+				OpenemsComponent.ChannelId.values(), //
+				ModbusComponent.ChannelId.values(), //
+				ElectricityMeter.ChannelId.values(), //
+				MeterEastronSdm120.ChannelId.values() //
+		);
+
+		SinglePhaseMeter.calculateSinglePhaseFromActivePower(this);
+	}
+
+	@Activate
+	private void activate(ComponentContext context, Config config) throws OpenemsException {
+		this.meterType = config.type();
+		if (super.activate(context, config.id(), config.alias(), config.enabled(), config.modbusUnitId(), this.cm,
+				"Modbus", config.modbus_id())) {
+			return;
+		}
+	}
+
+	@Override
+	@Deactivate
+	protected void deactivate() {
+		super.deactivate();
+	}
+
+	@Override
+	public MeterType getMeterType() {
+		return this.meterType;
+	}
+
+	@Override
+	protected ModbusProtocol defineModbusProtocol() throws OpenemsException {
+		final var offset = 30001;
+		return new ModbusProtocol(this, //
+				new FC4ReadInputRegistersTask(30001 - offset, Priority.HIGH,
+						m(ElectricityMeter.ChannelId.VOLTAGE,
+								new FloatDoublewordElement(30001 - offset).wordOrder(WordOrder.MSWLSW)
+										.byteOrder(ByteOrder.BIG_ENDIAN),
+								SCALE_FACTOR_3),
+						m(ElectricityMeter.ChannelId.CURRENT,
+								new FloatDoublewordElement(30007 - offset).wordOrder(WordOrder.MSWLSW)
+										.byteOrder(ByteOrder.BIG_ENDIAN),
+								SCALE_FACTOR_3),
+						m(ElectricityMeter.ChannelId.ACTIVE_POWER,
+								new FloatDoublewordElement(30013 - offset).wordOrder(WordOrder.MSWLSW)
+										.byteOrder(ByteOrder.BIG_ENDIAN),
+								DIRECT_1_TO_1),
+						new DummyRegisterElement(30019 - offset, 30024 - offset),
+						m(ElectricityMeter.ChannelId.REACTIVE_POWER,
+								new FloatDoublewordElement(30025 - offset).wordOrder(WordOrder.MSWLSW)
+										.byteOrder(ByteOrder.BIG_ENDIAN),
+								DIRECT_1_TO_1),
+						new DummyRegisterElement(30063 - offset, 30070 - offset),
+						m(ElectricityMeter.ChannelId.FREQUENCY,
+								new FloatDoublewordElement(30071 - offset).wordOrder(WordOrder.MSWLSW)
+										.byteOrder(ByteOrder.BIG_ENDIAN),
+								DIRECT_1_TO_1),
+						new DummyRegisterElement(30073 - offset, 30076 - offset), //
+						m(MeterEastronSdm120.ChannelId.REACTIVE_PRODUCTION_ENERGY,
+								new FloatDoublewordElement(30077 - offset).wordOrder(WordOrder.MSWLSW)
+										.byteOrder(ByteOrder.BIG_ENDIAN),
+								SCALE_FACTOR_3),
+						m(MeterEastronSdm120.ChannelId.REACTIVE_CONSUMPTION_ENERGY,
+								new FloatDoublewordElement(30079 - offset).wordOrder(WordOrder.MSWLSW)
+										.byteOrder(ByteOrder.BIG_ENDIAN),
+								SCALE_FACTOR_3)));
+	}
+
+	@Override
+	public String debugLog() {
+		return "L:" + this.getActivePower().asString();
+	}
+
+	@Override
+	public ModbusSlaveTable getModbusSlaveTable(AccessMode accessMode) {
+		return new ModbusSlaveTable(//
+				OpenemsComponent.getModbusSlaveNatureTable(accessMode), //
+				ElectricityMeter.getModbusSlaveNatureTable(accessMode) //
+		);
+	}
+
+	@Override
+	public Timedata getTimedata() {
+		return this.timedata;
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+		if (!this.isEnabled()) {
+			return;
+		}
+		switch (event.getTopic()) {
+		case EdgeEventConstants.TOPIC_CYCLE_AFTER_PROCESS_IMAGE -> this.calculateEnergy();
+		}
+	}
+
+	/**
+	 * Calculate the Energy values from ActivePower.
+	 */
+	private void calculateEnergy() {
+		// Calculate Energy
+		final var activePower = this.getActivePower().get();
+		if (activePower == null) {
+			this.calculateProductionEnergy.update(null);
+			this.calculateConsumptionEnergy.update(null);
+		} else if (activePower >= 0) {
+			this.calculateProductionEnergy.update(activePower);
+			this.calculateConsumptionEnergy.update(0);
+		} else {
+			this.calculateProductionEnergy.update(0);
+			this.calculateConsumptionEnergy.update(-activePower);
+		}
+	}
+
+	@Override
+	public SinglePhase getPhase() {
+		return this.phase;
+	}
+}
