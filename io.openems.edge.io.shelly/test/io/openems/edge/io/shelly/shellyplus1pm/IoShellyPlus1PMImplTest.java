d9833ea5f91940b9b214973adb3993968813bc08
Hannes <hallerjo97@gmail.com>
2024-02-25 10:26:44 +0100

    Add files via upload

---
+++ io.openems.edge.io.shelly/test/io/openems/edge/io/shelly/shellyplus1pm/IoShellyPlus1PMImplTest.java
@@ -0,0 +1,25 @@
+package io.openems.edge.io.shelly.shellyplus1pm;
+
+import org.junit.Test;
+
+import io.openems.edge.bridge.http.dummy.DummyBridgeHttpFactory;
+import io.openems.edge.common.test.ComponentTest;
+import io.openems.edge.meter.api.MeterType;
+
+public class IoShellyPlus1PMImplTest {
+
+	private static final String COMPONENT_ID = "io0";
+
+	@Test
+	public void test() throws Exception {
+		new ComponentTest(new IoShellyPlus1PmImpl()) //
+				.addReference("httpBridgeFactory", new DummyBridgeHttpFactory()) //
+				.activate(MyConfig.create() //
+						.setId(COMPONENT_ID) //
+						.setIp("127.0.0.1") //
+						.setType(MeterType.CONSUMPTION_METERED) //
+						.build()) //
+		;
+	}
+
+}
