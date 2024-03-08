085fb47e2229482e452cfde9303cd815834cf948
Sn0w3y <hallerjo97@gmail.com>
2024-03-08 15:49:45 +0100

    Update IoOpenDtuImplTest.java

--- io.openems.edge.io.opendtu/test/io/openems/edge/io/opendtu/inverter/IoOpenDtuImplTest.java
+++ io.openems.edge.io.opendtu/test/io/openems/edge/io/opendtu/inverter/IoOpenDtuImplTest.java
@@ -8,29 +8,27 @@ import io.openems.edge.meter.api.MeterType;
 
 public class IoOpenDtuImplTest {
 
-    private static final String COMPONENT_ID = "io0";
+	private static final String COMPONENT_ID = "io0";
 
-    @Test
-    public void test() throws Exception {
-        MyConfig config = MyConfig.create()
-                .setId(COMPONENT_ID)
-                .setIp("127.0.0.1")
-                .setType(MeterType.PRODUCTION)
-                .setUsername("admin")
-                .setPassword("admin")
-                .setSerialNumberL1("1234567890L1")
-                .setSerialNumberL2("1234567890L2")
-                .setSerialNumberL3("1234567890L3")
-                .setRelativeLimit(100)
-                .setAbsoluteLimit(800)
-                .setThreshold(50)
-                .setDelay(30)
-                .setDebugMode(true)
-                .build();
-
-        new ComponentTest(new OpendtuImpl()) //
-                .addReference("httpBridgeFactory", new DummyBridgeHttpFactory()) //
-                .activate(config) //
-        ;
-    }
+	@Test
+	public void test() throws Exception {
+		new ComponentTest(new OpendtuImpl()) //
+				.addReference("httpBridgeFactory", new DummyBridgeHttpFactory()) //
+				.activate(MyConfig.create() //
+						.setId(COMPONENT_ID) //
+						.setIp("127.0.0.1") //
+						.setType(MeterType.PRODUCTION) //
+						.setUsername("admin") //
+						.setPassword("admin") //
+						.setSerialNumberL1("1234567890L1") //
+						.setSerialNumberL2("1234567890L2") //
+						.setSerialNumberL3("1234567890L3") //
+						.setRelativeLimit(100) //
+						.setAbsoluteLimit(800) //
+						.setThreshold(50) //
+						.setDelay(30) //
+						.setDebugMode(true) //
+						.build()); //
+		;
+	}
 }
