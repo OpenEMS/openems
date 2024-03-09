8da7c0a8ea60bbb2873ff4cf00f624e0f5221f43
Sn0w3y <hallerjo97@gmail.com>
2024-03-09 14:10:11 +0100

    Added tests

---
+++ io.openems.edge.io.shelly/test/io/openems/edge/io/shelly/shellypro3em/IoShelly3EmImplTest.java
@@ -0,0 +1,31 @@
+package io.openems.edge.io.shelly.shellypro3em;
+
+import org.junit.Test;
+
+import io.openems.edge.bridge.modbus.test.DummyModbusBridge;
+import io.openems.edge.common.test.ComponentTest;
+import io.openems.edge.common.test.DummyConfigurationAdmin;
+import io.openems.edge.io.shelly.shelly3em.IoShelly3EmImpl;
+import io.openems.edge.meter.api.MeterType;
+
+public class IoShelly3EmImplTest {
+
+	private static final String METER_ID = "io0";
+	private static final String MODBUS_ID = "modbus0";
+
+	private static IoShelly3EmImpl meter;
+
+	@Test
+	public void setup() throws Exception {
+		meter = new IoShelly3EmImpl();
+		new ComponentTest(meter) //
+				.addReference("cm", new DummyConfigurationAdmin()) //
+				.addReference("setModbus", new DummyModbusBridge(MODBUS_ID)) //
+				.activate(MyConfig.create() //
+						.setId(METER_ID) //
+						.setModbusId(MODBUS_ID) //
+						.setType(MeterType.GRID) //
+						.build()); //
+	}
+
+}
