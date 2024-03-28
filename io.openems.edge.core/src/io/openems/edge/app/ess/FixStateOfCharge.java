de63cc32ed6db75ea9de9fdd570c0ab97ed246b0
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2024-03-28 11:50:26 +0100

    AppCenter: Add definition to set min Role to delete an App & validation
    - Add Permission Role to set which Role is required to delete an App Instance
    - Currently CanSeeRole == CanDeleteRole so there is no UI change neccesarry to add a special request to hide/show/disable delete button.
    
    Co-authored-by: Michael Grill <59126309+michaelgrill@users.noreply.github.com>
    Reviewed-by: Hueseyin Sahutoglu <34771592+huseyinsaht@users.noreply.github.com>
    Reviewed-by: Stefan Feilmeier <3515268+sfeilmeier@users.noreply.github.com>

--- io.openems.edge.core/src/io/openems/edge/app/ess/FixStateOfCharge.java
+++ io.openems.edge.core/src/io/openems/edge/app/ess/FixStateOfCharge.java
@@ -235,6 +235,7 @@ public class FixStateOfCharge extends AbstractOpenemsAppWithProps<FixStateOfChar
 	public OpenemsAppPermissions getAppPermissions() {
 		return OpenemsAppPermissions.create() //
 				.setCanSee(Role.ADMIN) //
+				.setCanDelete(Role.ADMIN) //
 				.build();
 	}
 
