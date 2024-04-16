54b5452c8c2434a03d17d0b0b5f6eacc5c4d6c6f
Sn0w3y <hallerjo97@gmail.com>
2024-04-16 23:22:41 +0200

    Checkstyle and Fixes

--- io.openems.edge.edge2edge/test/io/openems/edge/edge2edge/meter/Edge2EdgeEssMeterImplTest.java
+++ io.openems.edge.edge2edge/test/io/openems/edge/edge2edge/meter/Edge2EdgeEssMeterImplTest.java
@@ -10,21 +10,21 @@ import io.openems.edge.meter.api.MeterType;
 
 public class Edge2EdgeEssMeterImplTest {
 
-    private static final String COMPONENT_ID = "meter0";
-    private static final String MODBUS_ID = "modbus0";
+	private static final String COMPONENT_ID = "meter0";
+	private static final String MODBUS_ID = "modbus0";
 
-    @Test
-    public void test() throws Exception {
-	new ComponentTest(new Edge2EdgeMeterImpl()) //
-		.addReference("cm", new DummyConfigurationAdmin()) //
-		.addReference("setModbus", new DummyModbusBridge(MODBUS_ID)) //
-		.activate(MyConfig.create() //
-			.setId(COMPONENT_ID) //
-			.setModbusId(MODBUS_ID) //
-			.setRemoteComponentId(COMPONENT_ID) //
-			.setMeterType(MeterType.PRODUCTION) //
-			.build())
-		.next(new TestCase());
-    }
+	@Test
+	public void test() throws Exception {
+		new ComponentTest(new Edge2EdgeMeterImpl()) //
+				.addReference("cm", new DummyConfigurationAdmin()) //
+				.addReference("setModbus", new DummyModbusBridge(MODBUS_ID)) //
+				.activate(MyConfig.create() //
+						.setId(COMPONENT_ID) //
+						.setModbusId(MODBUS_ID) //
+						.setRemoteComponentId(COMPONENT_ID) //
+						.setMeterType(MeterType.PRODUCTION) //
+						.build())
+				.next(new TestCase());
+	}
 
 }
--- io.openems.edge.edge2edge/test/io/openems/edge/edge2edge/meter/MyConfig.java
+++ io.openems.edge.edge2edge/test/io/openems/edge/edge2edge/meter/MyConfig.java
@@ -1,6 +1,5 @@
 package io.openems.edge.edge2edge.meter;
 
-import io.openems.common.channel.AccessMode;
 import io.openems.common.test.AbstractComponentConfig;
 import io.openems.common.utils.ConfigUtils;
 import io.openems.edge.meter.api.MeterType;
@@ -8,85 +7,85 @@ import io.openems.edge.meter.api.MeterType;
 @SuppressWarnings("all")
 public class MyConfig extends AbstractComponentConfig implements Config {
 
-    protected static class Builder {
-	private String id;
-	private String modbusId = null;
-	public int modbusUnitId;
-	public String remoteComponentId;
-	public MeterType type;
+	protected static class Builder {
+		private String id;
+		private String modbusId = null;
+		private int modbusUnitId;
+		private String remoteComponentId;
+		private MeterType type;
 
-	private Builder() {
+		private Builder() {
+		}
+
+		public Builder setId(String id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder setModbusId(String modbusId) {
+			this.modbusId = modbusId;
+			return this;
+		}
+
+		public Builder setModbusUnitId(int modbusUnitId) {
+			this.modbusUnitId = modbusUnitId;
+			return this;
+		}
+
+		public Builder setRemoteComponentId(String remoteComponentId) {
+			this.remoteComponentId = remoteComponentId;
+			return this;
+		}
+
+		public Builder setMeterType(MeterType type) {
+			this.type = type;
+			return this;
+		}
+
+		public MyConfig build() {
+			return new MyConfig(this);
+		}
 	}
 
-	public Builder setId(String id) {
-	    this.id = id;
-	    return this;
+	/**
+	 * Create a Config builder.
+	 * 
+	 * @return a {@link Builder}
+	 */
+	public static Builder create() {
+		return new Builder();
 	}
 
-	public Builder setModbusId(String modbusId) {
-	    this.modbusId = modbusId;
-	    return this;
+	private final Builder builder;
+
+	private MyConfig(Builder builder) {
+		super(Config.class, builder.id);
+		this.builder = builder;
 	}
 
-	public Builder setModbusUnitId(int modbusUnitId) {
-	    this.modbusUnitId = modbusUnitId;
-	    return this;
+	@Override
+	public String modbus_id() {
+		return this.builder.modbusId;
 	}
 
-	public Builder setRemoteComponentId(String remoteComponentId) {
-	    this.remoteComponentId = remoteComponentId;
-	    return this;
+	@Override
+	public String Modbus_target() {
+		return ConfigUtils.generateReferenceTargetFilter(this.id(), this.modbus_id());
 	}
 
-	public Builder setMeterType(MeterType type) {
-	    this.type = type;
-	    return this;
+	@Override
+	public int modbusUnitId() {
+		return this.builder.modbusUnitId;
 	}
 
-	public MyConfig build() {
-	    return new MyConfig(this);
+	@Override
+	public String remoteComponentId() {
+		return this.builder.remoteComponentId;
 	}
-    }
 
-    /**
-     * Create a Config builder.
-     * 
-     * @return a {@link Builder}
-     */
-    public static Builder create() {
-	return new Builder();
-    }
-
-    private final Builder builder;
-
-    private MyConfig(Builder builder) {
-	super(Config.class, builder.id);
-	this.builder = builder;
-    }
-
-    @Override
-    public String modbus_id() {
-	return this.builder.modbusId;
-    }
-
-    @Override
-    public String Modbus_target() {
-	return ConfigUtils.generateReferenceTargetFilter(this.id(), this.modbus_id());
-    }
-
-    @Override
-    public int modbusUnitId() {
-	return this.builder.modbusUnitId;
-    }
-
-    @Override
-    public String remoteComponentId() {
-	return this.builder.remoteComponentId;
-    }
-
-    @Override
-    public MeterType type() {
-	return this.builder.type;
-    }
+	@Override
+	public MeterType type() {
+		return this.builder.type;
+	}
 
 }
