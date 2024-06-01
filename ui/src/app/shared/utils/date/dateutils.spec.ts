e853ca8bf02d210b41a1ed76819184d8910be631
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2024-06-01 15:38:00 +0200

    UI: add notification infrastructure
    Co-authored-by: Sebastian Asen <47855186+sebastianasen@users.noreply.github.com>
    Reviewed-by: Sebastian Asen <47855186+sebastianasen@users.noreply.github.com>
    Reviewed-by: Stefan Feilmeier <3515268+sfeilmeier@users.noreply.github.com>
    Co-authored-by: Lukas Rieger <73471197+lukasrgr@users.noreply.github.com>
    Co-committed-by: Lukas Rieger <73471197+lukasrgr@users.noreply.github.com>

--- ui/src/app/shared/utils/date/dateutils.spec.ts
+++ ui/src/app/shared/utils/date/dateutils.spec.ts
@@ -35,4 +35,13 @@ describe('DateUtils', () => {
     expect(DateUtils.stringToDate('2023-01-02')).toEqual(new Date(Date.parse('2023-01-02')));
     expect(DateUtils.stringToDate('wrong format')).toEqual(null);
   });
+
+  it('#isDateBefore - checks if given date is before date to be compared to', () => {
+    const date: Date = DateUtils.stringToDate('2023-01-01');
+    expect(DateUtils.isDateBefore(date, DateUtils.stringToDate("2023-01-31"))).toEqual(true);
+    expect(DateUtils.isDateBefore(date, DateUtils.stringToDate("2022-12-31"))).toEqual(false);
+    expect(DateUtils.isDateBefore(date, DateUtils.stringToDate("2023-01-01"))).toEqual(false);
+    expect(DateUtils.isDateBefore(date, null)).toEqual(false);
+    expect(DateUtils.isDateBefore(null, DateUtils.stringToDate("2023-01-01"))).toEqual(false);
+  });
 });
