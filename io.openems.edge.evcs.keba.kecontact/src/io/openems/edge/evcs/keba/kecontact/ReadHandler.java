f7474db196c429164eacb3d867ad5d3cdd7ffc8f
Sn0w3y <hallerjo97@gmail.com>
2024-06-06 22:27:36 +0200

    do not set x2 1 in ReadHandler
    This should fix the TCH-Errors !

--- io.openems.edge.evcs.keba.kecontact/src/io/openems/edge/evcs/keba/kecontact/ReadHandler.java
+++ io.openems.edge.evcs.keba.kecontact/src/io/openems/edge/evcs/keba/kecontact/ReadHandler.java
@@ -160,25 +160,27 @@ public class ReadHandler implements Consumer<String> {
 					this.setBoolean(EvcsKebaKeContact.ChannelId.INPUT, jsonMessage, "Input");
 					this.setInt(EvcsKebaKeContact.ChannelId.MAX_CURR, jsonMessage, "Curr HW");
 					this.setInt(EvcsKebaKeContact.ChannelId.CURR_USER, jsonMessage, "Curr user");
-					
-					this.setInt(EvcsKebaKeContact.ChannelId.X2_PHASE_SWITCH, jsonMessage, "X2 phaseSwitch");
-					this.setInt(EvcsKebaKeContact.ChannelId.X2_PHASE_SWITCH_SOURCE, jsonMessage, "X2 phaseSwitch source");
 
-					// Check configuration for phase switch activation and parse the "X2
-					// phaseSwitch" status
+					this.setInt(EvcsKebaKeContact.ChannelId.X2_PHASE_SWITCH, jsonMessage, "X2 phaseSwitch");
+					this.setInt(EvcsKebaKeContact.ChannelId.X2_PHASE_SWITCH_SOURCE, jsonMessage,
+							"X2 phaseSwitch source");
+
+					// Check configuration for phase switch activation
 					boolean isPhaseSwitchConfiguredActive = this.parent.phaseSwitchActive();
-					JsonElement x2PhaseSwitchElement = jsonMessage.get("X2 phaseSwitch"); //
+					JsonElement x2PhaseSwitchElement = jsonMessage.get("X2 phaseSwitch");
 					Integer x2PhaseSwitchStatus = x2PhaseSwitchElement != null && x2PhaseSwitchElement.isJsonPrimitive()
 							? x2PhaseSwitchElement.getAsInt()
-							: null; //
+							: null;
 
+					// Only set the phase switch source if the phase switch is configured and
+					// currently reported as inactive
 					if (isPhaseSwitchConfiguredActive && (x2PhaseSwitchStatus != null && x2PhaseSwitchStatus == 0)) {
-						// If configured active in Config but reported inactive from Keba, attempt to activate commands via UDP
-						boolean isSetControlChannelSuccess = this.parent.send("x2src 4");
-						if (isSetControlChannelSuccess && this.parent.send("x2 1")) {
-							this.log.info("Phase switch activation via UDP successful.");
+						// Set the phase switch source to UDP control without activating the switch
+						// logic now !
+						if (this.parent.send("x2src 4")) {
+							this.log.info("Phase switch source set to UDP control successfully.");
 						} else {
-							this.log.error("Failed to activate phase switch via UDP.");
+							this.log.error("Failed to set phase switch source to UDP control.");
 						}
 					} else if (!isPhaseSwitchConfiguredActive) {
 						this.log.debug("Phase switch activation not enabled in configuration.");
