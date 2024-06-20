77afbaf2d77f232ac07645594bc0b953b7c22d45
Hiromasa Ihara <iharahiromasa@gmail.com>
2024-06-20 23:13:14 +0900

    feat: migration .eslintrc.json to eslint.config.mjs

--- ui/eslint.config.mjs
+++ ui/eslint.config.mjs
@@ -1,35 +1,54 @@
-{
-  "root": true,
-  "ignorePatterns": [
-    "projects/**/*"
-  ],
-  "overrides": [
-    {
-      "files": [
-        "*.ts"
-      ],
-      "env": {
-        "browser": true,
-        "node": true,
-        "jest": true
-      },
-      "parserOptions": {
-        "project": [
-          "tsconfig.json"
-        ],
-        "createDefaultProgram": true
-      },
-      "plugins": [
-        "unused-imports",
-        "@stylistic"
-      ],
-      "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@angular-eslint/recommended",
-        "plugin:@angular-eslint/template/process-inline-templates"
-      ],
-      "rules": {
+import unusedImports from "eslint-plugin-unused-imports";
+import stylistic from "@stylistic/eslint-plugin";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    "ignores": ["projects/**/*"],
+}, ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@angular-eslint/recommended",
+    "plugin:@angular-eslint/template/process-inline-templates",
+).map(config => ({
+    ...config,
+    "files": ["**/*.ts"],
+})), {
+    "files": ["**/*.ts"],
+
+    "plugins": {
+        "unused-imports": unusedImports,
+        "@stylistic": stylistic,
+    },
+
+    "languageOptions": {
+        "globals": {
+            ...globals.browser,
+            ...globals.node,
+            ...globals.jest,
+        },
+
+        "ecmaVersion": 5,
+        "sourceType": "commonjs",
+
+        "parserOptions": {
+            "project": ["tsconfig.json"],
+            "createDefaultProgram": true,
+        },
+    },
+
+    "rules": {
         "unused-imports/no-unused-imports": "error",
         "@stylistic/semi": [
           "error",
@@ -96,16 +115,11 @@
             }
           }
         ]
-      }
     },
-    {
-      "files": [
-        "*.html"
-      ],
-      "extends": [
-        "plugin:@angular-eslint/template/recommended"
-      ],
-      "rules": {}
-    }
-  ]
-}
+}, ...compat.extends("plugin:@angular-eslint/template/recommended").map(config => ({
+    ...config,
+    "files": ["**/*.html"],
+})), {
+    "files": ["**/*.html"],
+    "rules": {},
+}];
