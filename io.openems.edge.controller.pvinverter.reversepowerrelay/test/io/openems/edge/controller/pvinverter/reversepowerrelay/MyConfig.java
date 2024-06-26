ba0b50842a2888979ca5aabafc937584d4300f9a
DerWahreKlinki <thomas@partec.org>
2024-06-26 12:26:11 +0200

    Improved Test class

--- io.openems.edge.controller.pvinverter.reversepowerrelay/test/io/openems/edge/controller/pvinverter/reversepowerrelay/MyConfig.java
+++ io.openems.edge.controller.pvinverter.reversepowerrelay/test/io/openems/edge/controller/pvinverter/reversepowerrelay/MyConfig.java
@@ -5,78 +5,112 @@ import io.openems.common.test.AbstractComponentConfig;
 @SuppressWarnings("all")
 public class MyConfig extends AbstractComponentConfig implements Config {
 
-	protected static class Builder {
-		private String id;
+    protected static class Builder {
+        private String id;
+        private String pvInverterId;
+        private String inputChannelAddress0Percent;
+        private String inputChannelAddress30Percent;
+        private String inputChannelAddress60Percent;
+        private String inputChannelAddress100Percent;
+        private int powerLimit30;
+        private int powerLimit60;
 
-		private Builder() {
-		}
+        private Builder() {
+        }
 
-		public Builder setId(String id) {
-			this.id = id;
-			return this;
-		}
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
 
-		public MyConfig build() {
-			return new MyConfig(this);
-		}
-	}
+        public Builder setPvInverterId(String pvInverterId) {
+            this.pvInverterId = pvInverterId;
+            return this;
+        }
 
-	/**
-	 * Create a Config builder.
-	 * 
-	 * @return a {@link Builder}
-	 */
-	public static Builder create() {
-		return new Builder();
-	}
+        public Builder setInputChannelAddress0Percent(String inputChannelAddress0Percent) {
+            this.inputChannelAddress0Percent = inputChannelAddress0Percent;
+            return this;
+        }
 
-	private final Builder builder;
+        public Builder setInputChannelAddress30Percent(String inputChannelAddress30Percent) {
+            this.inputChannelAddress30Percent = inputChannelAddress30Percent;
+            return this;
+        }
 
-	private MyConfig(Builder builder) {
-		super(Config.class, builder.id);
-		this.builder = builder;
-	}
+        public Builder setInputChannelAddress60Percent(String inputChannelAddress60Percent) {
+            this.inputChannelAddress60Percent = inputChannelAddress60Percent;
+            return this;
+        }
 
-	@Override
-	public String pvInverter_id() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        public Builder setInputChannelAddress100Percent(String inputChannelAddress100Percent) {
+            this.inputChannelAddress100Percent = inputChannelAddress100Percent;
+            return this;
+        }
 
-	@Override
-	public String inputChannelAddress0Percent() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        public Builder setPowerLimit30(int powerLimit30) {
+            this.powerLimit30 = powerLimit30;
+            return this;
+        }
 
-	@Override
-	public String inputChannelAddress30Percent() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        public Builder setPowerLimit60(int powerLimit60) {
+            this.powerLimit60 = powerLimit60;
+            return this;
+        }
 
-	@Override
-	public String inputChannelAddress60Percent() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        public MyConfig build() {
+            return new MyConfig(this);
+        }
+    }
 
-	@Override
-	public String inputChannelAddress100Percent() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /**
+     * Create a Config builder.
+     * 
+     * @return a {@link Builder}
+     */
+    public static Builder create() {
+        return new Builder();
+    }
 
-	@Override
-	public int powerLimit30() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    private final Builder builder;
 
-	@Override
-	public int powerLimit60() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    private MyConfig(Builder builder) {
+        super(Config.class, builder.id);
+        this.builder = builder;
+    }
 
+    @Override
+    public String pvInverter_id() {
+        return builder.pvInverterId;
+    }
+
+    @Override
+    public String inputChannelAddress0Percent() {
+        return builder.inputChannelAddress0Percent;
+    }
+
+    @Override
+    public String inputChannelAddress30Percent() {
+        return builder.inputChannelAddress30Percent;
+    }
+
+    @Override
+    public String inputChannelAddress60Percent() {
+        return builder.inputChannelAddress60Percent;
+    }
+
+    @Override
+    public String inputChannelAddress100Percent() {
+        return builder.inputChannelAddress100Percent;
+    }
+
+    @Override
+    public int powerLimit30() {
+        return builder.powerLimit30;
+    }
+
+    @Override
+    public int powerLimit60() {
+        return builder.powerLimit60;
+    }
 }
