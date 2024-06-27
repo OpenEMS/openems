b59bd05050ab63c7da25a9f083a96b8707a5e839
DerWahreKlinki <thomas@partec.org>
2024-06-27 12:21:33 +0200

    Improved JuniTests

--- io.openems.edge.controller.pvinverter.reversepowerrelay/test/io/openems/edge/controller/pvinverter/reversepowerrelay/MyConfig.java
+++ io.openems.edge.controller.pvinverter.reversepowerrelay/test/io/openems/edge/controller/pvinverter/reversepowerrelay/MyConfig.java
@@ -5,139 +5,139 @@ import io.openems.common.test.AbstractComponentConfig;
 @SuppressWarnings("all")
 public class MyConfig extends AbstractComponentConfig implements Config {
 
-    protected static class Builder {
-        private String id;
-        private String alias = ""; // Default to empty string
-        private boolean enabled = true; // Default to true
-        private String pvInverterId;
-        private String inputChannelAddress0Percent;
-        private String inputChannelAddress30Percent;
-        private String inputChannelAddress60Percent;
-        private String inputChannelAddress100Percent;
-        private int powerLimit30;
-        private int powerLimit60;
+	protected static class Builder {
+		private String id;
+		private String alias = ""; // Default to empty string
+		private boolean enabled = true; // Default to true
+		private String pvInverterId;
+		private String inputChannelAddress0Percent;
+		private String inputChannelAddress30Percent;
+		private String inputChannelAddress60Percent;
+		private String inputChannelAddress100Percent;
+		private int powerLimit30;
+		private int powerLimit60;
 
-        private Builder() {
-        }
+		private Builder() {
+		}
 
-        public Builder setId(String id) {
-            this.id = id;
-            return this;
-        }
+		public Builder setId(String id) {
+			this.id = id;
+			return this;
+		}
 
-        public Builder setAlias(String alias) {
-            this.alias = alias;
-            return this;
-        }
+		public Builder setAlias(String alias) {
+			this.alias = alias;
+			return this;
+		}
 
-        public Builder setEnabled(boolean enabled) {
-            this.enabled = enabled;
-            return this;
-        }
+		public Builder setEnabled(boolean enabled) {
+			this.enabled = enabled;
+			return this;
+		}
 
-        public Builder setPvInverterId(String pvInverterId) {
-            this.pvInverterId = pvInverterId;
-            return this;
-        }
+		public Builder setPvInverterId(String pvInverterId) {
+			this.pvInverterId = pvInverterId;
+			return this;
+		}
 
-        public Builder setInputChannelAddress0Percent(String inputChannelAddress0Percent) {
-            this.inputChannelAddress0Percent = inputChannelAddress0Percent;
-            return this;
-        }
+		public Builder setInputChannelAddress0Percent(String inputChannelAddress0Percent) {
+			this.inputChannelAddress0Percent = inputChannelAddress0Percent;
+			return this;
+		}
 
-        public Builder setInputChannelAddress30Percent(String inputChannelAddress30Percent) {
-            this.inputChannelAddress30Percent = inputChannelAddress30Percent;
-            return this;
-        }
+		public Builder setInputChannelAddress30Percent(String inputChannelAddress30Percent) {
+			this.inputChannelAddress30Percent = inputChannelAddress30Percent;
+			return this;
+		}
 
-        public Builder setInputChannelAddress60Percent(String inputChannelAddress60Percent) {
-            this.inputChannelAddress60Percent = inputChannelAddress60Percent;
-            return this;
-        }
+		public Builder setInputChannelAddress60Percent(String inputChannelAddress60Percent) {
+			this.inputChannelAddress60Percent = inputChannelAddress60Percent;
+			return this;
+		}
 
-        public Builder setInputChannelAddress100Percent(String inputChannelAddress100Percent) {
-            this.inputChannelAddress100Percent = inputChannelAddress100Percent;
-            return this;
-        }
+		public Builder setInputChannelAddress100Percent(String inputChannelAddress100Percent) {
+			this.inputChannelAddress100Percent = inputChannelAddress100Percent;
+			return this;
+		}
 
-        public Builder setPowerLimit30(int powerLimit30) {
-            this.powerLimit30 = powerLimit30;
-            return this;
-        }
+		public Builder setPowerLimit30(int powerLimit30) {
+			this.powerLimit30 = powerLimit30;
+			return this;
+		}
 
-        public Builder setPowerLimit60(int powerLimit60) {
-            this.powerLimit60 = powerLimit60;
-            return this;
-        }
+		public Builder setPowerLimit60(int powerLimit60) {
+			this.powerLimit60 = powerLimit60;
+			return this;
+		}
 
-        public MyConfig build() {
-            return new MyConfig(this);
-        }
-    }
+		public MyConfig build() {
+			return new MyConfig(this);
+		}
+	}
 
-    /**
+	/**
 	 * Create a Config builder.
 	 *
 	 * @return a {@link Builder}
 	 */
-    public static Builder create() {
-        return new Builder();
-    }
+	public static Builder create() {
+		return new Builder();
+	}
 
-    private final Builder builder;
+	private final Builder builder;
 
-    private MyConfig(Builder builder) {
-        super(Config.class, builder.id);
-        this.builder = builder;
-    }
+	private MyConfig(Builder builder) {
+		super(Config.class, builder.id);
+		this.builder = builder;
+	}
 
-    @Override
-    public String id() {
-        return this.builder.id;
-    }
+	@Override
+	public String id() {
+		return this.builder.id;
+	}
 
-    @Override
-    public String alias() {
-        return this.builder.alias;
-    }
+	@Override
+	public String alias() {
+		return this.builder.alias;
+	}
 
-    @Override
-    public boolean enabled() {
-        return this.builder.enabled;
-    }
+	@Override
+	public boolean enabled() {
+		return this.builder.enabled;
+	}
 
-    @Override
-    public String pvInverter_id() {
-        return this.builder.pvInverterId;
-    }
+	@Override
+	public String pvInverter_id() {
+		return this.builder.pvInverterId;
+	}
 
-    @Override
-    public String inputChannelAddress0Percent() {
-        return this.builder.inputChannelAddress0Percent;
-    }
+	@Override
+	public String inputChannelAddress0Percent() {
+		return this.builder.inputChannelAddress0Percent;
+	}
 
-    @Override
-    public String inputChannelAddress30Percent() {
-        return this.builder.inputChannelAddress30Percent;
-    }
+	@Override
+	public String inputChannelAddress30Percent() {
+		return this.builder.inputChannelAddress30Percent;
+	}
 
-    @Override
-    public String inputChannelAddress60Percent() {
-        return this.builder.inputChannelAddress60Percent;
-    }
+	@Override
+	public String inputChannelAddress60Percent() {
+		return this.builder.inputChannelAddress60Percent;
+	}
 
-    @Override
-    public String inputChannelAddress100Percent() {
-        return this.builder.inputChannelAddress100Percent;
-    }
+	@Override
+	public String inputChannelAddress100Percent() {
+		return this.builder.inputChannelAddress100Percent;
+	}
 
-    @Override
-    public int powerLimit30() {
-        return this.builder.powerLimit30;
-    }
+	@Override
+	public int powerLimit30() {
+		return this.builder.powerLimit30;
+	}
 
-    @Override
-    public int powerLimit60() {
-        return this.builder.powerLimit60;
-    }
+	@Override
+	public int powerLimit60() {
+		return this.builder.powerLimit60;
+	}
 }
--- io.openems.edge.controller.pvinverter.reversepowerrelay/test/io/openems/edge/controller/pvinverter/reversepowerrelay/ReversePowerRelayImplTest.java
+++ io.openems.edge.controller.pvinverter.reversepowerrelay/test/io/openems/edge/controller/pvinverter/reversepowerrelay/ReversePowerRelayImplTest.java
@@ -3,61 +3,82 @@ package io.openems.edge.controller.pvinverter.reversepowerrelay;
 import org.junit.Test;
 
 import io.openems.common.types.ChannelAddress;
+
 import io.openems.edge.common.test.AbstractComponentTest.TestCase;
 import io.openems.edge.common.test.DummyComponentManager;
+
 import io.openems.edge.controller.test.ControllerTest;
+import io.openems.edge.meter.test.DummyElectricityMeter;
+import io.openems.edge.pvinverter.test.DummyManagedSymmetricPvInverter;
+
+import io.openems.edge.io.test.DummyInputOutput;
 
 public class ReversePowerRelayImplTest {
 
-    private static final String CTRL_ID = "ctrl0";
-    private static final String PV_INVERTER = "pvInverter0";
+	private static final String CTRL_ID = "ctrl0";
 
-    private static final ChannelAddress INPUT_CHANNEL_0_PERCENT = new ChannelAddress("MyRelay", "Input1");
-    private static final ChannelAddress INPUT_CHANNEL_30_PERCENT = new ChannelAddress("MyRelay", "Input2");
-    private static final ChannelAddress INPUT_CHANNEL_60_PERCENT = new ChannelAddress("MyRelay", "Input3");
-    private static final ChannelAddress INPUT_CHANNEL_100_PERCENT = new ChannelAddress("MyRelay", "Input4");
+	private static final String METER_ID = "meter0";
+	private static final ChannelAddress GRID_ACTIVE_POWER = new ChannelAddress(METER_ID, "ActivePower");
 
-    private static final ChannelAddress PV_INVERTER_ACTIVE_POWER_LIMIT = new ChannelAddress(PV_INVERTER, "ActivePowerLimit");
+	private static final String PV_INVERTER = "pvInverter0";
+	private static final ChannelAddress PV_INVERTER_ACTIVE_POWER = new ChannelAddress(PV_INVERTER, "ActivePower");
+	private static final ChannelAddress PV_INVERTER_SET_ACTIVE_POWER_EQUALS = new ChannelAddress(PV_INVERTER,
+			"ActivePowerLimit");
 
-    @Test
-    public void testReversePowerRelay() throws Exception {
-        new ControllerTest(new ReversePowerRelayImpl()) //
-                .addReference("componentManager", new DummyComponentManager()) //
-                .activate(MyConfig.create() //
-                        .setId(CTRL_ID) //
-                        .setAlias("TestAlias") // Set alias to a non-null value
-                        .setEnabled(true) // Ensure enabled is set
-                        .setPvInverterId(PV_INVERTER) //
-                        .setInputChannelAddress0Percent("MyRelay/Input1") //
-                        .setInputChannelAddress30Percent("MyRelay/Input2") //
-                        .setInputChannelAddress60Percent("MyRelay/Input3") //
-                        .setInputChannelAddress100Percent("MyRelay/Input4") //
-                        .setPowerLimit30(300) //
-                        .setPowerLimit60(600) //
-                        .build())
-                .next(new TestCase() //
-                        .input(INPUT_CHANNEL_0_PERCENT, true) //
-                        .input(INPUT_CHANNEL_30_PERCENT, false) //
-                        .input(INPUT_CHANNEL_60_PERCENT, false) //
-                        .input(INPUT_CHANNEL_100_PERCENT, false) //
-                        .output(PV_INVERTER_ACTIVE_POWER_LIMIT, 0)) //
-                .next(new TestCase() //
-                        .input(INPUT_CHANNEL_0_PERCENT, false) //
-                        .input(INPUT_CHANNEL_30_PERCENT, true) //
-                        .input(INPUT_CHANNEL_60_PERCENT, false) //
-                        .input(INPUT_CHANNEL_100_PERCENT, false) //
-                        .output(PV_INVERTER_ACTIVE_POWER_LIMIT, 300)) //
-                .next(new TestCase() //
-                        .input(INPUT_CHANNEL_0_PERCENT, false) //
-                        .input(INPUT_CHANNEL_30_PERCENT, false) //
-                        .input(INPUT_CHANNEL_60_PERCENT, true) //
-                        .input(INPUT_CHANNEL_100_PERCENT, false) //
-                        .output(PV_INVERTER_ACTIVE_POWER_LIMIT, 600)) //
-                .next(new TestCase() //
-                        .input(INPUT_CHANNEL_0_PERCENT, false) //
-                        .input(INPUT_CHANNEL_30_PERCENT, false) //
-                        .input(INPUT_CHANNEL_60_PERCENT, false) //
-                        .input(INPUT_CHANNEL_100_PERCENT, true) //
-                        .output(PV_INVERTER_ACTIVE_POWER_LIMIT, null));
-    }
+	private static final String IO_ID = "io0";
+	private static final ChannelAddress IO_INPUT_OUTPUT0 = new ChannelAddress(IO_ID, "InputOutput0");
+	private static final ChannelAddress IO_INPUT_OUTPUT1 = new ChannelAddress(IO_ID, "InputOutput1");
+	private static final ChannelAddress IO_INPUT_OUTPUT2 = new ChannelAddress(IO_ID, "InputOutput2");
+	private static final ChannelAddress IO_INPUT_OUTPUT3 = new ChannelAddress(IO_ID, "InputOutput3");
+
+	@Test
+	public void testReversePowerRelay() throws Exception {
+		new ControllerTest(new ReversePowerRelayImpl()) //
+				.addReference("componentManager", new DummyComponentManager()) //
+				.addComponent(new DummyElectricityMeter(METER_ID)) //
+				.addComponent(new DummyManagedSymmetricPvInverter(PV_INVERTER)) //
+
+				.addComponent(new DummyInputOutput(IO_ID)) //
+
+				.activate(MyConfig.create() //
+						.setId(CTRL_ID) //
+						.setPvInverterId(PV_INVERTER) //
+						.setId(CTRL_ID) //
+						.setInputChannelAddress0Percent(IO_INPUT_OUTPUT0.toString()) //
+						.setInputChannelAddress30Percent(IO_INPUT_OUTPUT1.toString()) //
+						.setInputChannelAddress60Percent(IO_INPUT_OUTPUT2.toString()) //
+						.setInputChannelAddress100Percent(IO_INPUT_OUTPUT3.toString()) //
+						.setPowerLimit30(30) //
+						.setPowerLimit60(60) //
+						.build())
+				.next(new TestCase() // undefined
+						.input(GRID_ACTIVE_POWER, -15000) //
+						.input(PV_INVERTER_ACTIVE_POWER, 15000) //
+						.input(IO_INPUT_OUTPUT0, true) //
+						.input(IO_INPUT_OUTPUT1, true) //
+						.input(IO_INPUT_OUTPUT2, true) //
+						.input(IO_INPUT_OUTPUT3, true) //
+						.output(PV_INVERTER_SET_ACTIVE_POWER_EQUALS, 0))
+
+				.next(new TestCase() // 100% -> limit null
+						.input(GRID_ACTIVE_POWER, -15000) //
+						.input(PV_INVERTER_ACTIVE_POWER, 1000) //
+						.input(IO_INPUT_OUTPUT0, false) //
+						.input(IO_INPUT_OUTPUT1, false) //
+						.input(IO_INPUT_OUTPUT2, false) //
+						.input(IO_INPUT_OUTPUT3, true) //
+						.output(PV_INVERTER_SET_ACTIVE_POWER_EQUALS, null))
+
+				.next(new TestCase() // 100% -> limit null
+						.input(GRID_ACTIVE_POWER, -15000) //
+						.input(PV_INVERTER_ACTIVE_POWER, 1000) //
+						.input(IO_INPUT_OUTPUT0, false) //
+						.input(IO_INPUT_OUTPUT1, true) //
+						.input(IO_INPUT_OUTPUT2, false) //
+						.input(IO_INPUT_OUTPUT3, false) //
+						.output(PV_INVERTER_SET_ACTIVE_POWER_EQUALS, 30))
+
+		; //
+
+	}
 }
