ce58b887cb9d9d4518272db8912cd749d20460b4
Felix Mumme <felix.mumme@mumme-it.de>
2024-06-30 01:31:59 +0200

    Add timeframe controller to reach specified SoC in specified time frame

---
+++ io.openems.edge.controller.ess.timeframe/src/io/openems/edge/controller/ess/timeframe/ControllerEssTimeframeImpl.java
@@ -0,0 +1,166 @@
+package io.openems.edge.controller.ess.timeframe;
+
+import io.openems.common.exceptions.InvalidValueException;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.metatype.annotations.Designate;
+
+import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
+import io.openems.edge.common.component.AbstractOpenemsComponent;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.controller.api.Controller;
+import io.openems.edge.ess.api.ManagedSymmetricEss;
+import io.openems.edge.ess.api.PowerConstraint;
+import io.openems.edge.ess.power.api.Pwr;
+import io.openems.edge.timedata.api.Timedata;
+import io.openems.edge.timedata.api.TimedataProvider;
+import io.openems.edge.timedata.api.utils.CalculateActiveTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+@Designate(ocd = Config.class, factory = true)
+@Component(//
+        name = "Controller.Ess.Timeframe", //
+        immediate = true, //
+        configurationPolicy = ConfigurationPolicy.REQUIRE //
+)
+public class ControllerEssTimeframeImpl extends AbstractOpenemsComponent
+        implements ControllerEssTimeframe, Controller, OpenemsComponent, TimedataProvider {
+
+    private final CalculateActiveTime calculateCumulatedActiveTime = new CalculateActiveTime(this,
+            ControllerEssTimeframe.ChannelId.CUMULATED_ACTIVE_TIME);
+
+    @Reference
+    private ConfigurationAdmin cm;
+
+    @Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
+    private ManagedSymmetricEss ess;
+
+    private Config config;
+
+    @Reference(policy = ReferencePolicy.DYNAMIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.OPTIONAL)
+    private volatile Timedata timedata = null;
+
+
+    private final Logger log = LoggerFactory.getLogger(io.openems.edge.controller.ess.timeframe.ControllerEssTimeframeImpl.class);
+
+    public ControllerEssTimeframeImpl() {
+        super(//
+                OpenemsComponent.ChannelId.values(), //
+                Controller.ChannelId.values(), //
+                ControllerEssTimeframe.ChannelId.values() //
+        );
+    }
+
+    @Activate
+    private void activate(ComponentContext context, Config config) {
+        super.activate(context, config.id(), config.alias(), config.enabled());
+        if (this.applyConfig(context, config)) {
+            return;
+        }
+    }
+
+    @Modified
+    private void modified(ComponentContext context, Config config) {
+        super.modified(context, config.id(), config.alias(), config.enabled());
+        if (this.applyConfig(context, config)) {
+            return;
+        }
+    }
+
+    private boolean applyConfig(ComponentContext context, Config config) {
+        this.config = config;
+        return OpenemsComponent.updateReferenceFilter(this.cm, this.servicePid(), "ess", config.ess_id());
+    }
+
+    @Override
+    @Deactivate
+    protected void deactivate() {
+        super.deactivate();
+    }
+
+    @Override
+    public void run() throws OpenemsNamedException {
+        var isActive = false;
+        try {
+            isActive = switch (this.config.mode()) {
+                case MANUAL -> {
+                    // Apply Active-Power Set-Point
+                    var acPower = getAcPower(this.ess, this.config.ess_capacity(), this.config.targetSoC(), this.config.startTime(), this.config.endTime());
+
+                    if (acPower == null) {
+                        logDebug(log, "Out of specified timeframe");
+                        yield false; // is not active
+                    } else {
+                        logDebug(log, "Setting AC Power to: [" + acPower + "W]");
+                        PowerConstraint.apply(this.ess, this.id(), //
+                                this.config.phase(), Pwr.ACTIVE, this.config.relationship(), acPower);
+                        yield true; // is active
+                    }
+                }
+                case OFF -> {
+                    // Do nothing
+                    yield false; // is not active
+                }
+            };
+
+        } finally {
+            this.calculateCumulatedActiveTime.update(isActive);
+        }
+    }
+
+    /**
+     * Gets the required AC power set-point for AC-ESS.
+     *
+     * @param ess       the {@link ManagedSymmetricEss}
+     * @param targetSoC the target SoC
+     * @param startTime the start time of the timeframe
+     * @param endTime   the end time of the timeframe
+     * @return the AC power set-point
+     */
+    protected static Integer getAcPower(ManagedSymmetricEss ess, int ess_capacity, int targetSoC, String startTime, String endTime) throws InvalidValueException {
+        Date start = getDateFromISOString(startTime);
+        Date end = getDateFromISOString(endTime);
+        Date current = new Date();
+        if (current.after(start) && current.before(end)) {
+            int currentSoC = ess.getSoc().getOrError();
+            int maxEssCapacity = ess.getCapacity().orElse(ess_capacity);
+
+            int targetCapacity = maxEssCapacity * targetSoC / 100;
+            int currentCapacity = maxEssCapacity * currentSoC / 100;
+
+            float timeframeInHours = (end.getTime() - current.getTime()) / 1000f / 60f / 60f;
+
+           return Math.round((currentCapacity - targetCapacity) / timeframeInHours);
+        }
+
+        return null;
+    }
+
+    private static Date getDateFromISOString(String iso8601String) {
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ISO_DATE_TIME;
+
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse(iso8601String, timeFormatter);
+
+        return Date.from(Instant.from(offsetDateTime));
+    }
+
+    @Override
+    public Timedata getTimedata() {
+        return this.timedata;
+    }
+}
--- ui/src/app/shared/genericComponents/modal/modal-line/modal-line.ts
+++ ui/src/app/shared/genericComponents/modal/modal-line/modal-line.ts
@@ -1,36 +1,38 @@
 // @ts-strict-ignore
-import { Component, Input } from "@angular/core";
-import { AbstractModalLine } from "../abstract-modal-line";
-import { ButtonLabel } from "../modal-button/modal-button";
+import {Component, Input} from "@angular/core";
+import {AbstractModalLine} from "../abstract-modal-line";
+import {ButtonLabel} from "../modal-button/modal-button";
 
 @Component({
-    selector: 'oe-modal-line',
-    templateUrl: './modal-line.html',
+  selector: 'oe-modal-line',
+  templateUrl: './modal-line.html',
 })
 export class ModalLineComponent extends AbstractModalLine {
 
-    // Width of Left Column, Right Column is (100% - leftColumn)
-    @Input()
-    protected leftColumnWidth: number;
+  // Width of Left Column, Right Column is (100% - leftColumn)
+  @Input()
+  protected leftColumnWidth: number;
 
-    /** ControlName for Form Field */
-    @Input() public override controlName: string;
-    @Input() protected button: ButtonLabel | null = null;
-    /** ControlName for Toggle Button */
-    @Input() protected control:
-        { type: 'TOGGLE' } |
-        { type: 'INPUT', properties?: {unit:'W'} } |
-        /* the available select options*/
-        { type: 'SELECT', options: { value: string, name: string }[] } |
-        /* the properties for range slider*/
-        { type: 'RANGE', properties: { min: number, max: number, unit: 'H', step?: number } };
+  /** ControlName for Form Field */
+  @Input() public override controlName: string;
+  @Input() protected button: ButtonLabel | null = null;
+  /** ControlName for Toggle Button */
+  @Input() protected control:
+    { type: 'TOGGLE' } |
+    { type: 'INPUT', properties?: { unit: 'W' | '%' } } |
+    /* the available select options*/
+    { type: 'SELECT', options: { value: string, name: string }[] } |
+    /* the properties for range slider*/
+    { type: 'RANGE', properties: { min: number, max: number, unit: 'H' | '%', step?: number } } |
+    /* the properties for range slider*/
+    { type: 'DATE_PICKER', properties?: { label: string } };
 
-    /** Fixed indentation of the modal-line */
-    @Input() protected textIndent: TextIndentation = TextIndentation.NONE;
+  /** Fixed indentation of the modal-line */
+  @Input() protected textIndent: TextIndentation = TextIndentation.NONE;
 }
 
 export enum TextIndentation {
-    NONE = '0%',
-    SINGLE = '5%',
-    DOUBLE = '10%',
+  NONE = '0%',
+  SINGLE = '5%',
+  DOUBLE = '10%',
 }
