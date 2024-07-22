ffffa5c4b0186a8dbaa810a30968d93c5b9f9cac
FENECON\pooran.c <pooran.c@fenecon.de>
2024-07-22 16:34:47 +0200

    no message

--- io.openems.edge.predictor.lstmmodel/src/io/openems/edge/predictor/lstmmodel/interpolation/InterpolationManager.java
+++ io.openems.edge.predictor.lstmmodel/src/io/openems/edge/predictor/lstmmodel/interpolation/InterpolationManager.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import io.openems.edge.predictor.lstmmodel.common.HyperParameters;
 import io.openems.edge.predictor.lstmmodel.utilities.UtilityConversion;
