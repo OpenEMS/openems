71bbb1da5718e0e910e889667a626baefa97fd4b
Felix Mumme <felix.mumme@mumme-it.de>
2024-07-27 15:25:28 +0200

    add a max charge / discharge limit

--- io.openems.edge.controller.ess.timeframe/test/io/openems/edge/controller/ess/timeframe/MyConfig.java
+++ io.openems.edge.controller.ess.timeframe/test/io/openems/edge/controller/ess/timeframe/MyConfig.java
@@ -7,45 +7,47 @@ import io.openems.edge.ess.power.api.Relationship;
 @SuppressWarnings("all")
 public class MyConfig extends AbstractComponentConfig implements Config {
 
-	protected static class Builder {
-		private String id;
-		private String essId;
-		private Mode mode;
-		private Phase phase;
-		private Relationship relationship;
+    protected static class Builder {
+        private String id;
+        private String essId;
+        private Mode mode;
+        private Phase phase;
+        private Relationship relationship;
 
         private int ess_capacity;
         private int targetSoC;
+        private int maxChargePower;
+        private int maxDischargePower;
         private String startTime;
         private String endTime;
 
-		private Builder() {
-		}
+        private Builder() {
+        }
 
-		public Builder setId(String id) {
-			this.id = id;
-			return this;
-		}
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
 
-		public Builder setEssId(String essId) {
-			this.essId = essId;
-			return this;
-		}
+        public Builder setEssId(String essId) {
+            this.essId = essId;
+            return this;
+        }
 
-		public Builder setMode(Mode mode) {
-			this.mode = mode;
-			return this;
-		}
+        public Builder setMode(Mode mode) {
+            this.mode = mode;
+            return this;
+        }
 
-		public Builder setPhase(Phase phase) {
-			this.phase = phase;
-			return this;
-		}
+        public Builder setPhase(Phase phase) {
+            this.phase = phase;
+            return this;
+        }
 
-		public Builder setRelationship(Relationship relationship) {
-			this.relationship = relationship;
-			return this;
-		}
+        public Builder setRelationship(Relationship relationship) {
+            this.relationship = relationship;
+            return this;
+        }
 
         public Builder setEss_capacity(int ess_capacity) {
             this.ess_capacity = ess_capacity;
@@ -62,39 +64,50 @@ public class MyConfig extends AbstractComponentConfig implements Config {
             return this;
         }
 
+        public Builder setMaxChargePower(int maxChargePower) {
+            this.maxChargePower = maxChargePower;
+            return this;
+        }
+
+        public Builder setMaxDishargePower(int maxDischargePower) {
+            this.maxDischargePower = maxDischargePower;
+            return this;
+        }
+
+
         public Builder setEndTime(String endTime) {
             this.endTime = endTime;
             return this;
         }
 
-		public MyConfig build() {
-			return new MyConfig(this);
-		}
-	}
+        public MyConfig build() {
+            return new MyConfig(this);
+        }
+    }
 
-	/**
-	 * Create a Config builder.
-	 *
-	 * @return a {@link Builder}
-	 */
-	public static Builder create() {
-		return new Builder();
-	}
+    /**
+     * Create a Config builder.
+     *
+     * @return a {@link Builder}
+     */
+    public static Builder create() {
+        return new Builder();
+    }
 
-	private final Builder builder;
+    private final Builder builder;
 
-	private MyConfig(Builder builder) {
-		super(Config.class, builder.id);
-		this.builder = builder;
-	}
-
-	@Override
-	public String ess_id() {
-		return this.builder.essId;
-	}
+    private MyConfig(Builder builder) {
+        super(Config.class, builder.id);
+        this.builder = builder;
+    }
 
     @Override
-    public int fallback_ess_capacity() {
+    public String ess_id() {
+        return this.builder.essId;
+    }
+
+    @Override
+    public int ess_capacity() {
         return this.builder.ess_capacity;
     }
 
@@ -103,7 +116,17 @@ public class MyConfig extends AbstractComponentConfig implements Config {
         return this.builder.targetSoC;
     }
 
-    @Override
+	@Override
+	public int maxChargePower() {
+		return this.builder.maxChargePower;
+	}
+
+	@Override
+	public int maxDischargePower() {
+		return this.builder.maxDischargePower;
+	}
+
+	@Override
     public String startTime() {
         return this.builder.startTime;
     }
@@ -114,19 +137,19 @@ public class MyConfig extends AbstractComponentConfig implements Config {
     }
 
     @Override
-	public Mode mode() {
-		return this.builder.mode;
-	}
+    public Mode mode() {
+        return this.builder.mode;
+    }
 
 
-	@Override
-	public Relationship relationship() {
-		return this.builder.relationship;
-	}
+    @Override
+    public Relationship relationship() {
+        return this.builder.relationship;
+    }
 
-	@Override
-	public Phase phase() {
-		return this.builder.phase;
-	}
+    @Override
+    public Phase phase() {
+        return this.builder.phase;
+    }
 
 }
