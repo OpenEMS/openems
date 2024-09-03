b7b3847b2a3297be35aa5cec45f49df0ebe4f0bd
luzpaz <luzpaz@users.noreply.github.com>
2024-09-03 09:46:25 -0400

    Fix various typos (#2730)
    * Fix various typos
    Found via `codespell -q 3 -S "./.git,*.pdf,*.po,*.pot,*.ts" -L abl,addess,afe,als,atleast,ba,corrently,datas,ist,plattform,pres,repid,sie`
    
    ---------
    
    Co-authored-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>

--- io.openems.edge.timedata.rrd4j/src/io/openems/edge/timedata/rrd4j/Rrd4jReadHandler.java
+++ io.openems.edge.timedata.rrd4j/src/io/openems/edge/timedata/rrd4j/Rrd4jReadHandler.java
@@ -150,7 +150,7 @@ public class Rrd4jReadHandler {
 	 * @param rrdDbId             the id of the rrdb
 	 * @param notSendChannel      the channel with the timestamps where the data got
 	 *                            not send
-	 * @param lastResendTimestamp the timstamp of the last resend
+	 * @param lastResendTimestamp the timestamp of the last resend
 	 * @param debugMode           if debugMode is active
 	 * @return the {@link Timeranges}
 	 * @throws OpenemsNamedException on error
