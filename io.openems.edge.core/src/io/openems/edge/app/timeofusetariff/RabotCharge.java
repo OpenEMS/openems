72baecebfafed20860fed46bd170960224dfea8c
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2024-10-11 22:09:29 +0200

    Fix JUnit tests

--- io.openems.edge.core/src/io/openems/edge/app/timeofusetariff/RabotCharge.java
+++ io.openems.edge.core/src/io/openems/edge/app/timeofusetariff/RabotCharge.java
@@ -3,6 +3,7 @@ package io.openems.edge.app.timeofusetariff;
 import static io.openems.edge.core.appmanager.formly.enums.InputType.PASSWORD;
 import static io.openems.edge.core.appmanager.validator.Checkables.checkCommercial92;
 import static io.openems.edge.core.appmanager.validator.Checkables.checkHome;
+import static io.openems.edge.core.appmanager.validator.Checkables.checkOr;
 
 import java.util.Map;
 import java.util.function.Function;
@@ -178,7 +179,7 @@ public class RabotCharge extends AbstractOpenemsAppWithProps<RabotCharge, Proper
 	@Override
 	protected ValidatorConfig.Builder getValidateBuilder() {
 		return ValidatorConfig.create() //
-				.setCompatibleCheckableConfigs(checkHome().or(checkCommercial92()));
+				.setCompatibleCheckableConfigs(checkOr(checkHome(), checkCommercial92()));
 	}
 
 	@Override
