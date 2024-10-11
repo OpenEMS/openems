dfa4fd6ce5134ea705ee5584b22740a09196456c
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2024-10-11 21:23:42 +0200

    UI: Username to lowerCase & eslintRule
    - eslint-rule to prevent getting fdescribe or xdescribe into production
    - lowercasing username
    
    Co-authored-by: Fabian Brandtner <10850256+fabian94533@users.noreply.github.com>
    Reviewed-by: Fabian Brandtner <10850256+fabian94533@users.noreply.github.com>
    Reviewed-by: Stefan Feilmeier <3515268+sfeilmeier@users.noreply.github.com>
    Co-authored-by: Lukas Rieger <73471197+lukasrgr@users.noreply.github.com>
    Co-committed-by: Lukas Rieger <73471197+lukasrgr@users.noreply.github.com>

--- ui/src/app/index/login.spec.ts
+++ ui/src/app/index/login.spec.ts
@@ -12,26 +12,30 @@ describe("Login", () => {
     }).compileComponents();
   });
 
-  it("#trimCredentials should trim password and username", () => {
+  it("#preprocessCredentials should trim password and username and should lowerCase username", () => {
     {
       // Username and password - OpenEMS Backend
-      expect(LoginComponent.trimCredentials(password, username)).toEqual({ password: "password", username: "username" });
+      expect(LoginComponent.preprocessCredentials(password, username)).toEqual({ password: "password", username: "username" });
     }
     {
       // Only Password - OpenEMS Edge
-      expect(LoginComponent.trimCredentials(password)).toEqual({ password: "password" });
+      expect(LoginComponent.preprocessCredentials(password)).toEqual({ password: "password" });
     }
     {
       // Password is null
-      expect(LoginComponent.trimCredentials(null)).toEqual({ password: undefined });
+      expect(LoginComponent.preprocessCredentials(null)).toEqual({ password: undefined });
     }
     {
       // Username is null
-      expect(LoginComponent.trimCredentials(password, null)).toEqual({ password: "password" });
+      expect(LoginComponent.preprocessCredentials(password, null)).toEqual({ password: "password" });
     }
     {
       // Username and password are null
-      expect(LoginComponent.trimCredentials(null, null)).toEqual({ password: undefined });
+      expect(LoginComponent.preprocessCredentials(null, null)).toEqual({ password: undefined });
+    }
+    {
+      // Username in Upper case
+      expect(LoginComponent.preprocessCredentials(password, username.toUpperCase())).toEqual({ password: "password", username: "username" });
     }
   });
 });
