864d7ce2a89af8dbe4031e390436be1ef96e872a
Kai J. <da.kai.creator@gmail.com>
2024-10-13 14:55:00 +0200

    UI: fix display error + percentagebar calculation (#2837)
    * fix reserve bar
    * fix energy chart cutoff
    * fix percentbar warning
    * fix on reserve soc

--- ui/src/app/shared/components/flat/flat-widget-percentagebar/flat-widget-percentagebar.ts
+++ ui/src/app/shared/components/flat/flat-widget-percentagebar/flat-widget-percentagebar.ts
@@ -5,4 +5,8 @@ import { AbstractFlatWidgetLine } from "../abstract-flat-widget-line";
     selector: "oe-flat-widget-percentagebar",
     templateUrl: "./flat-widget-percentagebar.html",
 })
-export class FlatWidgetPercentagebarComponent extends AbstractFlatWidgetLine { }
+export class FlatWidgetPercentagebarComponent extends AbstractFlatWidgetLine {
+    protected get displayPercent(): number {
+        return Math.round(Number.parseFloat(this.displayValue));
+    }
+}
