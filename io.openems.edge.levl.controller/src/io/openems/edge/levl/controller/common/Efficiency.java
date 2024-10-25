d3f55d46005da73f7d9e89de042533536198e08a
Dennis Eitle <dennis.eitle@levl.energy>
2024-10-25 12:12:59 +0200

    levl-1290/1295: add efficiency class

---
+++ io.openems.edge.levl.controller/src/io/openems/edge/levl/controller/common/Efficiency.java
@@ -0,0 +1,39 @@
+package io.openems.edge.levl.controller.common;
+
+public class Efficiency {
+	
+	/**
+	 * Applies an efficiency to a power/energy outside of the battery. 
+	 * negative values for Charge; positive for Discharge
+	 * 
+	 * @param value power/energy to which the efficiency should be applied
+	 * @param efficiencyPercent efficiency which should be applied
+	 * @return the power/energy inside the battery after applying the efficiency
+	 */
+	public static long apply(long value, double efficiencyPercent) {		
+		if(value <= 0) { // charge
+			return Math.round(value * efficiencyPercent/100);
+		}
+		
+		// discharge
+		return Math.round(value / (efficiencyPercent/100));
+	}
+	
+	/**
+	 * Unapplies an efficiency to a power/energy inside of the battery. 
+	 * negative values for Charge; positive for Discharge
+	 * 
+	 * @param value power/energy to which the efficiency should be applied
+	 * @param efficiencyPercent efficiency which should be applied
+	 * @return the power/energy outside the battery after unapplying the efficiency
+	 */
+	public static long unapply(long value, double efficiencyPercent) {		
+		if(value <= 0) { // charge
+			return Math.round(value / (efficiencyPercent/100));
+		}
+		
+		// discharge
+		return Math.round(value * efficiencyPercent/100);
+	}
+	
+}
