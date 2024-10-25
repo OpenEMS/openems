1a1503804ce14ef5a7f5c8fed14d835001a611c3
Kai J. <da.kai.creator@gmail.com>
2024-10-25 16:10:19 +0200

    use masonry-layout package

--- ui/src/app/edge/live/live.component.ts
+++ ui/src/app/edge/live/live.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, HostListener, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { RefresherCustomEvent } from "@ionic/angular";
+import Masonry from "masonry-layout";
 import { Subject } from "rxjs";
 import { DataService } from "src/app/shared/components/shared/dataservice";
 import { Edge, EdgeConfig, EdgePermission, Service, Utils, Websocket, Widgets } from "src/app/shared/shared";
@@ -16,6 +17,7 @@ export class LiveComponent implements OnInit, OnDestroy {
   public widgets: Widgets | null = null;
   protected isModbusTcpWidgetAllowed: boolean = false;
   private stopOnDestroy: Subject<void> = new Subject<void>();
+  private masonry: Masonry | null = null;
 
   constructor(
     private route: ActivatedRoute,
@@ -25,6 +27,13 @@ export class LiveComponent implements OnInit, OnDestroy {
     private dataService: DataService,
   ) { }
 
+  @HostListener("window:resize")
+  onWindowResize() {
+    if (this.masonry) {
+      this.masonry.layout()
+    }
+  }
+
   public ngOnInit() {
     this.service.currentEdge.subscribe((edge) => {
       this.edge = edge;
@@ -33,13 +42,35 @@ export class LiveComponent implements OnInit, OnDestroy {
     this.service.getConfig().then(config => {
       this.config = config;
       this.widgets = config.widgets;
+      setTimeout(this.initMasonry, 0);
     });
   }
 
   public ngOnDestroy() {
+    if (this.masonry) {
+      this.masonry.destroy();
+    }
     this.stopOnDestroy.next();
     this.stopOnDestroy.complete();
   }
 
-  protected handleRefresh: (ev: RefresherCustomEvent) => void = (ev: RefresherCustomEvent) => this.dataService.refresh(ev);
+  private initMasonry() {
+    const masonryGrid = document.querySelector(".masonry-grid");
+    if (!masonryGrid) {
+      console.error("Masonry grid not found!")
+      return;
+    }
+    this.masonry = new Masonry(masonryGrid, {
+      itemSelector: ".masonry-item",
+      columnWidth: ".masonry-sizer",
+      gutter: 0,
+      fitWidth: false,
+    });
+    setTimeout(() => this.masonry.layout(), 200);
+  }
+
+  protected handleRefresh: (ev: RefresherCustomEvent) => void = (ev: RefresherCustomEvent) => {
+    this.dataService.refresh(ev);
+    this.masonry.layout();
+  }
 }
