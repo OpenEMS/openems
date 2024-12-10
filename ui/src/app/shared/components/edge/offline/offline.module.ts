e913ac6112577061e4d1b912d7a1594b17b2bdba
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2024-12-10 22:09:40 +0100

    UI: Offline-Page Translations
    - applying translations to offline FEMS, using local dedicated translation file.
    
    Co-authored-by: Lukas Rieger <73471197+lukasrgr@users.noreply.github.com>
    Reviewed-by: Lukas Rieger <73471197+lukasrgr@users.noreply.github.com>
    Reviewed-by: Stefan Feilmeier <3515268+sfeilmeier@users.noreply.github.com>
    Co-authored-by: FENECON\anas.shetla <anas.shetla@fenecon.de>
    Co-committed-by: FENECON\anas.shetla <anas.shetla@fenecon.de>

--- ui/src/app/shared/components/edge/offline/offline.module.ts
+++ ui/src/app/shared/components/edge/offline/offline.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { IonicModule } from "@ionic/angular";
+import { TranslateModule } from "@ngx-translate/core";
 import { OfflineComponent } from "./offline.component";
+import { ManualComponent } from "./producttype/default";
 
 @NgModule({
     imports: [
         BrowserModule,
         IonicModule,
+        TranslateModule,
     ],
     declarations: [
         OfflineComponent,
     ],
     exports: [
+        ManualComponent,
         OfflineComponent,
     ],
 })
