f2319348eafa684dda913b39c4393d84f761ad0f
Helge Klein <3407928+helgeklein@users.noreply.github.com>
2024-12-31 16:01:45 +0100

    UI Environment: access websocket via WSS instead of WS if page is accessed via HTTPS (#2937)
    * Websocket: WSS instead of WS if page is accessed via HTTPS
    
    Prevents mixed content errors in the browser by selecting WSS or WS for the websocket depending on the page's scheme (HTTPS or HTTP).
    
    ---------
    
    Co-authored-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>

--- ui/src/themes/openems/environments/backend-prod.ts
+++ ui/src/themes/openems/environments/backend-prod.ts
@@ -1,11 +1,12 @@
 import { Environment } from "src/environments";
+import { getWebsocketScheme } from "src/environments";
 import { theme } from "./theme";
 
 export const environment: Environment = {
     ...theme, ...{
 
         backend: "OpenEMS Backend",
-        url: "ws://" + location.hostname + ":8082",
+        url: getWebsocketScheme() + location.hostname + ":8082",
 
         production: true,
         debugMode: false,
--- ui/src/themes/openems/environments/edge-prod.ts
+++ ui/src/themes/openems/environments/edge-prod.ts
@@ -1,11 +1,12 @@
 import { Environment } from "src/environments";
+import { getWebsocketScheme } from "src/environments";
 import { theme } from "./theme";
 
 export const environment: Environment = {
     ...theme, ...{
 
         backend: "OpenEMS Edge",
-        url: "ws://" + location.hostname + ":8075",
+        url: getWebsocketScheme() + location.hostname + ":8075",
 
         production: true,
         debugMode: false,
