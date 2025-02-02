d955bd9413d26d7c946584f1e021dfa563b3bbbf
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2025-02-02 11:36:09 +0100

    Host: extend getIpAddresses Request
    Extended jsonRpc request for getIpAddresses with more data for networkInterfaces and IP routing table
    
    Reviewed-by: Michael Grill <59126309+michaelgrill@users.noreply.github.com>
    Reviewed-by: Stefan Feilmeier <3515268+sfeilmeier@users.noreply.github.com>
    Co-authored-by: Johann Kaufmann <165755282+johannk24@users.noreply.github.com>
    Co-committed-by: Johann Kaufmann <165755282+johannk24@users.noreply.github.com>

--- io.openems.edge.core/src/io/openems/edge/core/host/OperatingSystemMac.java
+++ io.openems.edge.core/src/io/openems/edge/core/host/OperatingSystemMac.java
@@ -13,6 +13,7 @@ import io.openems.edge.common.user.User;
 import io.openems.edge.core.host.jsonrpc.ExecuteSystemCommandRequest;
 import io.openems.edge.core.host.jsonrpc.ExecuteSystemCommandResponse;
 import io.openems.edge.core.host.jsonrpc.ExecuteSystemRestartRequest;
+import io.openems.edge.core.host.jsonrpc.GetNetworkInfo.Response;
 import io.openems.edge.core.host.jsonrpc.SetNetworkConfigRequest;
 
 public class OperatingSystemMac implements OperatingSystem {
@@ -55,6 +56,11 @@ public class OperatingSystemMac implements OperatingSystem {
 	@Override
 	public CompletableFuture<String> getOperatingSystemVersion() {
 		return CompletableFuture.completedFuture(System.getProperty("os.name"));
+	}
+
+	@Override
+	public Response getNetworkInfo() throws OpenemsNamedException {
+		throw new NotImplementedException("This request is not implemented for mac");
 	}
 
 }
