39b562cd28224a9d43587776c4d5f25c225fd41c
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2025-02-07 10:39:19 +0100

    EVCS: new PhaseRotation which supports better Modbus handling
    - Native EVCS support for PhaseRotation
    - switched nature enum `PhaseRotation` to a new format
    - added method `Evcs.getPhaseRotation()` (BREAKING CHANGE)
    - applied code cleanup
    
    Co-authored-by: Christian Lehne <51822163+clehne@users.noreply.github.com>
    Co-authored-by: Stefan Feilmeier <3515268+sfeilmeier@users.noreply.github.com>
    Reviewed-by: Thomas Sicking <91258335+tsicking@users.noreply.github.com>
    Reviewed-by: Sebastian Asen <47855186+sebastianasen@users.noreply.github.com>
    Co-authored-by: Christian Lehne <51822163+clehne@users.noreply.github.com>
    Co-committed-by: Christian Lehne <51822163+clehne@users.noreply.github.com>

--- io.openems.edge.evcs.webasto.next/src/io/openems/edge/evcs/webasto/next/EvcsWebastoNextImpl.java
+++ io.openems.edge.evcs.webasto.next/src/io/openems/edge/evcs/webasto/next/EvcsWebastoNextImpl.java
@@ -42,6 +42,7 @@ import io.openems.edge.evcs.api.ChargingType;
 import io.openems.edge.evcs.api.Evcs;
 import io.openems.edge.evcs.api.EvcsPower;
 import io.openems.edge.evcs.api.ManagedEvcs;
+import io.openems.edge.evcs.api.PhaseRotation;
 import io.openems.edge.evcs.api.Phases;
 import io.openems.edge.evcs.api.Status;
 import io.openems.edge.evcs.api.WriteHandler;
@@ -244,6 +245,12 @@ public class EvcsWebastoNextImpl extends AbstractOpenemsModbusComponent implemen
 	@Override
 	public MeterType getMeterType() {
 		return MeterType.MANAGED_CONSUMPTION_METERED;
+	}
+
+	@Override
+	public PhaseRotation getPhaseRotation() {
+		// TODO implement handling for rotated Phases
+		return PhaseRotation.L1_L2_L3;
 	}
 
 	@Override
