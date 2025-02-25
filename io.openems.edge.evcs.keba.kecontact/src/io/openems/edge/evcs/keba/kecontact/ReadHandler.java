bb95596a5dc313da939263df818475ec5390bafc
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2025-02-25 17:31:06 +0100

    KEBA: fix EnergyLimitReached when no Limit configured
    - Currently a bug starts to popup more and more in ticket.
    -> Keba Charging Station has the Status ENERGY_LIMIT_REACHED eventhough energyLimit is not activated
    
    - Added removed check for limit != 0 (limit 0 should mean that no limit is set) in status mapping in ReadHandler of Keba
    - made status mapping static
    - added test for status mapping
    
    Co-authored-by: Sebastian Asen <47855186+sebastianasen@users.noreply.github.com>
    Reviewed-by: Sebastian Asen <47855186+sebastianasen@users.noreply.github.com>
    Reviewed-by: Stefan Feilmeier <3515268+sfeilmeier@users.noreply.github.com>
    Co-authored-by: Johann Kaufmann <165755282+johannk24@users.noreply.github.com>
    Co-committed-by: Johann Kaufmann <165755282+johannk24@users.noreply.github.com>

--- io.openems.edge.evcs.keba.kecontact/src/io/openems/edge/evcs/keba/kecontact/ReadHandler.java
+++ io.openems.edge.evcs.keba.kecontact/src/io/openems/edge/evcs/keba/kecontact/ReadHandler.java
@@ -122,17 +122,7 @@ public class ReadHandler implements Consumer<String> {
 				 */
 				var limit = keba.getSetEnergyLimit().get();
 				var energy = keba.getEnergySession().get();
-				if (limit != null && energy != null && energy >= limit) {
-					yield Status.ENERGY_LIMIT_REACHED;
-				}
-				yield switch (r2State) {
-				case UNDEFINED -> Status.UNDEFINED;
-				case STARTUP -> Status.STARTING;
-				case NOT_READY -> Status.NOT_READY_FOR_CHARGING;
-				case INTERRUPTED, READY -> Status.READY_FOR_CHARGING;
-				case CHARGING -> Status.CHARGING;
-				case ERROR -> Status.ERROR;
-				};
+				yield getStatus(r2State, limit, energy);
 			}
 			};
 
@@ -254,6 +244,27 @@ public class ReadHandler implements Consumer<String> {
 			}
 		}
 		}
+	}
+	
+	/** Calculates the Status based on the raw status and energylimit.
+	 * 
+	 * @param r2State the r2 state
+	 * @param limit the set energy limit
+	 * @param energy the current energy session
+	 * @return the mapped Status
+	 */
+	public static Status  getStatus(R2State r2State, Integer limit, Integer energy) {
+		if (limit != null && energy != null && energy >= limit && limit != 0) {
+			return  Status.ENERGY_LIMIT_REACHED;
+		}
+		return switch (r2State) {
+		case UNDEFINED -> Status.UNDEFINED;
+		case STARTUP -> Status.STARTING;
+		case NOT_READY -> Status.NOT_READY_FOR_CHARGING;
+		case INTERRUPTED, READY -> Status.READY_FOR_CHARGING;
+		case CHARGING -> Status.CHARGING;
+		case ERROR -> Status.ERROR;
+		};
 	}
 
 	public record ActivePowerPerPhase(Integer activePowerL1, Integer activePowerL2, Integer activePowerL3) {
