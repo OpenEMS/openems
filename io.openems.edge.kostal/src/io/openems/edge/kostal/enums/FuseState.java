0a8361bc78062ddd9be566489f685383fca54c28
Ingo <71161062+iseeberg79@users.noreply.github.com>
2025-03-17 22:07:17 +0100

    initial

---
+++ io.openems.edge.kostal/src/io/openems/edge/kostal/enums/FuseState.java
@@ -0,0 +1,34 @@
+package io.openems.edge.kostal.enums;
+
+import io.openems.common.types.OptionsEnum;
+
+// 0xCA 202 PSSB fuse state 5 - Float 2 RO 0x03
+public enum FuseState implements OptionsEnum {
+    FUSE_FAIL(0x00, "Fuse fail"), //
+    FUSE_OK(0x01, "Fuse ok"), //
+    UNCHECKED(0xFF, "Unchecked"), //
+    UNDEFINED(-1, "Undefined");
+
+    private final int value;
+    private final String name;
+
+    private FuseState(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    @Override
+    public int getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public OptionsEnum getUndefined() {
+        return UNDEFINED;
+    }
+}
---
+++ io.openems.edge.kostal/test/io/openems/edge/kostal/ess/KostalManagedESSImplTest.java
@@ -0,0 +1,14 @@
+package io.openems.edge.kostal.ess;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KostalManagedESSImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
