e3ba3bd8266da48d6044678dcd93f6a6d79bfae0
Ingo <71161062+iseeberg79@users.noreply.github.com>
2025-03-24 01:18:40 +0100

    working solar forecast model

--- io.openems.edge.predictor.solartariff/src/io/openems/edge/predictor/solartariff/PredictorSolarTariffEvccImpl.java
+++ io.openems.edge.predictor.solartariff/src/io/openems/edge/predictor/solartariff/PredictorSolarTariffEvccImpl.java
@@ -2,6 +2,9 @@ package io.openems.edge.predictor.solartariff;
 
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
@@ -11,9 +14,6 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.event.Event;
-import org.osgi.service.event.EventConstants;
-import org.osgi.service.event.EventHandler;
 import org.osgi.service.metatype.annotations.Designate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,67 +26,61 @@ import io.openems.common.types.ChannelAddress;
 import io.openems.edge.common.component.ClockProvider;
 import io.openems.edge.common.component.ComponentManager;
 import io.openems.edge.common.component.OpenemsComponent;
-import io.openems.edge.common.event.EdgeEventConstants;
+import io.openems.edge.controller.api.Controller;
+import io.openems.edge.predictor.api.prediction.AbstractPredictor;
+import io.openems.edge.predictor.api.prediction.Prediction;
+import io.openems.edge.predictor.api.prediction.Predictor;
 
 @Designate(ocd = Config.class, factory = true)
 @Component(//
-		name = "Predictor.SolarTariffEvccImpl", //
+		name = "Predictor.PV.SolarForecastEvccModel", //
 		immediate = true, //
-		configurationPolicy = ConfigurationPolicy.REQUIRE, //
-		property = { //
-				EventConstants.EVENT_TOPIC + "="
-						+ EdgeEventConstants.TOPIC_CYCLE_BEFORE_PROCESS_IMAGE //
-		} //
+		configurationPolicy = ConfigurationPolicy.REQUIRE //
 )
-public class PredictorSolarTariffEvccImpl extends AbstractPredictorHours
+public class PredictorSolarTariffEvccImpl extends AbstractPredictor
 		implements
-			PredictorHours,
-			OpenemsComponent,
-			EventHandler {
+			Predictor,
+			OpenemsComponent {
 
 	private final Logger log = LoggerFactory
 			.getLogger(PredictorSolarTariffEvccImpl.class);
 
-	private PredictorSolarTariffEvccAPI solarForecastAPI;
 	private boolean executed = false;
 	private LocalDateTime prevHour = LocalDateTime.now();
 	private final TreeMap<LocalDateTime, Integer> hourlySolarData = new TreeMap<>();
+	Prediction cachedPrediction;
 
 	@Reference
 	private ComponentManager componentManager;
 
-	public PredictorSolarTariffEvccImpl() {
-		super(//
-				OpenemsComponent.ChannelId.values(), //
-				PredictorSolarTariffEvcc.ChannelId.values() //
-		);
+	private Config config;
+
+	private PredictorSolarTariffEvccAPI solarForecastAPI; // Service to fetch
+															// weather data
+
+	public PredictorSolarTariffEvccImpl() throws OpenemsNamedException {
+		super(OpenemsComponent.ChannelId.values(),
+				Controller.ChannelId.values(),
+				PredictorSolarTariffEvcc.ChannelId.values());
 	}
 
 	@Activate
-	protected void activate(ComponentContext context, Config config)
-			throws OpenemsNamedException {
-		super.activate(context, config.id(), config.alias(), config.enabled(),
-				config.channelAddresses());
-		this.solarForecastAPI = new PredictorSolarTariffEvccAPI(config.url());
-		log.info("PredictorSolarTariffEvccImpl activated.");
-	}
+	private void activate(ComponentContext context, Config config)
+			throws Exception {
+		this.config = config;
+		super.activate(context, this.config.id(), this.config.alias(),
+				this.config.enabled(), this.config.channelAddresses(),
+				this.config.logVerbosity());
 
-	@Deactivate
-	protected void deactivate() {
-		super.deactivate();
-		log.info("PredictorSolarTariffEvccImpl deactivated.");
+		// Fetch latest weather forecast data every 15 minutes
+		this.solarForecastAPI = new PredictorSolarTariffEvccAPI(config.url()); // initialize
+																				// here
 	}
 
 	@Override
-	protected PredictionHours createNewPrediction(
-			ChannelAddress channelAddress) {
-		Integer[] result = new Integer[24];
-		int i = 0;
-		for (Entry<LocalDateTime, Integer> entry : hourlySolarData.entrySet()) {
-			if (i < result.length)
-				result[i++] = entry.getValue();
-		}
-		return new PredictionHours(result);
+	@Deactivate
+	protected void deactivate() {
+		super.deactivate();
 	}
 
 	@Override
@@ -94,70 +88,129 @@ public class PredictorSolarTariffEvccImpl extends AbstractPredictorHours
 		return this.componentManager;
 	}
 
-	@Override
-	public void handleEvent(Event event) {
-		if (!this.isEnabled()) {
-			return;
-		}
-		switch (event.getTopic()) {
-			case EdgeEventConstants.TOPIC_CYCLE_BEFORE_PROCESS_IMAGE :
-				try {
-					this.calculatePrediction();
-					this.channel(
-							PredictorSolarTariffEvcc.ChannelId.UNABLE_TO_PREDICT)
-							.setNextValue(false);
-				} catch (OpenemsNamedException e) {
-					log.error(e.getMessage());
-					this.channel(
-							PredictorSolarTariffEvcc.ChannelId.UNABLE_TO_PREDICT)
-							.setNextValue(true);
+	protected Prediction createNewPrediction(ChannelAddress channelAddress) {
+		try {
+			System.out.println("SolarForecast.createNewPrediction is called.");
+			LocalDateTime currentHour = LocalDateTime
+					.now(this.componentManager.getClock()).withNano(0)
+					.withMinute(0).withSecond(0);
+			ZoneId zoneId = ZoneId.of("UTC");
+			
+			JsonArray js = null;
+
+			if (!executed) {
+				js = this.solarForecastAPI.getSolarForecast();
+				this.prevHour = currentHour;
+				this.executed = true;
+			} else if (currentHour.isAfter(this.prevHour)) {
+				js = this.solarForecastAPI.getSolarForecast();
+				this.prevHour = currentHour;
+			} else {
+				// TODO correct?
+				if (cachedPrediction != null) {
+					System.out.println(
+							"SolarForecast.createNewPrediction cached");
+					return cachedPrediction;
 				}
-				break;
-		}
-	}
-
-	private void calculatePrediction() throws OpenemsNamedException {
-		LocalDateTime currentHour = LocalDateTime
-				.now(this.componentManager.getClock()).withNano(0).withMinute(0)
-				.withSecond(0);
-		JsonArray js = null;
-
-		if (!executed) {
-			js = this.solarForecastAPI.getSolarForecast();
-			this.prevHour = currentHour;
-			this.executed = true;
-		} else if (currentHour.isAfter(this.prevHour)) {
-			js = this.solarForecastAPI.getSolarForecast();
-			this.prevHour = currentHour;
-		} else {
-			return;
-		}
-
-		if (js != null) {
-			hourlySolarData.clear();
-			for (int i = 0; i < js.size(); i++) {
-				JsonElement timeElement = js.get(i).getAsJsonObject()
-						.get("start");
-				OffsetDateTime utcTime = OffsetDateTime
-						.parse(timeElement.getAsString()); // UTC Zeit parsen
-				LocalDateTime localTime = utcTime.toLocalDateTime(); // Zeit in
-																		// LocalDateTime
-																		// konvertieren
-
-				JsonElement price = js.get(i).getAsJsonObject().get("price");
-				//log.debug(...);
-				this.logDebug(log, "SolarForecast prediction: " + localTime + " " + price.getAsInt() + " Wh");
-				hourlySolarData.put(localTime, price.getAsInt());
 			}
-			this.channel(PredictorSolarTariffEvcc.ChannelId.PREDICT_ENABLED)
-					.setNextValue(true);
+
+			System.out.println("SolarForecast.createNewPrediction refreshing");
+
+			if (js != null) {
+				hourlySolarData.clear();
+				for (int i = 0; i < js.size(); i++) {
+					JsonElement timeElement = js.get(i).getAsJsonObject()
+							.get("start");
+					OffsetDateTime utcTime = OffsetDateTime
+							.parse(timeElement.getAsString()); // UTC Zeit
+																// parsen
+					LocalDateTime localTime = utcTime.toLocalDateTime(); // Zeit
+																			// in
+																			// LocalDateTime
+																			// konvertieren
+
+					JsonElement price = js.get(i).getAsJsonObject()
+							.get("price");
+					// log.debug(...);
+					log.debug("SolarForecast prediction: " + localTime + " "
+							+ price.getAsInt() + " Wh");
+					// TODO remove syso
+					System.out.println("SolarForecast prediction: " + localTime
+							+ " " + price.getAsInt() + " Wh");
+					hourlySolarData.put(localTime, price.getAsInt());
+				}
+				this.channel(PredictorSolarTariffEvcc.ChannelId.PREDICT_ENABLED)
+						.setNextValue(true);
+
+				this.channel(PredictorSolarTariffEvcc.ChannelId.PREDICT)
+						.setNextValue(hourlySolarData.firstEntry().getValue());
+
+			} else {
+				log.warn("Failed to fetch solar forecast data.");
+				this.channel(PredictorSolarTariffEvcc.ChannelId.PREDICT_ENABLED)
+						.setNextValue(false);
+				System.out.println("SolarForecast.createNewPrediction failed.");
+				return Prediction.EMPTY_PREDICTION;
+			}
+
+			System.out
+					.println("SolarForecast.createNewPrediction transforming");
+
+			// // Calculate the index corresponding to the current 15-minute
+			// // interval
+			// int currentIntervalIndex = (int) ChronoUnit.HOURS
+			// .between(startOfDay, currentHour); // Index of current
+			// // 15-minute interval
+			//
+			// System.out.println("SolarForecast.createNewPrediction - getting
+			// first element");
+
+			// Entry<LocalDateTime, Integer> first = hourlySolarData.entrySet()
+			// .iterator().next();
+			// int firstIntervalIndex = (int)
+			// ChronoUnit.HOURS.between(startOfDay,
+			// first.getKey());
+
+			// Create an array to store the forecast values for the next 192
+			// intervals (48 hours in 15-minute steps)
+			var values = new Integer[192];
+
+			// System.out.println("SolarForecast/currentInvervalIndex: "
+			// + currentIntervalIndex);
+			// System.out.println(
+			// "SolarForecast/firstInvervalIndex: " + firstIntervalIndex);
+
+			int i = 0;
+			for (Entry<LocalDateTime, Integer> entry : hourlySolarData
+					.entrySet()) {
+				System.out.println("loop processing...");
+				if (!entry.getKey().atZone(zoneId).isBefore(currentHour.atZone(zoneId))
+						&& i < values.length) {
+					// convert hourly values in 15min steps (summarized later,
+					// therefore divided by 4 //TODO correct?
+					// limited for next 24h from current hour
+					values[i++] = entry.getValue();
+					values[i++] = entry.getValue();
+					values[i++] = entry.getValue();
+					values[i++] = entry.getValue();
+				}
+			}
+			System.out.println("loop completed");
 			
-			this.channel(PredictorSolarTariffEvcc.ChannelId.PREDICT).setNextValue(hourlySolarData.firstEntry().getValue());
-			
-		} else {
-			log.warn("Failed to fetch solar forecast data.");
-			this.channel(PredictorSolarTariffEvcc.ChannelId.PREDICT_ENABLED)
-					.setNextValue(false);
+			this.channel(PredictorSolarTariffEvcc.ChannelId.PREDICT)
+					.setNextValue(hourlySolarData.firstEntry().getValue());
+
+			Prediction prediction = Prediction.from(currentHour.atZone(zoneId),
+					values);
+
+			cachedPrediction = prediction;
+
+			// Return the prediction starting from the calculated time
+			return prediction;
+
+		} catch (Exception e) {
+			log.error("Error creating prediction: ", e);
+			return Prediction.EMPTY_PREDICTION;
 		}
 	}
 
