2d3b041aa9b2bfa2b8408500527b07b88bd7115c
Hannes <hallerjo97@gmail.com>
2025-03-26 23:33:31 +0100

    Update RestHandlerTest.java

--- io.openems.edge.controller.api.rest/test/io/openems/edge/controller/api/rest/RestHandlerTest.java
+++ io.openems.edge.controller.api.rest/test/io/openems/edge/controller/api/rest/RestHandlerTest.java
@@ -4,59 +4,76 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.util.List;
-import java.util.regex.PatternSyntaxException;
 
 import org.junit.Test;
 
 import io.openems.common.types.ChannelAddress;
 import io.openems.edge.common.component.OpenemsComponent;
 
+/**
+ * Test class for RestHandler.
+ */
 public class RestHandlerTest {
-
+	
+	/**
+	 * Tests the getChannels method.
+	 */
 	@Test
 	public void testGetChannels() {
 		var foo0 = new DummyComponent("foo0");
 		var bar0 = new DummyComponent("bar0");
 		var dummyComponent = new DummyComponent("dummyComponent");
 		var components = List.<OpenemsComponent>of(foo0, bar0, dummyComponent);
-
+		
+		// Create a dummy RestHandler for testing
+		AbstractRestApi dummyApi = new DummyRestApi();
+		RestHandler handler = new RestHandler(dummyApi);
+		
 		{
 			var channelAddress = new ChannelAddress("foo0", "DummyChannel");
-			var result = RestHandler.getChannels(components, channelAddress);
+			var result = handler.getChannels(components, channelAddress);
 			assertEquals(1, result.size());
 			assertEquals(foo0.channel("DummyChannel"), result.get(0));
 		}
-
+		
 		{
 			var channelAddress = new ChannelAddress(".*0", "Dummy.*");
-			var result = RestHandler.getChannels(components, channelAddress);
+			var result = handler.getChannels(components, channelAddress);
 			assertEquals(2, result.size());
 			assertEquals(foo0.channel("DummyChannel"), result.get(0));
 			assertEquals(bar0.channel("DummyChannel"), result.get(1));
 		}
-
+		
 		{
 			var channelAddress = new ChannelAddress(".*0", "DummyXY.*");
-			var result = RestHandler.getChannels(components, channelAddress);
+			var result = handler.getChannels(components, channelAddress);
 			assertEquals(0, result.size());
 		}
-
+		
 		{
 			var channelAddress = new ChannelAddress("dummyComponent", "Dummy.*");
-			var result = RestHandler.getChannels(components, channelAddress);
+			var result = handler.getChannels(components, channelAddress);
 			assertEquals(1, result.size());
 			assertEquals(dummyComponent.channel("DummyChannel"), result.get(0));
 		}
-
+		
+		{
+			var channelAddress = new ChannelAddress("[a-z]*0", "Dummy.*");
+			var result = handler.getChannels(components, channelAddress);
+			assertEquals(2, result.size());
+			assertEquals(foo0.channel("DummyChannel"), result.get(0));
+			assertEquals(bar0.channel("DummyChannel"), result.get(1));
+		}
+		
 		{
 			var channelAddress = new ChannelAddress("*", "");
 			try {
-				RestHandler.getChannels(components, channelAddress);
-				fail();
-			} catch (PatternSyntaxException e) {
-				// ignore
+				handler.getChannels(components, channelAddress);
+				fail("Expected PatternSyntaxException not thrown");
+			} catch (Exception e) {
+				// This is expected - should be a PatternSyntaxException
+				// or would be caught by isLikelyRegex check
 			}
 		}
 	}
-
 }
