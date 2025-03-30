23455c8bbdb7797755b6d247f42f2ad028efc83d
Ingo <71161062+iseeberg79@users.noreply.github.com>
2025-03-30 21:15:56 +0200

    meter corrections (KSEM) consumption/production (TESTING!)

--- io.openems.edge.kostal/test/io/openems/edge/kostal/ess/KostalManagedESSImplTest.java
+++ io.openems.edge.kostal/test/io/openems/edge/kostal/ess/KostalManagedESSImplTest.java
@@ -20,7 +20,7 @@ public class KostalManagedESSImplTest {
 	          .setModbusId("modbus0") //
 	          .setCapacity(10000) //
 	          //.setInverter_id("pvInverter0")
-	          .setControllerId("ctrlEssTimeOfUseTariff1")
+	          .setCtrlId("ctrlEssTimeOfUseTariff1")
 	          .setModbusUnitId(71) //
 	          .build()
 	      ); //
