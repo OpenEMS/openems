babd3ea6024b16c17ca44150604798930fb8f98d
Ingo <71161062+iseeberg79@users.noreply.github.com>
2025-05-06 22:27:55 +0200

    initial

---
+++ io.openems.edge.kostal.plenticore/src/io/openems/edge/kostal/plenticore/enums/ControlMode.java
@@ -0,0 +1,23 @@
+package io.openems.edge.kostal.plenticore.enums;
+
+public enum ControlMode {
+
+	/**
+	 * Uses the internal 'AUTO' mode of the inverter. Allows no remote
+	 * control of Set-Points. Requires a Smart Meter at the grid junction
+	 * point.
+	 */
+	INTERNAL,
+	/**
+	 * Full control of the inverter by OpenEMS. Slower than internal 'AUTO'
+	 * mode, but does not require a Smart Meter at the grid junction point.
+	 */
+	REMOTE,
+	/**
+	 * Uses the internal 'AUTO' mode of the inverter. Allows remote
+	 * control of Set-Points based on differences. Requires a Smart Meter at the grid junction
+	 * point.
+	 */
+	SMART;
+
+}
---
+++ io.openems.edge.kostal.plenticore/src/io/openems/edge/kostal/plenticore/enums/SensorType.java
@@ -0,0 +1,35 @@
+package io.openems.edge.kostal.plenticore.enums;
+
+import io.openems.common.types.OptionsEnum;
+
+public enum SensorType implements OptionsEnum {
+  SDM_630(0x00, "SDM 630 (B+G E-Tech GmbH)"), //
+  B_CONTROL_EM_300_LR(0x01, "B-Control EM-300 LR (TQ Systems)"), //
+  RESERVED(0x02, "reserved"), //
+  KOSTAL_SMART_ENERGY_METER(0x03, "KOSTAL Smart Energy Meter (KOSTAL)"), //
+  NO_SENSOR(0xFF, "No sensor"), //
+  UNDEFINED(-1, "Undefined");
+
+  private final int value;
+  private final String name;
+
+  private SensorType(int value, String name) {
+    this.value = value;
+    this.name = name;
+  }
+
+  @Override
+  public int getValue() {
+    return this.value;
+  }
+
+  @Override
+  public String getName() {
+    return this.name;
+  }
+
+  @Override
+  public OptionsEnum getUndefined() {
+    return UNDEFINED;
+  }
+}
