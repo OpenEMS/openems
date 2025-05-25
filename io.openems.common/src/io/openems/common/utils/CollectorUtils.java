6437d5f24de742376bf35c67b9b1ccc67b2fb70d
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2025-05-25 13:59:47 +0200

    Common: add CollectorUtils
    Co-authored-by: Michael Grill <59126309+michaelgrill@users.noreply.github.com>
    Reviewed-by: Michael Grill <59126309+michaelgrill@users.noreply.github.com>
    Reviewed-by: Hanna Späth <198875998+HaSp97@users.noreply.github.com>
    Reviewed-by: Stefan Feilmeier <3515268+sfeilmeier@users.noreply.github.com>
    Co-authored-by: Lukas Rieger <73471197+lukasrgr@users.noreply.github.com>
    Co-committed-by: Lukas Rieger <73471197+lukasrgr@users.noreply.github.com>

--- io.openems.common/src/io/openems/common/utils/CollectorUtils.java
+++ io.openems.common/src/io/openems/common/utils/CollectorUtils.java
@@ -2,6 +2,10 @@ package io.openems.common.utils;
 
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.NavigableMap;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -54,6 +58,86 @@ public final class CollectorUtils {
 			t.putAll(u);
 			return t;
 		});
+	}
+
+	/**
+	 * Returns a {@link Collector} that accumulates input elements into a
+	 * {@link SortedMap}. The keys and values are produced by applying the provided
+	 * mapping functions to the input elements.
+	 * 
+	 * <p>
+	 * If the mapped keys contain duplicates, an {@link IllegalStateException} will
+	 * be thrown.
+	 *
+	 * @param <T>           the type of input elements to the reduction operation
+	 * @param <K>           the output type of the key mapping function
+	 * @param <V>           the output type of the value mapping function
+	 * @param keyMapper     a mapping function to produce keys
+	 * @param valueMapper   a mapping function to produce values
+	 * @param mergeFunction a merge function to resolve collisions between values
+	 *                      associated with the same key
+	 * @return a {@code Collector} which collects elements into a {@code SortedMap}
+	 */
+	public static <T, K, V> Collector<T, ?, ? extends SortedMap<K, V>> toSortedMap(//
+			Function<? super T, ? extends K> keyMapper, //
+			Function<? super T, ? extends V> valueMapper, //
+			BinaryOperator<V> mergeFunction //
+	) {
+		return toTreeMap(keyMapper, valueMapper, mergeFunction);
+	}
+
+	/**
+	 * Returns a {@link Collector} that accumulates input elements into a
+	 * {@link NavigableMap}. The keys and values are produced by applying the
+	 * provided mapping functions to the input elements.
+	 * 
+	 * <p>
+	 * If the mapped keys contain duplicates, an {@link IllegalStateException} will
+	 * be thrown.
+	 *
+	 * @param <T>           the type of input elements to the reduction operation
+	 * @param <K>           the output type of the key mapping function
+	 * @param <V>           the output type of the value mapping function
+	 * @param keyMapper     a mapping function to produce keys
+	 * @param valueMapper   a mapping function to produce values
+	 * @param mergeFunction a merge function to resolve collisions between values
+	 *                      associated with the same key
+	 * @return a {@code Collector} which collects elements into a
+	 *         {@code NavigableMap}
+	 */
+	public static <T, K, V> Collector<T, ?, ? extends NavigableMap<K, V>> toNavigableMap(
+			Function<? super T, ? extends K> keyMapper, //
+			Function<? super T, ? extends V> valueMapper, //
+			BinaryOperator<V> mergeFunction //
+	) {
+		return toTreeMap(keyMapper, valueMapper, mergeFunction);
+	}
+
+	/**
+	 * Returns a {@link Collector} that accumulates input elements into a
+	 * {@link TreeMap}. The keys and values are produced by applying the provided
+	 * mapping functions to the input elements.
+	 *
+	 * <p>
+	 * If the mapped keys contain duplicates, an {@link IllegalStateException} will
+	 * be thrown.
+	 *
+	 * @param <T>           the type of input elements to the reduction operation
+	 * @param <K>           the output type of the key mapping function
+	 * @param <V>           the output type of the value mapping function
+	 * @param keyMapper     a mapping function to produce keys
+	 * @param valueMapper   a mapping function to produce values
+	 * @param mergeFunction a merge function to resolve collisions between values
+	 *                      associated with the same key
+	 * @return a {@code Collector} which collects elements into a
+	 *         {@code NavigableMap}
+	 */
+	public static <T, K, V> Collector<T, ?, TreeMap<K, V>> toTreeMap(//
+			Function<? super T, ? extends K> keyMapper, //
+			Function<? super T, ? extends V> valueMapper, //
+			BinaryOperator<V> mergeFunction //
+	) {
+		return Collectors.toMap(keyMapper, valueMapper, mergeFunction, TreeMap::new);
 	}
 
 }
