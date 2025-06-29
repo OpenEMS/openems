779fc64e9522b79e259d1d957f0c635a2927df65
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2025-06-29 23:38:55 +0200

    Fix merge errors

--- io.openems.edge.bridge.mbus/src/io/openems/edge/bridge/mbus/api/package-info.java
+++ io.openems.edge.bridge.mbus/src/io/openems/edge/bridge/mbus/api/package-info.java
@@ -1,7 +1,3 @@
 @org.osgi.annotation.versioning.Version("1.0.0")
 @org.osgi.annotation.bundle.Export
-<<<<<<<< HEAD:io.openems.edge.bridge.mbus/src/io/openems/edge/bridge/mbus/api/package-info.java
 package io.openems.edge.bridge.mbus.api;
-========
-package io.openems.edge.bridge.modbus.sunspec.battery;
->>>>>>>> d1865b7ff9 ([Edge] Nuvation BMS + Code cleanups (#2037)):io.openems.edge.bridge.modbus/src/io/openems/edge/bridge/modbus/sunspec/battery/package-info.java
--- io.openems.edge.controller.ess.balancing/src/io/openems/edge/controller/ess/balancing/ControllerEssBalancingImpl.java
+++ io.openems.edge.controller.ess.balancing/src/io/openems/edge/controller/ess/balancing/ControllerEssBalancingImpl.java
@@ -1,7 +1,5 @@
 package io.openems.edge.controller.ess.balancing;
 
-import io.openems.edge.common.channel.IntegerReadChannel;
-import io.openems.edge.common.channel.WriteChannel;
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
