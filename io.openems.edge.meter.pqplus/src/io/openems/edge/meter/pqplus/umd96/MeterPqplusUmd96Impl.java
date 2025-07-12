8b51e664257f483a46ba95b3a77fdfa33c8c3ad6
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2025-07-12 22:42:18 +0200

    [Edge] PQ Plus UMD 96: calculate energy values
    Reviewed-by: Michael Grill <59126309+michaelgrill@users.noreply.github.com>
    Reviewed-by: Jan Thäter <jan.thaeter@fenecon.de>
    Reviewed-by: Stefan Feilmeier <3515268+sfeilmeier@users.noreply.github.com>
    Co-authored-by: Fabian Brandtner <10850256+fabian94533@users.noreply.github.com>
    Co-committed-by: Fabian Brandtner <10850256+fabian94533@users.noreply.github.com>

--- io.openems.edge.meter.pqplus/src/io/openems/edge/meter/pqplus/umd96/MeterPqplusUmd96Impl.java
+++ io.openems.edge.meter.pqplus/src/io/openems/edge/meter/pqplus/umd96/MeterPqplusUmd96Impl.java
@@ -2,7 +2,13 @@ package io.openems.edge.meter.pqplus.umd96;
 
 import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.INVERT_IF_TRUE;
 import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.SCALE_FACTOR_3;
+import static io.openems.edge.common.event.EdgeEventConstants.TOPIC_CYCLE_BEFORE_PROCESS_IMAGE;
 
+import io.openems.edge.common.component.ComponentManager;
+import io.openems.edge.common.event.EdgeEventConstants;
+import io.openems.edge.timedata.api.Timedata;
+import io.openems.edge.timedata.api.TimedataProvider;
+import io.openems.edge.timedata.api.utils.CalculateEnergyFromPower;
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
@@ -13,6 +19,9 @@ import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+import org.osgi.service.event.propertytypes.EventTopics;
 import org.osgi.service.metatype.annotations.Designate;
 
 import io.openems.common.exceptions.OpenemsException;
@@ -32,8 +41,12 @@ import io.openems.edge.meter.api.ElectricityMeter;
  * Implements the PQ Plus UMD 96 meter.
  *
  * <ul>
- * <li>https://www.pq-plus.de/produkte/hardwarekomponenten/umd-96/
- * <li>https://www.pq-plus.de/site/assets/files/2795/pqplus-com-protokoll-modbus_3_0.pdf
+ * <li><a href=
+ * "https://www.pq-plus.de/produkte/hardwarekomponenten/umd-96/">UMD96 Product
+ * link</a>
+ * <li><a href=
+ * "https://www.pq-plus.de/site/assets/files/2795/pqplus-com-protokoll-modbus_3_0.pdf">PQPlus
+ * Protocol Modbus</a>
  * </ul>
  */
 @Designate(ocd = Config.class, factory = true)
@@ -42,14 +55,26 @@ import io.openems.edge.meter.api.ElectricityMeter;
 		immediate = true, //
 		configurationPolicy = ConfigurationPolicy.REQUIRE //
 )
-public class MeterPqplusUmd96Impl extends AbstractOpenemsModbusComponent
-		implements MeterPqplusUmd96, ElectricityMeter, ModbusComponent, OpenemsComponent {
+@EventTopics({ //
+		TOPIC_CYCLE_BEFORE_PROCESS_IMAGE, //
+})
+public class MeterPqplusUmd96Impl extends AbstractOpenemsModbusComponent implements MeterPqplusUmd96, ElectricityMeter,
+		ModbusComponent, OpenemsComponent, TimedataProvider, EventHandler {
+
+	private CalculateEnergyFromPower calculateProductionEnergy;
+	private CalculateEnergyFromPower calculateConsumptionEnergy;
 
 	private MeterType meterType = MeterType.PRODUCTION;
 	private boolean invert;
 
 	@Reference
 	private ConfigurationAdmin cm;
+
+	@Reference
+	private ComponentManager componentManager;
+
+	@Reference(policy = ReferencePolicy.DYNAMIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.OPTIONAL)
+	private volatile Timedata timedata;
 
 	@Override
 	@Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
@@ -74,6 +99,12 @@ public class MeterPqplusUmd96Impl extends AbstractOpenemsModbusComponent
 	private void activate(ComponentContext context, Config config) throws OpenemsException {
 		this.meterType = config.type();
 		this.invert = config.invert();
+
+		this.calculateProductionEnergy = new CalculateEnergyFromPower(this,
+				ElectricityMeter.ChannelId.ACTIVE_PRODUCTION_ENERGY, this.componentManager.getClock());
+		this.calculateConsumptionEnergy = new CalculateEnergyFromPower(this,
+				ElectricityMeter.ChannelId.ACTIVE_CONSUMPTION_ENERGY, this.componentManager.getClock());
+
 		if (super.activate(context, config.id(), config.alias(), config.enabled(), config.modbusUnitId(), this.cm,
 				"Modbus", config.modbus_id())) {
 			return;
@@ -131,8 +162,41 @@ public class MeterPqplusUmd96Impl extends AbstractOpenemsModbusComponent
 
 	}
 
+	/**
+	 * Calculate the Energy values from ActivePower.
+	 */
+	private void calculateEnergy() {
+		// Calculate Energy
+		final var activePower = this.getActivePower().get();
+		if (activePower == null) {
+			this.calculateProductionEnergy.update(null);
+			this.calculateConsumptionEnergy.update(null);
+		} else if (activePower >= 0) {
+			this.calculateProductionEnergy.update(activePower);
+			this.calculateConsumptionEnergy.update(0);
+		} else {
+			this.calculateProductionEnergy.update(0);
+			this.calculateConsumptionEnergy.update(-activePower);
+		}
+	}
+
 	@Override
 	public String debugLog() {
 		return "L:" + this.getActivePower().asString();
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+		if (!this.isEnabled()) {
+			return;
+		}
+		switch (event.getTopic()) {
+		case TOPIC_CYCLE_BEFORE_PROCESS_IMAGE -> this.calculateEnergy();
+		}
+	}
+
+	@Override
+	public Timedata getTimedata() {
+		return this.timedata;
 	}
 }
