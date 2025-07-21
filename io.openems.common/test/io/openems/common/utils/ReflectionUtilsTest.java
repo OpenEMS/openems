OpenEMS/openems

commit 633d0d0885d6d6e179e2309a17a3250f4d46528a

[Common] Update Java-WebSocket to 1.6.0 (#3212)
* New implementation for resetWebSocketClient method that allows bump org.java-websocket:Java-WebSocket from 1.5.4 to 1.6.0 in /cnf
* Use existing ReflectionUtils.invokeMethodWithoutArgumentsViaReflection() method
* Update bndrun files

Closes #2926

io.openems.common/src/io/openems/common/websocket/ClientReconnectorWorker.java
@@ -1,29 +1,25 @@
 package io.openems.common.websocket;
 
-import java.lang.reflect.Field;
-import java.net.Socket;
+import static io.openems.common.utils.ReflectionUtils.invokeMethodWithoutArgumentsViaReflection;
+
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Random;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 import org.java_websocket.WebSocket;
-import org.java_websocket.WebSocketImpl;
 import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.drafts.Draft;
 import org.java_websocket.enums.ReadyState;
-import org.java_websocket.framing.CloseFrame;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.openems.common.utils.ReflectionUtils.ReflectionException;
 import io.openems.common.worker.AbstractWorker;
 
 public class ClientReconnectorWorker extends AbstractWorker {
 
 	public record Config(int connectTimeoutSeconds, int maxWaitSeconds, int minWaitSeconds, int cycleTime) {
-
 	}
 
 	public static final ClientReconnectorWorker.Config DEFAULT_CONFIG = new Config(100, 100, 10,
@@ -67,7 +63,7 @@ public class ClientReconnectorWorker extends AbstractWorker {
 		if (ws.getReadyState() != ReadyState.NOT_YET_CONNECTED) {
 			// Copy of WebSocketClient#reconnectBlocking.
 			// Do not 'reset' if WebSocket has never been connected before.
-			resetWebSocketClient(ws, this.parent::createWsData, this.config.connectTimeoutSeconds());
+			resetWebSocketClient(ws, this.parent::createWsData);
 		}
 
 		var success = false;
@@ -80,7 +76,7 @@ public class ClientReconnectorWorker extends AbstractWorker {
 			// Catch "WebSocketClient objects are not reuseable" thrown by
 			// WebSocketClient#connect(). Set WebSocketClient#connectReadThread to `null`.
 			this.parent.logInfo(this.log, "# Reset WebSocket Client after Exception... " + e.getMessage());
-			resetWebSocketClient(ws, this.parent::createWsData, this.config.connectTimeoutSeconds());
+			resetWebSocketClient(ws, this.parent::createWsData);
 			this.parent.logInfo(this.log, "# Reset WebSocket Client after Exception... done");
 		}
 
@@ -97,96 +93,23 @@ public class ClientReconnectorWorker extends AbstractWorker {
 	}
 
 	/**
-	 * This method is a copy of {@link WebSocketClient} reset()-method, because the
-	 * original one may block at the call of 'closeBlocking()' method. It also sets
-	 * the new attachment from the attachment supplier.
+	 * This method uses the {@link WebSocketClient} reset()-method through
+	 * reflection, because it is private. It also sets the new attachment from the
+	 * attachment supplier.
 	 * 
-	 * <p>
-	 * Waiting for https://github.com/TooTallNate/Java-WebSocket/pull/1251 to be
-	 * merged.
-	 * 
-	 * @param <T>                   the type of the attachment
-	 * @param ws                    the {@link WebSocketClient}
-	 * @param wsData                {@link Function} to provide a the new attachment
-	 * @param connectTimeoutSeconds the max wait time to close the websocket
-	 * @throws Exception on error
+	 * @param <T>    the type of the attachment
+	 * @param ws     the {@link WebSocketClient}
+	 * @param wsData {@link Function} to provide a the new attachment
+	 * @throws ReflectionException on error
 	 */
-	protected static <T extends WsData> void resetWebSocketClient(WebSocketClient ws, Function<WebSocket, T> wsData,
-			int connectTimeoutSeconds) throws Exception {
-		/*
-		 * Get methods and fields via Reflection
-		 */
-		// WebSocketClient#writeThread
-		Field writeThreadField = WebSocketClient.class.getDeclaredField("writeThread");
-		writeThreadField.setAccessible(true);
-		final var writeThread = (Thread) writeThreadField.get(ws);
-		// WebSocketClient#connectReadThread
-		Field connectReadThreadField = WebSocketClient.class.getDeclaredField("connectReadThread");
-		connectReadThreadField.setAccessible(true);
-		final var connectReadThread = (Thread) connectReadThreadField.get(ws);
-		// WebSocketClient#draft
-		Field draftField = WebSocketClient.class.getDeclaredField("draft");
-		draftField.setAccessible(true);
-		final var draft = (Draft) draftField.get(ws);
-		// WebSocketClient#socket
-		Field socketField = WebSocketClient.class.getDeclaredField("socket");
-		socketField.setAccessible(true);
-		final var socket = (Socket) socketField.get(ws);
-		// WebSocketClient#connectLatch
-		Field connectLatchField = WebSocketClient.class.getDeclaredField("connectLatch");
-		connectLatchField.setAccessible(true);
-		// WebSocketClient#closeLatch
-		Field closeLatchField = WebSocketClient.class.getDeclaredField("closeLatch");
-		closeLatchField.setAccessible(true);
-		final var closeLatch = (CountDownLatch) closeLatchField.get(ws);
-		// WebSocketClient#closeLatch
-		Field engineField = WebSocketClient.class.getDeclaredField("engine");
-		engineField.setAccessible(true);
-		final var engine = (WebSocketImpl) engineField.get(ws);
+	protected static <T extends WsData> void resetWebSocketClient(WebSocketClient ws, Function<WebSocket, T> wsData)
+			throws ReflectionException {
+		// Call the private WebSocketClient#reset method via Reflection
+		invokeMethodWithoutArgumentsViaReflection(ws, "reset");
 
-		/*
-		 * From here it's a copy of #reset()
-		 */
-		Thread current = Thread.currentThread();
-		if (current == writeThread || current == connectReadThread) {
-			throw new IllegalStateException(
-					"You cannot initialize a reconnect out of the websocket thread. Use reconnect in another thread to ensure a successful cleanup.");
-		}
-		try {
-			// closeBlocking(); -> to reflection
-			ws.close();
-			closeLatch.await(connectTimeoutSeconds, TimeUnit.SECONDS);
-			// closeBlocking() END
-			if (writeThread != null) {
-				writeThread.interrupt();
-				// writeThread = null; -> to reflection
-				writeThreadField.set(ws, null);
-			}
-			if (connectReadThread != null) {
-				connectReadThread.interrupt();
-				// this.connectReadThread = null; -> to reflection
-				connectReadThreadField.set(ws, null);
-			}
-			draft.reset();
-			if (socket != null) {
-				socket.close();
-				// this.socket = null; -> to reflection
-				socketField.set(ws, null);
-			}
-		} catch (Exception e) {
-			ws.onError(e);
-			engine.closeConnection(CloseFrame.ABNORMAL_CLOSE, e.getMessage());
-			return;
-		}
-		// connectLatch = new CountDownLatch(1); -> to reflection
-		connectLatchField.set(ws, new CountDownLatch(1));
-		// closeLatch = new CountDownLatch(1); -> to reflection
-		closeLatchField.set(ws, new CountDownLatch(1));
-		// this.engine = new WebSocketImpl(this, this.draft); -> to reflection
-		final var newEngine = new WebSocketImpl(ws, draft);
+		// Set attachment to newly created engine
 		final var newAttachment = wsData.apply(ws);
-		newEngine.setAttachment(newAttachment);
-		engineField.set(ws, newEngine);
+		ws.setAttachment(newAttachment);
 	}
 
 	@Override

cnf/pom.xml
@@ -332,7 +332,7 @@
 		<dependency>
 			<groupId>org.java-websocket</groupId>
 			<artifactId>Java-WebSocket</artifactId>
-			<version>1.5.4</version>
+			<version>1.6.0</version>
 		</dependency>
 		<dependency>
 			<groupId>org.jetbrains.kotlin</groupId>

io.openems.backend.application/BackendApp.bndrun
@@ -61,7 +61,7 @@
 	bnd.identity;id='io.openems.backend.uiwebsocket',\
 
 -runbundles: \
-	Java-WebSocket;version='[1.5.4,1.5.5)',\
+	Java-WebSocket;version='[1.6.0,1.6.1)',\
 	com.fasterxml.aalto-xml;version='[1.3.3,1.3.4)',\
 	com.google.gson;version='[2.13.1,2.13.2)',\
 	com.google.guava;version='[33.4.8,33.4.9)',\

io.openems.common/test/io/openems/common/websocket/ClientReconnectorWorkerTest.java
@@ -79,7 +79,7 @@ public class ClientReconnectorWorkerTest {
 	@Test
 	public void testResetWebSocketClient() throws Exception {
 		final var websocketClient = new MyWebsocketClient("name", URI.create("ws://localhost"));
-		ClientReconnectorWorker.resetWebSocketClient(websocketClient.ws, websocketClient::createWsData, 5);
+		ClientReconnectorWorker.resetWebSocketClient(websocketClient.ws, websocketClient::createWsData);
 	}
 
 }

io.openems.edge.application/EdgeApp.bndrun
@@ -208,7 +208,7 @@
 	bnd.identity;id='io.openems.edge.weather.openmeteo',\
 
 -runbundles: \
-	Java-WebSocket;version='[1.5.4,1.5.5)',\
+	Java-WebSocket;version='[1.6.0,1.6.1)',\
 	bcpkix;version='[1.81.0,1.81.1)',\
 	bcprov;version='[1.81.0,1.81.1)',\
 	bcutil;version='[1.81.0,1.81.1)',\

commit d9f8d904d4ae188cea4c049aa1139680c576e9be

[Edge] Websocket: reconnect fix reset
- Fix NoSuchMethodException when trying to reset websocket connection
- Problem was introduced in https://github.com/OpenEMS/openems/pull/3212

Co-authored-by: Michael Grill <59126309+michaelgrill@users.noreply.github.com>
Co-committed-by: Michael Grill <59126309+michaelgrill@users.noreply.github.com>

io.openems.common/src/io/openems/common/utils/ReflectionUtils.java
@@ -88,17 +88,41 @@ public class ReflectionUtils {
 	}
 
 	/**
-	 * Invokes a {@link Method} that takes no arguments via Java Reflection.
+	 * Invokes a {@link Method} that takes no arguments via Java Reflection. Only
+	 * searches in the objects {@link Class} which is returned by
+	 * {@link Object#getClass()} not in its parent classes.
+	 * 
+	 * <p>
+	 * Use {@link #invokeMethodWithoutArgumentsViaReflection(Class, Object, String)}
+	 * to specify a {@link Class} and to avoid problems with subclasses or anonymous
+	 * classes.
 	 * 
 	 * @param <T>        the type of the result
 	 * @param object     the target object
 	 * @param memberName the name of the method
 	 * @return the result of the method
-	 * @throws Exception on error
+	 * @throws ReflectionException on error
 	 */
 	public static <T> T invokeMethodWithoutArgumentsViaReflection(Object object, String memberName)
 			throws ReflectionException {
-		var method = callGuarded(() -> object.getClass().getDeclaredMethod(memberName));
+		return invokeMethodWithoutArgumentsViaReflection(object.getClass(), object, memberName);
+	}
+
+	/**
+	 * Invokes a {@link Method} that takes no arguments via Java Reflection. Only
+	 * searches in the provided {@link Class} not in parent classes.
+	 * 
+	 * @param <T>        the type of the result
+	 * @param <C>        the type of the {@link Class} the method is defined in
+	 * @param clazz      the {@link Class} the method is defined in
+	 * @param object     the target object
+	 * @param memberName the name of the method
+	 * @return the result of the method
+	 * @throws ReflectionException on error
+	 */
+	public static <T, C> T invokeMethodWithoutArgumentsViaReflection(Class<? extends C> clazz, C object,
+			String memberName) throws ReflectionException {
+		var method = callGuarded(() -> clazz.getDeclaredMethod(memberName));
 		return invokeMethodViaReflection(object, method);
 	}
 

io.openems.common/src/io/openems/common/websocket/ClientReconnectorWorker.java
@@ -105,7 +105,7 @@ public class ClientReconnectorWorker extends AbstractWorker {
 	protected static <T extends WsData> void resetWebSocketClient(WebSocketClient ws, Function<WebSocket, T> wsData)
 			throws ReflectionException {
 		// Call the private WebSocketClient#reset method via Reflection
-		invokeMethodWithoutArgumentsViaReflection(ws, "reset");
+		invokeMethodWithoutArgumentsViaReflection(WebSocketClient.class, ws, "reset");
 
 		// Set attachment to newly created engine
 		final var newAttachment = wsData.apply(ws);

io.openems.common/test/io/openems/common/utils/ReflectionUtilsTest.java
@@ -1,0 +1,40 @@
+package io.openems.common.utils;
+
+import static io.openems.common.utils.ReflectionUtils.invokeMethodWithoutArgumentsViaReflection;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import org.junit.Test;
+
+public class ReflectionUtilsTest {
+
+	private static class ParentClass {
+
+		private String parentMethod() {
+			return "parent";
+		}
+
+	}
+
+	private static class ChildClass extends ParentClass {
+
+		private String childMethod() {
+			return "child";
+		}
+
+	}
+
+	@Test
+	public void testInvokeMethodWithoutArgumentsViaReflection() {
+		final var instance = new ChildClass();
+		assertEquals("parent", invokeMethodWithoutArgumentsViaReflection(ParentClass.class, instance, "parentMethod"));
+		assertEquals("child", invokeMethodWithoutArgumentsViaReflection(ChildClass.class, instance, "childMethod"));
+		assertEquals("child", invokeMethodWithoutArgumentsViaReflection(instance, "childMethod"));
+		assertThrows(ReflectionUtils.ReflectionException.class, () -> {
+			invokeMethodWithoutArgumentsViaReflection(ChildClass.class, instance, "parentMethod");
+		});
+		assertThrows(ReflectionUtils.ReflectionException.class, () -> {
+			invokeMethodWithoutArgumentsViaReflection(instance, "parentMethod");
+		});
+	}
+}