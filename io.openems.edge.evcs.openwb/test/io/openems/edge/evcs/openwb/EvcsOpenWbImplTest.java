81348e6b3c51944bf7f8cf6fdd8a11e09dc984cb
Stefan Hanshans <mail@hanshans.net>
2025-10-11 19:21:39 +0200

    Comment has incorrect indentation level 0

--- io.openems.edge.evcs.openwb/test/io/openems/edge/evcs/openwb/EvcsOpenWbImplTest.java
+++ io.openems.edge.evcs.openwb/test/io/openems/edge/evcs/openwb/EvcsOpenWbImplTest.java
@@ -9,15 +9,13 @@ public class EvcsOpenWbImplTest {
 
 	@Test
 	public void test() throws Exception {
-		new ComponentTest(new EvcsOpenWbImpl()) //
-				.activate(MyConfig.create() //
-						.setId("meterOpenWB0") //
-						.setIp("127.0.0.1") //
-						.setPort(443) //
-//						.setType(CONSUMPTION_METERED) //
-
-						.build()) //
-				.next(new TestCase()) //
+		new ComponentTest(new EvcsOpenWbImpl())
+				.activate(MyConfig.create()
+						.setId("meterOpenWB0")
+						.setIp("127.0.0.1")
+						.setPort(443)
+						.build())
+				.next(new TestCase())
 				.deactivate();
 	}
 }
