ae366c311f9cda5b36a20170d3a24c39f90e04c9
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2025-10-21 16:33:29 +0200

    Initialize Ruhfass Battery RBTI

---
+++ io.openems.edge.ruhfass/test/io/openems/edge/ruhfass/battery/rbti/MyConfig.java
@@ -0,0 +1,69 @@
+package io.openems.edge.ruhfass.battery.rbti;
+
+import static io.openems.common.utils.ConfigUtils.generateReferenceTargetFilter;
+
+import io.openems.common.test.AbstractComponentConfig;
+
+@SuppressWarnings("all")
+public class MyConfig extends AbstractComponentConfig implements Config {
+
+	protected static class Builder {
+		private String id;
+		private String modbusId = null;
+		private int modbusUnitId;
+
+		private Builder() {
+		}
+
+		public Builder setId(String id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder setModbusId(String modbusId) {
+			this.modbusId = modbusId;
+			return this;
+		}
+
+		public Builder setModbusUnitId(int modbusUnitId) {
+			this.modbusUnitId = modbusUnitId;
+			return this;
+		}
+
+		public MyConfig build() {
+			return new MyConfig(this);
+		}
+	}
+
+	/**
+	 * Create a Config builder.
+	 * 
+	 * @return a {@link Builder}
+	 */
+	public static Builder create() {
+		return new Builder();
+	}
+
+	private final Builder builder;
+
+	private MyConfig(Builder builder) {
+		super(Config.class, builder.id);
+		this.builder = builder;
+	}
+
+	@Override
+	public String modbus_id() {
+		return this.builder.modbusId;
+	}
+
+	@Override
+	public String Modbus_target() {
+		return generateReferenceTargetFilter(this.id(), this.modbus_id());
+	}
+
+	@Override
+	public int modbusUnitId() {
+		return this.builder.modbusUnitId;
+	}
+
+}
