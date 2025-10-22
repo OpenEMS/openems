d480c3d727cb32d35a717026eebc014c093a1794
da Kai <da.kai.creator@gmail.com>
2025-10-22 19:23:20 +0200

    linting

--- ui/src/app/edge/live/energymonitor/chart/section/animation.service.ts
+++ ui/src/app/edge/live/energymonitor/chart/section/animation.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, interval } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, interval, Observable } from "rxjs";
 
 @Injectable({
-    providedIn: 'root',
+    providedIn: "root",
 })
 export class AnimationService {
+    public readonly toggleAnimation$: Observable<boolean>;
+
     private readonly animationSpeed = 1150;
     private toggleAnimSubject = new BehaviorSubject(true);
-
-    toggleAnimation$ = this.toggleAnimSubject.asObservable();
     private value: boolean = true;
 
     constructor() {
+        this.toggleAnimation$ = this.toggleAnimSubject.asObservable();
         interval(this.animationSpeed).subscribe(() => {
             this.value = !this.value;
             this.toggleAnimSubject.next(this.value);
