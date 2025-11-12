f1c3baac4aa1cad2ba9fe7286e5bcf13fa382a61
Tom Florentin <tom.florentin@vev.com>
2025-11-12 16:11:52 +0100

    vev token metadata

--- ui/src/themes/openems/environments/edge-dev.ts
+++ ui/src/themes/openems/environments/edge-dev.ts
@@ -3,10 +3,8 @@ import { theme } from "./theme";
 
 export const environment: Environment = {
     ...theme, ...{
-
         backend: "OpenEMS Edge",
         url: getWebsocketScheme() + location.hostname + ":8085",
-
         production: false,
         debugMode: true,
         defaultLanguage: "en"
