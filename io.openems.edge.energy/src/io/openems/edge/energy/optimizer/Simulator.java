628d50460022fb699857dfbc2588585dca6ab099
Ingo <71161062+iseeberg79@users.noreply.github.com>
2025-12-01 07:41:13 +0100

    possible fix of memory leak #2
    -cache, limiting to 10000 entries

--- io.openems.edge.energy/src/io/openems/edge/energy/optimizer/Simulator.java
+++ io.openems.edge.energy/src/io/openems/edge/energy/optimizer/Simulator.java
@@ -54,17 +54,18 @@ public class Simulator {
 	public Simulator(GlobalOptimizationContext goc) {
 		this.goc = goc;
 		this.cache = CacheBuilder.newBuilder() //
+				.maximumSize(10000) // Limit cache to prevent memory leak
 				.recordStats() //
 				.build(new CacheLoader<int[], Fitness>() {
 
 					@Override
 					/**
 					 * Simulates a Schedule and calculates the cost.
-					 * 
+					 *
 					 * <p>
 					 * NOTE: do not throw an Exception here, because we use
 					 * {@link LoadingCache#getUnchecked(Object)} below.
-					 * 
+					 *
 					 * @param schedule the schedule as defined by {@link EshCodec}
 					 * @return the {@link Fitness}
 					 */
