131eacaccd4a2377e36150e86c84be947dcab1c8
himiflo <florian.himmelsbach@t-online.de>
2025-12-02 12:24:17 +0100

    Fixing wrong registers

--- io.openems.edge.meter.janitza/test/io/openems/edge/meter/janitza/umg806/MeterJanitzaUmg806ImplTest.java
+++ io.openems.edge.meter.janitza/test/io/openems/edge/meter/janitza/umg806/MeterJanitzaUmg806ImplTest.java
@@ -18,65 +18,64 @@ public class MeterJanitzaUmg806ImplTest {
 	public void setup() throws Exception {
 		this.test = new ComponentTest(new MeterJanitzaUmg806Impl()) //
 				.addReference("cm", new DummyConfigurationAdmin()) //
-				.addReference("setModbus", new DummyModbusBridge("modbus0")//
-						.withRegisters(19000,
-								// VOLTAGE_L1
-								0x3F80, 0x0000,
-								// VOLTAGE_L2
-								0x3F80, 0x0000,
-								// VOLTAGE_L3
-								0x3F80, 0x0000,
+				.addReference("setModbus", new DummyModbusBridge("modbus0")
+					    // ---- 19000–19010 Voltages ----
+					    .withRegisters(19000,
+					        0x3F80,0x0000, // U_L1
+					        0x3F80,0x0000, // U_L2
+					        0x3F80,0x0000, // U_L3
+					        
+					        0x0000,0x0000, // dummy 19006
+					        0x0000,0x0000, // dummy 19008
+					        0x0000,0x0000  // dummy 19010
+					    )
 
-								// DUMMY
-								0x0000, 0x0000,
+					    // ---- 19012–19018 Currents ----
+					    .withRegisters(19012,
+					        0x3F80,0x0000, // I_L1
+					        0x3F80,0x0000, // I_L2
+					        0x3F80,0x0000, // I_L3
+					        
+					        0x0000,0x0000  // dummy 19018
+					    )
 
-								// CURRENT_L1
-								0x3F80, 0x0000,
-								// CURRENT_L2
-								0x3F80, 0x0000,
-								// CURRENT_L3
-								0x3F80, 0x0000,
+					    // ---- 19020–19034 Active Power ----
+					    .withRegisters(19020,
+					        0x461C,0x4000, // P_L1
+					        0x461C,0x4000, // P_L2
+					        0x461C,0x4000, // P_L3
+					        0x464B,0x2000, // P_sum
+					        
+					        // dummy 19028–19034
+					        0x0000,0x0000,
+					        0x0000,0x0000,
+					        0x0000,0x0000,
+					        0x0000,0x0000
+					    )
 
-								// DUMMY
-								0x0000, 0x0000,
+					    // ---- 19036–19042 Reactive Power ----
+					    .withRegisters(19036,
+					        0x45DA,0xC000, // Q_L1
+					        0x45DA,0xC000, // Q_L2
+					        0x45DA,0xC000, // Q_L3
+					        0x45DA,0xC000  // Q_sum
+					    )
 
-								// ACTIVE_POWER_L1
-								0x461C, 0x4000,
-								// ACTIVE_POWER_L2
-								0x461C, 0x4000,
-								// ACTIVE_POWER_L3
-								0x461C, 0x4000,
+					    // ---- 19050 Frequency ----
+					    .withRegisters(19050,
+					        0x40A0,0x0000
+					    )
 
-								// DUMMY
-								0x0000, 0x0000,
-
-								// REACTIVE_POWER_L1
-								0x45DA, 0xC000,
-								// REACTIVE_POWER_L2
-								0x45DA, 0xC000,
-								// REACTIVE_POWER_L3
-								0x45DA, 0xC000,
-
-								// DUMMY
-								0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-								0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-
-								// ACTIVE_POWER
-								0x464B, 0x2000,
-								// REACTIVE_POWER
-								0x45DA, 0xC000)//
-						.withRegisters(19050,
-								// FREQUENCY
-								0x40A0, 0x0000)//
-						.withRegisters(19062,
-								// ACTIVE_PRODUCTION_ENERGY
-								0x464B, 0x2000,
-
-								// DUMMY
-								0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-
-								// REACTIVE_PRODUCTION_ENERGY
-								0x0000, 0x0000));
+					    // ---- 19062–19070 Energy ----
+					    .withRegisters(19062,
+					        // production / consumption depending on invert
+					        0x464B,0x2000,   // 19062 P_PROD
+					        0x0000,0x0000,   // 19064 dummy
+					        0x0000,0x0000,   // 19066 dummy
+					        0x0000,0x0000,   // 19068 dummy
+					        0x2634,0x2000    // 19070 P_CONS
+					    )
+					);
 	}
 
 	@Test
