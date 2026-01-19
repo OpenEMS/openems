96c685c9dbad9093a5b0cb2e93b90d960d78cb17
Marcus Breier <mbreier@slashwhy.de>
2026-01-19 17:08:44 +0100

    PLC-65: fix error on active power handling on PLCnext ESS component

--- io.openems.edge.phoenixcontact.plcnext/src/io/openems/edge/phoenixcontact/plcnext/ess/PlcNextEss.java
+++ io.openems.edge.phoenixcontact.plcnext/src/io/openems/edge/phoenixcontact/plcnext/ess/PlcNextEss.java
@@ -1,5 +1,8 @@
 package io.openems.edge.phoenixcontact.plcnext.ess;
 
+import io.openems.common.channel.AccessMode;
+import io.openems.common.channel.Unit;
+import io.openems.common.types.OpenemsType;
 import io.openems.edge.common.channel.Doc;
 import io.openems.edge.common.component.OpenemsComponent;
 import io.openems.edge.ess.api.ManagedSymmetricEss;
@@ -8,7 +11,13 @@ import io.openems.edge.ess.api.SymmetricEss;
 public interface PlcNextEss extends ManagedSymmetricEss, SymmetricEss, OpenemsComponent {
 
 	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
-		;
+		// IntegerWriteChannels
+		SET_ACTIVE_POWER(Doc.of(OpenemsType.INTEGER)//
+				.accessMode(AccessMode.WRITE_ONLY)//
+				.unit(Unit.WATT)), //
+		SET_REACTIVE_POWER(Doc.of(OpenemsType.INTEGER)//
+				.accessMode(AccessMode.WRITE_ONLY)//
+				.unit(Unit.VOLT_AMPERE)); //
 
 		private final Doc doc;
 
