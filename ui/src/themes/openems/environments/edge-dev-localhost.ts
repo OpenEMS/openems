5da67efcb04a37134173cf88ab7b5972353c2f60
Hannes <hallerjo97@gmail.com>
2026-01-30 07:31:14 +0100

    Add edge-dev-localhost environment configuration

---
+++ ui/src/themes/openems/environments/edge-dev-localhost.ts
@@ -0,0 +1,14 @@
+@ -0,0 +1,14 @@
+import { Environment, getWebsocketScheme } from "src/environments";
+import { theme } from "./theme";
+
+export const environment: Environment = {
+  ...theme, ...{
+
+    backend: "OpenEMS Edge",
+    url: getWebsocketScheme() + "://localhost:8085",
+
+    production: false,
+    debugMode: true,
+  },
+};
