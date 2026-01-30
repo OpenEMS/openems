79d569840b142cd08040773ff88700955c4f516a
Hannes <hallerjo97@gmail.com>
2026-01-30 08:03:01 +0100

    Update edge-dev-localhost.ts

--- ui/src/themes/openems/environments/edge-dev-localhost.ts
+++ ui/src/themes/openems/environments/edge-dev-localhost.ts
@@ -2,12 +2,9 @@ import { Environment, getWebsocketScheme } from "src/environments";
 import { theme } from "./theme";
 
 export const environment: Environment = {
-  ...theme, ...{
-
+    ...theme,
     backend: "OpenEMS Edge",
     url: getWebsocketScheme() + "://localhost:8085",
-
     production: false,
     debugMode: true,
-  },
 };
