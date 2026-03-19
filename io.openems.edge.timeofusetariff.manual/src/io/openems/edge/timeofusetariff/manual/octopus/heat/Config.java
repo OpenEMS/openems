3ac9f3e71a2114abed72a51e06a6c58024736aa6
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2026-03-19 20:44:01 +0100

    [Edge] Add Time-of-Use Feed-In Tariffs
    - Add TimeOfUseFeedInTariff interface
    - Add "Fixed" implementation with a configurable fixedFeedInPrice
    - Add "EEG 2025" tariff for German "Erneuerbare Energien Gesetz 2025" with fixed feed-in-price and "0" feed-in-price during periods with negative market price.
    
    Co-authored-by: Stefan Feilmeier <3515268+sfeilmeier@users.noreply.github.com>
    Reviewed-by: Michael Grill <59126309+michaelgrill@users.noreply.github.com>
    Reviewed-by: Stefan Feilmeier <3515268+sfeilmeier@users.noreply.github.com>
    Co-authored-by: Leonhard Anderle <132663596+leonhardanderle@users.noreply.github.com>
    Co-committed-by: Leonhard Anderle <132663596+leonhardanderle@users.noreply.github.com>

--- io.openems.edge.timeofusetariff.manual/src/io/openems/edge/timeofusetariff/manual/octopus/heat/Config.java
+++ io.openems.edge.timeofusetariff.manual/src/io/openems/edge/timeofusetariff/manual/octopus/heat/Config.java
@@ -25,7 +25,7 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 	@AttributeDefinition(name = "Low Price", description = "The low price, active from 02 to 06 am and 12 am to 04 pm [Cent/kWh]")
 	double lowPrice();
-	
+
 	@AttributeDefinition(name = "Ancillary Costs JSON", description = "Ancillary Costs in JSON format")
 	String ancillaryCosts();
 
