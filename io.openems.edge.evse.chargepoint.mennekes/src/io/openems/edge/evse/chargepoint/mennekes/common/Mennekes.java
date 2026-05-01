6c16e0e0c7b65dd2a9fc26716e8ada97e49dbcc4
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2026-05-01 17:50:04 +0200

    [Edge] Evse.ChargePoint.Mennekes: change SetPoint to Watt
    - Change Ability of Setpoint to Watt for Mennekes Chargepoint (evcs unchanged)
    - Remove Register 712 and 715 (threw illegal data address and are no longer needed)
    - isolated changes from #2932
    
    ---------
    
    Co-authored-by: Sebastian Asen <47855186+sebastianasen@users.noreply.github.com>
    Reviewed-by: Sebastian Asen <47855186+sebastianasen@users.noreply.github.com>
    Reviewed-by: Stefan Feilmeier <3515268+sfeilmeier@users.noreply.github.com>
    Co-authored-by: Johann Kaufmann <165755282+johannk24@users.noreply.github.com>
    Co-committed-by: Johann Kaufmann <165755282+johannk24@users.noreply.github.com>

--- io.openems.edge.evse.chargepoint.mennekes/src/io/openems/edge/evse/chargepoint/mennekes/common/Mennekes.java
+++ io.openems.edge.evse.chargepoint.mennekes/src/io/openems/edge/evse/chargepoint/mennekes/common/Mennekes.java
@@ -2,10 +2,13 @@ package io.openems.edge.evse.chargepoint.mennekes.common;
 
 import static io.openems.common.channel.AccessMode.WRITE_ONLY;
 import static io.openems.common.channel.Unit.AMPERE;
+import static io.openems.common.channel.Unit.WATT;
 import static io.openems.common.types.OpenemsType.INTEGER;
 
 import io.openems.edge.common.channel.Doc;
+import io.openems.edge.common.channel.IntegerReadChannel;
 import io.openems.edge.common.channel.IntegerWriteChannel;
+import io.openems.edge.common.channel.value.Value;
 import io.openems.edge.common.component.OpenemsComponent;
 
 /**
@@ -29,8 +32,18 @@ public interface Mennekes extends OpenemsComponent {
 				.unit(AMPERE)//
 				.accessMode(WRITE_ONLY)), //
 
+		SET_POWER_LIMIT(Doc.of(INTEGER)//
+				.unit(WATT)//
+				.accessMode(WRITE_ONLY)), //
+
 		EMS_CURRENT_LIMIT(Doc.of(INTEGER)//
 				.unit(AMPERE)), //
+		
+		HEMS_MIN_POWER(Doc.of(INTEGER)//
+				.unit(WATT)), //
+
+		HEMS_MAX_POWER(Doc.of(INTEGER)//
+				.unit(WATT)), //
 		;
 
 		private final Doc doc;
@@ -47,7 +60,18 @@ public interface Mennekes extends OpenemsComponent {
 	}
 
 	/**
+	 * Gets the Channel for {@link ChannelId#SET_POWER_LIMIT}.
+	 * Used for EVSE.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerWriteChannel getApplyPowerLimitChannel() {
+		return this.channel(ChannelId.SET_POWER_LIMIT);
+	}
+	
+	/**
 	 * Gets the Channel for {@link ChannelId#SET_CURRENT_LIMIT}.
+	 * Used for EVCS.
 	 *
 	 * @return the Channel
 	 */
@@ -55,4 +79,39 @@ public interface Mennekes extends OpenemsComponent {
 		return this.channel(ChannelId.SET_CURRENT_LIMIT);
 	}
 
+	/**
+	 * Gets the Channel for {@link ChannelId#HEMS_MIN_POWER}.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerReadChannel getHemsMinPowerChannel() {
+		return this.channel(ChannelId.HEMS_MIN_POWER);
+	}
+	
+	/**
+	 * Gets the minimum power value of the charging station.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getHemsMinPower() {
+		return this.getHemsMinPowerChannel().value();
+	}
+	
+	/**
+	 * Gets the Channel for {@link ChannelId#HEMS_MAX_POWER}.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerReadChannel getHemsMaxPowerChannel() {
+		return this.channel(ChannelId.HEMS_MAX_POWER);
+	}
+	
+	/**
+	 * Gets the maximum power value of the charging station.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getHemsMaxPower() {
+		return this.getHemsMaxPowerChannel().value();
+	}
 }
