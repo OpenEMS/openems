f6640bcf8f6cf2da94572e9599e1df1027518a44
Christian Poulter <devel@poulter.de>
2026-06-13 00:15:09 +0200

    Call getModalComponent after ioComponents are initialized

--- ui/src/app/edge/live/Io/Api_DigitalInput/Io_Api_DigitalInput.ts
+++ ui/src/app/edge/live/Io/Api_DigitalInput/Io_Api_DigitalInput.ts
@@ -29,11 +29,11 @@ export class Io_Api_DigitalInputComponent extends AbstractFlatWidget {
     }
 
     protected override afterIsInitialized(): void {
-        this.modalComponent = this.getModalComponent();
-
         this.service.getConfig().then(config => {
             this.ioComponents = config.getComponentsImplementingNature("io.openems.edge.io.api.DigitalInput").filter(component => component.isEnabled);
             this.ioComponentCount = this.ioComponents.length;
+
+            this.modalComponent = this.getModalComponent();
         });
     }
 
