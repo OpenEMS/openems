f0785aa4fb01da040556a0d14145fe57f1c36fb7
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2026-07-05 21:24:49 +0200

    [UI] Formly Field improvements
    Co-authored-by: Stefan Feilmeier <3515268+sfeilmeier@users.noreply.github.com>
    Reviewed-by: Hanna Späth <198875998+HaSp97@users.noreply.github.com>
    Reviewed-by: Andreas Mur <102706922+DerMuaaa@users.noreply.github.com>
    Reviewed-by: Lukas Rieger <73471197+lukasrgr@users.noreply.github.com>
    Co-authored-by: Sagar Venu <32655208+venu-sagar@users.noreply.github.com>
    Co-committed-by: Sagar Venu <32655208+venu-sagar@users.noreply.github.com>

--- ui/src/app/shared/components/formly/formly-field-range.ts
+++ ui/src/app/shared/components/formly/formly-field-range.ts
@@ -5,62 +5,79 @@ import { AssertionUtils } from "../../utils/assertions/assertions.utils";
 @Component({
     selector: "formly-range-type",
     template: `
-    @if (props.note) {
-      <p class="ion-padding-top"> {{ props.note }} </p>
-    }
-    <ion-range
-      class="ion-padding-top"
-      label-placement="stacked"
-      [class.range-invalid]="showError"
-      [label]="props.label"
-      [value]="props.defaultValue"
-      [min]="props.min"
-      [max]="props.max"
-      [step]="props.step || 1"
-      [pin]="props.pin ?? props.attributes?.pin"
-      [snaps]="props.snaps || false"
-      [formControl]="formControl"
-      [formlyAttributes]="field"
-      [pinFormatter]="boundPinFormatter"
-      (ionChange)="onChange($event)"
-      >
-
-      <ion-label slot="start">{{ props.min }}</ion-label>
-      <ion-label slot="end">{{ props.max }}</ion-label>
-    </ion-range>
-
-    <ion-text class="range-current-value">
-      {{ 'GENERAL.CURRENT_VALUE' | translate }}: {{ getCurrentValueLabel() }}%
-    </ion-text>
-
-    @if (to.description) {
-      <p class="description-text">
-        {{ to.description }}
-      </p>
-    }
-
-    <!-- Validation errors -->
-    @if (showError) {
-      <ion-text color="danger">
-        @if (formControl.errors?.required) {
-          <p>
-            {{ props.required ? (props.label + ' ' + ('GENERAL.FORMLY.REQUIRED' | translate)) : '' }}
-          </p>
+        @if (props.note) {
+            <p class="ion-padding-top">{{ props.note }}</p>
         }
-      </ion-text>
-    }
-    `,
-    styles: [`
-      ion-range.range-invalid::part(label) {
-        color: var(--ion-color-danger);
-      }
+        <ion-range
+            class="ion-padding-top"
+            label-placement="stacked"
+            [class.range-invalid]="showError"
+            [label]="props.label"
+            [value]="props.defaultValue"
+            [min]="props.min"
+            [max]="props.max"
+            [step]="props.step || 1"
+            [pin]="props.pin ?? props.attributes?.pin"
+            [snaps]="props.snaps || false"
+            [formControl]="formControl"
+            [formlyAttributes]="field"
+            [pinFormatter]="boundPinFormatter"
+            (ionChange)="onChange($event)"
+        >
+            <ion-label slot="start">{{ props.min }}</ion-label>
+            <ion-label slot="end">{{ props.max }}</ion-label>
+        </ion-range>
 
-      .range-current-value {
-        display: block;
-        text-align: left;
-        font-size: 0.8rem;
-      }
-    `],
+        <ion-text class="range-current-value">
+            {{ "GENERAL.CURRENT_VALUE" | translate }}:
+            {{ getCurrentValueLabel() }}%
+        </ion-text>
+
+        @if (to.description) {
+            <p class="description-text">
+                {{ props.description }}
+            </p>
+        }
+
+        @if (props.info) {
+            <div class="info-inline ion-padding-top">
+                <ion-icon color="success" name="oe-info"></ion-icon>
+                <span class="description-text ion-padding-start">{{
+                    props.info
+                }}</span>
+            </div>
+        }
+
+        <!-- Validation errors -->
+        @if (showError) {
+            <ion-text color="danger">
+                @if (formControl.errors?.required) {
+                    <p>
+                        {{
+                            props.required
+                                ? props.label +
+                                  " " +
+                                  ("GENERAL.FORMLY.REQUIRED" | translate)
+                                : ""
+                        }}
+                    </p>
+                }
+            </ion-text>
+        }
+    `,
+    styles: [
+        `
+            ion-range.range-invalid::part(label) {
+                color: var(--ion-color-danger);
+            }
+
+            .range-current-value {
+                display: block;
+                text-align: left;
+                font-size: 0.8rem;
+            }
+        `,
+    ],
     standalone: false,
 })
 export class FormlyRangeTypeComponent extends FieldType {
