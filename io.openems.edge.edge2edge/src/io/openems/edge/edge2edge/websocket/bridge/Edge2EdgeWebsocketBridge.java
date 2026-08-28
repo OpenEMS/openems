0863a9a7ca20d0763f17ea601f6ef966fdc86353
Sai Kumar <92803037+PunugupatiSaikumar@users.noreply.github.com>
2026-08-28 13:13:30 -0400

    [Edge] Edge2Edge.Websocket: migrate reference target & update JUnit tests (#3925)
    Co-authored-by: Sai Punugupati <sai.punugupati@fenecon.com>
    Co-authored-by: Stefan Feilmeier <stefan.feilmeir@fenecon.de>
    Co-authored-by: Stefan Feilmeier <stefan.feilmeier@fenecon.de>

--- io.openems.edge.edge2edge/src/io/openems/edge/edge2edge/websocket/bridge/Edge2EdgeWebsocketBridge.java
+++ io.openems.edge.edge2edge/src/io/openems/edge/edge2edge/websocket/bridge/Edge2EdgeWebsocketBridge.java
@@ -22,7 +22,7 @@ import io.openems.edge.common.component.OpenemsComponent;
 public interface Edge2EdgeWebsocketBridge extends OpenemsComponent {
 
 	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
-		CONNECTION_STATE(Doc.of(ConnectionStateOption.values()) //
+		CONNECTION_STATE(Doc.of(ConnectionStateOption.values())//
 				.persistencePriority(PersistencePriority.HIGH)), //
 		NO_CONNECTION(Doc.of(Level.WARNING)), //
 		NOT_AUTHENTICATED(Doc.of(Level.INFO)), //
@@ -30,7 +30,7 @@ public interface Edge2EdgeWebsocketBridge extends OpenemsComponent {
 		/**
 		 * Copies the _sum/State of the remote edge.
 		 */
-		REMOTE_SUM_STATE(Doc.of(Level.values()) //
+		REMOTE_SUM_STATE(Doc.of(Level.values())//
 				.persistencePriority(PersistencePriority.HIGH)), //
 		REMOTE_SUM_STATE_FAULT(Doc.of(Level.FAULT)), //
 		REMOTE_SUM_STATE_WARNING(Doc.of(Level.WARNING)), //
